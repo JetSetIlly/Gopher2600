c732f5c8114b1670bf26204972f6117e6e57e2f0
steve <steve@galactica.localdomain>
2019-05-10 18:37:00 +0100

    o video schedule delays     - tweaked delay value for playfield writes     - fixes Thrust

--- hardware/tia/video/scheduleDelays.go
+++ hardware/tia/video/scheduleDelays.go
@@ -11,14 +11,20 @@ package video
 // values are used.
 
 const (
-	delayWritePlayfield          = 3
+	// playfield write delay of two cycles is optimal - the effect of three
+	// cycles is hard to see but can be clearly shown with homebrew Thrust
+	delayWritePlayfield = 2
+
+	// the almost certain value of a two cycle delay for writing playfield data
+	// points us to a similar delay for other events
 	delayWritePlayer             = 2
 	delayEnableBall              = 2
 	delayEnableMissile           = 2
 	delayResetMissileToPlayerPos = 2
-	delayVDELBL                  = 1
-	delayVDELP                   = 1
-	delayNUSIZ                   = 3
+
+	delayVDELBL = 1
+	delayVDELP  = 1
+	delayNUSIZ  = 3
 
 	// TIA_HW_Notes.txt: "there are 5 CLK worth of clocking/latching to take
 	// into account". not entirely sure this passage is relevant to my solution
