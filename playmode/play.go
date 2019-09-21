f0fccb10c43915e3c6fb58b5ef668ce73c46cd9e
steve <steve@galactica.localdomain>
2019-09-21 20:28:49 +0100

    o playmode     - tidied up playmode.Play() function

--- playmode/play.go
+++ playmode/play.go
@@ -14,6 +14,12 @@ import (
 	"time"
 )
 
+func uniqueFilename(cartload memory.CartridgeLoader) string {
+	n := time.Now()
+	timestamp := fmt.Sprintf("%04d%02d%02d_%02d%02d%02d", n.Year(), n.Month(), n.Day(), n.Hour(), n.Minute(), n.Second())
+	return fmt.Sprintf("recording_%s_%s", cartload.ShortName(), timestamp)
+}
+
 // Play sets the emulation running - without any debugging features
 func Play(tvType string, scaling float32, stable bool, transcript string, newRecording bool, cartload memory.CartridgeLoader) error {
 	if recorder.IsPlaybackFile(cartload.Filename) {
@@ -37,57 +43,68 @@ func Play(tvType string, scaling float32, stable bool, transcript string, newRec
 		transcript = fmt.Sprintf("recording_%s_%s", cartload.ShortName(), timestamp)
 	}
 
-	// note that we attach the cartridge in three different branches below - we
-	// need to do this at different times depending on whether a new recording
-	// or playback is taking place; or if it's just a regular playback
+	// note that we attach the cartridge in three different branches below,
+	// depending on
 
-	if transcript != "" {
-		if newRecording {
-			rec, err := recorder.NewRecorder(transcript, vcs)
-			if err != nil {
-				return errors.New(errors.PlayError, err)
-			}
+	if newRecording {
+		// new recording requested
 
-			defer func() {
-				rec.End()
-			}()
-
-			vcs.Ports.Player0.AttachTranscriber(rec)
-			vcs.Ports.Player1.AttachTranscriber(rec)
-			vcs.Panel.AttachTranscriber(rec)
-
-			// attaching cartridge after recorder and transcribers have been
-			// setup because we want to catch any setup events in the recording
-
-			err = setup.AttachCartridge(vcs, cartload)
-			if err != nil {
-				return errors.New(errors.PlayError, err)
-			}
-
-		} else {
-			plb, err := recorder.NewPlayback(transcript)
-			if err != nil {
-				return err
-			}
-
-			if cartload.Filename != "" && cartload.Filename != plb.CartLoad.Filename {
-				return errors.New(errors.PlayError, "cartridge doesn't match name in the playback recording")
-			}
-
-			// not using setup.AttachCartridge. if the playback was recorded with setup
-			// changes the events will have been copied into the playback script and
-			// will be applied that way
-			err = vcs.AttachCartridge(plb.CartLoad)
-			if err != nil {
-				return errors.New(errors.PlayError, err)
-			}
-
-			err = plb.AttachToVCS(vcs)
-			if err != nil {
-				return errors.New(errors.PlayError, err)
-			}
+		// no transcript name given so we must generate a suitable default
+		if transcript == "" {
+			transcript = uniqueFilename(cartload)
 		}
+
+		rec, err := recorder.NewRecorder(transcript, vcs)
+		if err != nil {
+			return errors.New(errors.PlayError, err)
+		}
+
+		defer func() {
+			rec.End()
+		}()
+
+		vcs.Ports.Player0.AttachTranscriber(rec)
+		vcs.Ports.Player1.AttachTranscriber(rec)
+		vcs.Panel.AttachTranscriber(rec)
+
+		// attaching cartridge after recorder and transcribers have been
+		// setup because we want to catch any setup events in the recording
+
+		err = setup.AttachCartridge(vcs, cartload)
+		if err != nil {
+			return errors.New(errors.PlayError, err)
+		}
+
+	} else if transcript != "" {
+		// not a new recording but a transcript has been supplied. this is a
+		// playback request
+
+		plb, err := recorder.NewPlayback(transcript)
+		if err != nil {
+			return err
+		}
+
+		if cartload.Filename != "" && cartload.Filename != plb.CartLoad.Filename {
+			return errors.New(errors.PlayError, "cartridge doesn't match name in the playback recording")
+		}
+
+		// not using setup.AttachCartridge. if the playback was recorded with setup
+		// changes the events will have been copied into the playback script and
+		// will be applied that way
+		err = vcs.AttachCartridge(plb.CartLoad)
+		if err != nil {
+			return errors.New(errors.PlayError, err)
+		}
+
+		err = plb.AttachToVCS(vcs)
+		if err != nil {
+			return errors.New(errors.PlayError, err)
+		}
+
 	} else {
+		// no new recording requested and no transcript given. this is a 'normal'
+		// launch of the emalator for regular pla
+
 		err = setup.AttachCartridge(vcs, cartload)
 		if err != nil {
 			return errors.New(errors.PlayError, err)
