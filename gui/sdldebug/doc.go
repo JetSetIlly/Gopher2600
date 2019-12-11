69f48f329d4aff5fdbae4ad8eaa9f6d607746f72
steve <steve@galactica.localdomain>
2019-12-11 10:26:53 +0000

    o gui     - commentary and documentation

---
+++ gui/sdldebug/doc.go
@@ -0,0 +1,3 @@
+// Package sdldebug implements the GUI interface. Suitable for applications
+// that require a screen and debugging overlays.
+package sdldebug
---
+++ gui/sdlplay/doc.go
@@ -0,0 +1,4 @@
+// Package sdlplay implements the GUI interface. Suitable for applications that
+// require a minimalist screen intended for playing games/cartrdiges, rather
+// than debugging them.
+package sdlplay
