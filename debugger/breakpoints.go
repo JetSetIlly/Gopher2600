JetSetIlly/Gopher2600

commit c809f2d424e03ffdceadfb3880d46aa3be68a95d

LAST command and CPU window now show the bank of the last instruction executed
tidied up string output for M-Network cartridge type

fixed lazy evaluation of Cartridge.NumBanks()

disassembly/entry.go
@@ -29,6 +29,28 @@ import (
 	"github.com/jetsetilly/gopher2600/symbols"
 )
 
+// Bank refers to the cartridge bank. It is used in the Entry type to answer
+// the question, which bank was this execution.Result from?
+type Bank int
+
+// List of allowed Bank values
+const (
+	BankUnknown  Bank = -1
+	BankMultiple Bank = -2
+)
+
+func (b Bank) String() string {
+	if b == BankUnknown {
+		return "?"
+	}
+
+	if b == BankMultiple {
+		return "*"
+	}
+
+	return fmt.Sprintf("%d", b)
+}
+
 // EntryType describes the level of reliability of the Entry.
 type EntryType int
 
@@ -63,9 +85,9 @@ type Entry struct {
 	Result execution.Result
 
 	// execution.Result does not specify which bank the instruction is from
-	// because that information isn't available to the CPU. note that
-	// information here for completeness
-	Bank int
+	// because that information isn't available to the CPU. we note it here if
+	// possible.
+	Bank Bank
 
 	// formatted strings representations of information in execution.Result
 	Location string
@@ -99,6 +121,7 @@ func newEntry(result execution.Result, symtable *symbols.Table) (*Entry, error) 
 
 	d := &Entry{
 		Result: result,
+		Bank:   BankUnknown,
 	}
 
 	// if the operator hasn't been decoded yet then use placeholder strings for

debugger/commands.go
@@ -586,6 +586,9 @@ func (dbg *Debugger) processTokens(tokens *commandline.Tokens) (bool, error) {
 			}
 		}
 
+		if dbg.vcs.Mem.Cart.NumBanks() > 1 {
+			s.WriteString(fmt.Sprintf("[%s] ", e.Bank))
+		}
 		s.WriteString(dbg.disasm.GetField(disassembly.FldAddress, e))
 		s.WriteString(" ")
 		s.WriteString(dbg.disasm.GetField(disassembly.FldMnemonic, e))

disassembly/analysis.go
@@ -102,13 +102,13 @@ func (dsm *Disassembly) flowAnalysis(mc *cpu.CPU, flowedFrom uint16, subroutineD
 		}
 
 		// create new disassembly entry
-		d, err = dsm.FormatResult(mc.LastResult)
+		d, err = dsm.formatResult(mc.LastResult)
 		if err != nil {
 			return err
 		}
 
 		// add bank information
-		d.Bank = bank
+		d.Bank = Bank(bank)
 
 		// indicate that it was generated from the flow pass
 		d.Type = EntryTypeAnalysis

disassembly/decode.go
@@ -71,13 +71,13 @@ func (dsm *Disassembly) decode(mc *cpu.CPU) error {
 			}
 
 			// create a new disassembly entry using last result
-			ent, err := dsm.FormatResult(mc.LastResult)
+			ent, err := dsm.formatResult(mc.LastResult)
 			if err != nil {
 				return err
 			}
 
 			// add bank information
-			ent.Bank = b
+			ent.Bank = Bank(b)
 
 			// set entry type depending on whether we're at an expected decode
 			// point

disassembly/disassembly.go
@@ -63,9 +63,44 @@ func (dsm Disassembly) Get(bank int, address uint16) (*Entry, bool) {
 // Build string representations with GetField(). Also see Write*() functions for
 // less flexible but convenient alternative.
 //
-// WARNING: bank field of Entry type will be invalid. You need to set it
-// yourself if you need it.
+// The Bank field of the Entry type will be a best-guess. When the function
+// cannot guess, the value will be equal to BankUnknown. If the result looks
+// like it could come from more than one bank the Bank field will equal
+// BankMultiple.
 func (dsm Disassembly) FormatResult(result execution.Result) (*Entry, error) {
+	e, err := newEntry(result, dsm.Symtable)
+	if err != nil {
+		return nil, err
+	}
+
+	found := false
+
+	// figure out what the bank is by comparing to existing entries in the
+	// disassembly.
+	addr := result.Address & memorymap.AddressMaskCart
+	for b := 0; b < len(dsm.Entries); b++ {
+		oe := dsm.Entries[b][addr]
+
+		if oe != nil && oe.Result.Defn != nil && result.Defn != nil &&
+			oe.Result.Defn.OpCode == result.Defn.OpCode &&
+			oe.Result.InstructionData == result.InstructionData {
+
+			if !found {
+				e.Bank = Bank(b)
+				found = true
+			} else {
+				e.Bank = BankMultiple
+				break // for loop
+			}
+		}
+	}
+
+	return e, err
+}
+
+// like FormatResult but without the best-guess bank value. callers to this
+// function must set the bank value explicitely
+func (dsm Disassembly) formatResult(result execution.Result) (*Entry, error) {
 	return newEntry(result, dsm.Symtable)
 }
 

gui/sdlimgui/lazyvalues/cart.go
@@ -56,7 +56,7 @@ func (lz *LazyCart) update() {
 
 	lz.val.Dbg.PushRawEvent(func() {
 		lz.atomicString.Store(lz.val.VCS.Mem.Cart.String())
-		lz.atomicNumBanks.Store(lz.val.VCS.Mem.Cart.NumBanks)
+		lz.atomicNumBanks.Store(lz.val.VCS.Mem.Cart.NumBanks())
 
 		// uses lazy PCaddr value from lazyvalues.CPU
 		lz.atomicCurrBank.Store(lz.val.VCS.Mem.Cart.GetBank(PCaddr))

gui/sdlimgui/win_cpu.go
@@ -180,7 +180,11 @@ func (win *winCPU) drawLastResult() {
 			imgui.Text(fmt.Sprintf("%s", e.Bytecode))
 			imgui.Text(fmt.Sprintf("%s %s", e.Mnemonic, e.Operand))
 			imgui.Text(fmt.Sprintf("%s cyc.", e.ActualCycles))
-			imgui.Text(fmt.Sprintf("(%s)", e.Address))
+			if win.img.lazy.Cart.NumBanks == 1 {
+				imgui.Text(fmt.Sprintf("(%s)", e.Address))
+			} else {
+				imgui.Text(fmt.Sprintf("(%s) [%s]", e.Address, e.Bank))
+			}
 		} else {
 			// if there's a problem with the accuracy of what is being
 			// displayed, the problem probably isn't here and it probably isn't

hardware/memory/cartridge/cartridge_mnetwork.go
@@ -153,13 +153,11 @@ func newMnetwork(data []byte) (cartMapper, error) {
 func (cart mnetwork) String() string {
 	s := strings.Builder{}
 	s.WriteString(cart.method)
-	s.WriteString(" Banks: 8")
+	s.WriteString(fmt.Sprintf(" Bank: %d ", cart.bank))
+	s.WriteString(fmt.Sprintf(" RAM: %d", cart.ram256byteIdx))
 	if cart.bank == 7 {
-		s.WriteString("+1k RAM, ")
-	} else {
-		s.WriteString(fmt.Sprintf(", %d +", cart.bank))
+		s.WriteString(" [+1k RAM]")
 	}
-	s.WriteString(fmt.Sprintf("256byte RAM segment %d", cart.ram256byteIdx))
 	return s.String()
 }
 

commit d99aa40719979c36b23f45c7988b609f21bd15a3

DISASM mode will now optionally print "raw" disassembly
fixed typing issue when breaking on cartridge banks, caused by
changes in commit c809f2d424e03ffdceadfb3880d46aa3be68a95d

debugger/breakpoints.go
@@ -49,7 +49,9 @@ type breakpoints struct {
 
 // breaker defines a specific break condition
 type breaker struct {
-	target      *target
+	target *target
+
+	// must be comparable types and the same type as each other
 	value       interface{}
 	ignoreValue interface{}
 
@@ -114,29 +116,36 @@ func (bk breaker) cmp(ck breaker) bool {
 	return true
 }
 
+type checkResult int
+
+const (
+	checkMatch checkResult = iota
+	checkNoMatch
+	checkIgnoredValue
+)
+
 // check checks the specific break condition with the current value of
 // the break target
-func (bk *breaker) check() bool {
+func (bk *breaker) check() checkResult {
 	currVal := bk.target.TargetValue()
 	m := currVal == bk.value
 	if !m {
-		bk.ignoreValue = nil
-		return false
+		return checkNoMatch
 	}
 
 	if currVal == bk.ignoreValue {
-		return false
+		return checkIgnoredValue
 	}
 
 	if bk.next != nil {
-		if !bk.next.check() {
-			return false
+		if bk.next.check() == checkNoMatch {
+			return checkNoMatch
 		}
 	}
 
 	bk.ignoreValue = currVal
 
-	return true
+	return checkMatch
 }
 
 // add a new breaker by linking it to the end of an existing breaker
@@ -201,7 +210,7 @@ func (bp *breakpoints) check(previousResult string) string {
 	checkString.WriteString(previousResult)
 	for i := range bp.breaks {
 		// check current value of target with the requested value
-		if bp.breaks[i].check() {
+		if bp.breaks[i].check() == checkMatch {
 			checkString.WriteString(fmt.Sprintf("break on %s\n", bp.breaks[i]))
 		}
 	}
@@ -416,7 +425,10 @@ func (bp *breakpoints) hasBreak(e *disassembly.Entry) (BreakGroup, int) {
 	// we start with the very specific - address and bank
 	check.next = &breaker{
 		target: bp.checkBankBreak,
-		value:  e.Bank,
+
+		// critical that we cast to int because we'll be comparing against the
+		// result of cartridge.GetBank()
+		value: int(e.Bank),
 	}
 
 	// check for a breaker for the PC value AND bank value. if
@@ -460,13 +472,17 @@ func (bp *breakpoints) togglePCBreak(e *disassembly.Entry) {
 	ai := bp.dbg.dbgmem.mapAddress(e.Result.Address, true)
 	nb := breaker{
 		target: bp.checkPcBreak,
-		value:  int(ai.mappedAddress),
+
+		// see above for casting commentary
+		value: int(ai.mappedAddress),
 	}
 
 	if bp.dbg.vcs.Mem.Cart.NumBanks() > 1 {
 		nb.next = &breaker{
 			target: bp.checkBankBreak,
-			value:  e.Bank,
+
+			// see above for casting commentary
+			value: int(e.Bank),
 		}
 	}
 

debugger/targets.go
@@ -235,7 +235,7 @@ func bankTarget(dbg *Debugger) *target {
 	return &target{
 		label: "Bank",
 		currentValue: func() interface{} {
-			return dbg.vcs.Mem.Cart.GetBank(dbg.vcs.CPU.PC.Address())
+			return int(dbg.vcs.Mem.Cart.GetBank(dbg.vcs.CPU.PC.Address()))
 		},
 	}
 }

disassembly/entry.go
@@ -76,6 +76,21 @@ const (
 	EntryTypeAnalysis
 )
 
+func (t EntryType) String() string {
+	// adding space to short strings so that they line up (we're only using
+	// this in a single place for a specific purpose so this is okay)
+	switch t {
+	case EntryTypeNaive:
+		return "naive   "
+	case EntryTypeDecode:
+		return "decode  "
+	case EntryTypeAnalysis:
+		return "analysis"
+	}
+
+	return ""
+}
+
 // Entry is a disassambled instruction. The constituent parts of the
 // disassembly. It is a represenation of execution.Instruction
 type Entry struct {

disassembly/write.go
@@ -30,6 +30,7 @@ import (
 type WriteAttr struct {
 	ByteCode bool
 	FlowInfo bool
+	Raw      bool
 }
 
 // Write the entire disassembly to io.Writer
@@ -62,13 +63,21 @@ func (dsm *Disassembly) WriteBank(output io.Writer, attr WriteAttr, bank int) er
 
 // WriteEntry writes a single Instruction to io.Writer
 func (dsm *Disassembly) WriteEntry(output io.Writer, attr WriteAttr, e *Entry) {
-	if e == nil || e.Type < EntryTypeDecode {
+	if e == nil {
+		return
+	}
+
+	if !attr.Raw && e.Type < EntryTypeDecode {
 		return
 	}
 
 	if e.Location != "" {
 		output.Write([]byte(dsm.GetField(FldLocation, e)))
 		output.Write([]byte("\n"))
+	}
+
+	if attr.Raw {
+		output.Write([]byte(fmt.Sprintf("%s  ", e.Type)))
 	}
 
 	if attr.ByteCode {

gopher2600.go
@@ -287,6 +287,7 @@ func play(md *modalflag.Modes, sync *mainSync) error {
 		if err != nil {
 			return err
 		}
+
 		if *record {
 			fmt.Println("! recording completed")
 		}
@@ -428,6 +429,7 @@ func disasm(md *modalflag.Modes) error {
 
 	cartFormat := md.AddString("cartformat", "AUTO", "force use of cartridge format")
 	bytecode := md.AddBool("bytecode", false, "include bytecode in disassembly")
+	raw := md.AddBool("raw", false, "raw disassembly. show every byte with the disasm decision.")
 	bank := md.AddInt("bank", -1, "show disassembly for a specific bank")
 
 	p, err := md.Parse()
@@ -439,7 +441,10 @@ func disasm(md *modalflag.Modes) error {
 	case 0:
 		return fmt.Errorf("2600 cartridge required for %s mode", md)
 	case 1:
-		attr := disassembly.WriteAttr{ByteCode: *bytecode}
+		attr := disassembly.WriteAttr{
+			ByteCode: *bytecode,
+			Raw:      *raw,
+		}
 
 		cartload := cartridgeloader.Loader{
 			Filename: md.GetArg(0),