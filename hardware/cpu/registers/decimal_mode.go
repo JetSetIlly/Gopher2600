JetSetIlly/Gopher2600

commit b50914354929dfab7a9118f86c3f08ba1566c609

o cpu/registers     - corrected decimal mode     - flags should now be "correct" for undefined decimal values

hardware/cpu/registers/decimal_mode.go
@@ -1,70 +1,109 @@
 package registers
 
-// !!TODO: handle all the nuances of 6507 decimal mode
-//  . invalid BCD values (ie. nibble values A to F) correctly
-//  . probably others
+// these decimal functions return information about zero and sign bits in
+// addition to the carry and overflow. the cpu can use these value to set the
+// status flags. this is different to binary addition/subtraction which only
+// returns information for the carry and overflow flags.
+//
+// details of this has been taken from "Flags on Decimal mode in the NMOS 6502"
+// v1.0 by Jorge Cwik:
+//
+// https://atariage.com/forums/applications/core/interface/file/attachment.php?id=163231
 
 func addDecimal(a, b uint8, carry bool) (r uint8, rcarry bool) {
 	r = a + b
 	if carry {
 		r++
 	}
-	if r >= 10 {
-		r -= 10
-		rcarry = true
-	}
-	return r, rcarry
+	return r, r > 9
 }
 
 // AddDecimal adds value to register as though both registers are decimal
-// representations. Returns new carry state
-func (r *Register) AddDecimal(val uint8, carry bool) (rcarry bool) {
+// representations. Returns new carry state, zero, overflow, sign bit
+// information.
+func (r *Register) AddDecimal(val uint8, carry bool) (bool, bool, bool, bool) {
+	var zero, overflow, sign bool
+	var ucarry, tcarry bool
+
+	// binary addition of units and tens
 	runits := uint8(r.value) & 0x0f
-	rtens := (uint8(r.value) & 0xf0) >> 4
-
 	vunits := uint8(val) & 0x0f
+	runits, ucarry = addDecimal(runits, vunits, carry)
+
+	rtens := (uint8(r.value) & 0xf0) >> 4
 	vtens := (uint8(val) & 0xf0) >> 4
+	rtens, tcarry = addDecimal(rtens, vtens, ucarry)
 
-	runits, rcarry = addDecimal(runits, vunits, carry)
-	rtens, rcarry = addDecimal(rtens, vtens, rcarry)
+	// from the Cwik document:
+	//
+	// "The Z flag is computed before performing any decimal adjust."
+	zero = runits == 0x00 && rtens == 0x00
 
+	// decimal correction for units
+	if ucarry {
+		runits -= 10
+	}
+
+	// from the Cwik document:
+	//
+	// "The N and V flags are computed after a decimal adjust of the low
+	// nibble, but before adjusting the high nibble."
+	//
+	// not forgetting that the tens value has not been shifted into the upper
+	// nibble yet
+	overflow = rtens&0x04 == 0x04
+	sign = rtens&0x08 == 0x08
+
+	// decimal correction for tens
+	if tcarry {
+		rtens -= 10
+	}
+
+	// pack units/tens nibbles into register
 	r.value = (rtens << 4) | runits
 
-	return rcarry
+	return tcarry, zero, overflow, sign
 }
 
 func subtractDecimal(a, b uint8, carry bool) (r uint8, rcarry bool) {
-	rcarry = b > a || carry && b == a
-
 	r = a - b
 	if carry {
 		r--
 	}
-
-	if rcarry {
-		r += 10
-	}
-
-	return r, rcarry
+	return r, b > a || carry && b == a
 }
 
 // SubtractDecimal subtracts value to from as though both registers are decimal
-// representations. Returns new carry state
-func (r *Register) SubtractDecimal(val uint8, carry bool) (rcarry bool) {
-	runits := r.value & 0x0f
-	rtens := (r.value & 0xf0) >> 4
-
-	vunits := val & 0x0f
-	vtens := (val & 0xf0) >> 4
+// representations. Returns new carry state, zero, overflow, sign bit
+// information.
+func (r *Register) SubtractDecimal(val uint8, carry bool) (bool, bool, bool, bool) {
+	var zero, overflow, sign bool
+	var ucarry, tcarry bool
 
 	// invert carry flag - the 6507 uses the carry flag opposite to what you
 	// might expect when subtracting
-	runits, rcarry = subtractDecimal(runits, vunits, !carry)
+	carry = !carry
 
-	// rcarry from previous call to subtractDecimal() is correct
-	rtens, rcarry = subtractDecimal(rtens, vtens, rcarry)
+	runits := r.value & 0x0f
+	vunits := val & 0x0f
+	runits, ucarry = subtractDecimal(runits, vunits, carry)
 
+	rtens := (r.value & 0xf0) >> 4
+	vtens := (val & 0xf0) >> 4
+	rtens, tcarry = subtractDecimal(rtens, vtens, ucarry)
+
+	// decimal correction for units
+	if ucarry {
+		runits += 10
+	}
+
+	// decimal correction for tens
+	if tcarry {
+		rtens += 10
+	}
+
+	// pack units/tens nibbles into register
 	r.value = (rtens << 4) | runits
 
-	return !rcarry
+	return !tcarry, zero, overflow, sign
 }

hardware/cpu/registers/decimal_mode_test.go
@@ -14,43 +14,53 @@ func TestDecimalMode(t *testing.T) {
 	r8 := registers.NewRegister(0, "test")
 
 	// addition without carry
-	rcarry = r8.AddDecimal(1, false)
+	rcarry, _, _, _ = r8.AddDecimal(1, false)
 	rtest.EquateRegisters(t, r8, 0x01)
 	test.Equate(t, rcarry, false)
 
 	// addition with carry
-	rcarry = r8.AddDecimal(1, true)
+	rcarry, _, _, _ = r8.AddDecimal(1, true)
 	rtest.EquateRegisters(t, r8, 0x03)
 	test.Equate(t, rcarry, false)
 
 	// subtraction with carry (subtract value)
 	r8.Load(9)
 	rtest.EquateRegisters(t, r8, 0x09)
-	rcarry = r8.SubtractDecimal(1, true)
+	rcarry, _, _, _ = r8.SubtractDecimal(1, true)
 	rtest.EquateRegisters(t, r8, 0x08)
 
 	// subtraction without carry (subtract value and another 1)
-	rcarry = r8.SubtractDecimal(1, false)
+	rcarry, _, _, _ = r8.SubtractDecimal(1, false)
 	rtest.EquateRegisters(t, r8, 0x06)
 
 	// addition on tens boundary
 	r8.Load(9)
 	rtest.EquateRegisters(t, r8, 0x09)
-	rcarry = r8.AddDecimal(1, false)
+	rcarry, _, _, _ = r8.AddDecimal(1, false)
 	rtest.EquateRegisters(t, r8, 0x10)
 
 	// subtraction on tens boundary
-	rcarry = r8.SubtractDecimal(1, true)
+	rcarry, _, _, _ = r8.SubtractDecimal(1, true)
 	rtest.EquateRegisters(t, r8, 0x09)
 
 	// addition on hundreds boundary
 	r8.Load(0x99)
 	rtest.EquateRegisters(t, r8, 0x99)
-	rcarry = r8.AddDecimal(1, false)
+	rcarry, _, _, _ = r8.AddDecimal(1, false)
 	rtest.EquateRegisters(t, r8, 0x00)
 	test.Equate(t, rcarry, true)
 
 	// subtraction on hundreds boundary
-	rcarry = r8.SubtractDecimal(1, true)
+	rcarry, _, _, _ = r8.SubtractDecimal(1, true)
 	rtest.EquateRegisters(t, r8, 0x99)
+}
+
+func TestDecimalModeInvalid(t *testing.T) {
+	var rcarry, rzero bool
+
+	r8 := registers.NewRegister(0x99, "test")
+	rcarry, rzero, _, _ = r8.AddDecimal(1, false)
+	rtest.EquateRegisters(t, r8, 0x00)
+	test.Equate(t, rcarry, true)
+	test.Equate(t, rzero, false)
 }

hardware/cpu/cpu.go
@@ -954,23 +954,27 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 	case "ADC":
 		if mc.Status.DecimalMode {
-			mc.Status.Carry = mc.A.AddDecimal(value, mc.Status.Carry)
-			// decimal mode doesn't affect overflow flag (yet?)
+			mc.Status.Carry,
+				mc.Status.Zero,
+				mc.Status.Overflow,
+				mc.Status.Sign = mc.A.AddDecimal(value, mc.Status.Carry)
 		} else {
 			mc.Status.Carry, mc.Status.Overflow = mc.A.Add(value, mc.Status.Carry)
+			mc.Status.Zero = mc.A.IsZero()
+			mc.Status.Sign = mc.A.IsNegative()
 		}
-		mc.Status.Zero = mc.A.IsZero()
-		mc.Status.Sign = mc.A.IsNegative()
 
 	case "SBC":
 		if mc.Status.DecimalMode {
-			mc.Status.Carry = mc.A.SubtractDecimal(value, mc.Status.Carry)
-			// decimal mode doesn't affect overflow flag (yet?)
+			mc.Status.Carry,
+				mc.Status.Zero,
+				mc.Status.Overflow,
+				mc.Status.Sign = mc.A.SubtractDecimal(value, mc.Status.Carry)
 		} else {
 			mc.Status.Carry, mc.Status.Overflow = mc.A.Subtract(value, mc.Status.Carry)
+			mc.Status.Zero = mc.A.IsZero()
+			mc.Status.Sign = mc.A.IsNegative()
 		}
-		mc.Status.Zero = mc.A.IsZero()
-		mc.Status.Sign = mc.A.IsNegative()
 
 	case "ROR":
 		var r *registers.Register

commit 1aa99a9b38b0759cf07170d3bfecf8d98329dd6b

CPU decimal mode
zero, carry and overflow flags set correctly.

You can see this in game end condition for Barnstormer. Not sure how
this got past. testing not rigorous enough I suppose.

bug most probably introduced in b50914354929dfab7a9118f86c3f08ba1566c609

hardware/cpu/registers/decimal_mode.go
@@ -111,10 +111,25 @@ func (r *Register) SubtractDecimal(val uint8, carry bool) (bool, bool, bool, boo
 	vtens := (val & 0xf0) >> 4
 	rtens, tcarry = subtractDecimal(rtens, vtens, ucarry)
 
+	// from the Cwik document:
+	//
+	// "The Z flag is computed before performing any decimal adjust."
+	zero = runits == 0x00 && rtens == 0x00
+
 	// decimal correction for units
 	if ucarry {
 		runits += 10
 	}
+
+	// from the Cwik document:
+	//
+	// "The N and V flags are computed after a decimal adjust of the low
+	// nibble, but before adjusting the high nibble."
+	//
+	// not forgetting that the tens value has not been shifted into the upper
+	// nibble yet
+	overflow = rtens&0x04 == 0x04
+	sign = rtens&0x08 == 0x08
 
 	// decimal correction for tens
 	if tcarry {

hardware/cpu/registers/decimal_mode_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/jetsetilly/gopher2600/test"
 )
 
-func TestDecimalMode(t *testing.T) {
+func TestDecimalModeCarry(t *testing.T) {
 	var rcarry bool
 
 	// initialisation
@@ -73,6 +73,20 @@ func TestDecimalMode(t *testing.T) {
 	// subtraction on hundreds boundary
 	rcarry, _, _, _ = r8.SubtractDecimal(1, true)
 	rtest.EquateRegisters(t, r8, 0x99)
+}
+
+func TestDecimalModeZero(t *testing.T) {
+	var zero bool
+
+	// initialisation
+	r8 := registers.NewRegister(0, "test")
+
+	// subtract to zero
+	r8.Load(0x02)
+	_, zero, _, _ = r8.SubtractDecimal(1, true)
+	test.Equate(t, zero, false)
+	_, zero, _, _ = r8.SubtractDecimal(1, true)
+	test.Equate(t, zero, true)
 }
 
 func TestDecimalModeInvalid(t *testing.T) {