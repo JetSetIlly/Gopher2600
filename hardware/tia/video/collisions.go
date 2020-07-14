de34b0a6d8c8c4025f696803fe6effa640c026c3
JetSetIlly <stephen.t.illingworth@gmail.com>
2020-07-14 10:11:21 +0100

    optimised collision register ticking

--- hardware/tia/video/collisions.go
+++ hardware/tia/video/collisions.go
@@ -66,10 +66,125 @@ func (col *Collisions) Clear() {
 	col.CXM1FB = 0
 	col.CXBLPF = 0
 	col.CXPPMM = 0
+	col.mem.ChipWrite(addresses.CXM0P, col.CXM0P)
+	col.mem.ChipWrite(addresses.CXM1P, col.CXM1P)
+	col.mem.ChipWrite(addresses.CXP0FB, col.CXP0FB)
+	col.mem.ChipWrite(addresses.CXP1FB, col.CXP1FB)
+	col.mem.ChipWrite(addresses.CXM0FB, col.CXM0FB)
+	col.mem.ChipWrite(addresses.CXM1FB, col.CXM1FB)
+	col.mem.ChipWrite(addresses.CXBLPF, col.CXBLPF)
+	col.mem.ChipWrite(addresses.CXPPMM, col.CXPPMM)
 }
 
-// tick
+// optimised tick of collision registers. memory is only written to when
+// necessary.
 func (col *Collisions) tick(p0, p1, m0, m1, bl, pf bool) {
+	col.Activity.Reset()
+
+	if m0 {
+		if p1 || p0 {
+			if p1 {
+				col.CXM0P |= 0x80
+				col.Activity.WriteString("M0 ^ P1")
+			}
+			if p0 {
+				col.CXM0P |= 0x40
+				col.Activity.WriteString("M0 ^ P0")
+			}
+			col.mem.ChipWrite(addresses.CXM0P, col.CXM0P)
+		}
+
+		if pf || bl {
+			if pf {
+				col.CXM0FB |= 0x80
+				col.Activity.WriteString("M0 ^ PF")
+			}
+			if bl {
+				col.CXM0FB |= 0x40
+				col.Activity.WriteString("M1 ^ BL")
+			}
+			col.mem.ChipWrite(addresses.CXM0FB, col.CXM0FB)
+		}
+	}
+
+	if m1 {
+		if p1 || p0 {
+			if p0 {
+				col.CXM1P |= 0x80
+				col.Activity.WriteString("M1 ^ P0")
+			}
+			if p1 {
+				col.CXM1P |= 0x40
+				col.Activity.WriteString("M1 ^ P1")
+			}
+			col.mem.ChipWrite(addresses.CXM1P, col.CXM1P)
+		}
+
+		if pf || bl {
+			if pf {
+				col.CXM1FB |= 0x80
+				col.Activity.WriteString("M1 ^ PF")
+			}
+			if bl {
+				col.CXM1FB |= 0x40
+				col.Activity.WriteString("M1 ^ BL")
+			}
+			col.mem.ChipWrite(addresses.CXM1FB, col.CXM1FB)
+		}
+	}
+
+	if p0 {
+		if pf || bl {
+			if pf {
+				col.CXP0FB |= 0x80
+				col.Activity.WriteString("P0 ^ PF")
+			}
+			if bl {
+				col.CXP0FB |= 0x40
+				col.Activity.WriteString("P0 ^ BL")
+			}
+			col.mem.ChipWrite(addresses.CXP0FB, col.CXP0FB)
+		}
+	}
+
+	if p1 {
+		if pf || bl {
+			if pf {
+				col.CXP1FB |= 0x80
+				col.Activity.WriteString("P1 ^ PF")
+			}
+			if bl {
+				col.CXP1FB |= 0x40
+				col.Activity.WriteString("P1 ^ BL")
+			}
+			col.mem.ChipWrite(addresses.CXP1FB, col.CXP1FB)
+		}
+	}
+
+	if bl && pf {
+		col.CXBLPF |= 0x80
+		col.Activity.WriteString("BL ^ PF")
+		col.mem.ChipWrite(addresses.CXBLPF, col.CXBLPF)
+	}
+	// no bit 6 for CXBLPF
+
+	if (p0 && p1) || (m0 && m1) {
+		if p0 && p1 {
+			col.CXPPMM |= 0x80
+			col.Activity.WriteString("P0 ^ P1")
+		}
+		if m0 && m1 {
+			col.CXPPMM |= 0x40
+			col.Activity.WriteString("M0 ^ M1")
+		}
+		col.mem.ChipWrite(addresses.CXPPMM, col.CXPPMM)
+	}
+}
+
+// this is a naive implementation of the collision registers checking. the
+// version above is "optimised" but the reference implementation below is maybe
+// easier to understand.
+func (col *Collisions) tick_reference(p0, p1, m0, m1, bl, pf bool) {
 	col.Activity.Reset()
 
 	if m0 && p1 {
