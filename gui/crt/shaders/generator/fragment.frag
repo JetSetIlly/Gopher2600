JetSetIlly/Gopher2600

commit 364fef5228d098ebf60cd5c2261cd84b79e8dde4

CRT phosphor now works with debug screen
textures (regular pixels and phosphor pixels) were not being rendered if
emulator was too fast for the vsync. this was okay but it caused visible
artefacts in playmode when vsync was enabled and the CRT prefs was
visible, particular on fast machines.

gui/sdlimgui/glsl.go
@@ -224,10 +224,12 @@ func (rnd *glsl) render(displaySize [2]float32, framebufferSize [2]float32, draw
 				switch textureID {
 				case rnd.img.wm.dbgScr.screenTexture:
 					gl.Uniform1i(rnd.attribImageType, shaders.DebugScr)
+					gl.Uniform1i(rnd.attribPhosphorTexture, 1)
 				case rnd.img.wm.dbgScr.overlayTexture:
 					gl.Uniform1i(rnd.attribImageType, shaders.Overlay)
 				case rnd.img.wm.playScr.screenTexture:
 					gl.Uniform1i(rnd.attribImageType, shaders.PlayScr)
+					gl.Uniform1i(rnd.attribPhosphorTexture, 1)
 				case rnd.img.wm.crtPrefs.crtTexture:
 					gl.Uniform1i(rnd.attribImageType, shaders.PrefsCRT)
 				default:
@@ -248,19 +250,29 @@ func (rnd *glsl) render(displaySize [2]float32, framebufferSize [2]float32, draw
 }
 
 func (rnd *glsl) setShaderOptions() {
-	// !!TODO: different scaling values for different screen windows
-	vertScaling := rnd.img.wm.dbgScr.getScaling(false)
-	horizScaling := rnd.img.wm.dbgScr.getScaling(true)
+	// attribImageType and attribPhosphorTexture set in render() function in the gl commands list
 
-	// crt preferences
+	// scaling of screen
+	var vertScaling float32
+	var horizScaling float32
+	if rnd.img.isPlaymode() {
+		vertScaling = rnd.img.wm.playScr.getScaling(false)
+		horizScaling = rnd.img.wm.playScr.getScaling(true)
+	} else {
+		vertScaling = rnd.img.wm.dbgScr.getScaling(false)
+		horizScaling = rnd.img.wm.dbgScr.getScaling(true)
+	}
+
+	// crt preferences. for playmode the stored preferences are used and for
+	// the debug screen the local CRT boolean is used
 	var crt bool
 	if rnd.img.isPlaymode() {
 		crt = rnd.img.crtPrefs.Enabled.Get().(bool)
 	} else {
 		crt = rnd.img.wm.dbgScr.crt
 	}
+
 	gl.Uniform1i(rnd.attribCRT, boolToInt32(crt))
-	gl.Uniform1i(rnd.attribPhosphorTexture, 1)
 	gl.Uniform1i(rnd.attribPhosphor, boolToInt32(rnd.img.crtPrefs.Phosphor.Get().(bool)))
 	gl.Uniform1i(rnd.attribMask, boolToInt32(rnd.img.crtPrefs.Mask.Get().(bool)))
 	gl.Uniform1i(rnd.attribScanlines, boolToInt32(rnd.img.crtPrefs.Scanlines.Get().(bool)))

gui/sdlimgui/screen.go
@@ -421,47 +421,51 @@ func (scr *screen) render() {
 		if scr.crit.renderIdx == scr.crit.plotIdx {
 			scr.crit.renderIdx = v
 			scr.crit.section.Unlock()
-			return
-		}
+		} else {
+			scr.copyPixels()
+			scr.crit.section.Unlock()
 
-		// copy pixels from render buffer to the live copy.
-		for i := 0; i < len(scr.crit.bufferPixels[scr.crit.renderIdx].Pix); i += 4 {
-			scr.crit.pixels.Pix[i] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i]
-			scr.crit.pixels.Pix[i+1] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+1]
-			scr.crit.pixels.Pix[i+2] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+2]
-			scr.crit.pixels.Pix[i+3] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+3]
-
-			if scr.crit.pixels.Pix[i] == 0 && scr.crit.pixels.Pix[i+1] == 0 && scr.crit.pixels.Pix[i+2] == 0 {
-				// alpha channel records the number of frames the phosphor has
-				// been active. starting at 255 and counting down to 0
-				if scr.crit.phosphor.Pix[i+3] > 0 {
-					scr.crit.phosphor.Pix[i+3]--
-				}
-			} else {
-				// copy current render pixels into phosphor
-				scr.crit.phosphor.Pix[i] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i]
-				scr.crit.phosphor.Pix[i+1] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+1]
-				scr.crit.phosphor.Pix[i+2] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+2]
-				scr.crit.phosphor.Pix[i+3] = 0xff
+			// let the emulator thread know it's okay to continue
+			select {
+			case <-scr.emuWait:
+				scr.emuWaitAck <- true
+			default:
 			}
 		}
-
-		scr.crit.section.Unlock()
-
-		// let the emulator thread know it's okay to continue
-		select {
-		case <-scr.emuWait:
-			scr.emuWaitAck <- true
-		default:
-		}
 	} else {
-		// for non-playmode we use the plotIdx directly, without any buffering
-		copy(scr.crit.pixels.Pix, scr.crit.bufferPixels[scr.crit.plotIdx].Pix)
+		// for non-playmode copy pixels directly (no alteration of the
+		// renderIdx value) without the vsync buffer
+		scr.copyPixels()
 		scr.crit.section.Unlock()
 	}
 
 	// update attached renderers
 	for _, r := range scr.renderers {
 		r.render()
+	}
+}
+
+// copy pixels from buffer to the pixels and update phosphor pixels
+func (scr *screen) copyPixels() {
+	// copy pixels from render buffer to the live copy.
+	for i := 0; i < len(scr.crit.bufferPixels[scr.crit.renderIdx].Pix); i += 4 {
+		scr.crit.pixels.Pix[i] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i]
+		scr.crit.pixels.Pix[i+1] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+1]
+		scr.crit.pixels.Pix[i+2] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+2]
+		scr.crit.pixels.Pix[i+3] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+3]
+
+		if scr.crit.pixels.Pix[i] == 0 && scr.crit.pixels.Pix[i+1] == 0 && scr.crit.pixels.Pix[i+2] == 0 {
+			// alpha channel records the number of frames the phosphor has
+			// been active. starting at 255 and counting down to 0
+			if scr.crit.phosphor.Pix[i+3] > 0 {
+				scr.crit.phosphor.Pix[i+3]--
+			}
+		} else {
+			// copy current render pixels into phosphor
+			scr.crit.phosphor.Pix[i] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i]
+			scr.crit.phosphor.Pix[i+1] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+1]
+			scr.crit.phosphor.Pix[i+2] = scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i+2]
+			scr.crit.phosphor.Pix[i+3] = 0xff
+		}
 	}
 }

gui/sdlimgui/win_dbgscr.go
@@ -43,8 +43,9 @@ type winDbgScr struct {
 	overlay     bool
 
 	// textures
-	screenTexture  uint32
-	overlayTexture uint32
+	screenTexture   uint32
+	overlayTexture  uint32
+	phosphorTexture uint32
 
 	// (re)create textures on next render()
 	createTextures bool
@@ -107,6 +108,12 @@ func newWinDbgScr(img *SdlImgui) (window, error) {
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.GenTextures(1, &win.overlayTexture)
 	gl.BindTexture(gl.TEXTURE_2D, win.overlayTexture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+
+	gl.ActiveTexture(gl.TEXTURE1)
+	gl.GenTextures(1, &win.phosphorTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.phosphorTexture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 
@@ -479,7 +486,8 @@ func (win *winDbgScr) resize() {
 // screen critical section.
 func (win *winDbgScr) render() {
 	var pixels *image.RGBA
-	var overlayPixels *image.RGBA
+	var overlay *image.RGBA
+	var phosphor *image.RGBA
 
 	if win.cropped {
 		if win.debugColors {
@@ -487,14 +495,16 @@ func (win *winDbgScr) render() {
 		} else {
 			pixels = win.scr.crit.cropPixels
 		}
-		overlayPixels = win.scr.crit.cropOverlayPixels
+		overlay = win.scr.crit.cropOverlayPixels
+		phosphor = win.scr.crit.cropPhosphor
 	} else {
 		if win.debugColors {
 			pixels = win.scr.crit.elementPixels
 		} else {
 			pixels = win.scr.crit.pixels
 		}
-		overlayPixels = win.scr.crit.overlayPixels
+		overlay = win.scr.crit.overlayPixels
+		phosphor = win.scr.crit.phosphor
 	}
 
 	gl.PixelStorei(gl.UNPACK_ROW_LENGTH, int32(pixels.Stride)/4)
@@ -513,7 +523,14 @@ func (win *winDbgScr) render() {
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
 			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(overlayPixels.Pix))
+			gl.Ptr(overlay.Pix))
+
+		gl.ActiveTexture(gl.TEXTURE1)
+		gl.BindTexture(gl.TEXTURE_2D, win.phosphorTexture)
+		gl.TexImage2D(gl.TEXTURE_2D, 0,
+			gl.RGBA, int32(phosphor.Bounds().Size().X), int32(phosphor.Bounds().Size().Y), 0,
+			gl.RGBA, gl.UNSIGNED_BYTE,
+			gl.Ptr(phosphor.Pix))
 
 		win.createTextures = false
 	} else {
@@ -527,7 +544,14 @@ func (win *winDbgScr) render() {
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(overlayPixels.Pix))
+			gl.Ptr(overlay.Pix))
+
+		gl.ActiveTexture(gl.TEXTURE1)
+		gl.BindTexture(gl.TEXTURE_2D, win.phosphorTexture)
+		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
+			0, 0, int32(phosphor.Bounds().Size().X), int32(phosphor.Bounds().Size().Y),
+			gl.RGBA, gl.UNSIGNED_BYTE,
+			gl.Ptr(phosphor.Pix))
 	}
 
 	// set screen image scaling (and image padding) based on the current window size

gui/sdlimgui/win_playscr.go
@@ -37,8 +37,8 @@ type winPlayScr struct {
 	scr *screen
 
 	// textures
-	screenTexture uint32
-	phosphor      uint32
+	screenTexture   uint32
+	phosphorTexture uint32
 
 	// (re)create textures on next render()
 	createTextures bool
@@ -77,8 +77,8 @@ func newWinPlayScr(img *SdlImgui) window {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 
 	gl.ActiveTexture(gl.TEXTURE1)
-	gl.GenTextures(1, &win.phosphor)
-	gl.BindTexture(gl.TEXTURE_2D, win.phosphor)
+	gl.GenTextures(1, &win.phosphorTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.phosphorTexture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 
@@ -177,7 +177,7 @@ func (win *winPlayScr) render() {
 			gl.Ptr(pixels.Pix))
 
 		gl.ActiveTexture(gl.TEXTURE1)
-		gl.BindTexture(gl.TEXTURE_2D, win.phosphor)
+		gl.BindTexture(gl.TEXTURE_2D, win.phosphorTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
 			gl.RGBA, int32(phosphor.Bounds().Size().X), int32(phosphor.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
@@ -191,7 +191,7 @@ func (win *winPlayScr) render() {
 			gl.Ptr(pixels.Pix))
 
 		gl.ActiveTexture(gl.TEXTURE1)
-		gl.BindTexture(gl.TEXTURE_2D, win.phosphor)
+		gl.BindTexture(gl.TEXTURE_2D, win.phosphorTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(phosphor.Bounds().Size().X), int32(phosphor.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,

commit a3c0fca9f91d285634b2ae81ec33fceb09e08037

fixed blur in playmode
broken in 364fef5228d098ebf60cd5c2261cd84b79e8dde4

tweaked phosphor strength in fragment shader

gui/crt/shaders/generator/fragment.frag
@@ -17,7 +17,7 @@ uniform int ImageType;
 uniform int ShowCursor;  
 uniform int IsCropped; 
 uniform vec2 ScreenDim;
-uniform vec2 CropScreenDim;
+uniform vec2 UncroppedScreenDim;
 uniform float ScalingX;
 uniform float ScalingY;
 uniform float LastX; 
@@ -67,6 +67,10 @@ float gold_noise(in vec2 xy){
 	return fract(tan(distance(xy*PHI, xy)*RandSeed)*xy.x);
 }
 
+float relativeLuminance(vec3 rgb) {
+	return (float(rgb.r) / 255 * 0.2126) + (float(rgb.g) / 255 * 0.7152) + (float(rgb.b) / 255 * 0.0722);
+}
+
 vec2 coords = Frag_UV.xy;
 
 float hblank;
@@ -90,22 +94,22 @@ void crt() {
 		if (Crt_Color.r == 0 && Crt_Color.g == 0 && Crt_Color.b == 0) {
 			vec4 ph = texture(PhosphorTexture, vec2(coords.x, coords.y)).rgba;
 			Crt_Color.rgb = ph.rgb;
-			Crt_Color.r *= pow(ph.a, 85*PhosphorSpeed);
-			Crt_Color.g *= pow(ph.a, 100*PhosphorSpeed);
-			Crt_Color.b *= pow(ph.a, 70*PhosphorSpeed);
+			Crt_Color.r *= pow(ph.a, 145*PhosphorSpeed);
+			Crt_Color.g *= pow(ph.a, 160*PhosphorSpeed);
+			Crt_Color.b *= pow(ph.a, 130*PhosphorSpeed);
 		}
 	}
 
 	// noise
 	if (EnableNoise == True) {
-		float r;
-		r = gold_noise(gl_FragCoord.xy);
-		if (r < 0.33) {
-			Crt_Color.r *= max(1.0-NoiseLevel, gold_noise(gl_FragCoord.xy));
-		} else if (r < 0.66) {
-			Crt_Color.g *= max(1.0-NoiseLevel, gold_noise(gl_FragCoord.xy));
+		float n;
+		n = gold_noise(gl_FragCoord.xy);
+		if (n < 0.33) {
+			Crt_Color.r *= max(1.0-NoiseLevel, n);
+		} else if (n < 0.66) {
+			Crt_Color.g *= max(1.0-NoiseLevel, n);
 		} else {
-			Crt_Color.b *= max(1.0-NoiseLevel, gold_noise(gl_FragCoord.xy));
+			Crt_Color.b *= max(1.0-NoiseLevel, n);
 		}
 	}
 
@@ -156,7 +160,7 @@ void crt() {
 			// understand this well enough to say for sure what the relationship
 			// between 25 and 10 is, but the following ratio between
 			// cropped/uncropped widths gives us a value of 23.5
-			float f =ScreenDim.x/(ScreenDim.x-CropScreenDim.x);
+			float f =UncroppedScreenDim.x/(UncroppedScreenDim.x-ScreenDim.x);
 			vignette = (f*(coords.x-hblank)*(coords.y-topScanline)*(1.0-coords.x)*(1.0-coords.y));
 		}
 
@@ -175,12 +179,12 @@ void debugscr() {
 	float pixelY;
 
 	if (IsCropped == True) {
-		texelX = ScalingX / CropScreenDim.x;
-		texelY = ScalingY / CropScreenDim.y;
-		hblank = Hblank / CropScreenDim.x;
-		lastX = LastX / CropScreenDim.x;
+		texelX = ScalingX / ScreenDim.x;
+		texelY = ScalingY / ScreenDim.y;
+		hblank = Hblank / ScreenDim.x;
+		lastX = LastX / ScreenDim.x;
 		topScanline = 0;
-		botScanline = (BotScanline - TopScanline) / CropScreenDim.y;
+		botScanline = (BotScanline - TopScanline) / ScreenDim.y;
 
 		// the LastY coordinate refers to the full-frame scanline. the cropped
 		// texture however counts from zero at the visible edge so we need to
@@ -188,15 +192,15 @@ void debugscr() {
 		//
 		// note that there's no need to do this for LastX because the
 		// horizontal position is counted from -68 in all instances.
-		lastY = (LastY - TopScanline) / CropScreenDim.y;
+		lastY = (LastY - TopScanline) / ScreenDim.y;
 	} else {
-		texelX = ScalingX / ScreenDim.x;
-		texelY = ScalingY / ScreenDim.y;
-		hblank = Hblank / ScreenDim.x;
-		topScanline = TopScanline / ScreenDim.y;
-		botScanline = BotScanline / ScreenDim.y;
-		lastX = LastX / ScreenDim.x;
-		lastY = LastY / ScreenDim.y;
+		texelX = ScalingX / UncroppedScreenDim.x;
+		texelY = ScalingY / UncroppedScreenDim.y;
+		hblank = Hblank / UncroppedScreenDim.x;
+		topScanline = TopScanline / UncroppedScreenDim.y;
+		botScanline = BotScanline / UncroppedScreenDim.y;
+		lastX = LastX / UncroppedScreenDim.x;
+		lastY = LastY / UncroppedScreenDim.y;
 	}
 
 	pixelX = texelX / ScalingX;
@@ -302,8 +306,8 @@ void debugscr() {
 }
 
 void playscr() {
-	texelX = ScalingX / CropScreenDim.x;
-	texelY = ScalingY / CropScreenDim.y;
+	texelX = ScalingX / ScreenDim.x;
+	texelY = ScalingY / ScreenDim.y;
 	if (EnableCRT == True) {
 		crt();
 		return;
@@ -312,8 +316,8 @@ void playscr() {
 }
 
 void prefscrt() {
-	texelX = ScalingX / CropScreenDim.x;
-	texelY = ScalingY / CropScreenDim.y;
+	texelX = ScalingX / ScreenDim.x;
+	texelY = ScalingY / ScreenDim.y;
 	crt();
 }
 

gui/crt/shaders/shaders.go
@@ -38,7 +38,7 @@ uniform int ImageType;
 uniform int ShowCursor;  
 uniform int IsCropped; 
 uniform vec2 ScreenDim;
-uniform vec2 CropScreenDim;
+uniform vec2 UncroppedScreenDim;
 uniform float ScalingX;
 uniform float ScalingY;
 uniform float LastX; 
@@ -88,6 +88,10 @@ float gold_noise(in vec2 xy){
 	return fract(tan(distance(xy*PHI, xy)*RandSeed)*xy.x);
 }
 
+float relativeLuminance(vec3 rgb) {
+	return (float(rgb.r) / 255 * 0.2126) + (float(rgb.g) / 255 * 0.7152) + (float(rgb.b) / 255 * 0.0722);
+}
+
 vec2 coords = Frag_UV.xy;
 
 float hblank;
@@ -111,22 +115,22 @@ void crt() {
 		if (Crt_Color.r == 0 && Crt_Color.g == 0 && Crt_Color.b == 0) {
 			vec4 ph = texture(PhosphorTexture, vec2(coords.x, coords.y)).rgba;
 			Crt_Color.rgb = ph.rgb;
-			Crt_Color.r *= pow(ph.a, 85*PhosphorSpeed);
-			Crt_Color.g *= pow(ph.a, 100*PhosphorSpeed);
-			Crt_Color.b *= pow(ph.a, 70*PhosphorSpeed);
+			Crt_Color.r *= pow(ph.a, 145*PhosphorSpeed);
+			Crt_Color.g *= pow(ph.a, 160*PhosphorSpeed);
+			Crt_Color.b *= pow(ph.a, 130*PhosphorSpeed);
 		}
 	}
 
 	// noise
 	if (EnableNoise == True) {
-		float r;
-		r = gold_noise(gl_FragCoord.xy);
-		if (r < 0.33) {
-			Crt_Color.r *= max(1.0-NoiseLevel, gold_noise(gl_FragCoord.xy));
-		} else if (r < 0.66) {
-			Crt_Color.g *= max(1.0-NoiseLevel, gold_noise(gl_FragCoord.xy));
+		float n;
+		n = gold_noise(gl_FragCoord.xy);
+		if (n < 0.33) {
+			Crt_Color.r *= max(1.0-NoiseLevel, n);
+		} else if (n < 0.66) {
+			Crt_Color.g *= max(1.0-NoiseLevel, n);
 		} else {
-			Crt_Color.b *= max(1.0-NoiseLevel, gold_noise(gl_FragCoord.xy));
+			Crt_Color.b *= max(1.0-NoiseLevel, n);
 		}
 	}
 
@@ -177,7 +181,7 @@ void crt() {
 			// understand this well enough to say for sure what the relationship
 			// between 25 and 10 is, but the following ratio between
 			// cropped/uncropped widths gives us a value of 23.5
-			float f =ScreenDim.x/(ScreenDim.x-CropScreenDim.x);
+			float f =UncroppedScreenDim.x/(UncroppedScreenDim.x-ScreenDim.x);
 			vignette = (f*(coords.x-hblank)*(coords.y-topScanline)*(1.0-coords.x)*(1.0-coords.y));
 		}
 
@@ -196,12 +200,12 @@ void debugscr() {
 	float pixelY;
 
 	if (IsCropped == True) {
-		texelX = ScalingX / CropScreenDim.x;
-		texelY = ScalingY / CropScreenDim.y;
-		hblank = Hblank / CropScreenDim.x;
-		lastX = LastX / CropScreenDim.x;
+		texelX = ScalingX / ScreenDim.x;
+		texelY = ScalingY / ScreenDim.y;
+		hblank = Hblank / ScreenDim.x;
+		lastX = LastX / ScreenDim.x;
 		topScanline = 0;
-		botScanline = (BotScanline - TopScanline) / CropScreenDim.y;
+		botScanline = (BotScanline - TopScanline) / ScreenDim.y;
 
 		// the LastY coordinate refers to the full-frame scanline. the cropped
 		// texture however counts from zero at the visible edge so we need to
@@ -209,15 +213,15 @@ void debugscr() {
 		//
 		// note that there's no need to do this for LastX because the
 		// horizontal position is counted from -68 in all instances.
-		lastY = (LastY - TopScanline) / CropScreenDim.y;
+		lastY = (LastY - TopScanline) / ScreenDim.y;
 	} else {
-		texelX = ScalingX / ScreenDim.x;
-		texelY = ScalingY / ScreenDim.y;
-		hblank = Hblank / ScreenDim.x;
-		topScanline = TopScanline / ScreenDim.y;
-		botScanline = BotScanline / ScreenDim.y;
-		lastX = LastX / ScreenDim.x;
-		lastY = LastY / ScreenDim.y;
+		texelX = ScalingX / UncroppedScreenDim.x;
+		texelY = ScalingY / UncroppedScreenDim.y;
+		hblank = Hblank / UncroppedScreenDim.x;
+		topScanline = TopScanline / UncroppedScreenDim.y;
+		botScanline = BotScanline / UncroppedScreenDim.y;
+		lastX = LastX / UncroppedScreenDim.x;
+		lastY = LastY / UncroppedScreenDim.y;
 	}
 
 	pixelX = texelX / ScalingX;
@@ -323,8 +327,8 @@ void debugscr() {
 }
 
 void playscr() {
-	texelX = ScalingX / CropScreenDim.x;
-	texelY = ScalingY / CropScreenDim.y;
+	texelX = ScalingX / ScreenDim.x;
+	texelY = ScalingY / ScreenDim.y;
 	if (EnableCRT == True) {
 		crt();
 		return;
@@ -333,8 +337,8 @@ void playscr() {
 }
 
 void prefscrt() {
-	texelX = ScalingX / CropScreenDim.x;
-	texelY = ScalingY / CropScreenDim.y;
+	texelX = ScalingX / ScreenDim.x;
+	texelY = ScalingY / ScreenDim.y;
 	crt();
 }
 

gui/sdlimgui/glsl.go
@@ -83,17 +83,17 @@ type glsl struct {
 	attribImageType int32 // uniform
 
 	// the following attrib variables are strictly for the screen texture
-	attribShowCursor    int32 // uniform
-	attribIsCropped     int32 // uniform
-	attribScreenDim     int32 // uniform
-	attribCropScreenDim int32 // uniform
-	attribScalingX      int32 // uniform
-	attribScalingY      int32 // uniform
-	attribLastX         int32 // uniform
-	attribLastY         int32 // uniform
-	attribHblank        int32 // uniform
-	attribTopScanline   int32 // uniform
-	attribBotScanline   int32 // uniform
+	attribShowCursor         int32 // uniform
+	attribIsCropped          int32 // uniform
+	attribScreenDim          int32 // uniform
+	attribUncroppedScreenDim int32 // uniform
+	attribScalingX           int32 // uniform
+	attribScalingY           int32 // uniform
+	attribLastX              int32 // uniform
+	attribLastY              int32 // uniform
+	attribHblank             int32 // uniform
+	attribTopScanline        int32 // uniform
+	attribBotScanline        int32 // uniform
 
 	attribEnableCRT           int32 // uniform
 	attribEnablePhosphor      int32 // uniform
@@ -331,7 +331,7 @@ func (rnd *glsl) setOptions(textureID uint32) {
 	gl.Uniform1f(rnd.attribScanlinesBrightness, float32(rnd.img.crtPrefs.ScanlinesBrightness.Get().(float64)))
 	gl.Uniform1f(rnd.attribNoiseLevel, float32(rnd.img.crtPrefs.NoiseLevel.Get().(float64)))
 	gl.Uniform1f(rnd.attribBlurLevel, float32(rnd.img.crtPrefs.BlurLevel.Get().(float64)))
-	gl.Uniform1f(rnd.attribRandSeed, float32(time.Now().Nanosecond())/1000000000.0)
+	gl.Uniform1f(rnd.attribRandSeed, float32(time.Now().Nanosecond())/100000000.0)
 
 	// critical section
 	rnd.img.screen.crit.section.Lock()
@@ -341,10 +341,10 @@ func (rnd *glsl) setOptions(textureID uint32) {
 	case rnd.img.wm.dbgScr.screenTexture:
 		fallthrough
 	case rnd.img.wm.dbgScr.overlayTexture:
-		gl.Uniform2f(rnd.attribScreenDim, rnd.img.wm.dbgScr.getScaledWidth(false), rnd.img.wm.dbgScr.getScaledHeight(false))
 		gl.Uniform1f(rnd.attribScalingX, rnd.img.wm.dbgScr.getScaling(true))
 		gl.Uniform1f(rnd.attribScalingY, rnd.img.wm.dbgScr.getScaling(false))
-		gl.Uniform2f(rnd.attribCropScreenDim, rnd.img.wm.dbgScr.getScaledWidth(true), rnd.img.wm.dbgScr.getScaledHeight(true))
+		gl.Uniform2f(rnd.attribUncroppedScreenDim, rnd.img.wm.dbgScr.getScaledWidth(false), rnd.img.wm.dbgScr.getScaledHeight(false))
+		gl.Uniform2f(rnd.attribScreenDim, rnd.img.wm.dbgScr.getScaledWidth(true), rnd.img.wm.dbgScr.getScaledHeight(true))
 		gl.Uniform1i(rnd.attribIsCropped, boolToInt32(rnd.img.wm.dbgScr.cropped))
 
 		cursorX := rnd.img.screen.crit.lastX
@@ -454,7 +454,7 @@ func (rnd *glsl) setup() {
 	rnd.attribShowCursor = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("ShowCursor"+"\x00"))
 	rnd.attribIsCropped = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("IsCropped"+"\x00"))
 	rnd.attribScreenDim = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("ScreenDim"+"\x00"))
-	rnd.attribCropScreenDim = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("CropScreenDim"+"\x00"))
+	rnd.attribUncroppedScreenDim = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("UncroppedScreenDim"+"\x00"))
 	rnd.attribScalingX = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("ScalingX"+"\x00"))
 	rnd.attribScalingY = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("ScalingY"+"\x00"))
 	rnd.attribLastX = gl.GetUniformLocation(rnd.shaderHandle, gl.Str("LastX"+"\x00"))