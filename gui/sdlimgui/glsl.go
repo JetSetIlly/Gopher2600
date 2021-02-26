JetSetIlly/Gopher2600

commit 987b40349b3943a3b80a59d7ff2e54a7e6f28843

playscreen now draws to background drawlist
simpler code path so there maybe a small performance improvement

playscr fps moved to playscr.go

play screen mouse capture now done with RMB

gui/sdlimgui/glsl.go
@@ -163,7 +163,11 @@ func boolToInt32(v bool) int32 {
 }
 
 // render translates the ImGui draw data to OpenGL3 commands.
-func (rnd *glsl) render(displaySize [2]float32, framebufferSize [2]float32, drawData imgui.DrawData) {
+func (rnd *glsl) render() {
+	displaySize := rnd.img.plt.displaySize()
+	framebufferSize := rnd.img.plt.framebufferSize()
+	drawData := imgui.RenderedDrawData()
+
 	st := storeGLState()
 	defer st.restore()
 
@@ -249,7 +253,7 @@ func (rnd *glsl) render(displaySize [2]float32, framebufferSize [2]float32, draw
 					rnd.debugScr()
 				case rnd.img.wm.dbgScr.overlayTexture:
 					rnd.overlay()
-				case rnd.img.wm.playScr.screenTexture:
+				case rnd.img.playScr.screenTexture:
 					rnd.playScr()
 				case rnd.img.wm.crtPrefs.crtTexture:
 					rnd.prefsCRT()
@@ -300,11 +304,11 @@ func (rnd *glsl) setOptions(textureID uint32) {
 	var vertScaling float32
 	var horizScaling float32
 	if rnd.img.isPlaymode() {
-		vertScaling = rnd.img.wm.playScr.getScaling(false)
-		horizScaling = rnd.img.wm.playScr.getScaling(true)
+		vertScaling = rnd.img.playScr.scaling
+		horizScaling = rnd.img.playScr.horizScaling()
 	} else {
-		vertScaling = rnd.img.wm.dbgScr.getScaling(false)
-		horizScaling = rnd.img.wm.dbgScr.getScaling(true)
+		vertScaling = rnd.img.wm.dbgScr.scaling
+		horizScaling = rnd.img.wm.dbgScr.horizScaling()
 	}
 
 	// crt preferences. for playmode the stored preferences are used and for
@@ -318,20 +322,21 @@ func (rnd *glsl) setOptions(textureID uint32) {
 
 	// preferences
 	gl.Uniform1i(rnd.attribEnableCRT, boolToInt32(crt))
+	if crt {
+		gl.Uniform1i(rnd.attribEnablePhosphor, boolToInt32(rnd.img.crtPrefs.Phosphor.Get().(bool)))
+		gl.Uniform1i(rnd.attribEnableShadowMask, boolToInt32(rnd.img.crtPrefs.Mask.Get().(bool)))
+		gl.Uniform1i(rnd.attribEnableScanlines, boolToInt32(rnd.img.crtPrefs.Scanlines.Get().(bool)))
+		gl.Uniform1i(rnd.attribEnableNoise, boolToInt32(rnd.img.crtPrefs.Noise.Get().(bool)))
+		gl.Uniform1i(rnd.attribEnableBlur, boolToInt32(rnd.img.crtPrefs.Blur.Get().(bool)))
+		gl.Uniform1i(rnd.attribEnableVignette, boolToInt32(rnd.img.crtPrefs.Vignette.Get().(bool)))
 
-	gl.Uniform1i(rnd.attribEnablePhosphor, boolToInt32(rnd.img.crtPrefs.Phosphor.Get().(bool)))
-	gl.Uniform1i(rnd.attribEnableShadowMask, boolToInt32(rnd.img.crtPrefs.Mask.Get().(bool)))
-	gl.Uniform1i(rnd.attribEnableScanlines, boolToInt32(rnd.img.crtPrefs.Scanlines.Get().(bool)))
-	gl.Uniform1i(rnd.attribEnableNoise, boolToInt32(rnd.img.crtPrefs.Noise.Get().(bool)))
-	gl.Uniform1i(rnd.attribEnableBlur, boolToInt32(rnd.img.crtPrefs.Blur.Get().(bool)))
-	gl.Uniform1i(rnd.attribEnableVignette, boolToInt32(rnd.img.crtPrefs.Vignette.Get().(bool)))
-
-	gl.Uniform1f(rnd.attribPhosphorSpeed, float32(rnd.img.crtPrefs.PhosphorSpeed.Get().(float64)))
-	gl.Uniform1f(rnd.attribMaskBrightness, float32(rnd.img.crtPrefs.MaskBrightness.Get().(float64)))
-	gl.Uniform1f(rnd.attribScanlinesBrightness, float32(rnd.img.crtPrefs.ScanlinesBrightness.Get().(float64)))
-	gl.Uniform1f(rnd.attribNoiseLevel, float32(rnd.img.crtPrefs.NoiseLevel.Get().(float64)))
-	gl.Uniform1f(rnd.attribBlurLevel, float32(rnd.img.crtPrefs.BlurLevel.Get().(float64)))
-	gl.Uniform1f(rnd.attribRandSeed, float32(time.Now().Nanosecond())/100000000.0)
+		gl.Uniform1f(rnd.attribPhosphorSpeed, float32(rnd.img.crtPrefs.PhosphorSpeed.Get().(float64)))
+		gl.Uniform1f(rnd.attribMaskBrightness, float32(rnd.img.crtPrefs.MaskBrightness.Get().(float64)))
+		gl.Uniform1f(rnd.attribScanlinesBrightness, float32(rnd.img.crtPrefs.ScanlinesBrightness.Get().(float64)))
+		gl.Uniform1f(rnd.attribNoiseLevel, float32(rnd.img.crtPrefs.NoiseLevel.Get().(float64)))
+		gl.Uniform1f(rnd.attribBlurLevel, float32(rnd.img.crtPrefs.BlurLevel.Get().(float64)))
+		gl.Uniform1f(rnd.attribRandSeed, float32(time.Now().Nanosecond())/100000000.0)
+	}
 
 	// critical section
 	rnd.img.screen.crit.section.Lock()
@@ -341,10 +346,10 @@ func (rnd *glsl) setOptions(textureID uint32) {
 	case rnd.img.wm.dbgScr.screenTexture:
 		fallthrough
 	case rnd.img.wm.dbgScr.overlayTexture:
-		gl.Uniform1f(rnd.attribScalingX, rnd.img.wm.dbgScr.getScaling(true))
-		gl.Uniform1f(rnd.attribScalingY, rnd.img.wm.dbgScr.getScaling(false))
-		gl.Uniform2f(rnd.attribUncroppedScreenDim, rnd.img.wm.dbgScr.getScaledWidth(false), rnd.img.wm.dbgScr.getScaledHeight(false))
-		gl.Uniform2f(rnd.attribScreenDim, rnd.img.wm.dbgScr.getScaledWidth(true), rnd.img.wm.dbgScr.getScaledHeight(true))
+		gl.Uniform1f(rnd.attribScalingX, rnd.img.wm.dbgScr.horizScaling())
+		gl.Uniform1f(rnd.attribScalingY, rnd.img.wm.dbgScr.scaling)
+		gl.Uniform2f(rnd.attribUncroppedScreenDim, rnd.img.wm.dbgScr.scaledWidth(false), rnd.img.wm.dbgScr.scaledHeight(false))
+		gl.Uniform2f(rnd.attribScreenDim, rnd.img.wm.dbgScr.scaledWidth(true), rnd.img.wm.dbgScr.scaledHeight(true))
 		gl.Uniform1i(rnd.attribIsCropped, boolToInt32(rnd.img.wm.dbgScr.cropped))
 
 		cursorX := rnd.img.screen.crit.lastX
@@ -357,10 +362,10 @@ func (rnd *glsl) setOptions(textureID uint32) {
 		}
 		gl.Uniform1f(rnd.attribLastY, float32(cursorY)*vertScaling)
 
-	case rnd.img.wm.playScr.screenTexture:
-		gl.Uniform2f(rnd.attribScreenDim, rnd.img.wm.playScr.getScaledWidth(), rnd.img.wm.playScr.getScaledHeight())
-		gl.Uniform1f(rnd.attribScalingX, rnd.img.wm.playScr.getScaling(true))
-		gl.Uniform1f(rnd.attribScalingY, rnd.img.wm.playScr.getScaling(false))
+	case rnd.img.playScr.screenTexture:
+		gl.Uniform2f(rnd.attribScreenDim, rnd.img.playScr.scaledWidth(), rnd.img.playScr.scaledHeight())
+		gl.Uniform1f(rnd.attribScalingX, rnd.img.playScr.horizScaling())
+		gl.Uniform1f(rnd.attribScalingY, rnd.img.playScr.scaling)
 		gl.Uniform1i(rnd.attribIsCropped, shaders.True)
 
 	case rnd.img.wm.crtPrefs.crtTexture:

gui/sdlimgui/lazyvalues/lazy.go
@@ -103,7 +103,7 @@ func (val *LazyValues) SetActive(active bool) {
 
 // Refresh lazy values.
 func (val *LazyValues) Refresh() {
-	if !val.active || val.Dbg == nil {
+	if !val.active {
 		return
 	}
 

gui/sdlimgui/manager.go
@@ -48,6 +48,16 @@ type manager struct {
 	menu          map[menuGroup][]menuEntry
 	menuInfoWidth float32
 
+	// some windows need to be referenced beyond the capabilities of the window
+	// interface.
+	//
+	// if required, other windows can accessed by:
+	//		window[title].(*windowType)
+	//
+	// the following fields are provided for convenience.
+	dbgScr   *winDbgScr
+	crtPrefs *winCRTPrefs
+
 	// the position of the screen on the current display. the SDL function
 	// Window.GetPosition() is unsuitable for use in conjunction with imgui
 	// because it considers screen space across all display devices, imgui does
@@ -58,19 +68,6 @@ type manager struct {
 	// menu is always in the very top-left corner of the window it is a good
 	// proxy value
 	screenPos imgui.Vec2
-
-	// some windows need to be referenced beyond the capabilities of the window
-	// interface.
-	//
-	// if required, other windows can accessed by:
-	//		window[title].(*windowType)
-	//
-	dbgScr   *winDbgScr
-	crtPrefs *winCRTPrefs
-
-	// the playscreen does not appear in the window list and can only be
-	// referred to via the playScr field.
-	playScr *winPlayScr
 }
 
 // windowDef specifies a window creator, the menu it appears in whether it
@@ -175,9 +172,6 @@ func newManager(img *SdlImgui) (*manager, error) {
 	wm.dbgScr = wm.windows[winDbgScrID].(*winDbgScr)
 	wm.crtPrefs = wm.windows[winCRTPrefsID].(*winCRTPrefs)
 
-	// create play window. this is a special window that does not appear in the window list
-	wm.playScr = newWinPlayScr(img).(*winPlayScr)
-
 	return wm, nil
 }
 
@@ -194,14 +188,14 @@ func (wm *manager) draw() {
 	// playmode draws the screen and other windows that have been listed
 	// as being safe to draw in playmode
 	if wm.img.isPlaymode() {
-		wm.playScr.draw()
-
 		for _, s := range playmodeWindows {
 			wm.windows[s].draw()
 		}
-
 		return
 	}
+
+	// see commentary for screenPos in windowManager declaration
+	wm.screenPos = imgui.WindowPos()
 
 	// no debugger is ready yet so return immediately
 	if wm.img.lz.Dbg == nil {

gui/sdlimgui/manager_menu.go
@@ -35,11 +35,11 @@ const (
 )
 
 // restrict menu entry for those cartridges with specific buses.
-type menuBusRestrict int
+type menuRestrict int
 
 // list of valid menu bus restrictions.
 const (
-	menuRestrictCoProc menuBusRestrict = iota
+	menuRestrictCoProc menuRestrict = iota
 	menuRestrictRAM
 	menuRestrictRegister
 	menuRestrictStatic
@@ -51,7 +51,7 @@ type menuEntry struct {
 	group menuGroup
 
 	// restrictions on when menu entry can appear
-	restrictBus    menuBusRestrict
+	restrictBus    menuRestrict
 	restrictMapper []string
 
 	// the window thats referenced by the menu entry
@@ -65,9 +65,7 @@ func (wm *manager) drawMenu() {
 	if !imgui.BeginMainMenuBar() {
 		return
 	}
-
-	// see commentary for screenPos in windowManager declaration
-	wm.screenPos = imgui.WindowPos()
+	defer imgui.EndMainMenuBar()
 
 	// debugger menu
 	if imgui.BeginMenu("Debugger") {
@@ -149,8 +147,6 @@ func (wm *manager) drawMenu() {
 			imgui.Text(" ")
 		}
 	})
-
-	imgui.EndMainMenuBar()
 }
 
 func (wm *manager) drawMenuEntry(m menuEntry) {

gui/sdlimgui/playscr.go
@@ -16,22 +16,19 @@
 package sdlimgui
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/go-gl/gl/v3.2-core/gl"
 	"github.com/inkyblackness/imgui-go/v4"
 )
-
-const winPlayScrID = "Atari VCS"
 
 // note that values from the lazy package will not be updated in the service
 // loop when the emulator is in playmode. nothing in winPlayScr() therefore
 // should rely on any lazy value
 
-type winPlayScr struct {
-	img  *SdlImgui
-	open bool
-
-	// fps sub-window
-	fps *winPlayScrFPS
+type playScr struct {
+	img *SdlImgui
 
 	// reference to screen data
 	scr *screen
@@ -46,28 +43,27 @@ type winPlayScr struct {
 	// the tv screen has captured mouse input
 	isCaptured bool
 
-	// additional padding for the image so that it is centred in its content space
-	imagePadding imgui.Vec2
+	imagePosMin imgui.Vec2
+	imagePosMax imgui.Vec2
 
-	// size of window and content area in which to centre the image
-	winDim     imgui.Vec2
-	contentDim imgui.Vec2
-
-	// the basic amount by which the image should be scaled. image width
-	// is also scaled by pixelWidth and aspectBias value.
-	//
-	// use getScaling() and setScaling to access this value
+	// the basic amount by which the image should be scaled. this value is
+	// applie to the vertical axis directly. horizontal scaling is scaled by
+	// pixelWidth and aspectBias also. use horizScaling() for that.
 	scaling float32
+
+	// fps
+	fpsOpen  bool
+	fpsPulse *time.Ticker
+	fps      string
 }
 
-func newWinPlayScr(img *SdlImgui) window {
-	win := &winPlayScr{
-		img:     img,
-		scr:     img.screen,
-		scaling: 2.0,
+func newPlayScr(img *SdlImgui) *playScr {
+	win := &playScr{
+		img:      img,
+		scr:      img.screen,
+		scaling:  2.0,
+		fpsPulse: time.NewTicker(time.Second),
 	}
-
-	win.fps = newWinPlayScrFPS(win.img).(*winPlayScrFPS)
 
 	// set texture, creation of textures will be done after every call to resize()
 	gl.ActiveTexture(gl.TEXTURE0)
@@ -82,80 +78,53 @@ func newWinPlayScr(img *SdlImgui) window {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 
+	// set scale and padding on startup. scale and padding will be recalculated
+	// on window resize and textureRenderer.resize()
+	win.scr.crit.section.Lock()
+	win.setScaling()
+	win.scr.crit.section.Unlock()
+
 	return win
 }
 
-func (win *winPlayScr) init() {
-}
+func (win *playScr) draw() {
+	dl := imgui.BackgroundDrawList()
+	dl.AddImage(imgui.TextureID(win.screenTexture), win.imagePosMin, win.imagePosMax)
 
-func (win *winPlayScr) id() string {
-	return winPlayScrID
-}
+	if win.fpsOpen {
+		// update fps
+		select {
+		case <-win.fpsPulse.C:
+			win.fps = fmt.Sprintf("%03.1f fps", win.img.tv.GetActualFPS())
+		default:
+		}
 
-func (win *winPlayScr) isOpen() bool {
-	return win.open
-}
+		imgui.SetNextWindowPos(imgui.Vec2{0, 0})
 
-func (win *winPlayScr) setOpen(open bool) {
-	win.open = open
-}
+		imgui.PushStyleColor(imgui.StyleColorWindowBg, win.img.cols.Transparent)
+		imgui.PushStyleColor(imgui.StyleColorBorder, win.img.cols.Transparent)
 
-func (win *winPlayScr) draw() {
-	if !win.open {
-		return
+		imgui.BeginV("##playscrfps", &win.fpsOpen, imgui.WindowFlagsAlwaysAutoResize|
+			imgui.WindowFlagsNoScrollbar|imgui.WindowFlagsNoTitleBar|imgui.WindowFlagsNoDecoration)
+
+		imgui.Text(win.fps)
+
+		imgui.PopStyleColorV(2)
+		imgui.End()
 	}
-
-	dimen := win.img.plt.displaySize()
-	win.winDim = imgui.Vec2{dimen[0], dimen[1]}
-	imgui.SetNextWindowSizeV(win.winDim, 0)
-
-	imgui.SetNextWindowPosV(imgui.Vec2{0, 0}, 0, imgui.Vec2{0, 0})
-
-	imgui.PushStyleColor(imgui.StyleColorWindowBg, win.img.cols.PlayWindowBg)
-	imgui.PushStyleColor(imgui.StyleColorBorder, win.img.cols.PlayWindowBorder)
-
-	imgui.BeginV(win.id(), &win.open,
-		imgui.WindowFlagsNoDecoration|imgui.WindowFlagsNoMove|imgui.WindowFlagsNoBringToFrontOnFocus)
-
-	// note size of window
-	win.contentDim = imgui.ContentRegionAvail()
-
-	// add horiz/vert padding around screen image
-	imgui.SetCursorPos(imgui.CursorPos().Plus(win.imagePadding))
-
-	// actual display
-	imgui.Image(imgui.TextureID(win.screenTexture), imgui.Vec2{win.getScaledWidth(), win.getScaledHeight()})
-
-	// capture mouse on double click but only if image is being hovered over
-	// and there is no modal window.
-	if !win.img.hasModal && imgui.IsItemHovered() && imgui.IsMouseDoubleClicked(0) {
-		win.img.setCapture(true)
-	}
-
-	imgui.PopStyleColorV(2)
-	imgui.End()
-
-	win.fps.draw()
 }
 
 // resize() implements the textureRenderer interface.
-func (win *winPlayScr) resize() {
+func (win *playScr) resize() {
 	win.createTextures = true
+	win.setScaling()
 }
 
 // render() implements the textureRenderer interface.
 //
 // render is called by service loop (via screen.render()). must be inside
 // screen critical section.
-func (win *winPlayScr) render() {
-	if !win.open {
-		return
-	}
-
-	// set screen image scaling (and image padding) based on the current window size
-	win.setScaleAndPadding(win.contentDim)
-
-	// get pixels
+func (win *playScr) render() {
 	pixels := win.scr.crit.cropPixels
 	phosphor := win.scr.crit.cropPhosphor
 
@@ -194,40 +163,45 @@ func (win *winPlayScr) render() {
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(phosphor.Pix))
 	}
+
+	// unlike dbgscr, there is no need to call setScaling() every render()
 }
 
 // must be called from with a critical section.
-func (win *winPlayScr) setScaleAndPadding(sz imgui.Vec2) {
-	winAspectRatio := sz.X / sz.Y
+func (win *playScr) setScaling() {
+	sz := win.img.plt.displaySize()
+	dim := imgui.Vec2{sz[0], sz[1]}
 
-	imageW := float32(win.scr.crit.cropPixels.Bounds().Size().X)
-	imageH := float32(win.scr.crit.cropPixels.Bounds().Size().Y)
-	imageW *= pixelWidth * win.scr.aspectBias
-	aspectRatio := imageW / imageH
+	winAspectRatio := dim.X / dim.Y
+
+	w := float32(win.scr.crit.cropPixels.Bounds().Size().X)
+	h := float32(win.scr.crit.cropPixels.Bounds().Size().Y)
+	w *= pixelWidth * win.scr.aspectBias
+	aspectRatio := w / h
 
 	if aspectRatio < winAspectRatio {
-		win.scaling = sz.Y / imageH
-		win.imagePadding = imgui.Vec2{X: float32(int((sz.X - (imageW * win.scaling)) / 2))}
+		win.scaling = dim.Y / h
+		win.imagePosMin = imgui.Vec2{X: float32(int((dim.X - (w * win.scaling)) / 2))}
 	} else {
-		win.scaling = sz.X / imageW
-		win.imagePadding = imgui.Vec2{Y: float32(int((sz.Y - (imageH * win.scaling)) / 2))}
+		win.scaling = dim.X / w
+		win.imagePosMin = imgui.Vec2{Y: float32(int((dim.Y - (h * win.scaling)) / 2))}
 	}
+
+	win.imagePosMax = dim.Minus(win.imagePosMin)
 }
 
 // must be called from with a critical section.
-func (win *winPlayScr) getScaledWidth() float32 {
+func (win *playScr) scaledWidth() float32 {
 	// we always use cropped pixels on the playscreen
-	return float32(win.scr.crit.cropPixels.Bounds().Size().X) * win.getScaling(true)
+	return float32(win.scr.crit.cropPixels.Bounds().Size().X) * win.horizScaling()
 }
 
 // must be called from with a critical section.
-func (win *winPlayScr) getScaledHeight() float32 {
-	return float32(win.scr.crit.cropPixels.Bounds().Size().Y) * win.getScaling(false)
+func (win *playScr) scaledHeight() float32 {
+	return float32(win.scr.crit.cropPixels.Bounds().Size().Y) * win.scaling
 }
 
-func (win *winPlayScr) getScaling(horiz bool) float32 {
-	if horiz {
-		return pixelWidth * win.scr.aspectBias * win.scaling
-	}
-	return win.scaling
+// for vertical scaling simply refer to the scaling field
+func (win *playScr) horizScaling() float32 {
+	return pixelWidth * win.scr.aspectBias * win.scaling
 }

gui/sdlimgui/polling.go
@@ -103,49 +103,46 @@ func (pol *polling) wait() sdl.Event {
 	default:
 	}
 
+	if pol.img.isPlaymode() {
+		// wait for new SDL event or until the selected timeout period has elapsed
+		return sdl.WaitEventTimeout(playSleepPeriod)
+	}
+
 	// decide on timeout period
 	var timeout int
 
-	if pol.img.isPlaymode() {
-		timeout = playSleepPeriod
+	if pol.alerted {
+		pol.alerted = false
 	} else {
-		if pol.alerted {
-			pol.alerted = false
-		} else {
-			working := pol.awake ||
-				pol.img.lz.Debugger.HasChanged || pol.img.state != gui.StatePaused ||
-				pol.img.wm.dbgScr.crt || pol.img.wm.crtPrefs.open
+		working := pol.awake ||
+			pol.img.lz.Debugger.HasChanged || pol.img.state != gui.StatePaused ||
+			pol.img.wm.dbgScr.crt || pol.img.wm.crtPrefs.open
 
-			if working {
-				timeout = debugSleepPeriod
-			} else {
-				timeout = idleSleepPeriod
-			}
+		if working {
+			timeout = debugSleepPeriod
+		} else {
+			timeout = idleSleepPeriod
 		}
 	}
 
-	// wait for new SDL event or until the selected timeout period has elapsed
 	ev := sdl.WaitEventTimeout(timeout)
 
-	// nothing to do in playmode
-	if !pol.img.isPlaymode() {
-		if ev != nil {
-			// an event has been received so set awake flag and note time of event
-			pol.awake = true
-			pol.lastEvent = time.Now()
-		} else if pol.awake {
-			// keep awake flag set for wakefullnessPeriod milliseconds
-			pol.awake = time.Since(pol.lastEvent).Milliseconds() < wakefullnessPeriod
-		}
+	if ev != nil {
+		// an event has been received so set awake flag and note time of event
+		pol.awake = true
+		pol.lastEvent = time.Now()
+	} else if pol.awake {
+		// keep awake flag set for wakefullnessPeriod milliseconds
+		pol.awake = time.Since(pol.lastEvent).Milliseconds() < wakefullnessPeriod
+	}
 
-		// slow down mouse events unless input has been "captured". if we don't do
-		// this then waggling the mouse over the screen will increase CPU usage
-		// significantly. CPU usage will still increase but by a smaller margin.
-		if !pol.img.isCaptured() {
-			switch ev.(type) {
-			case *sdl.MouseMotionEvent:
-				time.Sleep(frictionPeriod * time.Millisecond)
-			}
+	// slow down mouse events unless input has been "captured". if we don't do
+	// this then waggling the mouse over the screen will increase CPU usage
+	// significantly. CPU usage will still increase but by a smaller margin.
+	if !pol.img.isCaptured() {
+		switch ev.(type) {
+		case *sdl.MouseMotionEvent:
+			time.Sleep(frictionPeriod * time.Millisecond)
 		}
 	}
 

gui/sdlimgui/screen.go
@@ -125,6 +125,7 @@ func newScreen(img *SdlImgui) *screen {
 	}
 
 	scr.crit.overlay = reflection.WSYNC
+	scr.crit.vsync = true
 	scr.Reset()
 
 	return scr
@@ -457,17 +458,18 @@ func (scr *screen) copyPixelsDebugmode() {
 
 // copy pixels from buffer to the pixels and update phosphor pixels.
 func (scr *screen) copyPixelsPlaymode() {
-	// let the emulator thread know it's okay to continue as soon as possible
-	select {
-	case <-scr.emuWait:
-		scr.emuWaitAck <- true
-	default:
-	}
-
 	scr.crit.section.Lock()
 	defer scr.crit.section.Unlock()
 
+	// attempt to sync frame generation with vertical sync
 	if scr.crit.vsync {
+		// let the emulator thread know it's okay to continue as soon as possible
+		select {
+		case <-scr.emuWait:
+			scr.emuWaitAck <- true
+		default:
+		}
+
 		// advance render index. keep note of existing index in case we
 		// bump into the plotting index.
 		v := scr.crit.renderIdx
@@ -489,14 +491,20 @@ func (scr *screen) copyPixelsPlaymode() {
 	// update phosphor carefully
 	for i := 0; i < len(scr.crit.bufferPixels[scr.crit.renderIdx].Pix); i += 4 {
 		if scr.crit.pixels.Pix[i] == 0 && scr.crit.pixels.Pix[i+1] == 0 && scr.crit.pixels.Pix[i+2] == 0 {
-			// alpha channel records the number of frames the phosphor has
-			// been active. starting at 255 and counting down to 0
+			// current color of the pixel is video-black so previous phosphor
+			// persists and fades over time.
+			//
+			// alpha channel records the number of frames the phosphor has been
+			// active. starting at 255 and counting down to 0
+			//
+			// fragment shader handles the rate at which the fade actually
+			// occurs. we're just recording the number of frames of video-black
 			if scr.crit.phosphor.Pix[i+3] > 0 {
 				scr.crit.phosphor.Pix[i+3]--
 			}
 		} else {
 			// copy current render pixels into phosphor
-			copy(scr.crit.phosphor.Pix[i:i+2], scr.crit.bufferPixels[scr.crit.renderIdx].Pix[i:i+2])
+			copy(scr.crit.phosphor.Pix[i:i+2], scr.crit.pixels.Pix[i:i+2])
 			scr.crit.phosphor.Pix[i+3] = 0xff
 		}
 	}

gui/sdlimgui/sdlimgui.go
@@ -48,8 +48,13 @@ type SdlImgui struct {
 	plt     *platform
 	glsl    *glsl
 
-	// references to the emulation
+	// lazy value system allows safe access to the debugger/emulation from the
+	// GUI thread
 	lz *lazyvalues.LazyValues
+
+	// the gui renders differently depending on EmulationState. use setState()
+	// to set the value
+	state gui.EmulationState
 
 	// vcs is set by ReqSetPlaymode or ReqSetDebugmode. in debug mode the VCS
 	// is accessible via lz.Dbg.VCS but for maximum compatibility between
@@ -57,19 +62,20 @@ type SdlImgui struct {
 	// where possible
 	vcs *hardware.VCS
 
-	// television
+	// television sends the GUI information throught the PixelRenderer and
+	// AudioMixer.
 	tv *television.Television
 
-	// the gui renders differently depending on EmulationState. use setState()
-	// to set the value
-	state gui.EmulationState
-
-	// terminal interface to the debugger
+	// terminal interface to the debugger. this is distinct from the
+	// winTerminal type.
 	term *term
 
 	// implementations of television protocols, PixelRenderer and AudioMixer
 	screen *screen
 	audio  *sdlaudio.Audio
+
+	// the playscreen is drawn to the background of the platform window
+	playScr *playScr
 
 	// imgui window management
 	wm *manager
@@ -138,6 +144,8 @@ func NewSdlImgui(tv *television.Television) (*SdlImgui, error) {
 	img.screen = newScreen(img)
 	img.term = newTerm()
 
+	img.playScr = newPlayScr(img)
+
 	img.wm, err = newManager(img)
 	if err != nil {
 		return nil, curated.Errorf("sdlimgui: %v", err)
@@ -202,6 +210,7 @@ func (img *SdlImgui) GetReflectionRenderer() reflection.Renderer {
 
 // draw gui. called from service loop.
 func (img *SdlImgui) draw() {
+	img.playScr.draw()
 	img.wm.draw()
 	img.drawPlusROMFirstInstallation()
 }
@@ -257,9 +266,8 @@ func (img *SdlImgui) setDbgAndVCS(dbg *debugger.Debugger, vcs *hardware.VCS) err
 			return curated.Errorf("sdlimgui: %v", err)
 		}
 
-		img.wm.playScr.setOpen(true)
 		img.screen.clearTextureRenderers()
-		img.screen.addTextureRenderer(img.wm.playScr)
+		img.screen.addTextureRenderer(img.playScr)
 
 		return nil
 	}
@@ -281,7 +289,6 @@ func (img *SdlImgui) setDbgAndVCS(dbg *debugger.Debugger, vcs *hardware.VCS) err
 		return curated.Errorf("sdlimgui: %v", err)
 	}
 
-	img.wm.playScr.setOpen(false)
 	img.screen.clearTextureRenderers()
 	img.screen.addTextureRenderer(img.wm.dbgScr)
 	img.screen.addTextureRenderer(img.wm.crtPrefs)
@@ -292,7 +299,7 @@ func (img *SdlImgui) setDbgAndVCS(dbg *debugger.Debugger, vcs *hardware.VCS) err
 // has mouse been grabbed. only called from gui thread.
 func (img *SdlImgui) isCaptured() bool {
 	if img.isPlaymode() {
-		return img.wm.playScr.isCaptured
+		return img.playScr.isCaptured
 	}
 	return img.wm.dbgScr.isCaptured
 }
@@ -300,7 +307,7 @@ func (img *SdlImgui) isCaptured() bool {
 // grab mouse. only called from gui thread.
 func (img *SdlImgui) setCapture(set bool) {
 	if img.isPlaymode() {
-		img.wm.playScr.isCaptured = set
+		img.playScr.isCaptured = set
 	} else {
 		img.wm.dbgScr.isCaptured = set
 	}

commit 12c03d5ddd326be148ae035dafc1b2ae7f3367ad

random seed set in all instances and not just when CRT effects are switched on.
bug introduced in 987b40349b3943a3b80a59d7ff2e54a7e6f28843

gui/sdlimgui/glsl.go
@@ -335,8 +335,12 @@ func (rnd *glsl) setOptions(textureID uint32) {
 		gl.Uniform1f(rnd.attribScanlinesBrightness, float32(rnd.img.crtPrefs.ScanlinesBrightness.Get().(float64)))
 		gl.Uniform1f(rnd.attribNoiseLevel, float32(rnd.img.crtPrefs.NoiseLevel.Get().(float64)))
 		gl.Uniform1f(rnd.attribBlurLevel, float32(rnd.img.crtPrefs.BlurLevel.Get().(float64)))
-		gl.Uniform1f(rnd.attribRandSeed, float32(time.Now().Nanosecond())/100000000.0)
 	}
+
+	// random seed always set. it's used for CRT noise but it's used sometimes
+	// (eg. CRT prefs) when crt is false, so it's easier to just set it every
+	// frame.
+	gl.Uniform1f(rnd.attribRandSeed, float32(time.Now().Nanosecond())/100000000.0)
 
 	// critical section
 	rnd.img.screen.crit.section.Lock()