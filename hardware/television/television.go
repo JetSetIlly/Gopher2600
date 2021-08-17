JetSetIlly/Gopher2600

commit 0f169f41bc3b8ac31830fd01a4e7990f6be55493

TV fps now adjusted to match incoming signal
TV frame will never resize if frame is unsynced

Play-screen FPS counter shows target frame frequency

sdlimgui playscreen: screen will not roll on first couple of non-synced
frames. the screen is tolerant of the odd rogue frame.

gui vsync options renamed to monitor sync to clarify distinction wth
emulated television VSYNC

hardware/television/television.go
@@ -74,14 +74,12 @@ type State struct {
 	// record of signal attributes from the last call to Signal()
 	lastSignal signal.SignalAttributes
 
-	// syncCount records the number of consecutive clocks the vsync signal
+	// vsyncCount records the number of consecutive clocks the VSYNC signal
 	// has been sustained. we use this to help correctly implement vsync.
-	syncCount int
+	vsyncCount int
 
-	// is current frame as a result of a VSYNC flyback or not (a "natural"
-	// flyback). we use this in the context of newFrame() so we should probably
-	// think of this as the previous frame.
-	syncedFrame bool
+	// the current frame was started as a result of a valid VSYNC signal
+	vsyncedFrame bool
 
 	// top and bottom of screen as detected by vblank/color signal
 	top    int
@@ -216,8 +214,8 @@ func (tv *Television) Reset(keepFrameNum bool) error {
 	tv.state.clock = 0
 	tv.state.scanline = 0
 	tv.state.stableFrames = 0
-	tv.state.syncedFrame = false
-	tv.state.syncCount = 0
+	tv.state.vsyncCount = 0
+	tv.state.vsyncedFrame = false
 	tv.state.lastSignal = signal.SignalAttributes{}
 
 	for i := range tv.signals {
@@ -369,11 +367,11 @@ func (tv *Television) Signal(sig signal.SignalAttributes) error {
 
 	// check vsync signal at the time of the flyback
 	if sig.VSync && !tv.state.lastSignal.VSync {
-		tv.state.syncCount = 0
 	} else if sig.VSync && tv.state.lastSignal.VSync {
-		tv.state.syncCount++
+		tv.state.vsyncCount++
 	} else if !sig.VSync && tv.state.lastSignal.VSync {
-		if tv.state.syncCount > 10 {
+		if tv.state.vsyncCount > 10 {
+			tv.state.vsyncCount = 0
 			err := tv.newFrame(true)
 			if err != nil {
 				return err
@@ -445,13 +443,20 @@ func (tv *Television) newScanline() error {
 	return nil
 }
 
-func (tv *Television) newFrame(synced bool) error {
-	// a synced frame is one which was generated from a valid VSYNC/VBLANK sequence
-	tv.state.syncedFrame = synced
+// the fromVsync arguments is true if a valid VSYNC signal has been detected. a
+// value of false means the frame flyback is unsynced.
+func (tv *Television) newFrame(fromVsync bool) error {
+	// update frame rate based on recent TV signal
+	hzChanged := tv.lmtr.update(tv.state.scanline)
+
+	// a synced frame is one which was generated from a valid VSYNC/VBLANK
+	// sequence and which hasn't cause the update frequency of the television
+	// to change.
+	tv.state.vsyncedFrame = fromVsync && !hzChanged
 
 	// increase or reset stable frame count as required
 	if tv.state.stableFrames <= stabilityThreshold {
-		if tv.state.syncedFrame {
+		if tv.state.vsyncedFrame {
 			tv.state.stableFrames++
 		} else {
 			tv.state.stableFrames = 0
@@ -492,13 +497,15 @@ func (tv *Television) newFrame(synced bool) error {
 
 	// process all FrameTriggers
 	for _, r := range tv.frameTriggers {
-		err := r.NewFrame(tv.state.syncedFrame, tv.IsStable())
+		// notifiying the FrameTrigger of whether the frame is synced or not
+		// depends on the tolerance of the television
+		err := r.NewFrame(tv.state.vsyncedFrame, tv.IsStable())
 		if err != nil {
 			return err
 		}
 	}
 
-	// check frame rate. checking even if synced == false.
+	// check frame rate
 	tv.lmtr.checkFrame()
 
 	return nil
@@ -684,7 +691,8 @@ func (tv *Television) SetFPSCap(limit bool) bool {
 }
 
 // SetFPS requests the number frames per second. This overrides the frame rate of
-// the specification. A negative  value restores the spec's frame rate.
+// the specification. A negative value restores frame rate to the ideal value
+// (the frequency of the incoming signal).
 func (tv *Television) SetFPS(fps float32) {
 	tv.lmtr.setRate(fps)
 }
@@ -697,12 +705,14 @@ func (tv *Television) GetReqFPS() float32 {
 	return tv.lmtr.requested.Load().(float32)
 }
 
-// GetActualFPS returns the current number of frames per second. Note that FPS
-// measurement still works even when frame capping is disabled.
+// GetActualFPS returns the current number of frames per second and the
+// detected frequency of the TV signal.
+//
+// Note that FPS measurement still works even when frame capping is disabled.
 //
 // IS goroutine safe.
-func (tv *Television) GetActualFPS() float32 {
-	return tv.lmtr.actual.Load().(float32)
+func (tv *Television) GetActualFPS() (float32, float32) {
+	return tv.lmtr.actual.Load().(float32), tv.lmtr.hz.Load().(float32)
 }
 
 // ReqAdjust requests the frame, scanline and clock values where the requested

gopher2600.go
@@ -346,7 +346,7 @@ func play(md *modalflag.Modes, sync *mainSync) error {
 
 		// set fps cap
 		tv.SetFPSCap(*fpsCap)
-		scr.SetFeature(gui.ReqVSync, *fpsCap)
+		scr.SetFeature(gui.ReqMonitorSync, *fpsCap)
 
 		// set full screen
 		scr.SetFeature(gui.ReqFullScreen, *fullScreen)
@@ -631,7 +631,7 @@ func perform(md *modalflag.Modes, sync *mainSync) error {
 		}
 		defer tv.End()
 
-		// fpscap for tv (see below for gui vsync option)
+		// fpscap for tv (see below for monitor sync option)
 		tv.SetFPSCap(*fpsCap)
 
 		// GUI instance if required
@@ -654,7 +654,7 @@ func perform(md *modalflag.Modes, sync *mainSync) error {
 			}
 
 			// fpscap for gui (see above for tv option)
-			scr.SetFeature(gui.ReqVSync, *fpsCap)
+			scr.SetFeature(gui.ReqMonitorSync, *fpsCap)
 		}
 
 		// check for profiling options

gui/requests.go
@@ -73,9 +73,10 @@ const (
 	// infomration, particularly the display of the PixelRenderer.
 	ReqState FeatureReq = "ReqState" // EmulationState
 
-	// whether gui should use vsync or not. not all gui modes have to obey this
-	// but for presentation/play modes it's a good idea to have it set.
-	ReqVSync FeatureReq = "ReqVSync" // bool
+	// whether gui should try to sync with the monitro refresh rate. not all
+	// gui modes have to obey this but for presentation/play modes it's a good
+	// idea to have it set.
+	ReqMonitorSync FeatureReq = "ReqMonitorSync" // bool
 
 	// whether the gui is visible or not. results in an error if requested in
 	// playmode.

gui/sdlimgui/lazyvalues/tv.go
@@ -26,23 +26,25 @@ import (
 type LazyTV struct {
 	val *LazyValues
 
-	spec       atomic.Value // television.Spec
-	tvStr      atomic.Value // string
-	lastSignal atomic.Value // television.SignalAttributes
-	frame      atomic.Value // int
-	scanline   atomic.Value // int
-	clock      atomic.Value // int
-	actualFPS  atomic.Value // float32
-	reqFPS     atomic.Value // float32
+	spec        atomic.Value // television.Spec
+	tvStr       atomic.Value // string
+	lastSignal  atomic.Value // television.SignalAttributes
+	frame       atomic.Value // int
+	scanline    atomic.Value // int
+	clock       atomic.Value // int
+	detectedFPS atomic.Value // float32
+	actualFPS   atomic.Value // float32
+	reqFPS      atomic.Value // float32
 
-	Spec       specification.Spec
-	TVstr      string
-	LastSignal signal.SignalAttributes
-	Frame      int
-	Scanline   int
-	Clock      int
-	ActualFPS  float32
-	ReqFPS     float32
+	Spec        specification.Spec
+	TVstr       string
+	LastSignal  signal.SignalAttributes
+	Frame       int
+	Scanline    int
+	Clock       int
+	DetectedFPS float32
+	ActualFPS   float32
+	ReqFPS      float32
 }
 
 func newLazyTV(val *LazyValues) *LazyTV {
@@ -63,7 +65,9 @@ func (lz *LazyTV) push() {
 	clock := lz.val.Dbg.VCS.TV.GetState(signal.ReqClock)
 	lz.clock.Store(clock)
 
-	lz.actualFPS.Store(lz.val.Dbg.VCS.TV.GetActualFPS())
+	actual, detected := lz.val.Dbg.VCS.TV.GetActualFPS()
+	lz.detectedFPS.Store(detected)
+	lz.actualFPS.Store(actual)
 
 	lz.reqFPS.Store(lz.val.Dbg.VCS.TV.GetReqFPS())
 }
@@ -75,6 +79,7 @@ func (lz *LazyTV) update() {
 	lz.Frame, _ = lz.frame.Load().(int)
 	lz.Scanline, _ = lz.scanline.Load().(int)
 	lz.Clock, _ = lz.clock.Load().(int)
+	lz.DetectedFPS, _ = lz.detectedFPS.Load().(float32)
 	lz.ActualFPS, _ = lz.actualFPS.Load().(float32)
 	lz.ReqFPS, _ = lz.reqFPS.Load().(float32)
 }

gui/sdlimgui/playscr.go
@@ -59,6 +59,7 @@ type playScr struct {
 	fpsOpen  bool
 	fpsPulse *time.Ticker
 	fps      string
+	hz       string
 
 	// controller alert
 	controllerAlertLeft  controllerAlert
@@ -124,7 +125,13 @@ func (win *playScr) draw() {
 		// update fps
 		select {
 		case <-win.fpsPulse.C:
-			win.fps = fmt.Sprintf("%03.1f fps", win.img.tv.GetActualFPS())
+			fps, hz := win.img.tv.GetActualFPS()
+			if win.scr.crit.vsynced {
+				win.fps = fmt.Sprintf("%03.2f fps", fps)
+			} else {
+				win.fps = "unsynced"
+			}
+			win.hz = fmt.Sprintf("%03.2fhz", hz)
 		default:
 		}
 
@@ -137,20 +144,25 @@ func (win *playScr) draw() {
 			imgui.WindowFlagsNoScrollbar|imgui.WindowFlagsNoTitleBar|imgui.WindowFlagsNoDecoration)
 
 		imgui.Text(win.fps)
+
+		imguiSeparator()
+
 		imgui.Text(fmt.Sprintf("%.1fx scaling", win.yscaling))
 		imgui.Text(fmt.Sprintf("%d visible scanlines", win.scr.crit.bottomScanline-win.scr.crit.topScanline))
+
+		if win.img.screen.crit.topScanline < win.img.screen.crit.spec.AtariSafeTop {
+			imgui.Text("extended screen")
+		} else {
+			imgui.Text("atari safe")
+		}
+
+		imguiSeparator()
 
 		win.img.screen.crit.section.Lock()
 		imgui.Text(win.img.screen.crit.spec.ID)
 		imgui.SameLine()
-		if win.img.screen.crit.topScanline < win.img.screen.crit.spec.AtariSafeTop {
-			imgui.Text("(extended)")
-		} else {
-			imgui.Text("(atari safe)")
-		}
-		if !win.img.screen.crit.synced {
-			imgui.Text("unsynced")
-		}
+		imgui.Text(win.hz)
+
 		win.img.screen.crit.section.Unlock()
 
 		imgui.PopStyleColorV(2)
@@ -241,7 +253,7 @@ func (win *playScr) render() {
 			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(pixels.Pix))
-	} else if win.scr.crit.isStable {
+	} else if win.scr.crit.stable {
 		gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),

gui/sdlimgui/requests.go
@@ -104,11 +104,11 @@ func (img *SdlImgui) serviceSetFeature(request featureRequest) {
 			img.setEmulationState(request.args[0].(gui.EmulationState))
 		}
 
-	case gui.ReqVSync:
+	case gui.ReqMonitorSync:
 		err = argLen(request.args, 1)
 		if err == nil {
 			img.screen.crit.section.Lock()
-			img.screen.crit.vsync = request.args[0].(bool)
+			img.screen.crit.monitorSync = request.args[0].(bool)
 			img.screen.crit.section.Unlock()
 		}
 

gui/sdlimgui/screen.go
@@ -61,27 +61,36 @@ type screenCrit struct {
 	// critical sectioning
 	section sync.Mutex
 
-	// whether to follow vsync rules or not
-	vsync bool
+	// whether or not to sync with monitor refresh rate
+	monitorSync bool
 
 	// copy of the spec being used by the TV. the TV notifies us through the
 	// Resize() function
 	spec specification.Spec
 
 	// whether the current frame was generated from a stable television state
-	isStable bool
+	stable bool
 
 	// whether the current frame is vsynced
-	synced bool
+	vsynced bool
+
+	// the number of consecutive unsynced frames received by NewFrame().
+	// * playmode only
+	unsyncedCt int
 
 	// when an unsynced frame is encountered the screen will roll. we only
 	// allow this in playmode. there's no value in seeing the screen-roll in
 	// debug mode.
 	//
 	// unsyncedScanline keeps track of the accumulated scanline position
+	//
+	// * playmode only
 	unsyncedScanline int
+
 	// unsyncedRecoveryCt is used to help the screen regain the correct
-	// position once a synced frame is received.
+	// position once a vsynced frame is received.
+	//
+	// * playmode only
 	unsyncedRecoveryCt int
 
 	// current values for *playable* area of the screen
@@ -136,7 +145,7 @@ func newScreen(img *SdlImgui) *screen {
 	}
 
 	scr.crit.overlay = overlayLabels[overlayNone]
-	scr.crit.vsync = true
+	scr.crit.monitorSync = true
 
 	// default to NTSC. this will change on the first instance of
 	scr.resize(specification.SpecNTSC, specification.SpecNTSC.AtariSafeTop, specification.SpecNTSC.AtariSafeBottom)
@@ -163,8 +172,8 @@ func (scr *screen) Reset() {
 	scr.crit.lastX = 0
 	scr.crit.lastY = 0
 
-	// screen is synced by definition on reset
-	scr.crit.synced = true
+	// screen is vsynced by definition on reset
+	scr.crit.vsynced = true
 }
 
 // clear all pixel information including reflection data.
@@ -266,37 +275,47 @@ func (scr *screen) Resize(spec specification.Spec, topScanline int, bottomScanli
 	return <-scr.img.polling.serviceErr
 }
 
+// the number of consecutive unsynced frames before the screen starts to roll.
+// there's no reason for this figure but the value of 2 was settled on through
+// observation. notably, a tolerance of 2 prevents the Chiphead demo from
+// rolling on the occasional unsynced frame.
+const unsyncedTolerance = 2
+
 // NewFrame implements the television.PixelRenderer interface
 //
 // MUST NOT be called from the gui thread.
-func (scr *screen) NewFrame(synced bool, isStable bool) error {
+func (scr *screen) NewFrame(vsynced bool, stable bool) error {
 	// unlocking must be done carefully
 	scr.crit.section.Lock()
 
-	scr.crit.isStable = isStable
-	scr.crit.synced = synced
+	scr.crit.stable = stable
+	scr.crit.vsynced = vsynced
 
 	if scr.img.isPlaymode() {
 		// screen rolling
-		if synced {
+		if vsynced {
+			scr.crit.unsyncedCt = 0
+
 			// recovery required
 			if scr.crit.unsyncedScanline > 0 {
-				// slow recovery down to every other frame. this helps to
-				// create the rolling effect when alternate frames are
-				// sync/unscynced
 				scr.crit.unsyncedRecoveryCt++
-				if scr.crit.unsyncedRecoveryCt%2 == 0 {
-					scr.crit.unsyncedScanline *= 8
-					scr.crit.unsyncedScanline /= 10
+				scr.crit.unsyncedScanline *= 8
+				scr.crit.unsyncedScanline /= 10
+
+				if scr.crit.unsyncedScanline == 0 {
+					scr.crit.unsyncedRecoveryCt = 0
 				}
 			}
 		} else {
-			// without the isStable check, the screen can descync and recover
+			// without the stable check, the screen can desync and recover
 			// from a roll on startup on most ROMs, which looks quite cool but
 			// we'll leave it disabled for now.
-			if isStable {
-				scr.crit.unsyncedScanline = (scr.crit.unsyncedScanline + scr.crit.lastY) % scr.crit.spec.ScanlinesTotal
-				scr.crit.unsyncedRecoveryCt = 0
+			if stable {
+				scr.crit.unsyncedCt++
+				if scr.crit.unsyncedCt > unsyncedTolerance {
+					scr.crit.unsyncedScanline = (scr.crit.unsyncedScanline + scr.crit.lastY) % specification.AbsoluteMaxScanlines
+					scr.crit.unsyncedRecoveryCt = 0
+				}
 			}
 		}
 
@@ -306,7 +325,7 @@ func (scr *screen) NewFrame(synced bool, isStable bool) error {
 		}
 
 		// if plot index has crashed into the render index then
-		if scr.crit.plotIdx == scr.crit.renderIdx && scr.crit.vsync {
+		if scr.crit.plotIdx == scr.crit.renderIdx && scr.crit.monitorSync {
 			// ** screen update not keeping up with emulation **
 
 			// we must unlock the critical section or the gui thread will not
@@ -549,8 +568,8 @@ func (scr *screen) copyPixelsPlaymode() {
 	scr.crit.section.Lock()
 	defer scr.crit.section.Unlock()
 
-	// attempt to sync frame generation with vertical sync
-	if scr.crit.vsync {
+	// attempt to sync frame generation with monitor refresh rate
+	if scr.crit.monitorSync {
 		// advance render index
 		scr.crit.renderIdx++
 		if scr.crit.renderIdx >= len(scr.crit.bufferPixels) {

gui/sdlimgui/win_dbgscr.go
@@ -358,7 +358,7 @@ func (win *winDbgScr) drawCoordsLine() {
 	imgui.Text(win.img.lz.TV.LastSignal.String())
 
 	// unsynced
-	if !win.scr.crit.synced {
+	if !win.scr.crit.vsynced {
 		imgui.SameLineV(0, 20)
 		imgui.Text("UNSYNCED")
 	}

commit 3761dc0bab80d1517f1c0a3a5eb78250ba012f95

VSYNC count reset correctly
fixes smooth playfield scrolling demos. bug introduced in
0f169f41bc3b8ac31830fd01a4e7990f6be55493. demo ROMs not in regression
database

hardware/television/television.go
@@ -366,11 +366,11 @@ func (tv *Television) Signal(sig signal.SignalAttributes) error {
 
 	// check vsync signal at the time of the flyback
 	if sig.VSync && !tv.state.lastSignal.VSync {
+		tv.state.vsyncCount = 0
 	} else if sig.VSync && tv.state.lastSignal.VSync {
 		tv.state.vsyncCount++
 	} else if !sig.VSync && tv.state.lastSignal.VSync {
 		if tv.state.vsyncCount > 10 {
-			tv.state.vsyncCount = 0
 			err := tv.newFrame(true)
 			if err != nil {
 				return err