00fbbeb12b90e61573398e7697db3b6bddc0294e
JetSetIlly <stephen.t.illingworth@gmail.com>
2021-08-19 12:26:50 +0100

    wavwriter now uses youpy/go-wav encoder

--- wavwriter/wav.go
+++ wavwriter/wav.go
@@ -13,30 +13,30 @@
 // You should have received a copy of the GNU General Public License
 // along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
 
-// Package wavwriter allows writing of audio data to disk as a WAV file.
+// Package wavwriter allows writing of audio data to disk as a WAV file. Note
+// that audio data is buffered in memory in its entirity, and written to disk
+// on program end. It is therefore probably only suitable for testing purposes.
 package wavwriter
 
 import (
 	"os"
 
 	"github.com/jetsetilly/gopher2600/curated"
-	tiaAudio "github.com/jetsetilly/gopher2600/hardware/tia/audio"
-
-	"github.com/go-audio/audio"
-	"github.com/go-audio/wav"
+	"github.com/jetsetilly/gopher2600/hardware/tia/audio"
+	"github.com/youpy/go-wav"
 )
 
 // WavWriter implements the television.AudioMixer interface.
 type WavWriter struct {
 	filename string
-	buffer   []int8
+	buffer   []wav.Sample
 }
 
 // New is the preferred method of initialisation for the Audio2Wav type.
 func New(filename string) (*WavWriter, error) {
 	aw := &WavWriter{
 		filename: filename,
-		buffer:   make([]int8, 0),
+		buffer:   make([]wav.Sample, 0),
 	}
 
 	return aw, nil
@@ -45,7 +45,10 @@ func New(filename string) (*WavWriter, error) {
 // SetAudio implements the television.AudioMixer interface.
 func (aw *WavWriter) SetAudio(audioData uint8) error {
 	// bring audioData into the correct range
-	aw.buffer = append(aw.buffer, int8(int16(audioData)-127))
+	s := wav.Sample{}
+	s.Values[0] = int(audioData)
+	s.Values[1] = s.Values[0]
+	aw.buffer = append(aw.buffer, s)
 	return nil
 }
 
@@ -64,31 +67,12 @@ func (aw *WavWriter) EndMixing() (rerr error) {
 
 	// see audio commentary in sdlplay package for thinking around sample rates
 
-	enc := wav.NewEncoder(f, tiaAudio.SampleFreq, 8, 1, 1)
+	enc := wav.NewWriter(f, uint32(len(aw.buffer)), 1, uint32(audio.SampleFreq), 8)
 	if enc == nil {
 		return curated.Errorf("wavwriter: %v", "bad parameters for wav encoding")
 	}
-	defer func() {
-		err := enc.Close()
-		if err != nil {
-			rerr = curated.Errorf("wavwriter: %v", err)
-		}
-	}()
 
-	buf := audio.PCMBuffer{
-		Format: &audio.Format{
-			NumChannels: 1,
-			SampleRate:  tiaAudio.SampleFreq,
-		},
-		I8:             aw.buffer,
-		DataType:       audio.DataTypeI8,
-		SourceBitDepth: 8,
-	}
-
-	err = enc.Write(buf.AsIntBuffer())
-	if err != nil {
-		return curated.Errorf("wavwriter: %v", err)
-	}
+	enc.WriteSamples(aw.buffer)
 
 	return nil
 }
