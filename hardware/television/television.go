JetSetIlly/Gopher2600

commit 9a8019d654248b85272df605cd6fe63558d7a2a8

SetAudio() in AudioMixer interface now expects a slice of SignalAttributes
This allows a single call to SetAudio() and for the loop to happen
inside that function - as opposed to looping over the slice and calling
SetAudio() multiple times

hardware/television/television.go
@@ -559,24 +559,16 @@ func (tv *Television) processSignals(current bool) error {
 
 		// mix audio
 		for _, m := range tv.mixers {
-			for i := 0; i < tv.currentIdx; i++ {
-				sig := tv.signals[i]
-				if sig&signal.AudioUpdate == signal.AudioUpdate {
-					err := m.SetAudio(uint8((sig & signal.AudioData) >> signal.AudioDataShift))
-					if err != nil {
-						return err
-					}
-				}
+			// err := m.SetAudio(uint8((sig & signal.AudioData) >> signal.AudioDataShift))
+			err := m.SetAudio(tv.signals[:tv.currentIdx])
+			if err != nil {
+				return curated.Errorf("television: %v", err)
 			}
+
 			if !current {
-				for i := tv.currentIdx + 1; i < tv.lastMaxIdx; i++ {
-					sig := tv.signals[i]
-					if sig&signal.AudioUpdate == signal.AudioUpdate {
-						err := m.SetAudio(uint8((sig & signal.AudioData) >> signal.AudioDataShift))
-						if err != nil {
-							return err
-						}
-					}
+				err = m.SetAudio(tv.signals[tv.currentIdx:tv.lastMaxIdx])
+				if err != nil {
+					return curated.Errorf("television: %v", err)
 				}
 			}
 		}

digest/audio.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/jetsetilly/gopher2600/curated"
 	"github.com/jetsetilly/gopher2600/hardware/television"
+	"github.com/jetsetilly/gopher2600/hardware/television/signal"
 )
 
 // the length of the buffer we're using isn't really important. that said, it
@@ -73,13 +74,19 @@ func (dig *Audio) ResetDigest() {
 }
 
 // SetAudio implements the protocol.AudioMixer interface.
-func (dig *Audio) SetAudio(audioData uint8) error {
-	dig.buffer[dig.bufferCt] = audioData
+func (dig *Audio) SetAudio(sig []signal.SignalAttributes) error {
+	for _, s := range sig {
+		if s&signal.AudioUpdate != signal.AudioUpdate {
+			continue
+		}
+		d := uint8((s & signal.AudioData) >> signal.AudioDataShift)
 
-	dig.bufferCt++
+		dig.buffer[dig.bufferCt] = d
 
-	if dig.bufferCt >= audioBufferLength {
-		return dig.flushAudio()
+		dig.bufferCt++
+		if dig.bufferCt >= audioBufferLength {
+			return dig.flushAudio()
+		}
 	}
 
 	return nil

gui/sdlaudio/audio.go
@@ -16,6 +16,7 @@
 package sdlaudio
 
 import (
+	"github.com/jetsetilly/gopher2600/hardware/television/signal"
 	"github.com/jetsetilly/gopher2600/hardware/tia/audio"
 	"github.com/jetsetilly/gopher2600/logger"
 
@@ -82,53 +83,60 @@ func NewAudio() (*Audio, error) {
 }
 
 // SetAudio implements the protocol.AudioMixer interface.
-func (aud *Audio) SetAudio(audioData uint8) error {
-	aud.buffer[aud.bufferCt] = audioData + aud.spec.Silence
-	aud.bufferCt++
-
-	if aud.bufferCt >= len(aud.buffer) {
-		// if buffer is full then queue audio unconditionally
-		err := sdl.QueueAudio(aud.id, aud.buffer)
-		if err != nil {
-			return err
+func (aud *Audio) SetAudio(sig []signal.SignalAttributes) error {
+	for _, s := range sig {
+		if s&signal.AudioUpdate != signal.AudioUpdate {
+			continue
 		}
-		aud.bufferCt = 0
-	} else {
-		remaining := int(sdl.GetQueuedAudioSize(aud.id))
+		d := uint8((s & signal.AudioData) >> signal.AudioDataShift)
 
-		if remaining < critQueueLength {
-			// if we're running short of bits in the queue the queue what we have
-			// in the buffer and NOT clearing the buffer
-			//
-			// condition valid when the frame rate is SIGNIFICANTLY LESS than 50/60fps
+		aud.buffer[aud.bufferCt] = d + aud.spec.Silence
+		aud.bufferCt++
+
+		if aud.bufferCt >= len(aud.buffer) {
+			// if buffer is full then queue audio unconditionally
 			err := sdl.QueueAudio(aud.id, aud.buffer)
 			if err != nil {
 				return err
 			}
-		} else if remaining < minQueueLength && aud.bufferCt > 10 {
-			// if we're running short of bits in the queue the queue what we have
-			// in the buffer.
-			//
-			// condition valid when the frame rate is LESS than 50/60fps
-			//
-			// the additional condition makes sure we're not queueing a slice
-			// that is too short. SDL has been known to hang with short audio
-			// queues
-			err := sdl.QueueAudio(aud.id, aud.buffer[:aud.bufferCt-1])
-			if err != nil {
-				return err
-			}
 			aud.bufferCt = 0
-		} else if remaining > maxQueueLength {
-			// if length of sdl: audio: queue is getting too long then clear it
-			//
-			// condition valid when the frame rate is SIGNIFICANTLY MORE than 50/60fps
-			//
-			// if we don't do this the video will get ahead of the audio (ie. the audio
-			// will lag)
-			//
-			// this is a brute force approach but it'll do for now
-			sdl.ClearQueuedAudio(aud.id)
+		} else {
+			remaining := int(sdl.GetQueuedAudioSize(aud.id))
+
+			if remaining < critQueueLength {
+				// if we're running short of bits in the queue the queue what we have
+				// in the buffer and NOT clearing the buffer
+				//
+				// condition valid when the frame rate is SIGNIFICANTLY LESS than 50/60fps
+				err := sdl.QueueAudio(aud.id, aud.buffer)
+				if err != nil {
+					return err
+				}
+			} else if remaining < minQueueLength && aud.bufferCt > 10 {
+				// if we're running short of bits in the queue the queue what we have
+				// in the buffer.
+				//
+				// condition valid when the frame rate is LESS than 50/60fps
+				//
+				// the additional condition makes sure we're not queueing a slice
+				// that is too short. SDL has been known to hang with short audio
+				// queues
+				err := sdl.QueueAudio(aud.id, aud.buffer[:aud.bufferCt-1])
+				if err != nil {
+					return err
+				}
+				aud.bufferCt = 0
+			} else if remaining > maxQueueLength {
+				// if length of sdl: audio: queue is getting too long then clear it
+				//
+				// condition valid when the frame rate is SIGNIFICANTLY MORE than 50/60fps
+				//
+				// if we don't do this the video will get ahead of the audio (ie. the audio
+				// will lag)
+				//
+				// this is a brute force approach but it'll do for now
+				sdl.ClearQueuedAudio(aud.id)
+			}
 		}
 	}
 

gui/sdlimgui/win_audio.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/inkyblackness/imgui-go/v4"
 	"github.com/jetsetilly/gopher2600/gui/fonts"
+	"github.com/jetsetilly/gopher2600/hardware/television/signal"
 )
 
 const winAudioID = "Audio"
@@ -119,10 +120,17 @@ func (win *winAudio) draw() {
 }
 
 // SetAudio implements protocol.AudioMixer.
-func (win *winAudio) SetAudio(audioData uint8) error {
-	select {
-	case win.newData <- float32(audioData) / 256:
-	default:
+func (win *winAudio) SetAudio(sig []signal.SignalAttributes) error {
+	for _, s := range sig {
+		if s&signal.AudioUpdate != signal.AudioUpdate {
+			continue
+		}
+		d := uint8((s & signal.AudioData) >> signal.AudioDataShift)
+
+		select {
+		case win.newData <- float32(d) / 256:
+		default:
+		}
 	}
 	return nil
 }

hardware/television/protocol.go
@@ -122,7 +122,22 @@ type FrameTrigger interface {
 // example of an AudioMixer that does not play sound but otherwise works with
 // it is the digest.Audio type.
 type AudioMixer interface {
-	SetAudio(audioData uint8) error
+	// for efficiency reasons, SetAudio() implementations can be sent
+	// SignalAttributes values that do not have valid AudioData (ie.
+	// AudioUpdate bit is zero). implemenations should therefore take care when
+	// processing the sig slice.
+	//
+	// the general algorithm for processing the sig slice is:
+	//
+	//	for _, s := range sig {
+	//		if s&signal.AudioUpdate != signal.AudioUpdate {
+	//			continue
+	//		}
+	//		d := uint8((s & signal.AudioData) >> signal.AudioDataShift)
+	//
+	//		...
+	//	}
+	SetAudio(sig []signal.SignalAttributes) error
 
 	// some mixers may need to conclude and/or dispose of resources gently.
 	// for simplicity, the AudioMixer should be considered unusable after

wavwriter/wav.go
@@ -22,7 +22,9 @@ import (
 	"os"
 
 	"github.com/jetsetilly/gopher2600/curated"
+	"github.com/jetsetilly/gopher2600/hardware/television/signal"
 	"github.com/jetsetilly/gopher2600/hardware/tia/audio"
+	"github.com/jetsetilly/gopher2600/logger"
 	"github.com/youpy/go-wav"
 )
 
@@ -43,12 +45,20 @@ func New(filename string) (*WavWriter, error) {
 }
 
 // SetAudio implements the television.AudioMixer interface.
-func (aw *WavWriter) SetAudio(audioData uint8) error {
-	// bring audioData into the correct range
-	s := wav.Sample{}
-	s.Values[0] = int(audioData)
-	s.Values[1] = s.Values[0]
-	aw.buffer = append(aw.buffer, s)
+func (aw *WavWriter) SetAudio(sig []signal.SignalAttributes) error {
+	for _, s := range sig {
+		if s&signal.AudioUpdate != signal.AudioUpdate {
+			continue
+		}
+		d := uint8((s & signal.AudioData) >> signal.AudioDataShift)
+
+		// bring audioData into the correct range
+		w := wav.Sample{}
+		w.Values[0] = int(d)
+		w.Values[1] = w.Values[0]
+		aw.buffer = append(aw.buffer, w)
+	}
+
 	return nil
 }
 
@@ -72,6 +82,7 @@ func (aw *WavWriter) EndMixing() (rerr error) {
 		return curated.Errorf("wavwriter: %v", "bad parameters for wav encoding")
 	}
 
+	logger.Logf("wavwriter", "writing audio to %s", aw.filename)
 	enc.WriteSamples(aw.buffer)
 
 	return nil

commit fe9a3b33f9201e4e2ea2b43641584ca3ca689b7f

fixed slicing bug introduced in 9a8019d654248b85272df605cd6fe63558d7a2a8
this may also be connected to a rewind bug that causes the screen redraw
to be incorrect. this fix is a boundary check similar to another
boundary check that would disguise the screen redraw bug

hardware/television/television.go
@@ -566,9 +566,11 @@ func (tv *Television) processSignals(current bool) error {
 			}
 
 			if !current {
-				err = m.SetAudio(tv.signals[tv.currentIdx:tv.lastMaxIdx])
-				if err != nil {
-					return curated.Errorf("television: %v", err)
+				if tv.currentIdx < tv.lastMaxIdx {
+					err = m.SetAudio(tv.signals[tv.currentIdx:tv.lastMaxIdx])
+					if err != nil {
+						return curated.Errorf("television: %v", err)
+					}
 				}
 			}
 		}