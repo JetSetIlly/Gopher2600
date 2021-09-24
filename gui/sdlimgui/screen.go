JetSetIlly/Gopher2600

commit 240f0680a2476dbd4a422bb29ac0ab1d214004d5

television no longer reports frame descyn on change of scanline number
frame sync is now purely based on whether a correct VSYNC signal is
detected (as it should be)

hardware/television/television.go
@@ -443,6 +443,7 @@ func (tv *Television) newScanline() error {
 // the fromVsync arguments is true if a valid VSYNC signal has been detected. a
 // value of false means the frame flyback is unsynced.
 func (tv *Television) newFrame(fromVsync bool) error {
+	// note whether newFrame() was the result of a valid VSYNC or a "natural" flyback
 	tv.state.frameInfo.VSynced = fromVsync
 
 	// update refresh rate
@@ -452,8 +453,10 @@ func (tv *Television) newFrame(fromVsync bool) error {
 
 		tv.lmtr.setRefreshRate(tv, tv.state.frameInfo.RefreshRate)
 
-		// if the refresh rate has changed then by definition the frame is not VSynced
-		tv.state.frameInfo.VSynced = false
+		// previous versions of this code forced frameInfo.VSynced to false if
+		// we reached this point. this is wrong however. even if the number of
+		// scanlines fluctates wildly, the screen is still vsynced if a valid
+		// VSYNC signal has been detected.
 	}
 
 	// increase or reset stable frame count as required

commit 486d587a996caff0c865e6f60ffdaaf034ba98de

fixed screen rolling broken in 240f0680a2476dbd4a422bb29ac0ab1d214004d5
we now detect a refresh rate change in the PixelRenderer implementation
rather than the television

gui/sdlimgui/screen.go
@@ -69,24 +69,15 @@ type screenCrit struct {
 	// whether or not to sync with monitor refresh rate
 	monitorSync bool
 
-	// the number of consecutive unsynced frames received by NewFrame().
+	// the number of consecutive frames where a screenroll might have happened.
+	// once it reaches a threshold then screenroll begins
 	// * playmode only
-	unsyncedCt int
+	screenrollCt int
 
-	// when an unsynced frame is encountered the screen will roll. we only
-	// allow this in playmode. there's no value in seeing the screen-roll in
-	// debug mode.
-	//
-	// unsyncedScanline keeps track of the accumulated scanline position
-	//
+	// the scanline currenty used to emulate a screenroll effect. if the value
+	// is zero then no screenroll is currently taking place
 	// * playmode only
-	unsyncedScanline int
-
-	// unsyncedRecoveryCt is used to help the screen regain the correct
-	// position once a vsynced frame is received.
-	//
-	// * playmode only
-	unsyncedRecoveryCt int
+	screenrollScanline int
 
 	// the pixels array is used in the presentation texture of the play and debug screen.
 	pixels *image.RGBA
@@ -277,41 +268,35 @@ func (scr *screen) NewFrame(frameInfo television.FrameInfo) error {
 	// unlocking must be done carefully
 	scr.crit.section.Lock()
 
-	scr.crit.frameInfo = frameInfo
+	defer func() {
+		scr.crit.frameInfo = frameInfo
+	}()
 
 	if scr.img.isPlaymode() {
 		// check screen rolling if crtprefs are enabled
 		if scr.img.crtPrefs.Enabled.Get().(bool) {
-			if scr.crit.frameInfo.VSynced {
-				scr.crit.unsyncedCt = 0
-
+			if frameInfo.RefreshRate == scr.crit.frameInfo.RefreshRate && frameInfo.VSynced {
 				// recovery required
-				if scr.crit.unsyncedScanline > 0 {
-					scr.crit.unsyncedRecoveryCt++
-					scr.crit.unsyncedScanline *= 8
-					scr.crit.unsyncedScanline /= 10
-
-					if scr.crit.unsyncedScanline == 0 {
-						scr.crit.unsyncedRecoveryCt = 0
-					}
+				if scr.crit.screenrollScanline > 0 {
+					scr.crit.screenrollCt = 0
+					scr.crit.screenrollScanline *= 8
+					scr.crit.screenrollScanline /= 10
 				}
-			} else {
+			} else if scr.crit.frameInfo.Stable {
 				// without the stable check, the screen can desync and recover
 				// from a roll on startup on most ROMs, which looks quite cool but
 				// we'll leave it disabled for now.
-				if scr.crit.frameInfo.Stable {
-					scr.crit.unsyncedCt++
-					if scr.crit.unsyncedCt > scr.img.crtPrefs.UnsyncTolerance.Get().(int) {
-						scr.crit.unsyncedScanline = (scr.crit.unsyncedScanline + scr.crit.lastY)
-						if scr.crit.unsyncedScanline >= specification.AbsoluteMaxScanlines {
-							scr.crit.unsyncedScanline -= specification.AbsoluteMaxScanlines
-						}
-						scr.crit.unsyncedRecoveryCt = 0
+
+				scr.crit.screenrollCt++
+				if scr.crit.screenrollCt > scr.img.crtPrefs.UnsyncTolerance.Get().(int) {
+					scr.crit.screenrollScanline = (scr.crit.screenrollScanline + scr.crit.lastY)
+					if scr.crit.screenrollScanline >= specification.AbsoluteMaxScanlines {
+						scr.crit.screenrollScanline -= specification.AbsoluteMaxScanlines
 					}
 				}
 			}
 		} else {
-			scr.crit.unsyncedScanline = 0
+			scr.crit.screenrollScanline = 0
 		}
 
 		if scr.img.emulation.State() == emulation.Running {
@@ -383,7 +368,7 @@ func (scr *screen) SetPixels(sig []signal.SignalAttributes, current bool) error 
 	cl := int((sig[0] & signal.Clock) >> signal.ClockShift)
 	sl := int((sig[0] & signal.Scanline) >> signal.ScanlineShift)
 
-	adjustedScanline := (sl + scr.crit.unsyncedScanline)
+	adjustedScanline := (sl + scr.crit.screenrollScanline)
 	if adjustedScanline >= scr.crit.bufferHeight {
 		adjustedScanline -= scr.crit.bufferHeight
 	}
@@ -403,21 +388,17 @@ func (scr *screen) SetPixels(sig []signal.SignalAttributes, current bool) error 
 		// bothering with an additional condition.
 
 		// check that we're not going to encounter an index-out-of-range error
-		//
-		// if the offset is too large then check the next signal in the slice
-		// and break the loop if it is a NoSignal
-		//
-		// otherwise adjust offset and continue. an example of a ROM where it
-		// is necessary to reset the offset is Andrew Davie's 3e+ Chess demos.
 		if offset >= len(scr.crit.bufferPixels[scr.crit.plotIdx].Pix)-4 {
-			if sig[i] == signal.NoSignal {
-				break // for loop
+
+			// if we're not currently in a screen roll then just stop drawing pixels
+			if scr.crit.screenrollScanline == 0 {
+				break
 			}
 
+			// otherwise continue drawing or we would be left with a lot of
+			// undrawn pixels and a poor screen roll effect
 			cl := int((sig[i] & signal.Clock) >> signal.ClockShift)
-			sl := int((sig[0] & signal.Scanline) >> signal.ScanlineShift)
 			offset = cl * 4
-			offset += sl * scr.crit.bufferPixels[scr.crit.plotIdx].Rect.Size().X * 4
 		}
 
 		// handle VBLANK by setting pixels to black

hardware/television/television.go
@@ -443,11 +443,6 @@ func (tv *Television) newFrame(fromVsync bool) error {
 		tv.state.frameInfo.RefreshRate = 15734.26 / float32(tv.state.frameInfo.TotalScanlines)
 
 		tv.lmtr.setRefreshRate(tv, tv.state.frameInfo.RefreshRate)
-
-		// previous versions of this code forced frameInfo.VSynced to false if
-		// we reached this point. this is wrong however. even if the number of
-		// scanlines fluctates wildly, the screen is still vsynced if a valid
-		// VSYNC signal has been detected.
 	}
 
 	// increase or reset stable frame count as required