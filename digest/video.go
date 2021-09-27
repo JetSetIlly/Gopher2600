JetSetIlly/Gopher2600

commit dda5ed5d5c4ce93fe8ea4fc40e3ce763eb0b10cf

added flyback line on debug screen
other guidelines (HBLANK, visible limits) tweaked.

when drawing the screen cursor the coordinates are taken from what the
TV is returning from television.GetState() (via the lazy system).
clarified that these values are the coordinates of the *most recent*
pixel to be drawn.

gui/sdlimgui/glsl_dbgscr.go
@@ -145,8 +145,9 @@ func (sh *dbgScreenShader) setAttributes(env shaderEnvironment) {
 	gl.Uniform1f(sh.scalingY, sh.img.wm.dbgScr.yscaling)
 	gl.Uniform2f(sh.screenDim, width, height)
 
-	cursorX := sh.img.screen.crit.lastX
-	cursorY := sh.img.screen.crit.lastY
+	// cursor is the coordinates of the *most recent* pixel to be drawn
+	cursorX := sh.img.lz.TV.Clock + specification.ClksHBlank
+	cursorY := sh.img.lz.TV.Scanline
 
 	// if crt preview is enabled then force cropping
 	if sh.img.wm.dbgScr.cropped || sh.img.wm.dbgScr.crtPreview {
@@ -159,7 +160,7 @@ func (sh *dbgScreenShader) setAttributes(env shaderEnvironment) {
 	gl.Uniform1f(sh.lastY, float32(cursorY)*yscaling)
 
 	// screen geometry
-	gl.Uniform1f(sh.hblank, specification.ClksHBlank*xscaling)
+	gl.Uniform1f(sh.hblank, (specification.ClksHBlank)*xscaling)
 	gl.Uniform1f(sh.visibleTop, float32(sh.img.screen.crit.frameInfo.VisibleTop)*yscaling)
 	gl.Uniform1f(sh.visibleBottom, float32(sh.img.screen.crit.frameInfo.VisibleBottom)*yscaling)
 	gl.Uniform1f(sh.lastNewFrameAtScanline, float32(sh.img.screen.crit.frameInfo.TotalScanlines)*yscaling)

gui/sdlimgui/win_dbgscr.go
@@ -469,7 +469,8 @@ func (win *winDbgScr) drawReflectionTooltip() {
 	imguiSeparator()
 
 	// if mouse is over a pixel from the previous frame then show nothing except a note
-	if win.mouseScanline > win.scr.crit.lastY || (win.mouseScanline == win.scr.crit.lastY && win.mouseClock > win.scr.crit.lastX) {
+	if win.mouseScanline > win.img.lz.TV.Scanline ||
+		(win.mouseScanline == win.img.lz.TV.Scanline && win.mouseClock > win.img.lz.TV.Clock+specification.ClksHBlank) {
 		imgui.Text("From previous frame")
 		imguiSeparator()
 	}

gui/sdlimgui/shaders/dbgscr.frag
@@ -31,8 +31,6 @@ bool isNearEqual(float x, float y, float epsilon)
 	return abs(x - y) <= epsilon;
 }
 
-const float cursorSize = 1.0;
-
 void main()
 {
 	Out_Color = Frag_Color * texture(Texture, Frag_UV.st);
@@ -56,10 +54,10 @@ void main()
 		visibleBottom = (VisibleBottom - VisibleTop) / ScreenDim.y;
 		lastY -=  pixelY * VisibleTop;
 	} else {
-		// top/bottom guides
+		// visible screen guides
 		float visibleTop = pixelY * VisibleTop;
 		visibleBottom = pixelY * VisibleBottom;
-		if (isNearEqual(Frag_UV.y, visibleTop-pixelY, pixelY) || isNearEqual(Frag_UV.y, visibleBottom+pixelY, pixelY)) {
+		if (isNearEqual(Frag_UV.y, visibleTop, pixelY) || isNearEqual(Frag_UV.y, visibleBottom, pixelY)) {
 			if (mod(floor(gl_FragCoord.x), 4) < 2.0) {
 				Out_Color.r = 1.0;
 				Out_Color.g = 1.0;
@@ -71,7 +69,7 @@ void main()
 
 		// hblank guide
 		float hblank = pixelX * Hblank;
-		if (isNearEqual(Frag_UV.x, hblank-pixelX, pixelX)) {
+		if (isNearEqual(Frag_UV.x, hblank, pixelX)) {
 			if (mod(floor(gl_FragCoord.y), 4) < 2.0) {
 				Out_Color.r = 1.0;
 				Out_Color.g = 1.0;
@@ -83,7 +81,7 @@ void main()
 
 		// frame flyback guide
 		float lastNewFrameAtScanline = pixelY * LastNewFrameAtScanline;
-		if (isNearEqual(Frag_UV.y, lastNewFrameAtScanline-pixelY, pixelY)) {
+		if (isNearEqual(Frag_UV.y, lastNewFrameAtScanline, pixelY)) {
 			if (mod(floor(gl_FragCoord.x), 8) < 3.0) {
 				Out_Color.r = 1.0;
 				Out_Color.g = 0.0;
@@ -94,11 +92,11 @@ void main()
 		}
 	}
 
-	// when ShowCursor is true then there is some additional processing we need to perform
+	// show cursor. the cursor illustrates the *most recent* pixel to be drawn
 	if (ShowCursor == 1) {
 		// draw cursor if pixel is at the last x/y position
 		if (lastY >= 0 && lastX >= 0) {
-			if (isNearEqual(Frag_UV.y, lastY+texelY, cursorSize*texelY) && isNearEqual(Frag_UV.x, lastX+texelX, cursorSize*texelX/2)) {
+			if (isNearEqual(Frag_UV.y, lastY+texelY, texelY) && isNearEqual(Frag_UV.x, lastX+texelX/2, texelX/2)) {
 				Out_Color.r = 1.0;
 				Out_Color.g = 1.0;
 				Out_Color.b = 1.0;
@@ -108,7 +106,7 @@ void main()
 		}
 
 		// draw off-screen cursor for HBLANK
-		if (lastX < 0 && isNearEqual(Frag_UV.y, lastY+texelY, cursorSize*texelY) && isNearEqual(Frag_UV.x, 0, cursorSize*texelX/2)) {
+		if (lastX < 0 && isNearEqual(Frag_UV.y, lastY+texelY, texelY) && isNearEqual(Frag_UV.x, 0, texelX/2)) {
 			Out_Color.r = 1.0;
 			Out_Color.g = 0.0;
 			Out_Color.b = 0.0;
@@ -120,9 +118,9 @@ void main()
 		// consider for drawing an off-screen cursor
 		if (IsCropped == 1) {
 			// when VBLANK is active but HBLANK is off
-			if (isNearEqual(Frag_UV.x, lastX, cursorSize * texelX/2)) {
+			if (isNearEqual(Frag_UV.x, lastX, texelX/2)) {
 				// top of screen
-				if (lastY < 0 && isNearEqual(Frag_UV.y, 0, cursorSize*texelY)) {
+				if (lastY < 0 && isNearEqual(Frag_UV.y, 0, texelY)) {
 					Out_Color.r = 1.0;
 					Out_Color.g = 0.0;
 					Out_Color.b = 0.0;
@@ -132,7 +130,7 @@ void main()
 			
 				// bottom of screen (knocking a pixel off the scanline
 				// boundary check to make sure the cursor is visible)
-				if (lastY > visibleBottom-pixelY && isNearEqual(Frag_UV.y, visibleBottom, cursorSize*texelY)) {
+				if (lastY > visibleBottom-pixelY && isNearEqual(Frag_UV.y, visibleBottom, texelY)) {
 					Out_Color.r = 1.0;
 					Out_Color.g = 0.0;
 					Out_Color.b = 0.0;
@@ -142,9 +140,9 @@ void main()
 			}
 
 			// when HBLANK and VBLANK are both active
-			if (lastX < 0 && isNearEqual(Frag_UV.x, 0, cursorSize*texelX/2)) {
+			if (lastX < 0 && isNearEqual(Frag_UV.x, 0, texelX/2)) {
 				// top/left corner of screen
-				if (lastY < 0 && isNearEqual(Frag_UV.y, 0, cursorSize*texelY)) {
+				if (lastY < 0 && isNearEqual(Frag_UV.y, 0, texelY)) {
 					Out_Color.r = 1.0;
 					Out_Color.g = 0.0;
 					Out_Color.b = 0.0;
@@ -155,7 +153,7 @@ void main()
 				// bottom/left corner of screen (knocking a pixel off the
 				// scanline boundary check to make sure the cursor is
 				// visible)
-				if (lastY > visibleBottom-pixelY && isNearEqual(Frag_UV.y, visibleBottom, cursorSize*texelY)) {
+				if (lastY > visibleBottom-pixelY && isNearEqual(Frag_UV.y, visibleBottom, texelY)) {
 					Out_Color.r = 1.0;
 					Out_Color.g = 0.0;
 					Out_Color.b = 0.0;

commit 3c1b30623dcbf0560d11bd9725b58fea794f8f6e

removed visible artefact on debug screen when pausing emulation
caused by changes introduced in dda5ed5d5c4ce93fe8ea4fc40e3ce763eb0b10cf

taking the clock/scanline values from LazyTV was a good idea but the
time it takes for the lazy system to update is just long enough for the
old value to be visible.

an additional argument to PixelRender.SetPixels() is required but it
can't be helped.

digest/video.go
@@ -103,7 +103,7 @@ func (dig *Video) NewScanline(scanline int) error {
 }
 
 // SetPixels implements television.PixelRenderer interface.
-func (dig *Video) SetPixels(sig []signal.SignalAttributes) error {
+func (dig *Video) SetPixels(sig []signal.SignalAttributes, _ int) error {
 	// offset always starts at after the digest leader
 	offset := len(dig.digest)
 

gui/sdlimgui/glsl_dbgscr.go
@@ -146,8 +146,8 @@ func (sh *dbgScreenShader) setAttributes(env shaderEnvironment) {
 	gl.Uniform2f(sh.screenDim, width, height)
 
 	// cursor is the coordinates of the *most recent* pixel to be drawn
-	cursorX := sh.img.lz.TV.Clock + specification.ClksHBlank
-	cursorY := sh.img.lz.TV.Scanline
+	cursorX := sh.img.screen.crit.lastClock
+	cursorY := sh.img.screen.crit.lastScanline
 
 	// if crt preview is enabled then force cropping
 	if sh.img.wm.dbgScr.cropped || sh.img.wm.dbgScr.crtPreview {

gui/sdlimgui/screen.go
@@ -122,6 +122,17 @@ type screenCrit struct {
 	// when paused we show two adjacent frames over-and-over. this flag tracks
 	// which of those frames to show
 	pauseFrame bool
+
+	// the coordinates of the most recent pixel to be set by the television
+	//
+	// we experimented with not having these fields and using the Scanline and
+	// Clock values from LazyTV. however, for one of the purposes we want to
+	// use it for, waiting for the lazy system to update causes a visible
+	// artefact in the rendering of the debug screen. it's unfortunate and it
+	// means PixelRenderer.SetPixels() needs an extra argument but it's worth
+	// it IMO
+	lastClock    int
+	lastScanline int
 }
 
 func newScreen(img *SdlImgui) *screen {
@@ -338,7 +349,7 @@ func (scr *screen) NewScanline(scanline int) error {
 }
 
 // SetPixels implements the television.PixelRenderer interface.
-func (scr *screen) SetPixels(sig []signal.SignalAttributes) error {
+func (scr *screen) SetPixels(sig []signal.SignalAttributes, last int) error {
 	scr.crit.section.Lock()
 	defer scr.crit.section.Unlock()
 
@@ -374,6 +385,9 @@ func (scr *screen) SetPixels(sig []signal.SignalAttributes) error {
 
 		offset += 4
 	}
+
+	scr.crit.lastScanline = last / specification.ClksScanline
+	scr.crit.lastClock = last % specification.ClksScanline
 
 	return nil
 }

gui/sdlimgui/win_dbgscr.go
@@ -470,8 +470,8 @@ func (win *winDbgScr) drawReflectionTooltip() {
 
 	// if mouse is over a pixel from the previous frame then show nothing except a note
 	if win.img.emulation.State() == emulation.Paused {
-		if win.mouseScanline > win.img.lz.TV.Scanline ||
-			(win.mouseScanline == win.img.lz.TV.Scanline && win.mouseClock > win.img.lz.TV.Clock+specification.ClksHBlank) {
+		if win.mouseScanline > win.img.screen.crit.lastScanline ||
+			(win.mouseScanline == win.img.screen.crit.lastScanline && win.mouseClock > win.img.screen.crit.lastClock) {
 			imgui.Text("From previous frame")
 			imguiSeparator()
 		}

hardware/television/protocol.go
@@ -72,6 +72,15 @@ type PixelRenderer interface {
 	// Setting the color of a pixel can be done by extracting the ColorSignal
 	// from the SignalAttributes (see signal package)
 	//
+	// The last parameter indicates the index of the array entry that was most
+	// recently set. This is useful to know when showing televison images when
+	// the emulation is paused. All entries upto and including last are from
+	// teh *current* frame. All entries afterwards are from the *previous*
+	// frame.
+	//
+	// If the entry contains signal.NoSignal then that screen pixel has not
+	// been written to recently.
+	//
 	// For renderers that are producing an accurate visual image, the pixel
 	// should always be set to video black if VBLANK is on. Some renderers
 	// however may find it useful to set the pixel to the RGB value regardless
@@ -87,7 +96,7 @@ type PixelRenderer interface {
 	// In other words, the PixelRenderer should not simply assume VBLANK is
 	// restricted to the "off-screen" areas as defined by the FrameInfo sent to
 	// Resize()
-	SetPixels(sig []signal.SignalAttributes) error
+	SetPixels(sig []signal.SignalAttributes, last int) error
 
 	// Reset all pixels. Called when TV is reset.
 	//

hardware/television/television.go
@@ -526,7 +526,7 @@ func (tv *Television) renderSignals() error {
 	// do not render pixels if emulation is in the rewinding state
 	if tv.emulationState != emulation.Rewinding {
 		for _, r := range tv.renderers {
-			err := r.SetPixels(tv.signals)
+			err := r.SetPixels(tv.signals, tv.currentSignalIdx)
 			if err != nil {
 				return curated.Errorf("television: %v", err)
 			}