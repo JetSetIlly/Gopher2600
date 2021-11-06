JetSetIlly/Gopher2600

commit 92d96cc4e965ec6a85036dfd8e2db0bcfd078e24

major refactor. amalgamated debugger/playmode emulations
switch between debugger and playmode with F6 or backtick key

pakage debugger remains for now but it will be renamed to emulator or
something like that. playmode package removed.

commandline options for both debugger and playmode are the same
currently, with some playmode features being lost (temporarily)

debugger/debugger.go
@@ -55,6 +55,11 @@ import (
 // example, disassembly on a cartridge change (which can happen at any time)
 // updates the Disasm field, it does not reinitialise it.
 type Debugger struct {
+	// current mode of the emulation
+	mode emulation.Mode
+
+	// state is an atomic value because we need to be able to read it from the
+	// GUI thread (see State() function)
 	state atomic.Value // emulation.State
 
 	vcs    *hardware.VCS
@@ -182,15 +187,16 @@ type Debugger struct {
 	catchupEnd      func()
 }
 
+// CreateUserInterface is used to initialise the user interface used by the emulation.
+type CreateUserInterface func(emulation.Emulation) (gui.GUI, terminal.Terminal, error)
+
 // NewDebugger creates and initialises everything required for a new debugging
-// session. Use the Start() method to actually begin the session.
-func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal, useSavekey bool) (*Debugger, error) {
-	var err error
-
+// session.
+//
+// It should be followed up with a call to AddUserInterface() and call the
+// Start() method to actually begin the emulation.
+func NewDebugger(create CreateUserInterface, mode emulation.Mode, spec string, useSavekey bool) (*Debugger, error) {
 	dbg := &Debugger{
-		gui:  scr,
-		term: term,
-
 		// by definition the state of debugger has changed during startup
 		hasChanged: true,
 
@@ -199,6 +205,13 @@ func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal,
 	}
 
 	dbg.state.Store(emulation.Initialising)
+
+	// creat a new television. this will be used during the initialisation of
+	// the VCS and not referred to directly again
+	tv, err := television.NewTelevision(spec)
+	if err != nil {
+		return nil, curated.Errorf("debugger: %v", err)
+	}
 
 	// create a new VCS instance
 	dbg.vcs, err = hardware.NewVCS(tv)
@@ -229,31 +242,6 @@ func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal,
 	// create a minimal lastResult for initialisation
 	dbg.lastResult = &disassembly.Entry{Result: execution.Result{Final: true}}
 
-	// setup reflection monitor
-	dbg.ref = reflection.NewGatherer(dbg.vcs)
-	if r, ok := dbg.gui.(reflection.Broker); ok {
-		dbg.ref.AddRenderer(r.GetReflectionRenderer())
-	}
-
-	// plug in rewind system
-	dbg.Rewind, err = rewind.NewRewind(dbg, dbg)
-	if err != nil {
-		return nil, curated.Errorf("debugger: %v", err)
-	}
-	dbg.Rewind.AddTimelineCounter(dbg.ref) // using reflection monitor for the timeline counter
-	dbg.deepPoking = make(chan bool, 1)
-
-	// frame triggers. deliberately adding rewind before reflection because the
-	// latter implements rewind.TimelineCounter. we want to reset
-	// TimelineCounts in reflection.Gatherer  but we need to call
-	// TimelineCounts() from the rewind packge before that happens
-	dbg.vcs.TV.AddFrameTrigger(dbg.Rewind)
-	dbg.vcs.TV.AddFrameTrigger(dbg.ref)
-
-	// add audio tracker
-	dbg.Tracker = tracker.NewTracker(dbg)
-	dbg.vcs.TIA.Audio.SetTracker(dbg.Tracker)
-
 	// halting coordination
 	dbg.halting, err = newHaltCoordination(dbg)
 	if err != nil {
@@ -277,23 +265,58 @@ func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal,
 	// connect Interrupt signal to dbg.events.intChan
 	signal.Notify(dbg.events.IntEvents, os.Interrupt)
 
-	// connect gui
-	err = dbg.gui.SetFeature(gui.ReqSetEmulation, dbg)
-	if err != nil {
-		if !curated.Is(err, gui.UnsupportedGuiFeature) {
-			return nil, curated.Errorf("debugger: %v", err)
-		}
-	}
-
 	// allocate memory for user input
 	dbg.input = make([]byte, 255)
+
+	// create GUI
+	dbg.gui, dbg.term, err = create(dbg)
+	if err != nil {
+		return nil, curated.Errorf("debugger: %v", err)
+	}
 
 	// add tab completion to terminal
 	dbg.term.RegisterTabCompletion(commandline.NewTabCompletion(debuggerCommands))
 
-	// try to add debugger (self) to gui context
+	// setup reflection monitor
+	dbg.ref = reflection.NewGatherer(dbg.vcs)
+	if r, ok := dbg.gui.(reflection.Broker); ok {
+		dbg.ref.AddRenderer(r.GetReflectionRenderer())
+	}
+
+	// plug in rewind system
+	dbg.Rewind, err = rewind.NewRewind(dbg, dbg)
+	if err != nil {
+		return nil, curated.Errorf("debugger: %v", err)
+	}
+	dbg.Rewind.AddTimelineCounter(dbg.ref) // using reflection monitor for the timeline counter
+	dbg.deepPoking = make(chan bool, 1)
+
+	// adding TV frame triggers in setMode(). what the TV triggers on depending
+	// on the mode for performance reasons (eg. no reflection required in
+	// playmode)
+
+	// add audio tracker
+	dbg.Tracker = tracker.NewTracker(dbg)
+	dbg.vcs.TIA.Audio.SetTracker(dbg.Tracker)
+
+	// set mode to the value requested in the function paramenters
+	err = dbg.setMode(mode)
+	if err != nil {
+		return nil, curated.Errorf("debugger: %v", err)
+	}
 
 	return dbg, nil
+}
+
+// End cleans up any resources that may be dangling.
+func (dbg *Debugger) End() {
+	dbg.vcs.End()
+
+	// set ending state
+	err := dbg.gui.SetFeature(gui.ReqEnd)
+	if err != nil {
+		logger.Log("debugger", err.Error())
+	}
 }
 
 // VCS implements the emulation.Emulation interface.
@@ -322,18 +345,79 @@ func (dbg *Debugger) State() emulation.State {
 }
 
 func (dbg *Debugger) setState(state emulation.State) {
-	dbg.vcs.TV.SetEmulationState(state)
-	dbg.ref.SetEmulationState(state)
-	dbg.state.Store(state)
+	dbg.setStateQuiet(state, false)
 }
 
-// Pause implements the emulation.Emulation interface.
-func (dbg *Debugger) Pause(set bool) {
-	if set {
-		dbg.setState(emulation.Paused)
-	} else {
-		dbg.setState(emulation.Running)
+// same as setState but with quiet argument, to indicate that EmulationEvent
+// should not be issued to the gui.
+func (dbg *Debugger) setStateQuiet(state emulation.State, quiet bool) {
+	dbg.vcs.TV.SetEmulationState(state)
+	dbg.ref.SetEmulationState(state)
+
+	prevState := dbg.State()
+	dbg.state.Store(state)
+
+	if !quiet && dbg.mode == emulation.ModePlay {
+		switch state {
+		case emulation.Paused:
+			dbg.gui.SetFeature(gui.ReqEmulationEvent, emulation.EventPause)
+		case emulation.Running:
+			if prevState != emulation.Initialising {
+				dbg.gui.SetFeature(gui.ReqEmulationEvent, emulation.EventRun)
+			}
+		}
 	}
+}
+
+func (dbg *Debugger) setMode(mode emulation.Mode) error {
+	if mode == dbg.mode {
+		return nil
+	}
+
+	prevMode := dbg.mode
+	dbg.mode = mode
+
+	// notify gui of change
+	err := dbg.gui.SetFeature(gui.ReqSetEmulationMode, mode)
+	if err != nil {
+		return err
+	}
+
+	// clear all frame triggers. we'll add what's required depending on the
+	// selected mode
+	dbg.vcs.TV.ClearFrameTriggers()
+
+	// swtich mode and make sure emulation is in correct state. we say that
+	// emulation is always running when entering playmode and always paused
+	// when entering debug mode.
+	//
+	// * the reason for this is simplicity. if we allow playmode to begin
+	// paused for example it complicates how we render the screen (see sdlimgui
+	// screen.go)
+
+	switch dbg.mode {
+	case emulation.ModePlay:
+		dbg.vcs.TV.AddFrameTrigger(dbg.Rewind)
+		dbg.setState(emulation.Running)
+	case emulation.ModeDebugger:
+		// frame triggers. deliberately adding rewind before reflection because the
+		// latter implements rewind.TimelineCounter. we want to reset
+		// TimelineCounts in reflection.Gatherer  but we need to call
+		// TimelineCounts() from the rewind packge before that happens
+		dbg.vcs.TV.AddFrameTrigger(dbg.ref)
+		dbg.vcs.TV.AddFrameTrigger(dbg.Rewind)
+		dbg.setState(emulation.Paused)
+
+		// debugger needs knowledge about previous frames (via the reflector)
+		// if we moving from playmode
+		if prevMode == emulation.ModePlay {
+			dbg.PushRerunLastNFrames(2)
+		}
+	default:
+		return curated.Errorf("emulation mode not supported: %s", mode)
+	}
+
+	return nil
 }
 
 // Start the main debugger sequence.
@@ -344,6 +428,36 @@ func (dbg *Debugger) Start(initScript string, cartload cartridgeloader.Loader) e
 		return curated.Errorf("debugger: %v", err)
 	}
 	defer dbg.term.CleanUp()
+
+	// simple detection of whether cartridge is ejected when starting in
+	// playmode. if it is ejected then open ROM selected.
+	if dbg.mode == emulation.ModePlay {
+		if cartload.Filename == "" {
+			filename := make(chan string, 1)
+			err = dbg.gui.SetFeature(gui.ReqROMSelector, filename)
+			if err != nil {
+				return curated.Errorf("playmode: %v", err)
+			}
+
+			// a ROM selector has been reqiested. now wait for an event, either a
+			// filename from the selector or a quit event.
+			done := false
+			for !done {
+				select {
+				case ev := <-dbg.events.UserInput:
+					if _, ok := ev.(userinput.EventQuit); ok {
+						done = true
+					}
+				case fn := <-filename:
+					cartload, err = cartridgeloader.NewLoader(fn, "AUTO")
+					if err != nil {
+						return curated.Errorf("debugger: %v", err)
+					}
+					done = true
+				}
+			}
+		}
+	}
 
 	err = dbg.attachCartridge(cartload)
 	if err != nil {
@@ -378,20 +492,41 @@ func (dbg *Debugger) Start(initScript string, cartload cartridgeloader.Loader) e
 
 	// inputloop will continue until debugger is to be terminated
 	for dbg.running {
-		err = dbg.inputLoop(dbg.term, false)
-		if err != nil {
-			return curated.Errorf("debugger: %v", err)
-		}
-
-		// handle inputLoopRestart and any on-restart function
-		if dbg.unwindLoopRestart != nil {
-			err := dbg.unwindLoopRestart()
+		switch dbg.mode {
+		case emulation.ModePlay:
+			err = dbg.playLoop()
 			if err != nil {
 				return curated.Errorf("debugger: %v", err)
 			}
-			dbg.unwindLoopRestart = nil
-		} else {
-			dbg.running = false
+		case emulation.ModeDebugger:
+			switch dbg.State() {
+			case emulation.Running:
+				dbg.runUntilHalt = true
+				dbg.continueEmulation = true
+			case emulation.Paused:
+				dbg.haltImmediately = true
+			case emulation.Rewinding:
+			default:
+				return curated.Errorf("emulation state not supported on *start* of debugging loop: %s", dbg.State())
+			}
+
+			err = dbg.inputLoop(dbg.term, false)
+			if err != nil {
+				return curated.Errorf("debugger: %v", err)
+			}
+
+			// handle inputLoopRestart and any on-restart function
+			if dbg.unwindLoopRestart != nil {
+				err := dbg.unwindLoopRestart()
+				if err != nil {
+					return curated.Errorf("debugger: %v", err)
+				}
+				dbg.unwindLoopRestart = nil
+			} else if dbg.State() == emulation.Ending {
+				dbg.running = false
+			}
+		default:
+			return curated.Errorf("emulation mode not supported: %s", dbg.mode)
 		}
 	}
 
@@ -450,11 +585,18 @@ func (dbg *Debugger) reset(newCartridge bool) error {
 // especially important is the repointing of the symbols table in the instance of dbgmem.
 func (dbg *Debugger) attachCartridge(cartload cartridgeloader.Loader) (e error) {
 	dbg.setState(emulation.Initialising)
+
+	// set state after initialisation according to the emulation mode
 	defer func() {
-		if dbg.runUntilHalt && e == nil {
+		switch dbg.mode {
+		case emulation.ModeDebugger:
+			if dbg.runUntilHalt && e == nil {
+				dbg.setState(emulation.Running)
+			} else {
+				dbg.setState(emulation.Paused)
+			}
+		case emulation.ModePlay:
 			dbg.setState(emulation.Running)
-		} else {
-			dbg.setState(emulation.Paused)
 		}
 	}()
 
@@ -502,18 +644,30 @@ func (dbg *Debugger) attachCartridge(cartload cartridgeloader.Loader) (e error) 
 					return err
 				}
 			case mapper.EventSuperchargerSoundloadStarted:
-				// not required for the debugger
-			case mapper.EventSuperchargerSoundloadEnded:
-				// !!TODO: it would be nice to see partial disassemblies of supercharger tapes
-				// during loading. not completely necessary I don't think, but it would be
-				// nice to have.
-				err := dbg.Disasm.FromMemory()
+				err := dbg.gui.SetFeature(gui.ReqCartridgeEvent, mapper.EventSuperchargerSoundloadStarted)
 				if err != nil {
 					return err
 				}
+			case mapper.EventSuperchargerSoundloadEnded:
+				err := dbg.gui.SetFeature(gui.ReqCartridgeEvent, mapper.EventSuperchargerSoundloadEnded)
+				if err != nil {
+					return err
+				}
+
+				// !!TODO: it would be nice to see partial disassemblies of supercharger tapes
+				// during loading. not completely necessary I don't think, but it would be
+				// nice to have.
+				err = dbg.Disasm.FromMemory()
+				if err != nil {
+					return err
+				}
+
 				return dbg.vcs.TV.Reset(true)
 			case mapper.EventSuperchargerSoundloadRewind:
-				// not required for the debugger
+				err := dbg.gui.SetFeature(gui.ReqCartridgeEvent, mapper.EventSuperchargerSoundloadRewind)
+				if err != nil {
+					return err
+				}
 			default:
 				logger.Logf("debugger", "unhandled hook event for supercharger (%v)", event)
 			}
@@ -530,7 +684,10 @@ func (dbg *Debugger) attachCartridge(cartload cartridgeloader.Loader) (e error) 
 					}
 				}
 			case mapper.EventPlusROMNetwork:
-				// not required for debugger
+				err := dbg.gui.SetFeature(gui.ReqCartridgeEvent, mapper.EventPlusROMNetwork)
+				if err != nil {
+					return err
+				}
 			default:
 				logger.Logf("debugger", "unhandled hook event for plusrom (%v)", event)
 			}
@@ -571,7 +728,7 @@ func (dbg *Debugger) hotload() (e error) {
 		if dbg.runUntilHalt && e == nil {
 			dbg.setState(emulation.Running)
 		} else {
-			dbg.setState(emulation.Paused)
+			dbg.gui.SetFeature(gui.ReqEmulationEvent, emulation.EventPause)
 		}
 	}()
 

Makefile
@@ -1,9 +1,9 @@
 compileFlags = '-c 3 -B -wb=false'
 
-profilingRom = roms/Homebrew/hs_2600.bin
+# profilingRom = roms/Homebrew/hs_2600.bin
 # profilingRom = roms/Homebrew/CDF/galaga_dmo_v2_NTSC.bin
 # profilingRom = roms/Homebrew/DPC+ARM/ZaxxonHDDemo_150927_NTSC.bin
-# profilingRom = roms/Rsboxing.bin
+profilingRom = roms/Rsboxing.bin
 # profilingRom = "test_roms/plusrom/sokoboo Plus.bin"
 # profilingRom = "roms/starpath/02 - Communist Mutants From Space (Ntsc).mp3"
 # profilingRom = "roms/The Official Frogger.bin"
@@ -84,6 +84,12 @@ profile_cpu: generate test
 	@echo "performance mode running for 20s"
 	@./gopher2600 performance --profile=cpu --fpscap=false --duration=20s $(profilingRom)
 	@$(goBinary) tool pprof -http : ./gopher2600 performance_cpu.profile
+
+profile_cpu_play: generate test
+	@$(goBinary) build -gcflags $(compileFlags)
+	@echo "performance mode running for 20s"
+	@./gopher2600 play --profile=cpu $(profilingRom)
+	@$(goBinary) tool pprof -http : ./gopher2600 play_cpu.profile
 
 profile_cpu_debug : generate test
 	@$(goBinary) build -gcflags $(compileFlags)

debugger/debugger_test.go
@@ -23,8 +23,8 @@ import (
 	"github.com/jetsetilly/gopher2600/cartridgeloader"
 	"github.com/jetsetilly/gopher2600/debugger"
 	"github.com/jetsetilly/gopher2600/debugger/terminal"
+	"github.com/jetsetilly/gopher2600/emulation"
 	"github.com/jetsetilly/gopher2600/gui"
-	"github.com/jetsetilly/gopher2600/hardware/television"
 	"github.com/jetsetilly/gopher2600/prefs"
 )
 
@@ -32,13 +32,6 @@ type mockGUI struct{}
 
 func (g *mockGUI) SetFeature(request gui.FeatureReq, args ...gui.FeatureReqData) error {
 	return nil
-}
-
-func (g *mockGUI) SetFeatureNoError(request gui.FeatureReq, args ...gui.FeatureReqData) {
-}
-
-func (g *mockGUI) GetFeature(request gui.FeatureReq) (gui.FeatureReqData, error) {
-	return nil, nil
 }
 
 type mockTerm struct {
@@ -149,14 +142,14 @@ func (trm *mockTerm) testSequence() {
 func TestDebugger_withNonExistantInitScript(t *testing.T) {
 	prefs.DisableSaving = true
 
-	trm := newMockTerm(t)
+	var trm *mockTerm
 
-	tv, err := television.NewTelevision("NTSC")
-	if err != nil {
-		t.Fatalf(err.Error())
+	create := func(e emulation.Emulation) (gui.GUI, terminal.Terminal, error) {
+		trm = newMockTerm(t)
+		return &mockGUI{}, trm, nil
 	}
 
-	dbg, err := debugger.NewDebugger(tv, &mockGUI{}, trm, false)
+	dbg, err := debugger.NewDebugger(create, emulation.ModeDebugger, "AUTO", false)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -172,13 +165,14 @@ func TestDebugger_withNonExistantInitScript(t *testing.T) {
 func TestDebugger(t *testing.T) {
 	prefs.DisableSaving = true
 
-	trm := newMockTerm(t)
-	tv, err := television.NewTelevision("NTSC")
-	if err != nil {
-		t.Fatalf(err.Error())
+	var trm *mockTerm
+
+	create := func(e emulation.Emulation) (gui.GUI, terminal.Terminal, error) {
+		trm = newMockTerm(t)
+		return &mockGUI{}, trm, nil
 	}
 
-	dbg, err := debugger.NewDebugger(tv, &mockGUI{}, trm, false)
+	dbg, err := debugger.NewDebugger(create, emulation.ModeDebugger, "AUTO", false)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}

debugger/events.go
@@ -18,10 +18,45 @@ package debugger
 import (
 	"github.com/jetsetilly/gopher2600/curated"
 	"github.com/jetsetilly/gopher2600/debugger/terminal"
+	"github.com/jetsetilly/gopher2600/emulation"
 	"github.com/jetsetilly/gopher2600/userinput"
 )
 
 func (dbg *Debugger) userInputHandler(ev userinput.Event) error {
+	switch dbg.mode {
+	case emulation.ModePlay:
+		handled := false
+
+		switch ev := ev.(type) {
+		case userinput.EventMouseWheel:
+			dbg.playmodeRewind(int(ev.Delta) * 2)
+			handled = true
+
+		case userinput.EventKeyboard:
+			if ev.Down {
+				switch ev.Key {
+				case "Left":
+					if ev.Mod == userinput.KeyModShift {
+						dbg.playmodeRewind(-2)
+						handled = true
+					}
+				case "Right":
+					if ev.Mod == userinput.KeyModShift {
+						dbg.playmodeRewind(2)
+						handled = true
+					}
+				}
+			} else if ev.Mod != userinput.KeyModNone {
+				handled = true
+			}
+		}
+
+		// the user event has triggered a rewind so return immediately
+		if handled {
+			return nil
+		}
+	}
+
 	quit, err := dbg.controllers.HandleUserInput(ev, dbg.vcs.RIOT.Ports)
 	if err != nil {
 		return curated.Errorf("debugger: %v", err)

debugger/loop_catchup.go
@@ -1,0 +1,77 @@
+// This file is part of Gopher2600.
+//
+// Gopher2600 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gopher2600 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
+
+package debugger
+
+import (
+	"github.com/jetsetilly/gopher2600/emulation"
+	"github.com/jetsetilly/gopher2600/hardware/television/coords"
+	"github.com/jetsetilly/gopher2600/rewind"
+)
+
+// CatchUpLoop implements the rewind.Runner interface.
+//
+// It is called from the rewind package and sets the functions that are
+// required for catchupLoop().
+func (dbg *Debugger) CatchUpLoop(tgt coords.TelevisionCoords, callback rewind.CatchUpLoopCallback) error {
+	switch dbg.mode {
+	case emulation.ModePlay:
+		fpscap := dbg.vcs.TV.SetFPSCap(false)
+		defer dbg.vcs.TV.SetFPSCap(fpscap)
+
+		dbg.vcs.Run(func() (emulation.State, error) {
+			// swallow any user events received while catchup is going on
+			// this prevents the rewind from feeling laggy
+			select {
+			case _ = <-dbg.events.UserInput:
+			default:
+			}
+
+			coords := dbg.vcs.TV.GetCoords()
+			if coords.Frame >= tgt.Frame {
+				return emulation.Ending, nil
+			}
+			return emulation.Running, nil
+		}, 1)
+
+	case emulation.ModeDebugger:
+		// turn off TV's fps frame limiter
+		fpsCap := dbg.vcs.TV.SetFPSCap(false)
+
+		// we've already set emulation state to emulation.Rewinding
+
+		dbg.catchupContinue = func() bool {
+			newCoords := dbg.vcs.TV.GetCoords()
+
+			callback(newCoords.Frame)
+
+			// returns true if we're to continue
+			return !coords.GreaterThanOrEqual(newCoords, tgt)
+		}
+
+		dbg.catchupEnd = func() {
+			dbg.vcs.TV.SetFPSCap(fpsCap)
+			dbg.catchupContinue = nil
+			dbg.catchupEnd = nil
+			dbg.setState(emulation.Paused)
+			dbg.continueEmulation = false
+			dbg.runUntilHalt = false
+		}
+
+		dbg.PushRawEventReturn(func() {})
+	}
+
+	return nil
+}

debugger/loop_debugger.go
@@ -23,9 +23,7 @@ import (
 	"github.com/jetsetilly/gopher2600/debugger/terminal"
 	"github.com/jetsetilly/gopher2600/disassembly"
 	"github.com/jetsetilly/gopher2600/emulation"
-	"github.com/jetsetilly/gopher2600/hardware/television/coords"
 	"github.com/jetsetilly/gopher2600/logger"
-	"github.com/jetsetilly/gopher2600/rewind"
 )
 
 // unwindLoop is called whenever it is required that the inputLoop/catchupLoop
@@ -40,39 +38,6 @@ import (
 // the caller of the function to set emulation.State appropriately.
 func (dbg *Debugger) unwindLoop(onRestart func() error) {
 	dbg.unwindLoopRestart = onRestart
-}
-
-// CatchUpLoop implements the rewind.Runner interface.
-//
-// It is called from the rewind package and sets the functions that are
-// required for catchupLoop().
-func (dbg *Debugger) CatchUpLoop(tgt coords.TelevisionCoords, callback rewind.CatchUpLoopCallback) error {
-	// turn off TV's fps frame limiter
-	fpsCap := dbg.vcs.TV.SetFPSCap(false)
-
-	// we've already set emulation state to emulation.Rewinding
-
-	dbg.catchupContinue = func() bool {
-		newCoords := dbg.vcs.TV.GetCoords()
-
-		callback(newCoords.Frame)
-
-		// returns true if we're to continue
-		return !coords.GreaterThanOrEqual(newCoords, tgt)
-	}
-
-	dbg.catchupEnd = func() {
-		dbg.vcs.TV.SetFPSCap(fpsCap)
-		dbg.catchupContinue = nil
-		dbg.catchupEnd = nil
-		dbg.setState(emulation.Paused)
-		dbg.continueEmulation = false
-		dbg.runUntilHalt = false
-	}
-
-	dbg.PushRawEventReturn(func() {})
-
-	return nil
 }
 
 // catchupLoop is a special purpose loop designed to run inside of the inputLoop. it is called only
@@ -157,6 +122,10 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, isVideoStep bool) error 
 	var err error
 
 	for dbg.running {
+		if dbg.mode != emulation.ModeDebugger {
+			return nil
+		}
+
 		if dbg.catchupContinue != nil {
 			if isVideoStep {
 				panic("refusing to run catchup loop inside a color clock cycle")
@@ -193,6 +162,11 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, isVideoStep bool) error 
 				} else {
 					dbg.printLine(terminal.StyleError, "%s", err)
 				}
+			}
+
+			// emulation has been put into a different mode. exit loop immediately
+			if dbg.mode != emulation.ModeDebugger {
+				return nil
 			}
 
 			// if debugger is no longer running after checking interrupts and
@@ -313,6 +287,11 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, isVideoStep bool) error 
 					return nil
 				}
 				return err
+			}
+
+			// emulation has been put into a different mode. exit loop immediately
+			if dbg.mode != emulation.ModeDebugger {
+				return nil
 			}
 
 			// hasChanged flag may have been false for a long time after the

debugger/loop_playmode.go
@@ -1,0 +1,71 @@
+// This file is part of Gopher2600.
+//
+// Gopher2600 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gopher2600 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
+
+package debugger
+
+import (
+	"github.com/jetsetilly/gopher2600/emulation"
+	"github.com/jetsetilly/gopher2600/gui"
+	"github.com/jetsetilly/gopher2600/hardware"
+)
+
+func (dbg *Debugger) playLoop() error {
+	// run and handle events
+	return dbg.vcs.Run(func() (emulation.State, error) {
+		select {
+		case <-dbg.eventCheckPulse.C:
+			err := dbg.readEventsHandler()
+			if err != nil {
+				return emulation.Ending, nil
+			}
+		default:
+		}
+		if dbg.state.Load().(emulation.State) == emulation.Running {
+			dbg.Rewind.RecordFrameState()
+		}
+
+		state := dbg.State()
+		if dbg.mode != emulation.ModePlay {
+			state = emulation.Ending
+		}
+
+		return state, nil
+	}, hardware.PerformanceBrake)
+}
+
+func (dbg *Debugger) playmodeRewind(amount int) {
+	coords := dbg.vcs.TV.GetCoords()
+	tl := dbg.Rewind.GetTimeline()
+
+	if amount < 0 && coords.Frame-1 <= tl.AvailableStart {
+		dbg.setState(emulation.Paused)
+		return
+	}
+	if amount > 0 && coords.Frame+1 >= tl.AvailableEnd {
+		dbg.setStateQuiet(emulation.Paused, true)
+		dbg.gui.SetFeature(gui.ReqEmulationEvent, emulation.EventRewindAtEnd)
+		return
+	}
+
+	dbg.setStateQuiet(emulation.Rewinding, true)
+	dbg.Rewind.GotoFrame(coords.Frame + amount)
+	dbg.setStateQuiet(emulation.Paused, true)
+
+	if amount < 0 {
+		dbg.gui.SetFeature(gui.ReqEmulationEvent, emulation.EventRewindBack)
+	} else {
+		dbg.gui.SetFeature(gui.ReqEmulationEvent, emulation.EventRewindFoward)
+	}
+}

debugger/requests.go
@@ -1,0 +1,69 @@
+// This file is part of Gopher2600.
+//
+// Gopher2600 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gopher2600 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
+
+package debugger
+
+import (
+	"github.com/jetsetilly/gopher2600/curated"
+	"github.com/jetsetilly/gopher2600/emulation"
+	"github.com/jetsetilly/gopher2600/logger"
+)
+
+func argLen(args []emulation.FeatureReqData, expectedLen int) error {
+	if len(args) != expectedLen {
+		return curated.Errorf("wrong number of arguments (%d instead of %d)", len(args), expectedLen)
+	}
+	return nil
+}
+
+// ReqFeature implements the emulation.Emulation interface.
+func (dbg *Debugger) SetFeature(request emulation.FeatureReq, args ...emulation.FeatureReqData) error {
+	var err error
+
+	switch request {
+	case emulation.ReqSetPause:
+		err = argLen(args, 1)
+		if err == nil {
+			switch dbg.mode {
+			case emulation.ModePlay:
+				dbg.PushRawEvent(func() {
+					// Pause implements the emulation.Emulation interface.
+					if args[0].(bool) {
+						dbg.setState(emulation.Paused)
+					} else {
+						dbg.setState(emulation.Running)
+					}
+				})
+			case emulation.ModeDebugger:
+				err = curated.Errorf("not reacting to %s in debug mode (use terminal input instead)", request)
+			}
+		}
+
+	case emulation.ReqSetMode:
+		err = argLen(args, 1)
+		if err == nil {
+			dbg.PushRawEventReturn(func() {
+				err := dbg.setMode(args[0].(emulation.Mode))
+				if err != nil {
+					logger.Logf("debugger", err.Error())
+				}
+			})
+		}
+
+	default:
+	}
+
+	return curated.Errorf("debugger: %v", err)
+}

commit 68b35ac5e27e51bbd7708007aa1e0b54b13247dc

fix bug introduced in 92d96cc4e965ec6a85036dfd8e2db0bcfd078e24
reflection resetting on NewFrame() *before* NewFrame() in rewind has
completed. rewind timeline relies on reflection

I managed to ignore a comment stipulating this requirement during the
refactor

debugger/debugger.go
@@ -400,12 +400,14 @@ func (dbg *Debugger) setMode(mode emulation.Mode) error {
 		dbg.vcs.TV.AddFrameTrigger(dbg.Rewind)
 		dbg.setState(emulation.Running)
 	case emulation.ModeDebugger:
-		// frame triggers. deliberately adding rewind before reflection because the
-		// latter implements rewind.TimelineCounter. we want to reset
-		// TimelineCounts in reflection.Gatherer  but we need to call
-		// TimelineCounts() from the rewind packge before that happens
-		dbg.vcs.TV.AddFrameTrigger(dbg.ref)
+		// frame triggers.
+		//
+		// deliberately adding rewind before reflection because the latter
+		// implements rewind.TimelineCounter. we want to reset TimelineCounts
+		// in reflection.Gatherer but we need to call TimelineCounts() from the
+		// rewind packge before that happens
 		dbg.vcs.TV.AddFrameTrigger(dbg.Rewind)
+		dbg.vcs.TV.AddFrameTrigger(dbg.ref)
 		dbg.setState(emulation.Paused)
 
 		// debugger needs knowledge about previous frames (via the reflector)