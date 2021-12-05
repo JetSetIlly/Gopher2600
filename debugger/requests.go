b86af325e4f90956fb7d43dd2bda623212742627
JetSetIlly <stephen.t.illingworth@gmail.com>
2021-12-05 20:16:36 +0000

    debugger SetFeature() sometimes returned a wrapped nil error
    this caused erroneous log entries

--- debugger/requests.go
+++ debugger/requests.go
@@ -35,35 +35,39 @@ func (dbg *Debugger) SetFeature(request emulation.FeatureReq, args ...emulation.
 	switch request {
 	case emulation.ReqSetPause:
 		err = argLen(args, 1)
-		if err == nil {
-			switch dbg.Mode() {
-			case emulation.ModePlay:
-				dbg.PushRawEvent(func() {
-					// Pause implements the emulation.Emulation interface.
-					if args[0].(bool) {
-						dbg.setState(emulation.Paused)
-					} else {
-						dbg.setState(emulation.Running)
-					}
-				})
-			case emulation.ModeDebugger:
-				err = curated.Errorf("not reacting to %s in debug mode (use terminal input instead)", request)
-			}
+		if err != nil {
+			return curated.Errorf("debugger: %v", err)
+		}
+
+		switch dbg.Mode() {
+		case emulation.ModePlay:
+			dbg.PushRawEvent(func() {
+				// Pause implements the emulation.Emulation interface.
+				if args[0].(bool) {
+					dbg.setState(emulation.Paused)
+				} else {
+					dbg.setState(emulation.Running)
+				}
+			})
+		case emulation.ModeDebugger:
+			err = curated.Errorf("not reacting to %s in debug mode (use terminal input instead)", request)
 		}
 
 	case emulation.ReqSetMode:
 		err = argLen(args, 1)
-		if err == nil {
-			dbg.PushRawEventReturn(func() {
-				err := dbg.setMode(args[0].(emulation.Mode))
-				if err != nil {
-					logger.Logf("debugger", err.Error())
-				}
-			})
+		if err != nil {
+			return curated.Errorf("debugger: %v", err)
 		}
+
+		dbg.PushRawEventReturn(func() {
+			err := dbg.setMode(args[0].(emulation.Mode))
+			if err != nil {
+				logger.Logf("debugger", err.Error())
+			}
+		})
 
 	default:
 	}
 
-	return curated.Errorf("debugger: %v", err)
+	return nil
 }
