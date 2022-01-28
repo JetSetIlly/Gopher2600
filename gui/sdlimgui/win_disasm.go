JetSetIlly/Gopher2600

commit 180b1200dd55132f58f2bda7b74f35143c9e20bd

fixed bug in disassembly.blessSequence() generation
slight change in how disasm window focuses on an address. the previous
belt-and-braces method would cause the scroller to "bounce" if the
scroller was at the bottom of its limit. removed the call to
SetScrollHereY() and will now rely on the calculated offset method

gui/sdlimgui/win_disasm.go
@@ -233,7 +233,7 @@ func (win *winDisasm) drawControlBar() {
 				win.selectedBank = n
 			}
 
-			// set scroll on first frame combo is open
+			// set scroll on the first frame that the combo is open
 			if !win.selectedBankComboOpen && n == win.selectedBank {
 				imgui.SetScrollHereY(0.0)
 			}
@@ -495,9 +495,11 @@ func (win *winDisasm) drawBank(focusAddr uint16) {
 			}
 		}()
 
-		// scroll to correct entry. this is in addition to the automated
-		// scrolling we do in drawEntry(). both are required as a consequence
-		// of how ListClipper works
+		// scroll to correct entry. we do this rather than a SetScrollHereY()
+		// call in drawEntry() because we may need to focus on an address that
+		// hasn't been drawn - ListClipper will only draw the entries that are
+		// currently visible and by it's nature, focusOnAddr will want to work
+		// with entries that may not be visible
 		if onBank && win.focusOnAddr && focusCtApply {
 			y := imgui.FontSize() + imgui.CurrentStyle().ItemInnerSpacing().Y
 
@@ -561,14 +563,6 @@ func (win *winDisasm) drawEntry(e *disassembly.Entry, focusAddr uint16, onBank b
 	// highligh current PC entry
 	if onBank && (e.Result.Address&memorymap.CartridgeBits == focusAddr) {
 		imgui.TableSetBgColor(imgui.TableBgTargetRowBg0, win.img.cols.DisasmStep)
-
-		// scroll to this entry if required. this is in addition to the
-		// calculated scrolling we do in the drawBank() loop. both are required
-		// as a consequence of how ListClipper works
-		if win.focusOnAddr {
-			imgui.SetScrollHereY(0.20)
-			win.focusOnAddr = false
-		}
 	}
 
 	// does this entry/address have a PC break applied to it

disassembly/decode.go
@@ -55,7 +55,7 @@ func (dsm *Disassembly) bless(mc *cpu.CPU, mem *disasmMemory) error {
 		// make sure reset address is valid
 		_, area := memorymap.MapAddress(resetAddr, true)
 		if area == memorymap.Cartridge {
-			_ = dsm.blessSequence(mem.startingBank, resetAddr, true)
+			_ = dsm.blessSequence(b, resetAddr, true)
 		}
 	}
 

commit ef3d28a8a77710a2d7e89d40122480ac7dcf9427

fixed disasm focus bug introduced in 180b120

gui/sdlimgui/win_disasm.go
@@ -437,7 +437,7 @@ func (win *winDisasm) drawBank(focusAddr uint16) {
 					ct++
 				}
 
-				if onBank && e.Result.Address&memorymap.CartridgeBits == focusAddr {
+				if win.focusOnAddr && onBank && e.Result.Address&memorymap.CartridgeBits == focusAddr {
 					focusCt = ct
 					focusCtApply = true
 				}
@@ -521,6 +521,10 @@ func (win *winDisasm) drawBank(focusAddr uint16) {
 
 			y = float32(focusCt-focusGap) * y
 			imgui.SetScrollY(y)
+
+			// address has been focused so we turn off the focus flag - it will
+			// be set again if necessary
+			win.focusOnAddr = false
 		}
 
 		imgui.EndTable()
@@ -575,6 +579,9 @@ func (win *winDisasm) drawEntry(e *disassembly.Entry, focusAddr uint16, onBank b
 	// highligh current PC entry
 	if onBank && (e.Result.Address&memorymap.CartridgeBits == focusAddr) {
 		imgui.TableSetBgColor(imgui.TableBgTargetRowBg0, win.img.cols.DisasmStep)
+
+		// focussed entry has been drawn so unset focus flag
+		win.focusOnAddr = false
 	}
 
 	// does this entry/address have a PC break applied to it