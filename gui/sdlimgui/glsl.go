JetSetIlly/Gopher2600

commit 54517978ee79f9024601936de6487a43eed111bd

crtsequencer run for dbg screen. scaling shader applied to dbg screen

gui/sdlimgui/glsl.go
@@ -176,7 +176,7 @@ func (rnd *glsl) render() {
 					shader = rnd.shaders[dbgscrShaderID]
 				case rnd.img.wm.dbgScr.overlayTexture:
 					shader = rnd.shaders[overlayShaderID]
-				case rnd.img.playScr.screenTexture:
+				case rnd.img.playScr.scaledTexture:
 					shader = rnd.shaders[playscrShaderID]
 				case rnd.img.wm.windows[winSelectROMID].(*winSelectROM).thmbTexture:
 					shader = rnd.shaders[colorShaderID]

gui/sdlimgui/glsl_dbgscr.go
@@ -26,7 +26,6 @@ type dbgScreenShader struct {
 	shader
 
 	img *SdlImgui
-
 	crt *crtSequencer
 
 	showCursor             int32 // uniform
@@ -121,14 +120,14 @@ func (sh *dbgScreenShader) setAttributes(env shaderEnvironment) {
 		// something for the future.
 		env.srcTextureID = sh.img.wm.dbgScr.normalTexture
 
-		env.srcTextureID = sh.crt.process(env, true, sh.img.wm.dbgScr.numScanlines, specification.ClksVisible)
+		env.srcTextureID = sh.crt.process(env, true, true, sh.img.wm.dbgScr.numScanlines, specification.ClksVisible, sh.img.wm.dbgScr)
 	} else {
 		// if crtPreview is disabled we still go through the crt process. we do
 		// this so that the phosphor is kept up to date, which is important
 		// for the moment the crtPreview is enabled.
 		//
 		// we don't do anything with the result of the process in this instance
-		_ = sh.crt.process(env, false, sh.img.wm.dbgScr.numScanlines, specification.ClksVisible)
+		env.srcTextureID = sh.crt.process(env, true, false, sh.img.wm.dbgScr.numScanlines, specification.ClksVisible, sh.img.wm.dbgScr)
 	}
 
 	sh.shader.setAttributes(env)

gui/sdlimgui/win_dbgscr.go
@@ -47,6 +47,10 @@ type winDbgScr struct {
 	normalTexture   uint32
 	elementsTexture uint32
 	overlayTexture  uint32
+	unscaledTexture uint32
+
+	// the pixels we use to clear normalTexture with
+	emptyScaledPixels []uint8
 
 	// how to present the screen in the window
 	elements bool
@@ -82,10 +86,12 @@ type winDbgScr struct {
 	screenOrigin imgui.Vec2
 
 	// scaling of texture and calculated dimensions
-	xscaling     float32
-	yscaling     float32
-	scaledWidth  float32
-	scaledHeight float32
+	xscaling       float32
+	yscaling       float32
+	scaledWidth    float32
+	scaledHeight   float32
+	unscaledWidth  float32
+	unscaledHeight float32
 
 	// the dimensions required for the combo widgets
 	specComboDim    imgui.Vec2
@@ -122,6 +128,13 @@ func newWinDbgScr(img *SdlImgui) (window, error) {
 	gl.BindTexture(gl.TEXTURE_2D, win.overlayTexture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+
+	gl.GenTextures(1, &win.unscaledTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT)
 
 	return win, nil
 }
@@ -640,11 +653,14 @@ func (win *winDbgScr) render() {
 	defer gl.PixelStorei(gl.UNPACK_ROW_LENGTH, 0)
 
 	if win.createTextures {
+		// empty pixels for screen texture
+		win.emptyScaledPixels = make([]uint8, int(win.scaledWidth)*int(win.scaledHeight)*4)
+
 		gl.BindTexture(gl.TEXTURE_2D, win.normalTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
 			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(pixels.Pix))
+			gl.Ptr(win.emptyScaledPixels))
 
 		gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
@@ -658,13 +674,20 @@ func (win *winDbgScr) render() {
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(overlay.Pix))
 
+		// unscaled screen texture
+		gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+		gl.TexImage2D(gl.TEXTURE_2D, 0,
+			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
+			gl.RGBA, gl.UNSIGNED_BYTE,
+			gl.Ptr(pixels.Pix))
+
 		win.createTextures = false
 	} else {
 		gl.BindTexture(gl.TEXTURE_2D, win.normalTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(pixels.Pix))
+			gl.Ptr(win.emptyScaledPixels))
 
 		gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
@@ -677,6 +700,13 @@ func (win *winDbgScr) render() {
 			0, 0, int32(overlay.Bounds().Size().X), int32(overlay.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(overlay.Pix))
+
+		// unscaled screen texture
+		gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
+			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),
+			gl.RGBA, gl.UNSIGNED_BYTE,
+			gl.Ptr(pixels.Pix))
 	}
 }
 
@@ -721,7 +751,19 @@ func (win *winDbgScr) setScaling() {
 	win.xscaling = scaling * pixelWidth * win.scr.crit.frameInfo.Spec.AspectBias
 	win.scaledWidth = w * win.xscaling
 	win.scaledHeight = h * win.yscaling
+	win.unscaledWidth = w
+	win.unscaledHeight = h
 
 	// get numscanlines while we're in critical section
 	win.numScanlines = win.scr.crit.frameInfo.VisibleBottom - win.scr.crit.frameInfo.VisibleTop
+}
+
+// unscaledTextureSpec implements the scalingImage specification
+func (win *winDbgScr) unscaledTextureSpec() (uint32, float32, float32) {
+	return win.unscaledTexture, win.unscaledWidth, win.unscaledHeight
+}
+
+// unscaledTextureSpec implements the scalingImage specification
+func (win *winDbgScr) scaledTextureSpec() (uint32, float32, float32) {
+	return win.normalTexture, win.scaledWidth, win.scaledHeight
 }

gui/sdlimgui/glsl_crtseq.go
@@ -20,8 +20,8 @@ import (
 )
 
 type crtSequencer struct {
-	seq                   *framebuffer.Sequence
 	img                   *SdlImgui
+	seq                   *framebuffer.Sequence
 	scalingShader         shaderProgram
 	phosphorShader        shaderProgram
 	blackCorrectionShader shaderProgram
@@ -71,7 +71,14 @@ func (sh *crtSequencer) destroy() {
 //
 // returns the last textureID drawn to as part of the process(). the texture
 // returned depends on the value of moreProcessing.
-func (sh *crtSequencer) process(env shaderEnvironment, moreProcessing bool, numScanlines int, numClocks int) uint32 {
+//
+// if effectsEnabled is turned off then phosphor accumulation and scaling still
+// occurs but crt effects are not applied.
+func (sh *crtSequencer) process(env shaderEnvironment,
+	moreProcessing bool, effectsEnabled bool,
+	numScanlines int, numClocks int,
+	scalingImage scalingImage) uint32 {
+
 	const (
 		// an accumulation of consecutive frames producing a phosphor effect
 		phosphor = iota
@@ -94,9 +101,6 @@ func (sh *crtSequencer) process(env shaderEnvironment, moreProcessing bool, numS
 	// we'll be chaining many shaders together so use internal projection
 	env.useInternalProj = true
 
-	// whether crt effects are enabled
-	enabled := sh.img.crtPrefs.Enabled.Get().(bool)
-
 	// phosphor draw
 	phosphorPasses := 1
 
@@ -107,13 +111,15 @@ func (sh *crtSequencer) process(env shaderEnvironment, moreProcessing bool, numS
 	}
 
 	// scale image
-	env.srcTextureID = sh.seq.Process(processedSrc, func() {
-		sh.scalingShader.(*scalingShader).setAttributesArgs(env, sh.img.playScr)
-		env.draw()
-	})
+	if scalingImage != nil {
+		env.srcTextureID = sh.seq.Process(processedSrc, func() {
+			sh.scalingShader.(*scalingShader).setAttributesArgs(env, scalingImage)
+			env.draw()
+		})
+	}
 
 	// apply ghosting filter to texture. this is useful for the zookeeper brick effect
-	if enabled && sh.img.crtPrefs.Ghosting.Get().(bool) {
+	if effectsEnabled && sh.img.crtPrefs.Ghosting.Get().(bool) {
 		env.srcTextureID = sh.seq.Process(processedSrc, func() {
 			sh.ghostingShader.(*ghostingShader).setAttributesArgs(env, float32(sh.img.crtPrefs.GhostingAmount.Get().(float64)))
 			env.draw()
@@ -122,7 +128,7 @@ func (sh *crtSequencer) process(env shaderEnvironment, moreProcessing bool, numS
 	src := env.srcTextureID
 
 	for i := 0; i < phosphorPasses; i++ {
-		if enabled {
+		if effectsEnabled {
 			if sh.img.crtPrefs.Phosphor.Get().(bool) {
 				// use blur shader to add bloom to previous phosphor
 				env.srcTextureID = sh.seq.Process(phosphor, func() {
@@ -150,7 +156,7 @@ func (sh *crtSequencer) process(env shaderEnvironment, moreProcessing bool, numS
 		}
 	}
 
-	if enabled {
+	if effectsEnabled {
 		// video-black correction
 		env.srcTextureID = sh.seq.Process(working, func() {
 			sh.blackCorrectionShader.(*blackCorrectionShader).setAttributesArgs(env, float32(sh.img.crtPrefs.BlackLevel.Get().(float64)))

gui/sdlimgui/glsl_playscr.go
@@ -66,5 +66,6 @@ func (sh *playscrShader) setAttributes(env shaderEnvironment) {
 	)
 
 	sh.screenshot.process(env)
-	sh.crt.process(env, false, sh.img.playScr.visibleScanlines, specification.ClksVisible)
+	effectEnabled := sh.img.crtPrefs.Enabled.Get().(bool)
+	sh.crt.process(env, false, effectEnabled, sh.img.playScr.visibleScanlines, specification.ClksVisible, sh.img.playScr)
 }

gui/sdlimgui/glsl_shaders.go
@@ -396,15 +396,23 @@ func newScalingShader() shaderProgram {
 	return sh
 }
 
+type scalingImage interface {
+	unscaledTextureSpec() (uint32, float32, float32)
+	scaledTextureSpec() (uint32, float32, float32)
+}
+
 // nolint: unparam
-func (sh *scalingShader) setAttributesArgs(env shaderEnvironment, win *playScr) {
+func (sh *scalingShader) setAttributesArgs(env shaderEnvironment, scalingImage scalingImage) {
+	ut, uw, uh := scalingImage.unscaledTextureSpec()
+	_, w, h := scalingImage.scaledTextureSpec()
+
 	sh.shader.setAttributes(env)
-	gl.Uniform1f(sh.unscaledWidth, win.unscaledWidth)
-	gl.Uniform1f(sh.unscaledHeight, win.unscaledHeight)
-	gl.Uniform1f(sh.scaledWidth, win.scaledWidth)
-	gl.Uniform1f(sh.scaledHeight, win.scaledHeight)
+	gl.Uniform1f(sh.unscaledWidth, uw)
+	gl.Uniform1f(sh.unscaledHeight, uh)
+	gl.Uniform1f(sh.scaledWidth, w)
+	gl.Uniform1f(sh.scaledHeight, h)
 
 	gl.ActiveTexture(gl.TEXTURE1)
-	gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+	gl.BindTexture(gl.TEXTURE_2D, ut)
 	gl.Uniform1i(sh.unscaledTexture, 1)
 }

gui/sdlimgui/playscr.go
@@ -36,10 +36,13 @@ type playScr struct {
 	// (re)create textures on next render()
 	createTextures bool
 
-	// textures
-	screenTexture   uint32
-	emptyPixels     []uint8
+	// textures. scaledTexture is the presentation texture. unscaledTexture is
+	// the unscaled pixels from screen that are passed through the scaling shader
+	scaledTexture   uint32
 	unscaledTexture uint32
+
+	// the pixels we use to clear scaledTexture with
+	emptyScaledPixels []uint8
 
 	// the tv screen has captured mouse input
 	isCaptured bool
@@ -86,8 +89,8 @@ func newPlayScr(img *SdlImgui) *playScr {
 	}
 
 	// set texture, creation of textures will be done after every call to resize()
-	gl.GenTextures(1, &win.screenTexture)
-	gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
+	gl.GenTextures(1, &win.scaledTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.scaledTexture)
 
 	// mag and min changed in setScaling() according to whether we want pixel
 	// perfect rendering
@@ -122,7 +125,7 @@ func (win *playScr) draw() {
 	defer win.img.screen.crit.section.Unlock()
 
 	dl := imgui.BackgroundDrawList()
-	dl.AddImage(imgui.TextureID(win.screenTexture), win.imagePosMin, win.imagePosMax)
+	dl.AddImage(imgui.TextureID(win.scaledTexture), win.imagePosMin, win.imagePosMax)
 
 	win.peripheralLeft.draw(win)
 	win.peripheralRight.draw(win)
@@ -209,14 +212,14 @@ func (win *playScr) render() {
 		// (re)create textures
 
 		// empty pixels for screen texture
-		win.emptyPixels = make([]uint8, int(win.scaledWidth)*int(win.scaledHeight)*4)
+		win.emptyScaledPixels = make([]uint8, int(win.scaledWidth)*int(win.scaledHeight)*4)
 
 		// screen texture
-		gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
+		gl.BindTexture(gl.TEXTURE_2D, win.scaledTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
 			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(win.emptyPixels))
+			gl.Ptr(win.emptyScaledPixels))
 
 		// unscaled screen texture
 		gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
@@ -231,11 +234,11 @@ func (win *playScr) render() {
 		// is "unstable"
 
 		// screen texture
-		gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
+		gl.BindTexture(gl.TEXTURE_2D, win.scaledTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(win.emptyPixels))
+			gl.Ptr(win.emptyScaledPixels))
 
 		// unscaled screen texture
 		gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
@@ -295,4 +298,14 @@ func (win *playScr) setScaling() {
 
 	// get visibleScanlines while we're in critical section
 	win.visibleScanlines = win.scr.crit.frameInfo.VisibleBottom - win.scr.crit.frameInfo.VisibleTop
+}
+
+// unscaledTextureSpec implements the scalingImage specification
+func (win *playScr) unscaledTextureSpec() (uint32, float32, float32) {
+	return win.unscaledTexture, win.unscaledWidth, win.unscaledHeight
+}
+
+// unscaledTextureSpec implements the scalingImage specification
+func (win *playScr) scaledTextureSpec() (uint32, float32, float32) {
+	return win.scaledTexture, win.scaledWidth, win.scaledHeight
 }

commit 05818a0973081edfcd0f8dc8b7abfec737d3d3b6

fixed debug colours option in debugger
broken in commit 54517978ee79f9024601936de6487a43eed111bd

gui/sdlimgui/glsl.go
@@ -170,7 +170,7 @@ func (rnd *glsl) render() {
 				var shader shaderProgram
 
 				switch env.srcTextureID {
-				case rnd.img.wm.dbgScr.normalTexture:
+				case rnd.img.wm.dbgScr.displayTexture:
 					shader = rnd.shaders[dbgscrShaderID]
 				case rnd.img.wm.dbgScr.elementsTexture:
 					shader = rnd.shaders[dbgscrShaderID]

gui/sdlimgui/glsl_dbgscr.go
@@ -118,7 +118,7 @@ func (sh *dbgScreenShader) setAttributes(env shaderEnvironment) {
 		//
 		// both alternative solutions seem baroque for a single use case. maybe
 		// something for the future.
-		env.srcTextureID = sh.img.wm.dbgScr.normalTexture
+		env.srcTextureID = sh.img.wm.dbgScr.displayTexture
 
 		env.srcTextureID = sh.crt.process(env, true, true, false, sh.img.wm.dbgScr.numScanlines, specification.ClksVisible, sh.img.wm.dbgScr)
 	} else {

gui/sdlimgui/win_dbgscr.go
@@ -44,10 +44,10 @@ type winDbgScr struct {
 	createTextures bool
 
 	// textures
-	normalTexture   uint32
+	displayTexture  uint32
 	elementsTexture uint32
 	overlayTexture  uint32
-	unscaledTexture uint32
+	screenTexture   uint32
 
 	// the pixels we use to clear normalTexture with
 	emptyScaledPixels []uint8
@@ -114,13 +114,8 @@ func newWinDbgScr(img *SdlImgui) (window, error) {
 	}
 
 	// set texture, creation of textures will be done after every call to resize()
-	gl.GenTextures(1, &win.normalTexture)
-	gl.BindTexture(gl.TEXTURE_2D, win.normalTexture)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-
-	gl.GenTextures(1, &win.elementsTexture)
-	gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
+	gl.GenTextures(1, &win.displayTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.displayTexture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 
@@ -129,8 +124,15 @@ func newWinDbgScr(img *SdlImgui) (window, error) {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 
-	gl.GenTextures(1, &win.unscaledTexture)
-	gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+	gl.GenTextures(1, &win.screenTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT)
+
+	gl.GenTextures(1, &win.elementsTexture)
+	gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT)
@@ -230,20 +232,10 @@ func (win *winDbgScr) draw() {
 	imgui.PushStyleColor(imgui.StyleColorButtonHovered, win.img.cols.Transparent)
 	imgui.PushStyleVarVec2(imgui.StyleVarFramePadding, imgui.Vec2{0.0, 0.0})
 
-	imageHovered := false
+	imgui.ImageButton(imgui.TextureID(win.displayTexture), imgui.Vec2{win.scaledWidth, win.scaledHeight})
+	imageHovered := imgui.IsItemHovered()
 
-	if win.crtPreview {
-		imgui.ImageButton(imgui.TextureID(win.normalTexture), imgui.Vec2{win.scaledWidth, win.scaledHeight})
-		imageHovered = imgui.IsItemHovered()
-	} else {
-		// choose which texture to use depending on whether elements is selected
-		if win.elements {
-			imgui.ImageButton(imgui.TextureID(win.elementsTexture), imgui.Vec2{win.scaledWidth, win.scaledHeight})
-		} else {
-			imgui.ImageButton(imgui.TextureID(win.normalTexture), imgui.Vec2{win.scaledWidth, win.scaledHeight})
-		}
-		imageHovered = imgui.IsItemHovered()
-
+	if !win.crtPreview {
 		// overlay texture on top of screen texture
 		imgui.SetCursorScreenPos(win.screenOrigin)
 		imgui.ImageButton(imgui.TextureID(win.overlayTexture), imgui.Vec2{win.scaledWidth, win.scaledHeight})
@@ -656,17 +648,11 @@ func (win *winDbgScr) render() {
 		// empty pixels for screen texture
 		win.emptyScaledPixels = make([]uint8, int(win.scaledWidth)*int(win.scaledHeight)*4)
 
-		gl.BindTexture(gl.TEXTURE_2D, win.normalTexture)
+		gl.BindTexture(gl.TEXTURE_2D, win.displayTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
 			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(win.emptyScaledPixels))
-
-		gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
-		gl.TexImage2D(gl.TEXTURE_2D, 0,
-			gl.RGBA, int32(elements.Bounds().Size().X), int32(elements.Bounds().Size().Y), 0,
-			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(elements.Pix))
 
 		gl.BindTexture(gl.TEXTURE_2D, win.overlayTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
@@ -675,25 +661,25 @@ func (win *winDbgScr) render() {
 			gl.Ptr(overlay.Pix))
 
 		// unscaled screen texture
-		gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+		gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
 		gl.TexImage2D(gl.TEXTURE_2D, 0,
 			gl.RGBA, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y), 0,
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(pixels.Pix))
 
+		gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
+		gl.TexImage2D(gl.TEXTURE_2D, 0,
+			gl.RGBA, int32(elements.Bounds().Size().X), int32(elements.Bounds().Size().Y), 0,
+			gl.RGBA, gl.UNSIGNED_BYTE,
+			gl.Ptr(elements.Pix))
+
 		win.createTextures = false
 	} else {
-		gl.BindTexture(gl.TEXTURE_2D, win.normalTexture)
+		gl.BindTexture(gl.TEXTURE_2D, win.displayTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(win.emptyScaledPixels))
-
-		gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
-		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
-			0, 0, int32(elements.Bounds().Size().X), int32(elements.Bounds().Size().Y),
-			gl.RGBA, gl.UNSIGNED_BYTE,
-			gl.Ptr(elements.Pix))
 
 		gl.BindTexture(gl.TEXTURE_2D, win.overlayTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
@@ -702,11 +688,17 @@ func (win *winDbgScr) render() {
 			gl.Ptr(overlay.Pix))
 
 		// unscaled screen texture
-		gl.BindTexture(gl.TEXTURE_2D, win.unscaledTexture)
+		gl.BindTexture(gl.TEXTURE_2D, win.screenTexture)
 		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
 			0, 0, int32(pixels.Bounds().Size().X), int32(pixels.Bounds().Size().Y),
 			gl.RGBA, gl.UNSIGNED_BYTE,
 			gl.Ptr(pixels.Pix))
+
+		gl.BindTexture(gl.TEXTURE_2D, win.elementsTexture)
+		gl.TexSubImage2D(gl.TEXTURE_2D, 0,
+			0, 0, int32(elements.Bounds().Size().X), int32(elements.Bounds().Size().Y),
+			gl.RGBA, gl.UNSIGNED_BYTE,
+			gl.Ptr(elements.Pix))
 	}
 }
 
@@ -760,10 +752,13 @@ func (win *winDbgScr) setScaling() {
 
 // unscaledTextureSpec implements the scalingImage specification
 func (win *winDbgScr) unscaledTextureSpec() (uint32, float32, float32) {
-	return win.unscaledTexture, win.unscaledWidth, win.unscaledHeight
+	if win.elements && !win.crtPreview {
+		return win.elementsTexture, win.unscaledWidth, win.unscaledHeight
+	}
+	return win.screenTexture, win.unscaledWidth, win.unscaledHeight
 }
 
 // unscaledTextureSpec implements the scalingImage specification
 func (win *winDbgScr) scaledTextureSpec() (uint32, float32, float32) {
-	return win.normalTexture, win.scaledWidth, win.scaledHeight
+	return win.displayTexture, win.scaledWidth, win.scaledHeight
 }