JetSetIlly/Gopher2600

commit a09082a777a8f3ebdb95b1124d067f93d53b1e08

coprocessor disasm inhibited for duration of rewinding
the coproc disasm window shows an appropriate message in the Last
Execution tab immediately after a rewind. this isn't ideal but because
the utility of this disasm window is currently limited the message will
suffice for now

coprocessor/disassembly/disassembly.go
@@ -76,6 +76,21 @@ func NewDisassembly(tv TV, cart mapper.CartCoProc) *Disassembly {
 	return dsm
 }
 
+// Inhibit should be used to temporarily disable disassembly functionality.
+// IsEnabled() will still return true as appropriate regardless of inhibit
+// state.
+func (dsm *Disassembly) Inhibit(inhibit bool) {
+	dsm.crit.Lock()
+	defer dsm.crit.Unlock()
+
+	if inhibit {
+		dsm.cart.SetDisassembler(nil)
+		dsm.disasm.LastExecution = dsm.disasm.LastExecution[:0]
+	} else {
+		dsm.cart.SetDisassembler(dsm)
+	}
+}
+
 // IsEnabled returns true if coprocessor disassembly is currently active.
 func (dsm *Disassembly) IsEnabled() bool {
 	dsm.crit.Lock()
@@ -91,6 +106,7 @@ func (dsm *Disassembly) Enable(enable bool) {
 	defer dsm.crit.Unlock()
 
 	dsm.disasm.Enabled = enable
+
 	if dsm.disasm.Enabled {
 		dsm.cart.SetDisassembler(dsm)
 	} else {

debugger/debugger.go
@@ -511,6 +511,12 @@ func (dbg *Debugger) setStateQuiet(state govern.State, quiet bool) {
 		if dbg.CoProcDev != nil {
 			dbg.CoProcDev.Disable(true)
 		}
+
+		// coprocessor disassembly is an inherently slow operation particuarly
+		// for StrongARM type ROMs
+		if dbg.CoProcDisasm != nil {
+			dbg.CoProcDisasm.Inhibit(true)
+		}
 	} else {
 		dbg.vcs.Mem.Cart.BreakpointsDisable(false)
 
@@ -521,6 +527,11 @@ func (dbg *Debugger) setStateQuiet(state govern.State, quiet bool) {
 		// will already have been enabled
 		if dbg.CoProcDev != nil {
 			dbg.CoProcDev.Disable(false)
+		}
+
+		// uninhibit coprocessor disassembly
+		if dbg.CoProcDisasm != nil {
+			dbg.CoProcDisasm.Inhibit(false)
 		}
 	}
 

debugger/loop_catchup.go
@@ -65,6 +65,9 @@ func (dbg *Debugger) CatchUpLoop(tgt coords.TelevisionCoords) error {
 			dbg.CoProcDev.Disable(false)
 		}
 
+		// we are *not* uninhibiting coprocessor disassembly. the performance
+		// loss is still too great to do so
+
 		dbg.catchupEnd = func() {
 			dbg.vcs.TV.SetFPSCap(fpsCap)
 			dbg.catchupContinue = nil

gui/sdlimgui/win_coproc_disasm.go
@@ -101,7 +101,12 @@ func (win *winCoProcDisasm) draw() {
 				}
 				if imgui.BeginTabItem("Last Execution") {
 					win.optionsLastExecution = true
-					win.drawDisasm(dsm, true)
+					if len(dsm.LastExecution) > 0 {
+						win.drawDisasm(dsm, true)
+					} else {
+						imgui.Spacing()
+						imgui.Text("No recent disassembly available.")
+					}
 					imgui.EndTabItem()
 				}
 				imgui.EndTabBar()
@@ -138,13 +143,18 @@ func (win *winCoProcDisasm) draw() {
 						imgui.SameLineV(0, 10)
 						imgui.Text("Executed in Immediate Mode")
 					} else {
-						imgui.SameLineV(0, 10)
-						imgui.Text(fmt.Sprintf("Total Cycles % 8d", summary.I+summary.N+summary.S))
-						imguiTooltip(func() {
-							imgui.Text(fmt.Sprintf("N cycles: % 8d", summary.N))
-							imgui.Text(fmt.Sprintf("I cycles: % 8d", summary.I))
-							imgui.Text(fmt.Sprintf("S cycles: % 8d", summary.S))
-						}, true)
+						if len(dsm.LastExecution) > 0 {
+							imgui.SameLineV(0, 10)
+							imgui.Text(fmt.Sprintf("Total Cycles % 8d", summary.I+summary.N+summary.S))
+							imguiTooltip(func() {
+								imgui.Text(fmt.Sprintf("N cycles: % 8d", summary.N))
+								imgui.Text(fmt.Sprintf("I cycles: % 8d", summary.I))
+								imgui.Text(fmt.Sprintf("S cycles: % 8d", summary.S))
+							}, true)
+						} else {
+							imgui.SameLineV(0, 10)
+							imgui.Text("Recent execution unavailable")
+						}
 					}
 				}
 			}
@@ -191,6 +201,7 @@ func (win *winCoProcDisasm) drawDisasm(dsm *disassembly.DisasmEntries, lastExecu
 
 	win.img.dbg.CoProcDev.BorrowSource(func(src *developer.Source) {
 		if lastExecution {
+			imgui.Text("State of execution has recently changed. Last execution details currently unavailable.")
 			clipper.Begin(len(dsm.LastExecution))
 			for clipper.Step() {
 				for i := clipper.DisplayStart; i < clipper.DisplayEnd; i++ {

commit 41d1424ee0b15a1b1848a4b4dae1e70fe74377e9

coproc disassembly Inhibit() re-enables disasm more carefully
bug introduced in a09082a777a. uninhibiting disassembly caused
disassembly to be enabled in all circumstances

coprocessor/disassembly/disassembly.go
@@ -87,7 +87,9 @@ func (dsm *Disassembly) Inhibit(inhibit bool) {
 		dsm.cart.SetDisassembler(nil)
 		dsm.disasm.LastExecution = dsm.disasm.LastExecution[:0]
 	} else {
-		dsm.cart.SetDisassembler(dsm)
+		if dsm.disasm.Enabled {
+			dsm.cart.SetDisassembler(dsm)
+		}
 	}
 }
 