JetSetIlly/Gopher2600

commit 6737d50c8a7a32f81d2bb7ef6011a6fff0b6fc41

ARM architecture map can now specify null read value

hardware/memory/cartridge/arm/memory_access.go
@@ -87,8 +87,9 @@ func (arm *ARM) nullAccess(event string, addr uint32) {
 }
 
 func (arm *ARM) read8bit(addr uint32) uint8 {
-	if !arm.stackHasCollided && addr < arm.mmap.NullAccessBoundary {
+	if addr < arm.mmap.NullAccessBoundary {
 		arm.nullAccess("Read 8bit", addr)
+		return uint8(arm.mmap.IllegalAccessValue)
 	}
 
 	var mem *[]uint8
@@ -124,15 +125,16 @@ func (arm *ARM) read8bit(addr uint32) uint8 {
 			arm.illegalAccess("Read 8bit", addr)
 		}
 
-		return 0
+		return uint8(arm.mmap.IllegalAccessValue)
 	}
 
 	return (*mem)[addr]
 }
 
 func (arm *ARM) write8bit(addr uint32, val uint8) {
-	if !arm.stackHasCollided && addr < arm.mmap.NullAccessBoundary {
+	if addr < arm.mmap.NullAccessBoundary {
 		arm.nullAccess("Write 8bit", addr)
+		return
 	}
 
 	var mem *[]uint8
@@ -175,15 +177,16 @@ func (arm *ARM) write8bit(addr uint32, val uint8) {
 }
 
 func (arm *ARM) read16bit(addr uint32, requiresAlignment bool) uint16 {
-	if !arm.stackHasCollided && addr < arm.mmap.NullAccessBoundary {
+	if addr < arm.mmap.NullAccessBoundary {
 		arm.nullAccess("Read 16bit", addr)
+		return uint16(arm.mmap.IllegalAccessValue)
 	}
 
 	// check 16 bit alignment
 	misaligned := addr&0x01 != 0x00
 	if misaligned && (requiresAlignment || arm.mmap.UnalignTrap) {
 		logger.Logf("ARM7", "misaligned 16 bit read (%08x) (PC: %08x)", addr, arm.state.registers[rPC])
-		return 0
+		return uint16(arm.mmap.IllegalAccessValue)
 	}
 
 	var mem *[]uint8
@@ -219,21 +222,22 @@ func (arm *ARM) read16bit(addr uint32, requiresAlignment bool) uint16 {
 			arm.illegalAccess("Read 16bit", addr)
 		}
 
-		return 0
+		return uint16(arm.mmap.IllegalAccessValue)
 	}
 
 	// ensure we're not accessing past the end of memory
 	if addr > uint32(len(*mem)-2) {
 		arm.illegalAccess("Read 16bit", addr)
-		return 0
+		return uint16(arm.mmap.IllegalAccessValue)
 	}
 
 	return uint16((*mem)[addr]) | (uint16((*mem)[addr+1]) << 8)
 }
 
 func (arm *ARM) write16bit(addr uint32, val uint16, requiresAlignment bool) {
-	if !arm.stackHasCollided && addr < arm.mmap.NullAccessBoundary {
+	if addr < arm.mmap.NullAccessBoundary {
 		arm.nullAccess("Write 16bit", addr)
+		return
 	}
 
 	// check 16 bit alignment
@@ -290,15 +294,16 @@ func (arm *ARM) write16bit(addr uint32, val uint16, requiresAlignment bool) {
 }
 
 func (arm *ARM) read32bit(addr uint32, requiresAlignment bool) uint32 {
-	if !arm.stackHasCollided && addr < arm.mmap.NullAccessBoundary {
+	if addr < arm.mmap.NullAccessBoundary {
 		arm.nullAccess("Read 32bit", addr)
+		return arm.mmap.IllegalAccessValue
 	}
 
 	// check 32 bit alignment
 	misaligned := addr&0x03 != 0x00
 	if misaligned && (requiresAlignment || arm.mmap.UnalignTrap) {
 		logger.Logf("ARM7", "misaligned 32 bit read (%08x) (PC: %08x)", addr, arm.state.registers[rPC])
-		return 0
+		return arm.mmap.IllegalAccessValue
 	}
 
 	var mem *[]uint8
@@ -334,21 +339,22 @@ func (arm *ARM) read32bit(addr uint32, requiresAlignment bool) uint32 {
 			arm.illegalAccess("Read 32bit", addr)
 		}
 
-		return 0
+		return arm.mmap.IllegalAccessValue
 	}
 
 	// ensure we're not accessing past the end of memory
 	if addr > uint32(len(*mem)-4) {
 		arm.illegalAccess("Read 32bit", addr)
-		return 0
+		return arm.mmap.IllegalAccessValue
 	}
 
 	return uint32((*mem)[addr]) | (uint32((*mem)[addr+1]) << 8) | (uint32((*mem)[addr+2]) << 16) | uint32((*mem)[addr+3])<<24
 }
 
 func (arm *ARM) write32bit(addr uint32, val uint32, requiresAlignment bool) {
-	if !arm.stackHasCollided && addr < arm.mmap.NullAccessBoundary {
+	if addr < arm.mmap.NullAccessBoundary {
 		arm.nullAccess("Write 32bit", addr)
+		return
 	}
 
 	// check 32 bit alignment

hardware/memory/cartridge/arm/architecture/architecture.go
@@ -95,6 +95,9 @@ type Map struct {
 	// the address below which a null access is considered to have happened
 	NullAccessBoundary uint32
 
+	// the value the is returned when an illegal memory address is read
+	IllegalAccessValue uint32
+
 	// the divisor to apply to the main clock when ticking the timers
 	ClkDiv float32
 
@@ -133,8 +136,9 @@ func NewMap(cart CartArchitecture) Map {
 		mmap.TIMERcontrol = 0xe0008004
 		mmap.TIMERvalue = 0xe0008008
 
-		// value is arbitrary and was suggested by John Champeau (09/04/2022)
+		// boundary value is arbitrary and was suggested by John Champeau (09/04/2022)
 		mmap.NullAccessBoundary = 0x00000751
+		mmap.IllegalAccessValue = 0x00000000
 
 		mmap.ClkDiv = 1.0
 		mmap.UnalignTrap = true
@@ -163,6 +167,10 @@ func NewMap(cart CartArchitecture) Map {
 		mmap.RNGCR = 0x50060800
 		mmap.RNGSR = 0x50060804
 		mmap.RNGDR = 0x50060808
+
+		// boundary value is arbitrary and was suggested by John Champeau (09/04/2022)
+		mmap.NullAccessBoundary = 0x00000751
+		mmap.IllegalAccessValue = 0xffffffff
 
 		mmap.ClkDiv = 0.5
 		mmap.UnalignTrap = false

commit fc8881cf44a24a280f9055f1047dfd82435f91af

null access of memory will trigger ARM breakpoint
broken during changes in 6737d50c8a

hardware/memory/cartridge/arm/memory_access.go
@@ -28,12 +28,28 @@ func (arm *ARM) illegalAccess(event string, addr uint32) {
 		return
 	}
 
-	log := arm.dev.IllegalAccess(event, arm.state.instructionPC, addr)
-	if log == "" {
+	detail := arm.dev.IllegalAccess(event, arm.state.instructionPC, addr)
+	if detail == "" {
 		return
 	}
 
-	arm.memoryErrorDetail = fmt.Errorf("%s: %s", event, log)
+	arm.memoryErrorDetail = fmt.Errorf("%s: %s", event, detail)
+}
+
+// nullAccess is a special condition of illegalAccess()
+func (arm *ARM) nullAccess(event string, addr uint32) {
+	arm.memoryError = fmt.Errorf("%s: probable null pointer dereference of %08x (PC: %08x)", event, addr, arm.state.instructionPC)
+
+	if arm.dev == nil {
+		return
+	}
+
+	detail := arm.dev.NullAccess(event, arm.state.instructionPC, addr)
+	if detail == "" {
+		return
+	}
+
+	arm.memoryErrorDetail = fmt.Errorf("%s: %s", event, detail)
 }
 
 // imperfect check of whether stack has collided with memtop
@@ -63,27 +79,14 @@ func (arm *ARM) stackCollision(stackPointerBeforeExecution uint32) (err error, d
 		return
 	}
 
-	log := arm.dev.StackCollision(arm.state.executingPC, arm.state.registers[rSP])
-	if log == "" {
+	detailStr := arm.dev.StackCollision(arm.state.executingPC, arm.state.registers[rSP])
+	if detailStr == "" {
 		return
 	}
 
-	detail = fmt.Errorf("stack: %s", log)
+	detail = fmt.Errorf("stack: %s", detailStr)
 
 	return err, detail
-}
-
-// nullAccess is a special condition of illegalAccess()
-func (arm *ARM) nullAccess(event string, addr uint32) {
-	logger.Logf("ARM7", "%s: probable null pointer dereference of %08x (PC: %08x)", event, addr, arm.state.instructionPC)
-	if arm.dev == nil {
-		return
-	}
-	log := arm.dev.NullAccess(event, arm.state.instructionPC, addr)
-	if log == "" {
-		return
-	}
-	logger.Logf("ARM7", "%s: %s", event, log)
 }
 
 func (arm *ARM) read8bit(addr uint32) uint8 {