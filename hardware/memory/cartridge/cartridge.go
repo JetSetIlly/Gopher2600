JetSetIlly/Gopher2600

commit 8497d95fb0269d9811b5be1476052a7d62c20fc3

extistance of BusStuff and CoProc interfaces decided in cartridge Attach() and Plumb()
BusStuff and CoProc functions are called a lot and type assertions are
moderately expensive and noticeable when done frequently. by making the
assertion in when the cartridge is inserted we can reduce the test to a
simple boolean comparison

hardware/memory/cartridge/cartridge.go
@@ -46,6 +46,14 @@ type Cartridge struct {
 	// the specific cartridge data, mapped appropriately to the memory
 	// interfaces
 	mapper mapper.CartMapper
+
+	// the CartBusStuff and CartCoProc interface are accessed a lot if
+	// available. rather than performing type assertions too frequently we do
+	// it in the Attach() function and the Plumb() function
+	hasBusStuff bool
+	busStuff    mapper.CartBusStuff
+	hasCoProc   bool
+	coproc      mapper.CartCoProc
 }
 
 // Sentinal error returned if operation is on the ejected cartridge type.
@@ -83,6 +91,8 @@ func (cart *Cartridge) Plumb(fromDifferentEmulation bool) {
 		}
 	}
 	cart.mapper.Plumb()
+	cart.busStuff, cart.hasBusStuff = cart.mapper.(mapper.CartBusStuff)
+	cart.coproc, cart.hasCoProc = cart.mapper.(mapper.CartCoProc)
 }
 
 // Reset volative contents of Cartridge.
@@ -191,6 +201,10 @@ func (cart *Cartridge) Attach(cartload cartridgeloader.Loader) error {
 		if cart.mapper == nil {
 			return
 		}
+
+		// get busstuff and coproc interfaces
+		cart.busStuff, cart.hasBusStuff = cart.mapper.(mapper.CartBusStuff)
+		cart.coproc, cart.hasCoProc = cart.mapper.(mapper.CartCoProc)
 
 		if _, ok := cart.mapper.(*ejected); !ok {
 			logger.Logf("cartridge", "inserted %s", cart.mapper.ID())
@@ -449,8 +463,8 @@ func (cart *Cartridge) GetCartHotspotsBus() mapper.CartHotspotsBus {
 // GetCoProc returns interface to the coprocessor interface or nil if no
 // coprocessor is available on the cartridge.
 func (cart *Cartridge) GetCoProc() mapper.CartCoProc {
-	if cpd, ok := cart.mapper.(mapper.CartCoProc); ok {
-		return cpd
+	if cart.hasCoProc {
+		return cart.coproc
 	}
 	return nil
 }
@@ -484,39 +498,39 @@ func (cart *Cartridge) ROMDump() (string, error) {
 
 // BreakpointHasTriggered implements the mapper.CartCoProc interface.
 func (cart *Cartridge) BreakpointHasTriggered() bool {
-	if bp, ok := cart.mapper.(mapper.CartCoProc); ok {
-		return bp.BreakpointHasTriggered()
+	if cart.hasCoProc {
+		return cart.coproc.BreakpointHasTriggered()
 	}
 	return false
 }
 
 // ResumeAfterBreakpoint implements the mapper.CartCoProc interface.
 func (cart *Cartridge) ResumeAfterBreakpoint() error {
-	if bp, ok := cart.mapper.(mapper.CartCoProc); ok {
-		return bp.ResumeAfterBreakpoint()
+	if cart.hasCoProc {
+		return cart.coproc.ResumeAfterBreakpoint()
 	}
 	return nil
 }
 
 // BreakpointsDisable implements the mapper.CartCoProc interface.
 func (cart *Cartridge) BreakpointsDisable(disable bool) {
-	if bp, ok := cart.mapper.(mapper.CartCoProc); ok {
-		bp.BreakpointsDisable(disable)
+	if cart.hasCoProc {
+		cart.coproc.BreakpointsDisable(disable)
 	}
 }
 
 // CoProcState implements the mapper.CartCoProc interface.
 func (cart *Cartridge) CoProcState() mapper.CoProcState {
-	if bp, ok := cart.mapper.(mapper.CartCoProc); ok {
-		return bp.CoProcState()
+	if cart.hasCoProc {
+		return cart.coproc.CoProcState()
 	}
 	return mapper.CoProcIdle
 }
 
 // BusStuff implements the mapper.CartBusStuff interface.
 func (cart *Cartridge) BusStuff() (uint8, bool) {
-	if bus, ok := cart.mapper.(mapper.CartBusStuff); ok {
-		return bus.BusStuff()
+	if cart.hasBusStuff {
+		return cart.busStuff.BusStuff()
 	}
 	return 0, false
 }

Makefile
@@ -3,9 +3,9 @@ compileFlags = '-c 3 -B -wb=false'
 # profilingRom = roms/Rsboxing.bin
 # profilingRom = "roms/starpath/02 - Communist Mutants From Space (Ntsc).mp3"
 # profilingRom = "roms/The Official Frogger.bin"
-# profilingRom = roms/Homebrew/CDF/zookeeper_20200308_demo2_NTSC.bin
+profilingRom = roms/Homebrew/CDF/zookeeper_20200308_demo2_NTSC.bin
 # profilingRom = roms/Pitfall.bin
-profilingRom = test_roms/ELF/raycaster/raycaster.bin
+# profilingRom = test_roms/ELF/raycaster/raycaster.bin
 
 .PHONY: all clean tidy generate check_lint lint lint_fix check_glsl glsl_validate check_pandoc readme_spell test race race_debug profile profile_cpu profile_cpu_play profile_cpu_debug profile_mem_play profile_mem_debug profile_trace build_assertions build check_upx release chec_rswc windows_manifest cross_windows cross_windows_development cross_windows_dynamic
 

commit 60bea7dfc6bb0c12103b2c88ce13d91dc2b85ea4

BusStuff and CoProc interfaces were not always plumbed correctly
when plumbing from a different emulation BusStuff and CoProc were not
updated. this was visible in the timeline thumbnailer for ROMs that
relied on bus stuffing for screen rendering from a coprocessor

error introduced in 8497d95f

hardware/memory/cartridge/cartridge.go
@@ -84,15 +84,17 @@ func (cart *Cartridge) Snapshot() *Cartridge {
 // See mapper.PlumbFromDifferentEmulation for how this affects mapper
 // implementations.
 func (cart *Cartridge) Plumb(fromDifferentEmulation bool) {
+	cart.busStuff, cart.hasBusStuff = cart.mapper.(mapper.CartBusStuff)
+	cart.coproc, cart.hasCoProc = cart.mapper.(mapper.CartCoProc)
+
 	if fromDifferentEmulation {
 		if m, ok := cart.mapper.(mapper.PlumbFromDifferentEmulation); ok {
 			m.PlumbFromDifferentEmulation()
 			return
 		}
 	}
+
 	cart.mapper.Plumb()
-	cart.busStuff, cart.hasBusStuff = cart.mapper.(mapper.CartBusStuff)
-	cart.coproc, cart.hasCoProc = cart.mapper.(mapper.CartCoProc)
 }
 
 // Reset volative contents of Cartridge.