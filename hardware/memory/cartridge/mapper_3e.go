JetSetIlly/Gopher2600

commit 8766e794dc8a012900ba650f63a0f18fc660371c

refined how cartridge is accessed
cartridge access is now divided into Access(), AccessVolatile() and
AccessPassive()

Access() and AccessVolatile() could easily be combined into one function
but I feel it is clearer to keep them separate

hardware/memory.go Read() function refined to be clearer about how and
when the cartridge is accessed

hardware/memory/cartridge/mapper_3e.go
@@ -22,7 +22,6 @@ import (
 	"github.com/jetsetilly/gopher2600/curated"
 	"github.com/jetsetilly/gopher2600/hardware/instance"
 	"github.com/jetsetilly/gopher2600/hardware/memory/cartridge/mapper"
-	"github.com/jetsetilly/gopher2600/hardware/memory/cpubus"
 	"github.com/jetsetilly/gopher2600/hardware/memory/memorymap"
 )
 
@@ -139,8 +138,8 @@ func (cart *m3e) Access(addr uint16, _ bool) (uint8, uint8, error) {
 	return data, mapper.CartDrivenPins, nil
 }
 
-// AccessDriven implements the mapper.CartMapper interface.
-func (cart *m3e) AccessDriven(addr uint16, data uint8, poke bool) error {
+// AccessVolatile implements the mapper.CartMapper interface.
+func (cart *m3e) AccessVolatile(addr uint16, data uint8, poke bool) error {
 	var segment int
 
 	if addr >= 0x0000 && addr <= 0x07ff {
@@ -151,13 +150,11 @@ func (cart *m3e) AccessDriven(addr uint16, data uint8, poke bool) error {
 
 	if cart.state.segmentIsRAM[segment] {
 		cart.state.ram[cart.state.segment[segment]][addr&0x03ff] = data
-		return nil
 	} else if poke {
 		cart.banks[cart.state.segment[segment]][addr&0x07ff] = data
-		return nil
 	}
 
-	return curated.Errorf("3E: %v", curated.Errorf(cpubus.AddressError, addr))
+	return nil
 }
 
 // NumBanks implements the mapper.CartMapper interface.

hardware/memory/cartridge/mapper_3eplus.go
@@ -22,7 +22,6 @@ import (
 	"github.com/jetsetilly/gopher2600/curated"
 	"github.com/jetsetilly/gopher2600/hardware/instance"
 	"github.com/jetsetilly/gopher2600/hardware/memory/cartridge/mapper"
-	"github.com/jetsetilly/gopher2600/hardware/memory/cpubus"
 	"github.com/jetsetilly/gopher2600/hardware/memory/memorymap"
 )
 
@@ -156,8 +155,8 @@ func (cart *m3ePlus) Access(addr uint16, _ bool) (uint8, uint8, error) {
 	return data, mapper.CartDrivenPins, nil
 }
 
-// AccessDriven implements the mapper.CartMapper interface.
-func (cart *m3ePlus) AccessDriven(addr uint16, data uint8, poke bool) error {
+// AccessVolatile implements the mapper.CartMapper interface.
+func (cart *m3ePlus) AccessVolatile(addr uint16, data uint8, poke bool) error {
 	var segment int
 
 	if addr >= 0x0000 && addr <= 0x03ff {
@@ -178,7 +177,7 @@ func (cart *m3ePlus) AccessDriven(addr uint16, data uint8, poke bool) error {
 		return nil
 	}
 
-	return curated.Errorf("3E+: %v", curated.Errorf(cpubus.AddressError, addr))
+	return nil
 }
 
 // NumBanks implements the mapper.CartMapper interface.

hardware/memory/cartridge/ace/ace.go
@@ -132,8 +132,8 @@ func (cart *Ace) Access(addr uint16, _ bool) (uint8, uint8, error) {
 	return 0, 0, nil
 }
 
-// AccessDriven implements the mapper.CartMapper interface.
-func (cart *Ace) AccessDriven(addr uint16, data uint8, _ bool) error {
+// AccessVolatile implements the mapper.CartMapper interface.
+func (cart *Ace) AccessVolatile(addr uint16, data uint8, _ bool) error {
 	return nil
 }
 

hardware/memory/cartridge/cartridge.go
@@ -145,7 +145,7 @@ func (cart *Cartridge) Peek(addr uint16) (uint8, error) {
 
 // Poke is an implementation of memory.DebugBus. Address must be normalised.
 func (cart *Cartridge) Poke(addr uint16, data uint8) error {
-	return cart.mapper.AccessDriven(addr&memorymap.CartridgeBits, data, true)
+	return cart.mapper.AccessVolatile(addr&memorymap.CartridgeBits, data, true)
 }
 
 // Patch writes to cartridge memory. Offset is measured from the start of
@@ -161,7 +161,7 @@ func (cart *Cartridge) Read(addr uint16) (uint8, uint8, error) {
 
 // Write is an implementation of memory.CPUBus.
 func (cart *Cartridge) Write(addr uint16, data uint8) error {
-	return cart.mapper.AccessDriven(addr&memorymap.CartridgeBits, data, false)
+	return cart.mapper.AccessVolatile(addr&memorymap.CartridgeBits, data, false)
 }
 
 // Eject removes memory from cartridge space and unlike the real hardware,

hardware/memory/cartridge/cdf/cdf.go
@@ -22,7 +22,6 @@ import (
 	"github.com/jetsetilly/gopher2600/hardware/instance"
 	"github.com/jetsetilly/gopher2600/hardware/memory/cartridge/arm"
 	"github.com/jetsetilly/gopher2600/hardware/memory/cartridge/mapper"
-	"github.com/jetsetilly/gopher2600/hardware/memory/cpubus"
 	"github.com/jetsetilly/gopher2600/hardware/memory/memorymap"
 )
 
@@ -300,8 +299,8 @@ func (cart *cdf) Access(addr uint16, peek bool) (uint8, uint8, error) {
 	return data, mapper.CartDrivenPins, nil
 }
 
-// AccessDriven implements the mapper.CartMapper interface.
-func (cart *cdf) AccessDriven(addr uint16, data uint8, poke bool) error {
+// AccessVolatile implements the mapper.CartMapper interface.
+func (cart *cdf) AccessVolatile(addr uint16, data uint8, poke bool) error {
 	// bank switches can not take place if coprocessor is active
 	if cart.state.callfn.IsActive() {
 		return nil
@@ -386,10 +385,7 @@ func (cart *cdf) AccessDriven(addr uint16, data uint8, poke bool) error {
 	default:
 		if poke {
 			cart.banks[cart.state.bank][addr] = data
-			return nil
 		}
-
-		return curated.Errorf("CDF: %v", curated.Errorf(cpubus.AddressError, addr))
 	}
 
 	return nil

hardware/memory/cartridge/dpcplus/dpcplus.go
@@ -317,8 +317,8 @@ func (cart *dpcPlus) Access(addr uint16, peek bool) (uint8, uint8, error) {
 	return data, mapper.CartDrivenPins, nil
 }
 
-// AccessDriven implements the mapper.CartMapper interface.
-func (cart *dpcPlus) AccessDriven(addr uint16, data uint8, poke bool) error {
+// AccessVolatile implements the mapper.CartMapper interface.
+func (cart *dpcPlus) AccessVolatile(addr uint16, data uint8, poke bool) error {
 	// bank switches can not take place if coprocessor is active
 	if cart.state.callfn.IsActive() {
 		return nil
@@ -649,10 +649,7 @@ func (cart *dpcPlus) AccessDriven(addr uint16, data uint8, poke bool) error {
 	default:
 		if poke {
 			cart.banks[cart.state.bank][addr] = data
-			return nil
 		}
-
-		return curated.Errorf("DPC+: %v", curated.Errorf(cpubus.AddressError, addr))
 	}
 
 	return nil

hardware/memory/cartridge/elf/elf.go
@@ -229,8 +229,8 @@ func (cart *Elf) Access(addr uint16, _ bool) (uint8, uint8, error) {
 	return cart.mem.gpio.data[DATA_ODR], mapper.CartDrivenPins, nil
 }
 
-// AccessDriven implements the mapper.CartMapper interface.
-func (cart *Elf) AccessDriven(addr uint16, data uint8, _ bool) error {
+// AccessVolatile implements the mapper.CartMapper interface.
+func (cart *Elf) AccessVolatile(addr uint16, data uint8, _ bool) error {
 	return nil
 }
 

hardware/memory/cartridge/mapper/mapper.go
@@ -49,19 +49,22 @@ type CartMapper interface {
 	// access the cartridge at the specified address. the cartridge is expected to
 	// drive the data bus and so this can be thought of as a "read" operation
 	//
-	// the mask return value allows the mapper to identify which data pins which are
-	// being driven (1 bits) by the cartridge. in most cases, the mask should be the
-	// CartDrivenPins value
+	// the mask return value allows the mapper to identify which data pins
+	// which are being driven by the cartridge. in most cases, the mask should
+	// be the CartDrivenPins value
 	//
 	// the address parameter should be normalised. ie. no mirror information
 	Access(addr uint16, peek bool) (data uint8, mask uint8, err error)
 
-	// access the cartridge at the specified address. the data bus is being driven
-	// by the CPU so we can think of this as a "write" operation. whether the data
-	// is actually "written" or otherwise used is down to the mapper itself
+	// access the cartridge at the specified volatile address. if the location
+	// at that address is not volatile, the data value can be ignored
+	//
+	// we can think of this as a write operation but it will be called during a
+	// read operation too. this is because the cartidge cannot distinguish read
+	// and write operations and any access of a volatile address will affect it
 	//
 	// the address parameter should be normalised. ie. no mirror information
-	AccessDriven(addr uint16, data uint8, poke bool) error
+	AccessVolatile(addr uint16, data uint8, poke bool) error
 
 	NumBanks() int
 	GetBank(addr uint16) BankInfo

commit 4d8fdbf2427c41d1bdc926e6ae5aede7f375e234

reimplementation of 3e and 3e+
previous implementations were broken by 8766e794dc8

hardware/memory/cartridge/mapper_3e.go
@@ -116,23 +116,23 @@ func (cart *m3e) Reset() {
 
 // Access implements the mapper.CartMapper interface.
 func (cart *m3e) Access(addr uint16, _ bool) (uint8, uint8, error) {
-	var segment int
-
-	if addr >= 0x0000 && addr <= 0x07ff {
-		segment = 0
-	} else if addr >= 0x0800 && addr <= 0x0fff {
-		segment = 1
-	}
-
 	var data uint8
 
-	if cart.state.segmentIsRAM[segment] {
-		data = cart.state.ram[cart.state.segment[segment]][addr&0x03ff]
-	} else {
-		bank := cart.state.segment[segment]
-		if bank < len(cart.banks) {
+	if addr >= 0x0000 && addr <= 0x07ff {
+		if cart.state.segmentIsRAM[0] {
+			if addr <= 0x3ff {
+				bank := cart.state.segment[0]
+				data = cart.state.ram[bank][addr&0x03ff]
+			} else {
+				return 0, 0, nil
+			}
+		} else {
+			bank := cart.state.segment[0]
 			data = cart.banks[bank][addr&0x07ff]
 		}
+	} else if addr >= 0x0800 && addr <= 0x0fff {
+		bank := cart.state.segment[1]
+		data = cart.banks[bank][addr&0x07ff]
 	}
 
 	return data, mapper.CartDrivenPins, nil
@@ -140,18 +140,17 @@ func (cart *m3e) Access(addr uint16, _ bool) (uint8, uint8, error) {
 
 // AccessVolatile implements the mapper.CartMapper interface.
 func (cart *m3e) AccessVolatile(addr uint16, data uint8, poke bool) error {
-	var segment int
-
 	if addr >= 0x0000 && addr <= 0x07ff {
-		segment = 0
-	} else if addr >= 0x0c00 && addr <= 0x0fff {
-		segment = 1
-	}
-
-	if cart.state.segmentIsRAM[segment] {
-		cart.state.ram[cart.state.segment[segment]][addr&0x03ff] = data
-	} else if poke {
-		cart.banks[cart.state.segment[segment]][addr&0x07ff] = data
+		bank := cart.state.segment[0]
+		if cart.state.segmentIsRAM[0] == false {
+			if poke {
+				cart.banks[bank][addr&0x07ff] = data
+			}
+			return nil
+		}
+		if addr >= 0x400 {
+			cart.state.ram[bank][addr&0x03ff] = data
+		}
 	}
 
 	return nil
@@ -165,9 +164,9 @@ func (cart *m3e) NumBanks() int {
 // GetBank implements the mapper.CartMapper interface.
 func (cart *m3e) GetBank(addr uint16) mapper.BankInfo {
 	if addr >= 0x0000 && addr <= 0x07ff {
-		return mapper.BankInfo{Number: cart.state.segment[0], IsRAM: false, IsSegmented: true, Segment: 0}
+		return mapper.BankInfo{Number: cart.state.segment[0], IsRAM: cart.state.segmentIsRAM[0], IsSegmented: true, Segment: 0}
 	}
-	return mapper.BankInfo{Number: cart.state.segment[1], IsRAM: false, IsSegmented: true, Segment: 1}
+	return mapper.BankInfo{Number: cart.state.segment[1], IsRAM: cart.state.segmentIsRAM[1], IsSegmented: true, Segment: 1}
 }
 
 // Patch implements the mapper.CartMapper interface.
@@ -189,15 +188,13 @@ func (cart *m3e) AccessPassive(addr uint16, data uint8) {
 
 	// bankswitch on hotspot access
 	if addr == 0x3f {
-		segment := data >> 6
-		bank := data & 0x3f
-		cart.state.segment[segment] = int(bank)
-		cart.state.segmentIsRAM[segment] = false
+		romBank := (data & 0x3f) % uint8(cart.NumBanks())
+		cart.state.segment[0] = int(romBank)
+		cart.state.segmentIsRAM[0] = false
 	} else if addr == 0x3e {
-		segment := data >> 6
-		bank := data & 0x3f
-		cart.state.segment[segment] = int(bank)
-		cart.state.segmentIsRAM[segment] = true
+		ramBank := (data & 0x3f) % uint8(len(cart.state.ram))
+		cart.state.segment[0] = int(ramBank)
+		cart.state.segmentIsRAM[0] = true
 	}
 }
 

hardware/memory/cartridge/mapper_3eplus.go
@@ -157,6 +157,7 @@ func (cart *m3ePlus) Access(addr uint16, _ bool) (uint8, uint8, error) {
 
 // AccessVolatile implements the mapper.CartMapper interface.
 func (cart *m3ePlus) AccessVolatile(addr uint16, data uint8, poke bool) error {
+
 	var segment int
 
 	if addr >= 0x0000 && addr <= 0x03ff {
@@ -169,12 +170,17 @@ func (cart *m3ePlus) AccessVolatile(addr uint16, data uint8, poke bool) error {
 		segment = 3
 	}
 
-	if cart.state.segmentIsRAM[segment] {
-		cart.state.ram[cart.state.segment[segment]][addr&0x01ff] = data
+	if cart.state.segmentIsRAM[segment] == false {
+		if poke {
+			bank := cart.state.segment[segment]
+			cart.banks[bank][addr&0x03ff] = data
+		}
 		return nil
-	} else if poke {
-		cart.banks[cart.state.segment[segment]][addr&0x03ff] = data
-		return nil
+	}
+
+	bank := cart.state.segment[segment]
+	if addr >= uint16(0x0200+(0x400*segment)) {
+		cart.state.ram[bank][addr&0x01ff] = data
 	}
 
 	return nil
@@ -224,13 +230,13 @@ func (cart *m3ePlus) AccessPassive(addr uint16, data uint8) {
 	// bankswitch on hotspot access
 	if addr == 0x3f {
 		segment := data >> 6
-		bank := data & 0x3f
-		cart.state.segment[segment] = int(bank)
+		romBank := (data & 0x3f) % uint8(cart.NumBanks())
+		cart.state.segment[segment] = int(romBank)
 		cart.state.segmentIsRAM[segment] = false
 	} else if addr == 0x3e {
 		segment := data >> 6
-		bank := data & 0x3f
-		cart.state.segment[segment] = int(bank)
+		ramBank := (data & 0x3f) % uint8(len(cart.state.ram))
+		cart.state.segment[segment] = int(ramBank)
 		cart.state.segmentIsRAM[segment] = true
 	}
 }