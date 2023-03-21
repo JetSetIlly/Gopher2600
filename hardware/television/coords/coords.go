b074e2cffb0d7b9750c4a4b61b7b18c07261d051
JetSetIlly <stephen.t.illingworth@gmail.com>
2023-03-21 19:57:54 +0000

    Frame field of coords.TelevisionCoords can now be 'undefined'

--- hardware/television/coords/coords.go
+++ hardware/television/coords/coords.go
@@ -30,6 +30,10 @@ import (
 	"github.com/jetsetilly/gopher2600/hardware/television/specification"
 )
 
+// FrameIsUndefined is used to indicate that the Frame field of the TelevisionCoords
+// struct is to be ignored
+const FrameIsUndefined = ^(0)
+
 // TelevisionCoords represents the state of the TV at any moment in time. It
 // can be used when all three values need to be stored or passed around.
 //
@@ -41,24 +45,46 @@ type TelevisionCoords struct {
 }
 
 func (c TelevisionCoords) String() string {
+	if c.Frame == FrameIsUndefined {
+		return fmt.Sprintf("Scanline: %03d  Clock: %03d", c.Scanline, c.Clock)
+	}
 	return fmt.Sprintf("Frame: %d  Scanline: %03d  Clock: %03d", c.Frame, c.Scanline, c.Clock)
 }
 
-// Equal compares two instances of TelevisionCoords and return true if
-// both are equal.
+// Equal compares two instances of TelevisionCoords and return true if both are
+// equal.
+//
+// If the Frame field is undefined for either argument then the Frame field is
+// ignored for the test.
 func Equal(A, B TelevisionCoords) bool {
+	if A.Frame == FrameIsUndefined || B.Frame == FrameIsUndefined {
+		return A.Scanline == B.Scanline && A.Clock == B.Clock
+	}
 	return A.Frame == B.Frame && A.Scanline == B.Scanline && A.Clock == B.Clock
 }
 
 // GreaterThanOrEqual compares two instances of TelevisionCoords and return
 // true if A is greater than or equal to B.
+//
+// If the Frame field is undefined for either argument then the Frame field is
+// ignored for the test.
 func GreaterThanOrEqual(A, B TelevisionCoords) bool {
+	if A.Frame == FrameIsUndefined || B.Frame == FrameIsUndefined {
+		return A.Scanline > B.Scanline || (A.Scanline == B.Scanline && A.Clock >= B.Clock)
+	}
+
 	return A.Frame > B.Frame || (A.Frame == B.Frame && A.Scanline > B.Scanline) || (A.Frame == B.Frame && A.Scanline == B.Scanline && A.Clock >= B.Clock)
 }
 
 // GreaterThan compares two instances of TelevisionCoords and return true if A
 // is greater than to B.
+//
+// If the Frame field is undefined for either argument then the Frame field is
+// ignored for the test.
 func GreaterThan(A, B TelevisionCoords) bool {
+	if A.Frame == FrameIsUndefined || B.Frame == FrameIsUndefined {
+		return A.Scanline > B.Scanline || (A.Scanline == B.Scanline && A.Clock > B.Clock)
+	}
 	return A.Frame > B.Frame || (A.Frame == B.Frame && A.Scanline > B.Scanline) || (A.Frame == B.Frame && A.Scanline == B.Scanline && A.Clock > B.Clock)
 }
 
@@ -66,34 +92,50 @@ func GreaterThan(A, B TelevisionCoords) bool {
 // scanlinesPerFrame value is the number of scanlines in a typical frame for
 // the ROM, implying that for best results, the television image should be
 // stable.
+//
+// If the Frame field is undefined for either TelevisionCoords argument then the
+// Frame field in the result of the function is also undefined.
 func Diff(A, B TelevisionCoords, scanlinesPerFrame int) TelevisionCoords {
-	C := TelevisionCoords{
+	D := TelevisionCoords{
 		Frame:    A.Frame - B.Frame,
 		Scanline: A.Scanline - B.Scanline,
 		Clock:    A.Clock - B.Clock,
 	}
 
-	if C.Clock < specification.ClksHBlank {
-		C.Scanline--
-		C.Clock += specification.ClksScanline
+	if D.Clock < specification.ClksHBlank {
+		D.Scanline--
+		D.Clock += specification.ClksScanline
 	}
 
-	if C.Scanline < 0 {
-		C.Frame--
-		C.Scanline += scanlinesPerFrame
+	if D.Scanline < 0 {
+		D.Frame--
+		D.Scanline += scanlinesPerFrame
 	}
 
-	if C.Frame < 0 {
-		C.Frame = 0
-		C.Scanline = 0
-		C.Clock -= specification.ClksScanline
+	if D.Frame < 0 {
+		D.Frame = 0
+		D.Scanline = 0
+		D.Clock -= specification.ClksScanline
 	}
 
-	return C
+	// if the Frame field in either A or B is undefined then we can set the diff
+	// Frame field as undefined alse
+	if A.Frame == FrameIsUndefined || B.Frame == FrameIsUndefined {
+		D.Frame = FrameIsUndefined
+	}
+
+	return D
 }
 
 // Sum the the number of clocks in the television coordinates.
+//
+// If the Frame field is undefined for the TelevisionCoords then the Frame field
+// in the result of the function is also undefined.
 func Sum(A TelevisionCoords, scanlinesPerFrame int) int {
+	if A.Frame == FrameIsUndefined {
+		return (A.Scanline * specification.ClksScanline) + A.Clock
+	}
+
 	numPerFrame := scanlinesPerFrame * specification.ClksScanline
 	return (A.Frame * numPerFrame) + (A.Scanline * specification.ClksScanline) + A.Clock
 }
---
+++ hardware/television/coords/coords_test.go
@@ -0,0 +1,59 @@
+// This file is part of Gopher2600.
+//
+// Gopher2600 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gopher2600 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
+
+package coords_test
+
+import (
+	"testing"
+
+	"github.com/jetsetilly/gopher2600/hardware/television/coords"
+	"github.com/jetsetilly/gopher2600/test"
+)
+
+func TestEqual(t *testing.T) {
+	A := coords.TelevisionCoords{
+		Frame:    0,
+		Scanline: 0,
+		Clock:    0,
+	}
+	B := coords.TelevisionCoords{
+		Frame:    0,
+		Scanline: 0,
+		Clock:    1,
+	}
+
+	// clock fields are different (other fields equal)
+	test.ExpectedFailure(t, coords.Equal(A, B))
+
+	// all fields are equal
+	B.Clock = 0
+	test.ExpectedSuccess(t, coords.Equal(A, B))
+
+	// scanline fields are different (other fields equal)
+	B.Scanline = 1
+	test.ExpectedFailure(t, coords.Equal(A, B))
+
+	// all fields are equal
+	A.Scanline = 1
+	test.ExpectedSuccess(t, coords.Equal(A, B))
+
+	// frame fields are different
+	A.Frame = 1
+	test.ExpectedFailure(t, coords.Equal(A, B))
+
+	// frame fields are different but one is undefined
+	B.Frame = coords.FrameIsUndefined
+	test.ExpectedSuccess(t, coords.Equal(A, B))
+}
