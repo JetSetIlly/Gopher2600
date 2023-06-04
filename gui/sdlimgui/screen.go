JetSetIlly/Gopher2600

commit a8009b513f85eef14dbcdf4c952ba0b2458ba6d8

simplified the setting of FPS cap from the command line
command line now only accepts true/false as options. previously, the
options were aimed at allowing different syncing methods with the
monitor but that's not needed now

gui/sdlimgui/screen.go
@@ -81,13 +81,15 @@ type screenCrit struct {
 	// Resize() and NewFrame()
 	frameInfo television.FrameInfo
 
-	// whether or not to sync with monitor refresh rate
-	monitorSync bool
+	// whether or not emulation is fps capped (to speed of television). the
+	// screen implementation uses this to decide whether to sync with monitor
+	// refresh rate
+	fpsCapped bool
 
-	// whether monitorSync is higher than the emulated TV's refresh rate
+	// whether monitor refresh rate is higher than the emulated TV's refresh rate
 	monitorSyncHigher bool
 
-	// whether monitorSync is similar to the emulated TV's refresh rate
+	// whether monitor refresh rate is similar to the emulated TV's refresh rate
 	monitorSyncSimilar bool
 
 	// the scanline currenty used to emulate a screenroll effect. if the value
@@ -179,7 +181,7 @@ func newScreen(img *SdlImgui) *screen {
 	scr.crit.section.Lock()
 
 	scr.crit.overlay = reflection.OverlayLabels[reflection.OverlayNone]
-	scr.crit.monitorSync = true
+	scr.crit.fpsCapped = true
 
 	scr.crit.presentationPixels = image.NewRGBA(image.Rect(0, 0, specification.ClksScanline, specification.AbsoluteMaxScanlines))
 	scr.crit.elementPixels = image.NewRGBA(image.Rect(0, 0, specification.ClksScanline, specification.AbsoluteMaxScanlines))
@@ -198,38 +200,49 @@ func newScreen(img *SdlImgui) *screen {
 
 	scr.crit.section.Unlock()
 
-	// default to NTSC. this will change on the first instance of
+	// default to NTSC
 	scr.resize(television.NewFrameInfo(specification.SpecNTSC))
 	scr.Reset()
 
 	return scr
 }
 
-// setRefreshRate decides on the buffering and syncing policy of the
-// screen based on the reported TV refresh rate.
+// SetFPSCap implements the television.FPSCap interface
+func (scr *screen) SetFPSCap(limit bool) {
+	scr.crit.section.Lock()
+	defer scr.crit.section.Unlock()
+
+	scr.crit.fpsCapped = limit
+	scr.setSyncPolicy(scr.crit.frameInfo.RefreshRate)
+}
+
+// setSyncPolicy decides on the monitor/tv syncing policy, based on the
+// TV refresh rate and the refresh rate of the monitor
 //
 // must be called from inside a critical section.
-func (scr *screen) setRefreshRate(tvRefreshRate float32) {
+func (scr *screen) setSyncPolicy(tvRefreshRate float32) {
 	high := float32(scr.img.plt.mode.RefreshRate) * 1.02
 	low := float32(scr.img.plt.mode.RefreshRate) * 0.98
 
-	higher := scr.crit.monitorSync && high >= tvRefreshRate
-	similar := scr.crit.monitorSync && high >= tvRefreshRate && low <= tvRefreshRate
+	scr.crit.monitorSyncHigher = scr.crit.fpsCapped && high >= tvRefreshRate
+	scr.crit.monitorSyncSimilar = scr.crit.fpsCapped && high >= tvRefreshRate && low <= tvRefreshRate
 
-	// no need to update frame queue if monitor sync higher condition hasn't changed
-	if higher != scr.crit.monitorSyncHigher {
-		scr.updateFrameQueue()
-	}
-
-	scr.crit.monitorSyncHigher = higher
-	scr.crit.monitorSyncSimilar = similar
+	scr.updateFrameQueue()
 }
 
-// must be called from inside a critical section.
+// updateFrameQueue() is called whenever a frameQueue preference is changed and
+// when setSyncPolicy() deems it necessary
+//
+// must be called from inside a critical section
 func (scr *screen) updateFrameQueue() {
-	// make local copies of frame queue preferences
-	scr.crit.frameQueueAuto = scr.img.prefs.frameQueueAuto.Get().(bool)
-	scr.crit.frameQueueLen = scr.img.prefs.frameQueue.Get().(int)
+	// make local copies of frame queue preferences if fpsCapped is enabled
+	if scr.crit.fpsCapped {
+		scr.crit.frameQueueAuto = scr.img.prefs.frameQueueAuto.Get().(bool)
+		scr.crit.frameQueueLen = scr.img.prefs.frameQueue.Get().(int)
+	} else {
+		scr.crit.frameQueueAuto = false
+		scr.crit.frameQueueLen = 1
+	}
 
 	// set queue recovery
 	scr.crit.queueRecovery = scr.crit.frameQueueLen
@@ -265,7 +278,7 @@ func (scr *screen) Reset() {
 	scr.crit.section.Lock()
 	defer scr.crit.section.Unlock()
 
-	scr.updateFrameQueue()
+	scr.setSyncPolicy(scr.crit.frameInfo.RefreshRate)
 	scr.clearPixels()
 }
 
@@ -321,8 +334,6 @@ func (scr *screen) clearPixels() {
 func (scr *screen) resize(frameInfo television.FrameInfo) {
 	scr.crit.section.Lock()
 	defer scr.crit.section.Unlock()
-
-	scr.setRefreshRate(frameInfo.RefreshRate)
 
 	// do nothing if resize values are the same as previously
 	if scr.crit.frameInfo.Spec.ID == frameInfo.Spec.ID &&
@@ -406,11 +417,12 @@ func (scr *screen) NewFrame(frameInfo television.FrameInfo) error {
 		}
 	}
 
-	// refresh rate might have changed
+	// set sync policy if refresh rate has changed
 	if scr.crit.frameInfo.RefreshRate != frameInfo.RefreshRate {
-		scr.setRefreshRate(frameInfo.RefreshRate)
+		scr.setSyncPolicy(frameInfo.RefreshRate)
 	}
 
+	// record frame info for future reference
 	scr.crit.frameInfo = frameInfo
 
 	return nil

debugger/commandline_options.go
@@ -26,7 +26,7 @@ type CommandLineOptions struct {
 	// common to debugger and play modes
 	Log       *bool
 	Spec      *string
-	FpsCap    *string
+	FpsCap    *bool
 	Multiload *int
 	Mapping   *string
 	Left      *string
@@ -63,7 +63,7 @@ func NewCommandLineOptions() CommandLineOptions {
 	return CommandLineOptions{
 		Log:             &b,
 		Spec:            &s,
-		FpsCap:          &s,
+		FpsCap:          &b,
 		Multiload:       &i,
 		Mapping:         &s,
 		Left:            &s,

debugger/debugger.go
@@ -448,33 +448,7 @@ func NewDebugger(opts CommandLineOptions, create CreateUserInterface) (*Debugger
 	dbg.vcs.RIOT.Ports.AttachPlugMonitor(dbg)
 
 	// set fps cap
-	switch strings.ToUpper(*opts.FpsCap) {
-	case "":
-		fallthrough
-	case "TV":
-		dbg.vcs.TV.SetFPSCap(true)
-		err := dbg.gui.SetFeature(gui.ReqMonitorSync, true)
-		if err != nil {
-			return nil, fmt.Errorf("debugger: %w", err)
-		}
-		logger.Log("debugger", "capping FPS to emulated TV")
-	case "MONITOR":
-		dbg.vcs.TV.SetFPSCap(false)
-		err := dbg.gui.SetFeature(gui.ReqMonitorSync, true)
-		if err != nil {
-			return nil, fmt.Errorf("debugger: %w", err)
-		}
-		logger.Log("debugger", "capping FPS to monitor refresh rate")
-	case "NONE":
-		dbg.vcs.TV.SetFPSCap(false)
-		err := dbg.gui.SetFeature(gui.ReqMonitorSync, false)
-		if err != nil {
-			return nil, fmt.Errorf("debugger: %w", err)
-		}
-		logger.Log("debugger", "not capping FPS")
-	default:
-		return nil, fmt.Errorf("debugger: unknown fpscap value (%s)", *opts.FpsCap)
-	}
+	dbg.vcs.TV.SetFPSCap(*opts.FpsCap)
 
 	// initialise terminal
 	err = dbg.term.Initialise()

gopher2600.go
@@ -292,7 +292,7 @@ func emulate(emulationMode govern.Mode, md *modalflag.Modes, sync *mainSync) err
 	// arguments common to both play and debugging modes
 	opts.Log = md.AddBool("log", false, "echo debugging log to stdout")
 	opts.Spec = md.AddString("tv", "AUTO", "television specification: AUTO, NTSC, PAL, PAL60")
-	opts.FpsCap = md.AddString("fpscap", "TV", "cap FPS: TV, MONITOR, NONE")
+	opts.FpsCap = md.AddBool("fpscap", true, "cap FPS to emulated match TV")
 	opts.Multiload = md.AddInt("multiload", -1, "force multiload byte (supercharger only; 0 to 255)")
 	opts.Mapping = md.AddString("mapping", "AUTO", "force use of cartridge mapping")
 	opts.Left = md.AddString("left", "AUTO", "left player port: AUTO, STICK, PADDLE, KEYPAD, GAMEPAD")

gui/gui.go
@@ -43,11 +43,6 @@ const (
 	// program is ending. gui should do anything required before final exit.
 	ReqEnd FeatureReq = "ReqEnd" // nil
 
-	// whether gui should try to sync with the monitor refresh rate. not all
-	// gui modes have to obey this but for presentation/play modes it's a good
-	// idea to have it set.
-	ReqMonitorSync FeatureReq = "ReqMonitorSync" // bool
-
 	// put gui output into full-screen mode (ie. no window border and content
 	// the size of the monitor).
 	ReqFullScreen FeatureReq = "ReqFullScreen" // bool

gui/sdlimgui/requests.go
@@ -64,14 +64,6 @@ func (img *SdlImgui) serviceSetFeature(request featureRequest) {
 			img.end()
 		}
 
-	case gui.ReqMonitorSync:
-		err = argLen(request.args, 1)
-		if err == nil {
-			img.screen.crit.section.Lock()
-			img.screen.crit.monitorSync = request.args[0].(bool)
-			img.screen.crit.section.Unlock()
-		}
-
 	case gui.ReqFullScreen:
 		err = argLen(request.args, 1)
 		if err == nil {

gui/sdlimgui/win_prefs.go
@@ -228,36 +228,42 @@ of the ROM.`)
 	}
 
 	imgui.Spacing()
+
 	if imgui.CollapsingHeader("Frame Queue") {
-		win.img.screen.crit.section.Lock()
-
 		imgui.Spacing()
 
-		frameQueueAuto := win.img.prefs.frameQueueAuto.Get().(bool)
-		if imgui.Checkbox("Automatic Frame Queue Length", &frameQueueAuto) {
-			win.img.prefs.frameQueueAuto.Set(frameQueueAuto)
-			win.img.screen.updateFrameQueue()
-		}
+		func() {
+			win.img.screen.crit.section.Lock()
+			defer win.img.screen.crit.section.Unlock()
 
-		imgui.Spacing()
+			if !win.img.screen.crit.fpsCapped {
+				imgui.PushItemFlag(imgui.ItemFlagsDisabled, true)
+				imgui.PushStyleVarFloat(imgui.StyleVarAlpha, disabledAlpha)
+				defer imgui.PopItemFlag()
+				defer imgui.PopStyleVar()
+			}
 
-		if frameQueueAuto {
-			imgui.PushItemFlag(imgui.ItemFlagsDisabled, true)
-			imgui.PushStyleVarFloat(imgui.StyleVarAlpha, disabledAlpha)
-		}
+			frameQueueAuto := win.img.prefs.frameQueueAuto.Get().(bool)
+			if imgui.Checkbox("Automatic Frame Queue Length", &frameQueueAuto) {
+				win.img.prefs.frameQueueAuto.Set(frameQueueAuto)
+				win.img.screen.updateFrameQueue()
+			}
 
-		frameQueue := int32(win.img.screen.crit.frameQueueLen)
-		if imgui.SliderInt("Frame Queue Length", &frameQueue, 1, maxFrameQueue) {
-			win.img.prefs.frameQueue.Set(frameQueue)
-			win.img.screen.updateFrameQueue()
-		}
+			imgui.Spacing()
 
-		if frameQueueAuto {
-			imgui.PopItemFlag()
-			imgui.PopStyleVar()
-		}
+			if frameQueueAuto {
+				imgui.PushItemFlag(imgui.ItemFlagsDisabled, true)
+				imgui.PushStyleVarFloat(imgui.StyleVarAlpha, disabledAlpha)
+				defer imgui.PopItemFlag()
+				defer imgui.PopStyleVar()
+			}
 
-		win.img.screen.crit.section.Unlock()
+			frameQueue := int32(win.img.screen.crit.frameQueueLen)
+			if imgui.SliderInt("Frame Queue Length", &frameQueue, 1, maxFrameQueue) {
+				win.img.prefs.frameQueue.Set(frameQueue)
+				win.img.screen.updateFrameQueue()
+			}
+		}()
 	}
 }
 

hardware/television/protocol.go
@@ -20,7 +20,7 @@ import (
 )
 
 // PixelRenderer implementations displays, or otherwise works with, visual
-// information from a television. For example digest.Video.
+// information from a television. For example digest.Video
 //
 // PixelRenderer implementations often find it convenient to maintain a reference to
 // the parent Television implementation and maybe even embed the Television
@@ -34,39 +34,36 @@ import (
 // The most useful source of information though is the FrameInfo type supplied
 // to the PixelRenderer through the Resize() and NewFrame() functions. A
 // current copy of this information is also available from the television type
-// GetFrameInfo() function.
+// GetFrameInfo() function
 type PixelRenderer interface {
 	// Resize is called when the television implementation detects that extra
-	// scanlines are required in the display.
+	// scanlines are required in the display
 	//
-	// Renderers must be prepared to resize to either a smaller or larger size.
+	// Renderers must be prepared to resize to either a smaller or larger size
 	//
-	// Resize can also be called speculatively so implementations should take
-	// care not to perform any resizing unless absolutely necessary.
-	//
-	// The VisibleTop and VisibleBottom fields in the FrameInfor argument,
-	// describe the top and bottom scanline that is *visible* on a normal
-	// screen. pixels outside this range that are sent by SetPixel() can be
-	// handled according to the renderers needs but would not normally be shown
-	// for game-playing purposes.
+	// Note that the Refresh Rate of the TV is not directly connected with the
+	// size of the screen. The RefreshRate is better dealt with in the
+	// NewFrame() function because that will catch instances when the refresh
+	// rate has changed but the size of the screen has not. Although also note
+	// that not all pixel renderer's need to worry about the refresh rate at all
 	Resize(FrameInfo) error
 
-	// NewFrame is called at the start of a new fame.
+	// NewFrame is called at the start of a new frame
 	NewFrame(FrameInfo) error
 
 	// NewScanline is called at the start of a new scanline
 	NewScanline(scanline int) error
 
 	// SetPixels is used to Render a series of signals. The number of signals
-	// will always be television.MaxSignalHistory.
+	// will always be television.MaxSignalHistory
 	//
 	// Producing a 2d image from the signals sent by SetPixels() can easily be
 	// done by first allocating a bitmap of width specification.ClksScanline
 	// and height specification.AbsoluateMaxScanlines. This bitmap will have
-	// television.MaxSignalHistory entries.
+	// television.MaxSignalHistory entries
 	//
 	// Every signal from SetPixels() therefore corresponds to a pixel in the
-	// bitmap - the first entry always referes to the top-left pixel.
+	// bitmap - the first entry always referes to the top-left pixel
 	//
 	// Setting the color of a pixel can be done by extracting the ColorSignal
 	// from the SignalAttributes (see signal package)
@@ -75,15 +72,15 @@ type PixelRenderer interface {
 	// recently set. This is useful to know when showing televison images when
 	// the emulation is paused. All entries upto and including last are from
 	// teh *current* frame. All entries afterwards are from the *previous*
-	// frame.
+	// frame
 	//
 	// If the entry contains signal.NoSignal then that screen pixel has not
-	// been written to recently.
+	// been written to recently
 	//
 	// For renderers that are producing an accurate visual image, the pixel
 	// should always be set to video black if VBLANK is on. Some renderers
 	// however may find it useful to set the pixel to the RGB value regardless
-	// of VBLANK.
+	// of VBLANK
 	//
 	// A very important note is that some ROMs use VBLANK to control pixel
 	// color within the visible display area. For example:
@@ -101,20 +98,27 @@ type PixelRenderer interface {
 	//
 	// Note that a Reset event does not imply a Resize() event. Implementations
 	// should not call the Resize() function as a byproduct of a Reset(). The
-	// television will send an explicit Resize() request if it is appropriate.
+	// television will send an explicit Resize() request if it is appropriate
 	Reset()
 
-	// Some renderers may need to conclude and/or dispose of resources gently.
+	// Some renderers may need to conclude and/or dispose of resources gently
 	EndRendering() error
 }
 
-// FrameTrigger implementations listen for Pause events.
+// PixelRendererFPSCap is an extension to the PixelRenderer interface. Pixel
+// renderers that implement this interface will be notified when the
+// television's frame capping policy is changed
+type PixelRendererFPSCap interface {
+	SetFPSCap(limit bool)
+}
+
+// FrameTrigger implementations listen for Pause events
 type PauseTrigger interface {
 	Pause(pause bool) error
 }
 
 // FrameTrigger implementations listen for NewFrame events. FrameTrigger is a
-// subset of PixelRenderer.
+// subset of PixelRenderer
 type FrameTrigger interface {
 	NewFrame(FrameInfo) error
 }
@@ -127,12 +131,12 @@ type ScanlineTrigger interface {
 
 // AudioMixer implementations work with sound; most probably playing it. An
 // example of an AudioMixer that does not play sound but otherwise works with
-// it is the digest.Audio type.
+// it is the digest.Audio type
 type AudioMixer interface {
 	// for efficiency reasons, SetAudio() implementations can be sent
 	// SignalAttributes values that do not have valid AudioData (ie.
 	// AudioUpdate bit is zero). implemenations should therefore take care when
-	// processing the sig slice.
+	// processing the sig slice
 	//
 	// the general algorithm for processing the sig slice is:
 	//
@@ -152,16 +156,16 @@ type AudioMixer interface {
 	EndMixing() error
 
 	// Reset buffered audio and anything else that might need doing on, for
-	// example, a cartridge change.
+	// example, a cartridge change
 	Reset()
 }
 
 // RealtimeAudioMixer is an extension for the AudioMixer interface.
 // Implementations of this interface expect to be given more audio data on
-// demand.
+// demand
 //
 // MoreAudio() is called periodically (every scanline) and the implementation
-// should return true if more audio data is required immediately.
+// should return true if more audio data is required immediately
 type RealtimeAudioMixer interface {
 	AudioMixer
 	MoreAudio() bool
@@ -169,7 +173,7 @@ type RealtimeAudioMixer interface {
 
 // VCSReturnChannel is used to send information from the TV back to the parent
 // console. Named because I think of it as being similar to the Audio Return
-// Channel (ARC) present in modern TVs.
+// Channel (ARC) present in modern TVs
 type VCSReturnChannel interface {
 	SetClockSpeed(tvSpec string) error
 }

commit e822feed71f3970d54c161ae368c13903a8f4e1f

fixed issue with "TV screen" window rewinding
bug introduced in commit a8009b513f

fixed by not updating "frame queue" when emulation is in rewinding state

gui/sdlimgui/screen.go
@@ -235,6 +235,10 @@ func (scr *screen) setSyncPolicy(tvRefreshRate float32) {
 //
 // must be called from inside a critical section
 func (scr *screen) updateFrameQueue() {
+	if scr.img.dbg.State() == govern.Rewinding {
+		return
+	}
+
 	// make local copies of frame queue preferences if fpsCapped is enabled
 	if scr.crit.fpsCapped {
 		scr.crit.frameQueueAuto = scr.img.prefs.frameQueueAuto.Get().(bool)