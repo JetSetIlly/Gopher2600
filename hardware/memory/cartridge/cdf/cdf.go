JetSetIlly/Gopher2600

commit 61b4c07b65b7eb8408c1767c87d77a37e74c4f06

thumbnailer (animation and image) implement CartYieldHook
a cartridge with a coprocessor that had failed for some reason would
cause the thumbnailer goroutine to halt

in the case of the thumbnailer.Image type this would lock the UI
goroutine. and in the case of the thumbnail.Anim type it would lock the
goroutine that had been launched to produce the animation

added YieldHookResponse to better indicate the intention of the
CartYieldHook

hardware/memory/cartridge/cdf/cdf.go
@@ -162,6 +162,7 @@ func (cart *cdf) Plumb(env *environment.Environment) {
 func (cart *cdf) PlumbFromDifferentEmulation(env *environment.Environment) {
 	cart.env = env
 	cart.arm = arm.NewARM(cart.version.mmap, cart.env.Prefs.ARM, cart.state.static, cart)
+	cart.yieldHook = &mapper.StubCartYieldHook{}
 }
 
 // Reset implements the mapper.CartMapper interface.
@@ -381,13 +382,15 @@ func (cart *cdf) AccessVolatile(addr uint16, data uint8, poke bool) error {
 			// call runArm() once and then check for yield conditions
 			cart.state.yield = cart.runArm()
 
-			// keep calling runArm() for as long as program has not ended...
+			// keep calling runArm() for as long as program has not ended
 			for cart.state.yield.Type != mapper.YieldProgramEnded {
-				// ... or if the yield hook says to return to the VCS immediately
-				if cart.yieldHook.CartYield(cart.state.yield.Type) {
-					break // for loop
+				fmt.Printf("3 %p\n", cart.yieldHook)
+				switch cart.yieldHook.CartYield(cart.state.yield.Type) {
+				case mapper.YieldHookEnd:
+					break
+				case mapper.YieldHookContinue:
+					cart.state.yield = cart.runArm()
 				}
-				cart.state.yield = cart.runArm()
 			}
 		}
 

hardware/memory/cartridge/dpcplus/dpcplus.go
@@ -153,6 +153,7 @@ func (cart *dpcPlus) Plumb(env *environment.Environment) {
 func (cart *dpcPlus) PlumbFromDifferentEmulation(env *environment.Environment) {
 	cart.env = env
 	cart.arm = arm.NewARM(cart.version.mmap, cart.env.Prefs.ARM, cart.state.static, cart)
+	cart.yieldHook = &mapper.StubCartYieldHook{}
 }
 
 // Reset implements the mapper.CartMapper interface.
@@ -509,13 +510,14 @@ func (cart *dpcPlus) AccessVolatile(addr uint16, data uint8, poke bool) error {
 
 			cart.state.yield = cart.runArm()
 
-			// keep calling runArm() for as long as program has not ended...
+			// keep calling runArm() for as long as program has not ended
 			for cart.state.yield.Type != mapper.YieldProgramEnded {
-				// ... or if the yield hook says to return to the VCS immediately
-				if cart.yieldHook.CartYield(cart.state.yield.Type) {
-					break // for loop
+				switch cart.yieldHook.CartYield(cart.state.yield.Type) {
+				case mapper.YieldHookEnd:
+					break
+				case mapper.YieldHookContinue:
+					cart.state.yield = cart.runArm()
 				}
-				cart.state.yield = cart.runArm()
 			}
 		}
 

debugger/debugger.go
@@ -858,27 +858,27 @@ func (dbg *Debugger) StartInPlayMode(filename string) error {
 }
 
 // CartYield implements the mapper.CartYieldHook interface.
-func (dbg *Debugger) CartYield(yield mapper.CoProcYieldType) bool {
+func (dbg *Debugger) CartYield(yield mapper.CoProcYieldType) mapper.YieldHookResponse {
 	// if the emulator wants to quit we need to return true to instruct the
 	// cartridge to return to the main loop immediately
 	if !dbg.running {
-		return true
+		return mapper.YieldHookEnd
 	}
 
 	switch yield {
 	case mapper.YieldProgramEnded:
 		// expected reason for CDF and DPC+ cartridges
-		return false
+		return mapper.YieldHookContinue
 
 	case mapper.YieldSyncWithVCS:
 		// expected reason for ACE and ELF cartridges
-		return false
+		return mapper.YieldHookContinue
 	}
 
 	// if emulation is in the initialisation state then we return true to
 	// indicate that the yield is not safe and execution should halt immediately
 	if dbg.State() == govern.Initialising {
-		return true
+		return mapper.YieldHookEnd
 	}
 
 	dbg.halting.cartridgeYield = true
@@ -893,7 +893,7 @@ func (dbg *Debugger) CartYield(yield mapper.CoProcYieldType) bool {
 		dbg.inputLoop(dbg.term, true)
 	}
 
-	return false
+	return mapper.YieldHookEnd
 }
 
 func (dbg *Debugger) run() error {

hardware/memory/cartridge/ace/ace.go
@@ -102,6 +102,7 @@ func (cart *Ace) PlumbFromDifferentEmulation(env *environment.Environment) {
 	cart.mem.Plumb(cart.arm)
 	cart.arm.Plumb(cart.armState, cart.mem, cart)
 	cart.armState = nil
+	cart.yieldHook = mapper.StubCartYieldHook{}
 }
 
 // Plumb implements the mapper.CartMapper interface.
@@ -147,18 +148,20 @@ func (cart *Ace) Patch(_ int, _ uint8) error {
 	return fmt.Errorf("ACE: patching unsupported")
 }
 
-func (cart *Ace) runARM() {
+func (cart *Ace) runARM() bool {
 	// call arm once and then check for yield conditions
 	cart.mem.yield, _ = cart.arm.Run()
 
-	// keep calling runArm() for as long as program does not need to sync with the VCS...
+	// keep calling runArm() for as long as program does not need to sync with the VCS
 	for cart.mem.yield.Type != mapper.YieldSyncWithVCS {
-		// ... or if the yield hook says to return to the VCS immedtiately
-		if cart.yieldHook.CartYield(cart.mem.yield.Type) {
-			return
+		switch cart.yieldHook.CartYield(cart.mem.yield.Type) {
+		case mapper.YieldHookEnd:
+			return false
+		case mapper.YieldHookContinue:
+			cart.mem.yield, _ = cart.arm.Run()
 		}
-		cart.mem.yield, _ = cart.arm.Run()
 	}
+	return true
 }
 
 // AccessPassive implements the mapper.CartMapper interface.

hardware/memory/cartridge/elf/elf.go
@@ -190,6 +190,7 @@ func (cart *Elf) PlumbFromDifferentEmulation(env *environment.Environment) {
 	cart.mem.Plumb(cart.arm)
 	cart.arm.Plumb(cart.armState, cart.mem, cart)
 	cart.armState = nil
+	cart.yieldHook = &mapper.StubCartYieldHook{}
 }
 
 // Plumb implements the mapper.CartMapper interface.
@@ -247,7 +248,7 @@ func (cart *Elf) Patch(_ int, _ uint8) error {
 	return fmt.Errorf("ELF: patching unsupported")
 }
 
-func (cart *Elf) runARM() {
+func (cart *Elf) runARM() bool {
 	if cart.dev != nil {
 		cart.dev.StartProfiling()
 		defer cart.dev.ProcessProfiling()
@@ -259,11 +260,15 @@ func (cart *Elf) runARM() {
 	// keep calling runArm() for as long as program does not need to sync with the VCS...
 	for cart.mem.yield.Type != mapper.YieldSyncWithVCS {
 		// ... or if the yield hook says to return to the VCS immediately
-		if cart.yieldHook.CartYield(cart.mem.yield.Type) {
-			return
+		switch cart.yieldHook.CartYield(cart.mem.yield.Type) {
+		case mapper.YieldHookEnd:
+			return false
+		case mapper.YieldHookContinue:
+			cart.mem.yield, _ = cart.arm.Run()
 		}
 		cart.mem.yield, _ = cart.arm.Run()
 	}
+	return true
 }
 
 // try to run strongarm function. returns success.

hardware/memory/cartridge/mapper/coproc.go
@@ -71,26 +71,32 @@ const (
 	CoProcParallel
 )
 
+// YieldHookResponse is returned by the CartYieldHook implementation to instruct
+// the mapper in how to proceed
+type YieldHookResponse int
+
+// List of valid YieldHookCommands
+const (
+	YieldHookContinue YieldHookResponse = iota
+	YieldHookEnd
+)
+
 // CartYieldHook allows a cartridge to halt execution if the cartridge
 // coprocessor has reached a breakpoint or some other yield point (eg.
 // undefined behaviour)
 type CartYieldHook interface {
-	// CartYield returns true if the yield type cannot be handled without
-	// breaking into a debugging loop
-	//
-	// CartYield will also return true if the cartridge mapper should cancel
-	// coprocessing immediately
-	//
-	// all other yield reasons will return false
-	CartYield(CoProcYieldType) bool
+	CartYield(CoProcYieldType) YieldHookResponse
 }
 
 // StubCartYieldHook is a stub implementation for the CartYieldHook interface.
 type StubCartYieldHook struct{}
 
 // CartYield is a stub implementation for the CartYieldHook interface.
-func (_ StubCartYieldHook) CartYield(_ CoProcYieldType) bool {
-	return true
+func (_ StubCartYieldHook) CartYield(yld CoProcYieldType) YieldHookResponse {
+	if yld.Normal() {
+		return YieldHookContinue
+	}
+	return YieldHookEnd
 }
 
 // CartCoProc is implemented by cartridge mappers that have a coprocessor that

thumbnailer/anim.go
@@ -175,16 +175,24 @@ func (thmb *Anim) Create(cartload cartridgeloader.Loader, numFrames int) {
 			thmb.isEmulating.Store(false)
 		}()
 
-		err := setup.AttachCartridge(thmb.vcs, cartload, true)
+		// not resetting with attach cartridge becasue
+		err := setup.AttachCartridge(thmb.vcs, cartload, false)
 		if err != nil {
 			logger.Logf("thumbnailer", err.Error())
 			return
 		}
 
+		// add yield hook
+		thmb.vcs.Mem.Cart.SetYieldHook(thmb)
+
+		// reset after setting the yield hook
+		thmb.vcs.Reset()
+
 		// if we get to this point then we can be reasonably sure that the
 		// cartridgeloader is emulatable
 		thmb.isEmulating.Store(true)
 
+		// run until target frame has been generated
 		tgtFrame := thmb.vcs.TV.GetCoords().Frame + numFrames
 
 		err = thmb.vcs.Run(func() (govern.State, error) {
@@ -204,6 +212,22 @@ func (thmb *Anim) Create(cartload cartridgeloader.Loader, numFrames int) {
 			return
 		}
 	}()
+}
+
+// CartYield implements the mapper.CartYieldHook interface.
+func (thmb *Anim) CartYield(yield mapper.CoProcYieldType) mapper.YieldHookResponse {
+	if yield.Normal() {
+		return mapper.YieldHookContinue
+	}
+
+	// an unexpected yield type so end the thumbnail emulation
+	select {
+	case thmb.emulationQuit <- true:
+	default:
+	}
+
+	// indicate that the mapper should return immediately
+	return mapper.YieldHookEnd
 }
 
 // Resize implements the television.PixelRenderer interface

thumbnailer/image.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/jetsetilly/gopher2600/debugger/govern"
 	"github.com/jetsetilly/gopher2600/hardware"
+	"github.com/jetsetilly/gopher2600/hardware/memory/cartridge/mapper"
 	"github.com/jetsetilly/gopher2600/hardware/preferences"
 	"github.com/jetsetilly/gopher2600/hardware/television"
 	"github.com/jetsetilly/gopher2600/hardware/television/signal"
@@ -99,7 +100,7 @@ func (thmb *Image) String() string {
 }
 
 func (thmb *Image) wait() {
-	// drain existing emulationQuit channel
+	// drain emulationQuit channel
 	select {
 	case <-thmb.emulationQuit:
 	default:
@@ -135,6 +136,9 @@ func (thmb *Image) Create(state *rewind.State) {
 	// emulator still remaining
 	thmb.vcs.DetatchEmulationExtras()
 
+	// add yield hook
+	thmb.vcs.Mem.Cart.SetYieldHook(thmb)
+
 	// run until target frame has been generated
 	tgtFrame := thmb.vcs.TV.GetCoords().Frame + 1
 
@@ -155,6 +159,22 @@ func (thmb *Image) Create(state *rewind.State) {
 		logger.Logf("thumbnailer", err.Error())
 		return
 	}
+}
+
+// CartYield implements the mapper.CartYieldHook interface.
+func (thmb *Image) CartYield(yield mapper.CoProcYieldType) mapper.YieldHookResponse {
+	if yield.Normal() {
+		return mapper.YieldHookContinue
+	}
+
+	// an unexpected yield type so end the thumbnail emulation
+	select {
+	case thmb.emulationQuit <- true:
+	default:
+	}
+
+	// indicate that the mapper should return immediately
+	return mapper.YieldHookEnd
 }
 
 // Resize implements the television.PixelRenderer interface

commit ec6b181acdf48ce958919c7faa3bf15ea64104ba

corrected yield handling in cdf/dpc+
bug introduced in 61b4c07b65

hardware/memory/cartridge/cdf/cdf.go
@@ -369,27 +369,21 @@ func (cart *cdf) AccessVolatile(addr uint16, data uint8, poke bool) error {
 			// generate interrupt to update AUDV0 while running ARM code
 			fallthrough
 		case 0xff:
-			if cart.dev != nil {
-				// StartProfiling() only at the start of the CALLFN.
-				// ProcessProfiling() won't happen until all cycles have been
-				// accounted for
-				//
-				// * maybe this should be different in the case of a breakpoint
-				// being triggered but I'm not sure yet
-				cart.dev.StartProfiling()
+			runArm := func() {
+				if cart.dev != nil {
+					cart.dev.StartProfiling()
+					defer cart.dev.ProcessProfiling()
+				}
+				cart.state.yield = cart.runArm()
 			}
 
-			// call runArm() once and then check for yield conditions
-			cart.state.yield = cart.runArm()
-
 			// keep calling runArm() for as long as program has not ended
+			runArm()
 			for cart.state.yield.Type != mapper.YieldProgramEnded {
-				switch cart.yieldHook.CartYield(cart.state.yield.Type) {
-				case mapper.YieldHookEnd:
+				if cart.yieldHook.CartYield(cart.state.yield.Type) == mapper.YieldHookEnd {
 					break
-				case mapper.YieldHookContinue:
-					cart.state.yield = cart.runArm()
 				}
+				runArm()
 			}
 		}
 

hardware/memory/cartridge/dpcplus/dpcplus.go
@@ -504,20 +504,21 @@ func (cart *dpcPlus) AccessVolatile(addr uint16, data uint8, poke bool) error {
 		case 254:
 			fallthrough
 		case 255:
-			if cart.dev != nil {
-				cart.dev.StartProfiling()
+			runArm := func() {
+				if cart.dev != nil {
+					cart.dev.StartProfiling()
+					defer cart.dev.ProcessProfiling()
+				}
+				cart.state.yield = cart.runArm()
 			}
 
-			cart.state.yield = cart.runArm()
-
 			// keep calling runArm() for as long as program has not ended
+			runArm()
 			for cart.state.yield.Type != mapper.YieldProgramEnded {
-				switch cart.yieldHook.CartYield(cart.state.yield.Type) {
-				case mapper.YieldHookEnd:
+				if cart.yieldHook.CartYield(cart.state.yield.Type) == mapper.YieldHookEnd {
 					break
-				case mapper.YieldHookContinue:
-					cart.state.yield = cart.runArm()
 				}
+				runArm()
 			}
 		}
 