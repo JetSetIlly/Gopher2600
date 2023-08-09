af085b46eb145d9c97b8c0eb1d19c6ad4fceb33e
JetSetIlly <stephen.t.illingworth@gmail.com>
2023-08-09 16:09:44 +0100

    corrected VSTM and VLDM and VMOV instructions

--- hardware/memory/cartridge/arm/thumb2_fpu.go
+++ hardware/memory/cartridge/arm/thumb2_fpu.go
@@ -570,69 +570,86 @@ func (arm *ARM) decodeThumb2FPU32bitTransfer(opcode uint16) *DisasmEntry {
 	}
 
 	A := (arm.state.function32bitOpcodeHi & 0x00e0) >> 5
+	B := (opcode & 0x0060) >> 5
 
 	if L {
-		if A == 0b111 {
-			// "A7.7.246 VMRS" of "ARMv7-M"
-			if arm.decodeOnly {
-				return &DisasmEntry{
-					Is32bit:  true,
-					Operator: "VMRS",
-				}
-			}
-			Rt := (opcode & 0xf000) >> 12
-			if Rt == 15 {
-				arm.state.status.negative = arm.state.fpu.Status.N()
-				arm.state.status.zero = arm.state.fpu.Status.Z()
-				arm.state.status.carry = arm.state.fpu.Status.C()
-				arm.state.status.overflow = arm.state.fpu.Status.V()
+		if C {
+			if A&0b110 == 0b000 && B == 0b00 {
+				// "A7.7.242 VMOV (scalar to Arm core register)" of "ARMv7-M
+				panic("VMOV (scalar to Arm core register)")
 			} else {
-				arm.state.registers[Rt] = arm.state.fpu.Status.Value()
+				panic("undefined 32bit FPU transfer")
 			}
 		} else {
-			if C {
-				// "A7.7.242 VMOV (scalar to Arm core register)" of "ARMv7-M"
-				panic("VMOV (L && C)")
-			} else {
+			if A == 0b111 {
+				// "A7.7.246 VMRS" of "ARMv7-M"
+				if arm.decodeOnly {
+					return &DisasmEntry{
+						Is32bit:  true,
+						Operator: "VMRS",
+					}
+				}
+				Rt := (opcode & 0xf000) >> 12
+				if Rt == 15 {
+					arm.state.status.negative = arm.state.fpu.Status.N()
+					arm.state.status.zero = arm.state.fpu.Status.Z()
+					arm.state.status.carry = arm.state.fpu.Status.C()
+					arm.state.status.overflow = arm.state.fpu.Status.V()
+				} else {
+					arm.state.registers[Rt] = arm.state.fpu.Status.Value()
+				}
+			} else if A == 0b000 {
 				// "A7.7.243 VMOV (between Arm core register and single-precision register)" of "ARMv7-M"
 				if arm.decodeOnly {
 					return &DisasmEntry{
 						Is32bit:  true,
 						Operator: "VMOV",
-						Operand:  "to ARM core from SP",
+						Operand:  "between ARM core and SP register",
 					}
 				}
+				op := arm.state.function32bitOpcodeHi&0x0010 == 0x0010
 				Vn := arm.state.function32bitOpcodeHi & 0x000f
 				Rt := (opcode & 0xf000) >> 12
-				n := (opcode & 0x0080) >> 7
-				Rfpu := (Vn << 1) | n
+				N := (opcode & 0x0080) >> 7
+				Rn := (Vn << 1) | N
 
-				arm.state.registers[Rt] = arm.state.fpu.Registers[Rfpu]
+				if op {
+					arm.state.registers[Rt] = arm.state.fpu.Registers[Rn]
+				} else {
+					arm.state.fpu.Registers[Rn] = arm.state.registers[Rt]
+				}
+			} else {
+				panic("undefined 32bit FPU transfer")
 			}
 		}
 	} else {
-		if A == 0b111 {
-			// "A7.7.247 VMSR" of "ARMv7-M"
-			panic("unimplemented VMSR")
+		if C {
+			// "A7.7.243 VMOV (Arm core register to scalar)" of "ARMv7-M
+			panic("VMOV (Arm core register to scalar)")
 		} else {
-			if C {
-				// "A7.7.242 VMOV (scalar to Arm core register)" of "ARMv7-M
-				panic("VMOV (!L && V)")
+			if A == 0b111 {
+				// "A7.7.247 VMSR" of "ARMv7-M"
+				panic("unimplemented VMSR")
 			} else {
 				// "A7.7.243 VMOV (between Arm core register and single-precision register)" of "ARMv7-M"
 				if arm.decodeOnly {
 					return &DisasmEntry{
 						Is32bit:  true,
 						Operator: "VMOV",
-						Operand:  "from SP to ARM core",
+						Operand:  "between ARM core and SP register",
 					}
 				}
+				op := arm.state.function32bitOpcodeHi&0x0010 == 0x0010
 				Vn := arm.state.function32bitOpcodeHi & 0x000f
 				Rt := (opcode & 0xf000) >> 12
-				n := (opcode & 0x0080) >> 7
-				Rfpu := (Vn << 1) | n
+				N := (opcode & 0x0080) >> 7
+				Rn := (Vn << 1) | N
 
-				arm.state.fpu.Registers[Rfpu] = arm.state.registers[Rt]
+				if op {
+					arm.state.registers[Rt] = arm.state.fpu.Registers[Rn]
+				} else {
+					arm.state.fpu.Registers[Rn] = arm.state.registers[Rt]
+				}
 			}
 		}
 	}
@@ -646,183 +663,9 @@ func (arm *ARM) decodeThumb2FPURegisterLoadStore(opcode uint16) *DisasmEntry {
 	op := (arm.state.function32bitOpcodeHi & 0x01f0) >> 4
 	Rn := arm.state.function32bitOpcodeHi & 0x000f
 
-	switch op & 0b11011 {
-	case 0b10000:
-		fallthrough
-	case 0b11000:
-		// "A7.7.259 VSTR" of "ARMv7-M"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Is32bit:  true,
-				Operator: "VSTR",
-			}
-		}
+	maskedOp := op & 0b11011
 
-		add := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
-		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
-		Rn := arm.state.function32bitOpcodeHi & 0x000f
-		Vd := (opcode & 0xf000) >> 12
-		imm8 := opcode & 0x00ff
-		imm32 := uint32(imm8 << 2)
-
-		addr := arm.state.registers[Rn]
-		if add {
-			addr += imm32
-		} else {
-			addr -= imm32
-		}
-
-		if opcode&0x0100 == 0x0100 {
-			// 64bit floats (T1 encoding)
-			d := (D << 4) | Vd
-			arm.write32bit(addr, arm.state.fpu.Registers[d], true)
-			addr += 4
-			arm.write32bit(addr, arm.state.fpu.Registers[d+1], true)
-		} else {
-			// 32bit floats (T1 encoding)
-			d := (Vd << 1) | D
-			arm.write32bit(addr, arm.state.fpu.Registers[d], true)
-		}
-
-	case 0b10010:
-		switch Rn {
-		case 0b1101:
-			// "A7.7.252 VPUSH" of "ARMv7-M"
-			if arm.decodeOnly {
-				return &DisasmEntry{
-					Is32bit:  true,
-					Operator: "VPUSH",
-				}
-			}
-
-			D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
-			Vd := (opcode & 0xf000) >> 12
-			imm8 := opcode & 0x00ff
-			imm32 := uint32(imm8 << 2)
-
-			// extent of stack
-			addr := arm.state.registers[rSP] - imm32
-
-			if opcode&0x0100 == 0x0100 {
-				// 64bit floats (T1 encoding)
-				d := (D << 4) | Vd
-				for i := uint16(0); i < imm8; i += 2 {
-					arm.write32bit(addr, arm.state.fpu.Registers[d+i], true)
-					addr += 4
-					arm.write32bit(addr, arm.state.fpu.Registers[d+i+1], true)
-					addr += 4
-				}
-			} else {
-				// 32bit floats (T2 encoding)
-				d := (Vd << 1) | D
-				for i := uint16(0); i < imm8; i++ {
-					arm.write32bit(addr, arm.state.fpu.Registers[d+i], true)
-					addr += 4
-				}
-			}
-
-			arm.state.registers[rSP] -= uint32(imm32)
-
-		default:
-			panic("unimplemented FPU register load/save instruction (VSTM)")
-		}
-
-	case 0b10001:
-		fallthrough
-	case 0b11001:
-		// "A7.7.236 VLDR" of "ARMv7-M"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Is32bit:  true,
-				Operator: "VLDR",
-			}
-		}
-
-		add := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
-		imm8 := opcode & 0x00ff
-		imm32 := uint32(imm8 << 2)
-		Vd := (opcode & 0xf000) >> 12
-		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
-		Rn := arm.state.function32bitOpcodeHi & 0x000f
-
-		addr := arm.state.registers[Rn]
-		if Rn == rPC {
-			addr = alignTo32bits(arm.state.registers[rPC])
-		}
-		if add {
-			addr += imm32
-		} else {
-			addr -= imm32
-		}
-
-		if opcode&0x0100 == 0x0100 {
-			// 64bit floats (T1 encoding)
-			d := (D << 4) | Vd
-			arm.state.fpu.Registers[d] = arm.read32bit(addr, true)
-			addr += 4
-			arm.state.fpu.Registers[d+1] = arm.read32bit(addr, true)
-		} else {
-			// 32bit floats (T2 encoding)
-			d := (Vd << 1) | D
-			arm.state.fpu.Registers[d] = arm.read32bit(addr, true)
-		}
-
-	case 0b01001:
-		fallthrough
-	case 0b01011:
-		if Rn == 0b1101 && op&0b11011 == 0b01011 {
-			panic("unimplemented FPU register load/save instruction (VPOP)")
-		}
-
-		// "A7.7.235 VLDM" of "ARMv7-M"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Is32bit:  true,
-				Operator: "VLDM",
-			}
-		}
-
-		add := (arm.state.function32bitOpcodeHi & 0x0080) == 0x0080
-		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
-		wback := (arm.state.function32bitOpcodeHi & 0x0020) == 0x0020
-
-		Vd := (opcode & 0xf000) >> 12
-		imm8 := opcode & 0x00ff
-		imm32 := imm8 << 2
-
-		addr := arm.state.registers[Rn]
-		if add {
-			addr += uint32(imm32)
-		} else {
-			addr -= uint32(imm32)
-		}
-
-		if wback {
-			if add {
-				arm.state.registers[Rn] += uint32(imm32)
-			} else {
-				arm.state.registers[Rn] -= uint32(imm32)
-			}
-		}
-
-		if opcode&0x0100 == 0x0100 {
-			// 64bit floats (T1 encoding)
-			d := (D << 4) | Vd
-			for i := uint16(0); i < imm8; i += 2 {
-				arm.state.fpu.Registers[d+i] = arm.read32bit(addr, true)
-				addr += 4
-				arm.state.fpu.Registers[d+i+1] = arm.read32bit(addr, true)
-				addr += 4
-			}
-		} else {
-			// 32bit floats (T2 encoding)
-			d := (Vd << 1) | D
-			for i := uint16(0); i < imm8; i++ {
-				arm.state.fpu.Registers[d+i] = arm.read32bit(addr, true)
-				addr += 4
-			}
-		}
-	case 0b01010:
+	if maskedOp == 0b01000 || maskedOp == 0b01010 || (maskedOp == 0b10010 && Rn != 0b1101) {
 		// "A7.7.258 VSTM" of "ARMv7-M"
 		if arm.decodeOnly {
 			return &DisasmEntry{
@@ -831,15 +674,66 @@ func (arm *ARM) decodeThumb2FPURegisterLoadStore(opcode uint16) *DisasmEntry {
 			}
 		}
 
-		add := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
+		// P := arm.state.function32bitOpcodeHi&0x0100 == 0x0100
+		U := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
+		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
+		W := arm.state.function32bitOpcodeHi&0x0020 == 0x0020
+		Vd := (opcode & 0xf000) >> 12
+
 		imm8 := opcode & 0x00ff
 		imm32 := uint32(imm8 << 2)
-		Vd := (opcode & 0xf000) >> 12
-		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
-		Rn := arm.state.function32bitOpcodeHi & 0x000f
 
 		addr := arm.state.registers[Rn]
-		if add {
+		if !U {
+			addr -= imm32
+		}
+
+		if W {
+			if U {
+				arm.state.registers[Rn] += imm32
+			} else {
+				arm.state.registers[Rn] -= imm32
+			}
+		}
+
+		if opcode&0x0100 == 0x0100 {
+			// 64bit floats (T1 encoding)
+			// d := (D << 4) | Vd
+			panic("double VSTM")
+		} else {
+			// 32bit floats (T2 encoding)
+			d := (Vd << 1) | D
+
+			if imm8 == 0 || imm8+d > 32 {
+				panic("too many registers for VSTM")
+			}
+
+			for i := uint16(0); i < imm8; i++ {
+				arm.write32bit(addr, arm.state.fpu.Registers[d+i], true)
+				addr += 4
+			}
+		}
+
+		return nil
+	}
+
+	if maskedOp == 0b10000 || maskedOp == 0b11000 {
+		// "A7.7.259 VSTR" of "ARMv7-M"
+		if arm.decodeOnly {
+			return &DisasmEntry{
+				Is32bit:  true,
+				Operator: "VSTR",
+			}
+		}
+
+		U := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
+		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
+		Vd := (opcode & 0xf000) >> 12
+		imm8 := opcode & 0x00ff
+		imm32 := uint32(imm8 << 2)
+
+		addr := arm.state.registers[Rn]
+		if U {
 			addr += imm32
 		} else {
 			addr -= imm32
@@ -847,20 +741,151 @@ func (arm *ARM) decodeThumb2FPURegisterLoadStore(opcode uint16) *DisasmEntry {
 
 		if opcode&0x0100 == 0x0100 {
 			// 64bit floats (T1 encoding)
-			d := (D << 4) | Vd
-			arm.write32bit(addr, arm.state.fpu.Registers[d], true)
-			addr += 4
-			arm.write32bit(addr, arm.state.fpu.Registers[d+1], true)
+			// d := (D << 4) | Vd
+			panic("double precision VSTR")
 		} else {
-			// 32bit floats (T2 encoding)
+			// 32bit floats (T1 encoding)
 			d := (Vd << 1) | D
 			arm.write32bit(addr, arm.state.fpu.Registers[d], true)
 		}
-	default:
-		panic("unimplemented FPU register load/save instruction")
+
+		return nil
 	}
 
-	return nil
+	if maskedOp == 0b10010 && Rn == 0b1101 {
+		// "A7.7.252 VPUSH" of "ARMv7-M"
+		if arm.decodeOnly {
+			return &DisasmEntry{
+				Is32bit:  true,
+				Operator: "VPUSH",
+			}
+		}
+
+		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
+		Vd := (opcode & 0xf000) >> 12
+		imm8 := opcode & 0x00ff
+		imm32 := uint32(imm8 << 2)
+
+		// extent of stack
+		addr := arm.state.registers[rSP] - imm32
+		arm.state.registers[rSP] -= uint32(imm32)
+
+		if opcode&0x0100 == 0x0100 {
+			// 64bit floats (T1 encoding)
+			d := (D << 4) | Vd
+			for i := uint16(0); i < imm8; i += 2 {
+				// TODO: better endian-ness
+				arm.write32bit(addr, arm.state.fpu.Registers[d+i], true)
+				addr += 4
+				arm.write32bit(addr, arm.state.fpu.Registers[d+i+1], true)
+				addr += 4
+			}
+		} else {
+			// 32bit floats (T2 encoding)
+			d := (Vd << 1) | D
+
+			if imm8 == 0 || imm8+d > 32 {
+				panic("too many registers for VPUSH")
+			}
+
+			for i := uint16(0); i < imm8; i++ {
+				arm.write32bit(addr, arm.state.fpu.Registers[d+i], true)
+				addr += 4
+			}
+		}
+
+		return nil
+	}
+
+	if maskedOp == 0b10001 || maskedOp == 0b11001 {
+		// "A7.7.236 VLDR" of "ARMv7-M"
+		if arm.decodeOnly {
+			return &DisasmEntry{
+				Is32bit:  true,
+				Operator: "VLDR",
+			}
+		}
+
+		U := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
+		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
+		Vd := (opcode & 0xf000) >> 12
+		imm8 := opcode & 0x00ff
+		imm32 := uint32(imm8 << 2)
+
+		addr := arm.state.registers[Rn]
+		if Rn == rPC {
+			addr = alignTo32bits(arm.state.registers[rPC])
+		}
+		if U {
+			addr += imm32
+		} else {
+			addr -= imm32
+		}
+
+		if opcode&0x0100 == 0x0100 {
+			panic("double precision VLDR")
+		} else {
+			// 32bit floats (T2 encoding)
+			d := (Vd << 1) | D
+			arm.state.fpu.Registers[d] = arm.read32bit(addr, true)
+		}
+
+		return nil
+	}
+
+	if maskedOp == 0b10011 || (maskedOp == 0b01011 && Rn != 0b1101) {
+		// "A7.7.235 VLDM" of "ARMv7-M"
+		if arm.decodeOnly {
+			return &DisasmEntry{
+				Is32bit:  true,
+				Operator: "VLDM",
+			}
+		}
+
+		// P := arm.state.function32bitOpcodeHi&0x0100 == 0x0100
+		U := arm.state.function32bitOpcodeHi&0x0080 == 0x0080
+		D := (arm.state.function32bitOpcodeHi & 0x0040) >> 6
+		W := arm.state.function32bitOpcodeHi&0x0020 == 0x0020
+		Vd := (opcode & 0xf000) >> 12
+
+		imm8 := opcode & 0x00ff
+		imm32 := uint32(imm8 << 2)
+
+		addr := arm.state.registers[Rn]
+		if !U {
+			addr -= imm32
+		}
+
+		if W {
+			if U {
+				arm.state.registers[Rn] += imm32
+			} else {
+				arm.state.registers[Rn] -= imm32
+			}
+		}
+
+		if opcode&0x0100 == 0x0100 {
+			// 64bit floats (T1 encoding)
+			// d := (D << 4) | Vd
+			panic("double VLDM")
+		} else {
+			// 32bit floats (T2 encoding)
+			d := (Vd << 1) | D
+
+			if imm8 == 0 || imm8+d > 32 {
+				panic("too many registers for VLDM")
+			}
+
+			for i := uint16(0); i < imm8; i++ {
+				arm.state.fpu.Registers[d+i] = arm.read32bit(addr, true)
+				addr += 4
+			}
+		}
+
+		return nil
+	}
+
+	panic("unimplemented FPU register load/save instruction")
 }
 
 func (arm *ARM) decodeThumb2FPUTransfers(opcode uint16) *DisasmEntry {
