a0afd5c03b2161b8282f6ce616af090a7c0cc6d4
JetSetIlly <stephen.t.illingworth@gmail.com>
2023-08-30 18:16:46 +0100

    improved caching of decoded Thumb2 (16bit) instructions

--- hardware/memory/cartridge/arm/thumb2.go
+++ hardware/memory/cartridge/arm/thumb2.go
@@ -139,9 +139,9 @@ func (arm *ARM) decodeThumb2Miscellaneous(opcode uint16) decodeFunction {
 
 	if opcode&0xff00 == 0xbf00 {
 		if opcode&0xff0f == 0xbf00 {
-			return arm.decodeThumb2MemoryHints
+			return arm.decodeThumb2MemoryHints(opcode)
 		} else {
-			return arm.decodeThumb2IfThen
+			return arm.decodeThumb2IfThen(opcode)
 		}
 	} else {
 		if opcode&0xff00 == 0xbe00 {
@@ -152,11 +152,11 @@ func (arm *ARM) decodeThumb2Miscellaneous(opcode uint16) decodeFunction {
 				return nil
 			}
 		} else if opcode&0xff00 == 0xba00 {
-			return arm.decodeThumb2ReverseBytes
+			return arm.decodeThumb2ReverseBytes(opcode)
 		} else if opcode&0xffe8 == 0xb668 {
 			panic(fmt.Sprintf("unpredictable 16-bit (miscellaneous) thumb-2 instruction (%04x)", opcode))
 		} else if opcode&0xffe8 == 0xb660 {
-			return arm.decodeThumb2ChangeProcessorState
+			return arm.decodeThumb2ChangeProcessorState(opcode)
 		} else if opcode&0xfff0 == 0xb650 {
 			// set endianness
 		} else if opcode&0xfff0 == 0xb640 {
@@ -167,10 +167,10 @@ func (arm *ARM) decodeThumb2Miscellaneous(opcode uint16) decodeFunction {
 			return arm.decodeThumbPushPopRegisters
 		} else if opcode&0xf500 == 0xb100 {
 			// compare and branch on (non-)zero
-			return arm.decodeThumb2CompareAndBranchOnNonZero
+			return arm.decodeThumb2CompareAndBranchOnNonZero(opcode)
 		} else if opcode&0xff00 == 0xb200 {
 			// sign/zero extend
-			return arm.decodeThumb2SignZeroExtend
+			return arm.decodeThumb2SignZeroExtend(opcode)
 		} else if opcode&0xff00 == 0xb000 {
 			// ** format 13 Add offset to stack pointer
 			// adjust stack pointer
@@ -181,60 +181,69 @@ func (arm *ARM) decodeThumb2Miscellaneous(opcode uint16) decodeFunction {
 	panic(fmt.Sprintf("undecoded 16-bit (miscellaneous) thumb-2 instruction (%04x)", opcode))
 }
 
-func (arm *ARM) decodeThumb2ReverseBytes(opcode uint16) *DisasmEntry {
+func (arm *ARM) decodeThumb2ReverseBytes(opcode uint16) decodeFunction {
 	opc := (opcode & 0x00c0) >> 6
 	Rn := (opcode & 0x0038) >> 3
 	Rd := opcode & 0x0007
 
 	switch opc {
 	case 0b00:
-		// "4.6.111 REV" of "Thumb-2 Supplement"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "REV",
+		return func(_ uint16) *DisasmEntry {
+			// "4.6.111 REV" of "Thumb-2 Supplement"
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "REV",
+				}
 			}
-		}
 
-		v := arm.state.registers[Rn]
-		r := ((v & 0x000000ff) << 24) | ((v & 0x0000ff00) << 8) | ((v & 0x00ff0000) >> 8) | ((v & 0xff000000) >> 24)
-		arm.state.registers[Rd] = r
+			v := arm.state.registers[Rn]
+			r := ((v & 0x000000ff) << 24) | ((v & 0x0000ff00) << 8) | ((v & 0x00ff0000) >> 8) | ((v & 0xff000000) >> 24)
+			arm.state.registers[Rd] = r
+			return nil
+		}
 	case 0b01:
-		// "4.6.112 REV16" of "Thumb-2 Supplement"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "REV16",
+		return func(_ uint16) *DisasmEntry {
+			// "4.6.112 REV16" of "Thumb-2 Supplement"
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "REV16",
+				}
 			}
-		}
 
-		v := arm.state.registers[Rn]
-		r := ((v & 0x00ff0000) << 8) | ((v & 0xff000000) >> 8) | ((v & 0x000000ff) << 8) | ((v & 0x0000ff00) >> 8)
-		arm.state.registers[Rd] = r
+			v := arm.state.registers[Rn]
+			r := ((v & 0x00ff0000) << 8) | ((v & 0xff000000) >> 8) | ((v & 0x000000ff) << 8) | ((v & 0x0000ff00) >> 8)
+			arm.state.registers[Rd] = r
+			return nil
+		}
 	default:
 		panic(fmt.Sprintf("unimplemented thumb2 reverse byte instruction (%02b)", opc))
 	}
-
-	return nil
 }
 
-func (arm *ARM) decodeThumb2ChangeProcessorState(opcode uint16) *DisasmEntry {
-	if arm.decodeOnly {
-		return &DisasmEntry{
-			Operator: "CPSID",
+func (arm *ARM) decodeThumb2ChangeProcessorState(opcode uint16) decodeFunction {
+	return func(_ uint16) *DisasmEntry {
+		if arm.decodeOnly {
+			return &DisasmEntry{
+				Operator: "CPSID",
+			}
 		}
+		logger.Logf("ARM7", "CPSID instruction does nothing")
+		return nil
 	}
-	logger.Logf("ARM7", "CPSID instruction does nothing")
-	return nil
 }
 
-func (arm *ARM) decodeThumb2MemoryHints(opcode uint16) *DisasmEntry {
+func (arm *ARM) decodeThumb2MemoryHints(opcode uint16) decodeFunction {
 	hint := (opcode & 0x00f0) >> 4
 
 	switch hint {
 	case 0b000:
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "NOP",
+		return func(_ uint16) *DisasmEntry {
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "NOP",
+				}
 			}
+			return nil
 		}
 	case 0b001:
 		panic("unimplemented YIELD instruction")
@@ -244,43 +253,49 @@ func (arm *ARM) decodeThumb2MemoryHints(opcode uint16) *DisasmEntry {
 		panic("unimplemented WFI instruction")
 	case 0b100:
 		panic("unimplemented SEV instruction")
-	default:
-		panic(fmt.Sprintf("undecoded 16bit (memory hint) thumb-2 instruction (%03b)", hint))
 	}
-	return nil
+
+	panic(fmt.Sprintf("undecoded 16bit (memory hint) thumb-2 instruction (%03b)", hint))
 }
 
-func (arm *ARM) decodeThumb2IfThen(opcode uint16) *DisasmEntry {
-	if arm.decodeOnly {
-		return &DisasmEntry{
-			Operator: "IT",
+func (arm *ARM) decodeThumb2IfThen(opcode uint16) decodeFunction {
+	itMask := uint8(opcode & 0x000f)
+	itCond := uint8((opcode & 0x00f0) >> 4)
+
+	return func(_ uint16) *DisasmEntry {
+		// "4.6.39 IT" of "Thumb-2 Supplement"
+
+		if arm.decodeOnly {
+			return &DisasmEntry{
+				Operator: "IT",
+			}
 		}
-	}
 
-	if arm.state.status.itMask != 0b0000 {
-		panic("unpredictable IT instruction - already in an IT block")
-	}
-
-	arm.state.status.itMask = uint8(opcode & 0x000f)
-	arm.state.status.itCond = uint8((opcode & 0x00f0) >> 4)
-
-	// switch table similar to the one in thumbConditionalBranch()
-	switch arm.state.status.itCond {
-	case 0b1110:
-		// any (al)
-		if !(arm.state.status.itMask == 0x1 || arm.state.status.itMask == 0x2 || arm.state.status.itMask == 0x4 || arm.state.status.itMask == 0x8) {
-			// it is not valid to specify an "else" for the "al" condition
-			// because it is not possible to negate
-			panic("unpredictable IT instruction - else for 'al' condition ")
+		if arm.state.status.itMask != 0b0000 {
+			panic("unpredictable IT instruction - already in an IT block")
 		}
-	case 0b1111:
-		panic("unpredictable IT instruction - first condition data is 1111")
-	}
 
-	return nil
+		arm.state.status.itMask = itMask
+		arm.state.status.itCond = itCond
+
+		// switch table similar to the one in thumbConditionalBranch()
+		switch arm.state.status.itCond {
+		case 0b1110:
+			// any (al)
+			if !(arm.state.status.itMask == 0x1 || arm.state.status.itMask == 0x2 || arm.state.status.itMask == 0x4 || arm.state.status.itMask == 0x8) {
+				// it is not valid to specify an "else" for the "AL" condition
+				// because it is not possible to negate
+				panic("unpredictable IT instruction - else for 'AL' condition ")
+			}
+		case 0b1111:
+			panic("unpredictable IT instruction - first condition data is 1111")
+		}
+
+		return nil
+	}
 }
 
-func (arm *ARM) decodeThumb2CompareAndBranchOnNonZero(opcode uint16) *DisasmEntry {
+func (arm *ARM) decodeThumb2CompareAndBranchOnNonZero(opcode uint16) decodeFunction {
 	// "4.6.22 CBNZ" of "Thumb-2 Supplement"
 	//
 	// and
@@ -292,83 +307,95 @@ func (arm *ARM) decodeThumb2CompareAndBranchOnNonZero(opcode uint16) *DisasmEntr
 	i := (opcode & 0x0200) >> 9
 	imm5 := (opcode & 0x00f8) >> 3
 
-	if nonZero {
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "CBNZ",
+	return func(_ uint16) *DisasmEntry {
+		if nonZero {
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "CBNZ",
+				}
+			}
+		} else {
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "CBZ",
+				}
 			}
 		}
-	} else {
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "CBZ",
-			}
+
+		if nonZero && arm.state.registers[Rn] != 0 || !nonZero && arm.state.registers[Rn] == 0 {
+			imm32 := (imm5 << 1) | (i << 6)
+			arm.state.registers[rPC] += uint32(imm32) + 2
 		}
-	}
 
-	if nonZero && arm.state.registers[Rn] != 0 || !nonZero && arm.state.registers[Rn] == 0 {
-		imm32 := (imm5 << 1) | (i << 6)
-		arm.state.registers[rPC] += uint32(imm32) + 2
+		return nil
 	}
-
-	return nil
 }
 
-func (arm *ARM) decodeThumb2SignZeroExtend(opcode uint16) *DisasmEntry {
+func (arm *ARM) decodeThumb2SignZeroExtend(opcode uint16) decodeFunction {
 	op := (opcode & 0xc0) >> 6
 	Rm := (opcode & 0x38) >> 3
 	Rd := opcode & 0x07
 
 	switch op {
 	case 0b00:
-		// "4.6.187 SXTH" in "Thumb-2 Supplement"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "SXTH",
+		return func(_ uint16) *DisasmEntry {
+			// "4.6.187 SXTH" in "Thumb-2 Supplement"
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "SXTH",
+				}
 			}
-		}
 
-		arm.state.registers[Rd] = arm.state.registers[Rm] & 0x0000ffff
-		if arm.state.registers[Rd]&0x8000 == 0x8000 {
-			arm.state.registers[Rd] |= 0xffff0000
+			arm.state.registers[Rd] = arm.state.registers[Rm] & 0x0000ffff
+			if arm.state.registers[Rd]&0x8000 == 0x8000 {
+				arm.state.registers[Rd] |= 0xffff0000
+			}
+			return nil
 		}
 	case 0b01:
-		// "4.6.185 SXTB" in "Thumb-2 Supplement"
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "SXTB",
+		return func(_ uint16) *DisasmEntry {
+			// "4.6.185 SXTB" in "Thumb-2 Supplement"
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "SXTB",
+				}
 			}
-		}
 
-		arm.state.registers[Rd] = arm.state.registers[Rm] & 0x000000ff
-		if arm.state.registers[Rd]&0x80 == 0x80 {
-			arm.state.registers[Rd] |= 0xffffff00
+			arm.state.registers[Rd] = arm.state.registers[Rm] & 0x000000ff
+			if arm.state.registers[Rd]&0x80 == 0x80 {
+				arm.state.registers[Rd] |= 0xffffff00
+			}
+			return nil
 		}
 	case 0b10:
 		// unsigned extend halfword
-		// "4.6.226 UXTH" in "Thumb-2 Supplement"
-		// T1 Encoding
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "UXTH",
+		return func(_ uint16) *DisasmEntry {
+			// "4.6.226 UXTH" in "Thumb-2 Supplement"
+			// T1 Encoding
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "UXTH",
+				}
 			}
-		}
 
-		arm.state.registers[Rd] = arm.state.registers[Rm] & 0x0000ffff
+			arm.state.registers[Rd] = arm.state.registers[Rm] & 0x0000ffff
+			return nil
+		}
 	case 0b11:
-		// unsigned extend byte UXTB
-		// "4.6.224 UXTB" in "Thumb-2 Supplement"
-		// T1 Encoding
-		if arm.decodeOnly {
-			return &DisasmEntry{
-				Operator: "UXTB",
+		return func(_ uint16) *DisasmEntry {
+			// unsigned extend byte UXTB
+			// "4.6.224 UXTB" in "Thumb-2 Supplement"
+			// T1 Encoding
+			if arm.decodeOnly {
+				return &DisasmEntry{
+					Operator: "UXTB",
+				}
 			}
-		}
 
-		arm.state.registers[Rd] = arm.state.registers[Rm] & 0x000000ff
-	default:
-		panic(fmt.Sprintf("unhandled sign/zero extend instruction (op %02b)", op))
+			arm.state.registers[Rd] = arm.state.registers[Rm] & 0x000000ff
+			return nil
+		}
 	}
 
-	return nil
+	panic(fmt.Sprintf("unhandled sign/zero extend instruction (op %02b)", op))
 }
