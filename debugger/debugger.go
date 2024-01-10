JetSetIlly/Gopher2600

commit a7513e3e90c84512901ef2c2c5516bd12fe9efad

corrected how rewind enters and leaves the inputLoop particular in relation to videoCycles
tidied up inputLoop. more comments. clarification of concepts

removed PauseRendering() concept from television

debugger/debugger.go
@@ -20,6 +20,7 @@ import (
 	"os/signal"
 	"strings"
 	"sync/atomic"
+	"time"
 
 	"github.com/jetsetilly/gopher2600/cartridgeloader"
 	"github.com/jetsetilly/gopher2600/curated"
@@ -115,7 +116,6 @@ type Debugger struct {
 
 	// the Rewind system stores and restores machine state.
 	Rewind     *rewind.Rewind
-	rewinding  chan bool
 	deepPoking chan bool
 
 	// whether the state of the emulation has changed since the last time it
@@ -124,8 +124,7 @@ type Debugger struct {
 
 	// \/\/\/ inputLoop \/\/\/
 
-	// is current inputloop inside a clock cycle
-	isClockCycleInputLoop bool
+	eventCheckPulse *time.Ticker
 
 	// buffer for user input
 	input []byte
@@ -150,8 +149,12 @@ type Debugger struct {
 
 	// some operations require that the input loop be restarted to make sure
 	// continued operation is not inside a video cycle loop
-	inputLoopRestart   bool
-	inputLoopOnRestart func() error
+	unwindLoopRestart func() error
+
+	// after a rewinding event it is necessary to make sure the emulation is in
+	// the correct place
+	catchupContinue func() bool
+	catchupEnd      func()
 }
 
 // NewDebugger creates and initialises everything required for a new debugging
@@ -166,6 +169,9 @@ func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal,
 
 		// by definition the state of debugger has changed during startup
 		hasChanged: true,
+
+		// the ticker to indicate whether we should check for events in the inputLoop
+		eventCheckPulse: time.NewTicker(50 * time.Millisecond),
 	}
 
 	dbg.state.Store(emulation.Initialising)
@@ -212,7 +218,6 @@ func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal,
 	if err != nil {
 		return nil, curated.Errorf("debugger: %v", err)
 	}
-	dbg.rewinding = make(chan bool, 1)
 	dbg.deepPoking = make(chan bool, 1)
 
 	// plug TV BoundaryTrigger into CPU
@@ -236,6 +241,7 @@ func NewDebugger(tv *television.Television, scr gui.GUI, term terminal.Terminal,
 		UserInputHandler: dbg.userInputHandler,
 		IntEvents:        make(chan os.Signal, 1),
 		RawEvents:        make(chan func(), 32),
+		RawEventsReturn:  make(chan func(), 32),
 	}
 
 	// connect Interrupt signal to dbg.events.intChan
@@ -281,6 +287,32 @@ func (dbg *Debugger) State() emulation.State {
 }
 
 func (dbg *Debugger) setState(state emulation.State) {
+	switch dbg.State() {
+	case emulation.Rewinding:
+		err := dbg.tv.DisableRendering(false)
+		if err != nil {
+			logger.Logf("debugger", "emulation.Rewinding: %v", err)
+		}
+	}
+
+	switch state {
+	case emulation.Paused:
+		err := dbg.tv.Pause(true)
+		if err != nil {
+			logger.Logf("debugger", "emulation.Paused: %v", err)
+		}
+	case emulation.Running:
+		err := dbg.tv.Pause(false)
+		if err != nil {
+			logger.Logf("debugger", "emulation.Running: %v", err)
+		}
+	case emulation.Rewinding:
+		err := dbg.tv.DisableRendering(true)
+		if err != nil {
+			logger.Logf("debugger", "emulation.Rewinding: %v", err)
+		}
+	}
+
 	dbg.state.Store(state)
 }
 
@@ -340,16 +372,12 @@ func (dbg *Debugger) Start(initScript string, cartload cartridgeloader.Loader) e
 		}
 
 		// handle inputLoopRestart and any on-restart function
-		if dbg.inputLoopRestart {
-			if dbg.inputLoopOnRestart != nil {
-				err := dbg.inputLoopOnRestart()
-				if err != nil {
-					logger.Log("input loop restart", err.Error())
-				}
+		if dbg.unwindLoopRestart != nil {
+			err := dbg.unwindLoopRestart()
+			if err != nil {
+				logger.Log("input loop restart", err.Error())
 			}
-
-			dbg.inputLoopRestart = false
-			dbg.inputLoopOnRestart = nil
+			dbg.unwindLoopRestart = nil
 		} else {
 			done = true
 		}
@@ -385,13 +413,6 @@ func (dbg *Debugger) reset() error {
 	dbg.Rewind.Reset()
 	dbg.lastResult = &disassembly.Entry{Result: execution.Result{Final: true}}
 	return nil
-}
-
-// in the event that the input loop needs to be unwound and restarted then use
-// the unwindInputLoop() function for convenience.
-func (dbg *Debugger) unwindInputLoop(onRestart func() error) {
-	dbg.inputLoopRestart = true
-	dbg.inputLoopOnRestart = onRestart
 }
 
 // attachCartridge makes sure that the cartridge loaded into vcs memory and the

debugger/commands.go
@@ -184,7 +184,7 @@ func (dbg *Debugger) processTokens(tokens *commandline.Tokens) error {
 	case cmdReset:
 		// resetting in the middle of a CPU instruction requires the input loop
 		// to be unwound before continuing
-		dbg.unwindInputLoop(dbg.reset)
+		dbg.unwindLoop(dbg.reset)
 		dbg.printLine(terminal.StyleFeedback, "machine reset")
 
 	case cmdRun:
@@ -244,7 +244,7 @@ func (dbg *Debugger) processTokens(tokens *commandline.Tokens) error {
 
 			dbg.setState(emulation.Stepping)
 
-			dbg.unwindInputLoop(func() error {
+			dbg.unwindLoop(func() error {
 				return dbg.Rewind.GotoCoords(f, s, c)
 			})
 
@@ -338,34 +338,29 @@ func (dbg *Debugger) processTokens(tokens *commandline.Tokens) error {
 		// using the debugger.PushRewind() function.
 		arg, ok := tokens.Get()
 		if ok {
-			// rewinding in the middle of a CPU instruction requires the input loop
-			// to be unwound before continuing
-			dbg.unwindInputLoop(func() error {
-				// stop emulation on rewind
-				dbg.runUntilHalt = false
+			// stop emulation on rewind
+			dbg.runUntilHalt = false
 
-				if arg == "LAST" {
-					dbg.setState(emulation.Rewinding)
-					dbg.Rewind.GotoLast()
-				} else if arg == "SUMMARY" {
-					dbg.printLine(terminal.StyleInstrument, dbg.Rewind.String())
-				} else {
-					frame, _ := strconv.Atoi(arg)
-					dbg.setState(emulation.Rewinding)
+			if arg == "LAST" {
+				dbg.setState(emulation.Rewinding)
+				dbg.unwindLoop(dbg.Rewind.GotoLast)
+			} else if arg == "SUMMARY" {
+				dbg.printLine(terminal.StyleInstrument, dbg.Rewind.String())
+			} else {
+				frame, _ := strconv.Atoi(arg)
+				dbg.setState(emulation.Rewinding)
+				dbg.unwindLoop(func() error {
 					err := dbg.Rewind.GotoFrame(frame)
 					if err != nil {
 						return err
 					}
-					frame = dbg.vcs.TV.GetState(signal.ReqFramenum)
-					dbg.printLine(terminal.StyleFeedback, fmt.Sprintf("rewind set to frame %d", frame))
-				}
-				return nil
-			})
+					return nil
+				})
+			}
+			return nil
 		}
 
 	case cmdGoto:
-		// note that we calling the rewind.GotoCoords() functions directly and not
-		// using the debugger.PushGotoCoords() function.
 		var clock int
 		var scanline = dbg.vcs.TV.GetState(signal.ReqScanline)
 		var frame = dbg.vcs.TV.GetState(signal.ReqFramenum)
@@ -382,10 +377,13 @@ func (dbg *Debugger) processTokens(tokens *commandline.Tokens) error {
 
 		dbg.setState(emulation.Rewinding)
 
-		err := dbg.Rewind.GotoCoords(frame, scanline, clock)
-		if err != nil {
-			return err
-		}
+		dbg.unwindLoop(func() error {
+			err := dbg.Rewind.GotoCoords(frame, scanline, clock)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
 
 	case cmdInsert:
 		cart, _ := tokens.Get()

debugger/commands_template.go
@@ -128,7 +128,6 @@ var commandTemplate = []string{
 
 	// halt conditions
 	cmdBreak + " [%<pc value>S|%<target>S %<value>N] {& %<value>S|%<target>S %<value>S}",
-
 	cmdTrap + " [%<target>S] {%<targets>S}",
 	cmdWatch + " (READ|WRITE) (STRICT) [%<address>S] (%<value>S)",
 	cmdTrace + " (%<address>S)",

debugger/events.go
@@ -37,7 +37,13 @@ func (dbg *Debugger) userInputHandler(ev userinput.Event) error {
 	return nil
 }
 
-func (dbg *Debugger) checkEvents() error {
+// readEventsHandler is called by inputLoop to make sure the program is
+// handling pushed events and/or user input.
+//
+// used alongside TermReadCheck() it means the inputLoop can react without
+// having to enter the TermRead() function. The TermRead() function is only
+// used when the emulation is halted.
+func (dbg *Debugger) readEventsHandler() error {
 	for {
 		select {
 		case <-dbg.events.IntEvents:
@@ -64,13 +70,17 @@ func (dbg *Debugger) checkEvents() error {
 			dbg.running = false
 
 		case ev := <-dbg.events.UserInput:
-			err := dbg.userInputHandler(ev)
+			err := dbg.events.UserInputHandler(ev)
 			if err != nil {
 				return err
 			}
 
 		case ev := <-dbg.events.RawEvents:
 			ev()
+
+		case ev := <-dbg.events.RawEventsReturn:
+			ev()
+			return nil
 
 		default:
 			return nil

debugger/inputloop.go
@@ -25,56 +25,201 @@ import (
 	"github.com/jetsetilly/gopher2600/disassembly"
 	"github.com/jetsetilly/gopher2600/emulation"
 	"github.com/jetsetilly/gopher2600/hardware/cpu/instructions"
+	"github.com/jetsetilly/gopher2600/hardware/television/signal"
 )
+
+// unwindLoop is called whenever it is required that the inputLoop/catchupLoop
+// converge on the next instruction boundary. in other words, if the emulation
+// is between CPU instructions, the loops must be unwound so that the emulation
+// can continue safely.
+//
+// generally, this means that unwindLoop should be called whenever a rewind
+// function is being called.
+func (dbg *Debugger) unwindLoop(onRestart func() error) {
+	dbg.unwindLoopRestart = onRestart
+}
+
+// CatchUpLoop implements the rewind.Runner interface.
+//
+// It is called from the rewind package and sets the functions that are
+// required for catchupLoop().
+func (dbg *Debugger) CatchUpLoop(frame int, scanline int, clock int) error {
+	// turn off TV's fps frame limiter
+	fpsCap := dbg.vcs.TV.SetFPSCap(false)
+
+	// we've already set emulation state to emulation.Rewinding
+
+	dbg.catchupContinue = func() bool {
+		nf := dbg.vcs.TV.GetState(signal.ReqFramenum)
+		ns := dbg.vcs.TV.GetState(signal.ReqScanline)
+		nc := dbg.vcs.TV.GetState(signal.ReqClock)
+
+		// returns true if we're to continue
+		return !(nf > frame || (nf == frame && ns > scanline) || (nf == frame && ns == scanline && nc >= clock))
+	}
+
+	dbg.catchupEnd = func() {
+		dbg.vcs.TV.SetFPSCap(fpsCap)
+		dbg.catchupContinue = nil
+		dbg.catchupEnd = nil
+		dbg.setState(emulation.Paused)
+		dbg.continueEmulation = false
+		dbg.runUntilHalt = false
+	}
+
+	dbg.PushRawEventReturn(func() {})
+
+	return nil
+}
+
+// catchupLoop is a special purpose loop designed to run inside of the inputLoop. it is called only
+// when catchupContinue has been set in CatchUpLoop(), which is called as a consequence of a rewind event.
+func (dbg *Debugger) catchupLoop(inputter terminal.Input) error {
+	callbackInstruction := func() error {
+		return dbg.ref.OnVideoCycle(dbg.lastBank)
+	}
+
+	// whether the catch up conditions have been met inside a video step (ie.
+	// between CPU instruction boundaries).
+	var endedInVideoStep bool
+
+	callbackVideo := func() error {
+		var err error
+
+		// update debugger the same way for video quantum as for cpu quantum
+		err = callbackInstruction()
+		if err != nil {
+			return err
+		}
+
+		if !endedInVideoStep && dbg.catchupContinue != nil && !dbg.catchupContinue() {
+			dbg.lastResult, err = dbg.Disasm.FormatResult(dbg.lastBank, dbg.vcs.CPU.LastResult, disassembly.EntryLevelExecuted)
+			if err != nil {
+				return err
+			}
+			endedInVideoStep = true
+			dbg.catchupEnd()
+			return dbg.inputLoop(inputter, true)
+		}
+
+		return nil
+	}
+
+	for dbg.catchupContinue() {
+		dbg.lastBank = dbg.vcs.Mem.Cart.GetBank(dbg.vcs.CPU.PC.Address())
+
+		switch dbg.quantum {
+		case QuantumInstruction:
+			err := dbg.vcs.Step(callbackInstruction)
+			if err != nil {
+				return err
+			}
+		case QuantumVideo:
+			err := dbg.vcs.Step(callbackVideo)
+			if err != nil {
+				return err
+			}
+		default:
+			err := fmt.Errorf("unknown quantum mode")
+			if err != nil {
+				return err
+			}
+		}
+
+		// make sure reflection has been updated at the end of the instruction
+		if err := dbg.ref.OnInstructionEnd(dbg.lastBank); err != nil {
+			return err
+		}
+
+		// catchup conditions have been met so return immediatly
+		if endedInVideoStep {
+			return nil
+		}
+	}
+
+	dbg.catchupEnd()
+
+	var err error
+
+	dbg.lastResult, err = dbg.Disasm.FormatResult(dbg.lastBank, dbg.vcs.CPU.LastResult, disassembly.EntryLevelExecuted)
+
+	return err
+}
 
 // inputLoop has two modes, defined by the clockCycle argument. when clockCycle
 // is true then user will be prompted every video cycle; when false the user
 // is prompted every cpu instruction.
 func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
-	dbg.isClockCycleInputLoop = clockCycle
+	var err error
 
-	// to speed things a bit we only check for input events every
-	// "inputCtDelay" iterations.
-	const inputCtDelay = 50
-	inputCt := 0
-
+	// unwindLoopRestart is checked frequently and will cause the inputLoop()
+	// to return early. the function also returns early
 	for dbg.running {
+		if dbg.unwindLoopRestart != nil {
+			return nil
+		}
+
+		// how we enter and leave the catchup loop is very important.
+		if dbg.catchupContinue != nil {
+			if clockCycle {
+				panic("refusing to run catchup loop inside a color clock cycle")
+			}
+
+			err = dbg.catchupLoop(inputter)
+			if err != nil {
+				return err
+			}
+
+			if dbg.unwindLoopRestart != nil {
+				return nil
+			}
+		}
+
 		// raise hasChanged flag every iteration
 		dbg.hasChanged = true
 
-		var err error
+		// checkTerm is used to decide whether to perform a full call to TermRead() and to potentially
+		// halt the inputLoop - which we don't want to do unless there is something to process
+		//
+		// it will be false unless TermReadCheck() returns true
 		var checkTerm bool
 
-		// check for events every inputCtDelay iteratins
-		inputCt++
-		if inputCt%inputCtDelay == 0 {
-			inputCt = 0
-
-			checkTerm = inputter.TermReadCheck()
-
-			err = dbg.checkEvents()
+		// the select will take the eventCheckPulse channel if there is a tick
+		// waiting only when we reach this point in the loop. it will not delay
+		// the loop if the tick has not happened yet
+		select {
+		case <-dbg.eventCheckPulse.C:
+			err = dbg.readEventsHandler()
 			if err != nil {
 				dbg.running = false
 				dbg.printLine(terminal.StyleError, "%s", err)
 			}
 
-			// check exit video loop
-			if dbg.inputLoopRestart {
+			// if debugger is no longer running after checking interrupts and
+			// events then break for loop
+			if !dbg.running {
+				break // dbg.running loop
+			}
+
+			// unwindLoopRestart or catchupContinue may have been set as a result
+			// of checkEvents()
+			if dbg.unwindLoopRestart != nil {
 				return nil
 			}
+			if dbg.catchupContinue != nil {
+				continue // dbg.running loop
+			}
+
+			checkTerm = inputter.TermReadCheck()
+		default:
 		}
 
-		// if debugger is no longer running after checking interrupts and
-		// events then break for loop
-		if !dbg.running {
-			break // for loop
-		}
-
-		// return immediately if this inputLoop() is a clockCycle, the current
-		// quantum mode has been changed to INSTRUCTION and the emulation has
-		// been asked to continue with (eg. with STEP)
+		// return immediately if this inputLoop() is a clockCycle AND the
+		// current quantum mode has been changed to instruction AND the
+		// emulation has been asked to continue (eg. with STEP)
 		//
 		// this is important in a very specific situation:
+		//
 		// a) the emulation has been in CLOCK quantum mode
 		// b) it is mid-way through a single CPU instruction
 		// c) the debugger has been changed to INSTRUCTION quantum mode
@@ -96,6 +241,16 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 			dbg.printLine(terminal.StyleFeedback, fmt.Sprintf(" <trace> %s", trace))
 		}
 
+		// check for halt conditions and process halt state
+		//
+		// note that at the moment if we've stopped after a catchupLoop then we
+		// may see break messages and commandOnHalt messages. this doesn't
+		// affect anything but it's arguably not the correct behaviour. in
+		// other words, rewinding should output any of these messages.
+		//
+		// TODO: halt messages should not be displayed after a rewind event
+
+		var haltEmulation bool
 		var stepTrapMessage string
 		var breakMessage string
 		var trapMessage string
@@ -114,7 +269,7 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 		}
 
 		// check for halt conditions
-		haltEmulation := stepTrapMessage != "" || breakMessage != "" ||
+		haltEmulation = stepTrapMessage != "" || breakMessage != "" ||
 			trapMessage != "" || watchMessage != "" ||
 			dbg.lastStepError || dbg.haltImmediately
 
@@ -146,11 +301,7 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 				}
 			}
 
-			// pause TV/GUI when emulation has halted
-			err = dbg.tv.Pause(true)
-			if err != nil {
-				return err
-			}
+			// set pause emulation state
 			dbg.setState(emulation.Paused)
 
 			// take note of current machine state if the emulation was in a running
@@ -172,7 +323,7 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 			dbg.continueEmulation = false
 
 			// read input from terminal inputter and parse/run commands
-			err = dbg.readTerminal(inputter)
+			err = dbg.termRead(inputter)
 			if err != nil {
 				if curated.Is(err, script.ScriptEnd) {
 					dbg.printLine(terminal.StyleFeedback, err.Error())
@@ -182,12 +333,15 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 			}
 
 			// hasChanged flag may have been false for a long time after the
-			// readTerminal() pause. set to true immediately.
+			// termRead() pause. set to true immediately.
 			dbg.hasChanged = true
 
-			// check exit video loop
-			if dbg.inputLoopRestart {
+			if dbg.unwindLoopRestart != nil {
 				return nil
+			}
+
+			if dbg.catchupContinue != nil {
+				continue // dbg.running loop
 			}
 
 			// unpause emulation if we're continuing emulation
@@ -200,10 +354,6 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 				// side-effects which would look ugly when we're only in fact
 				// stepping.
 				if dbg.stepTraps.isEmpty() {
-					err = dbg.tv.Pause(false)
-					if err != nil {
-						return err
-					}
 					if inputter.IsInteractive() {
 						dbg.setState(emulation.Running)
 					}
@@ -221,38 +371,48 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 		}
 
 		if checkTerm {
-			err := dbg.readTerminal(inputter)
+			err := dbg.termRead(inputter)
 			if err != nil {
 				return err
 			}
 
-			// check exit video loop
-			if dbg.inputLoopRestart {
+			if dbg.unwindLoopRestart != nil {
 				return nil
+			}
+
+			if dbg.catchupContinue != nil {
+				continue // dbg.running loop
 			}
 		}
 
 		if dbg.continueEmulation {
-			// input loops with the clockCycle flag must not execute another
-			// call to vcs.Step()
+			// input loops with the clockCycle flag must not execute another call to vcs.Step()
+			//
+			// when this happens the previous calls to inputLoop() unwind partially and continue
+			// from where the function was originally called.
+			//
+			// this will be in the callbackVideo callback function in either of:
+			//
+			// a) step()
+			// b) catchupLoop()
+			//
+			// in both cases the emulation will continue inside the vcs.Step() function via the
+			// step() function
 			if clockCycle {
 				return nil
 			}
 
-			err = dbg.contEmulation(inputter)
+			err = dbg.step(inputter, false)
 			if err != nil {
 				return err
 			}
 
-			// make sure videocyle info is current
-			dbg.isClockCycleInputLoop = clockCycle
-
 			// check exit video loop
-			if dbg.inputLoopRestart {
+			if dbg.unwindLoopRestart != nil {
 				return nil
 			}
 
-			// command on step is process every time emulation has continued one step
+			// commandOnStep is processed every time emulation has step
 			if dbg.commandOnStep != nil {
 				err := dbg.processTokensList(dbg.commandOnStep)
 				if err != nil {
@@ -265,71 +425,16 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, clockCycle bool) error {
 	return nil
 }
 
-func (dbg *Debugger) readTerminal(inputter terminal.Input) error {
-	// get user input
-	inputLen, err := inputter.TermRead(dbg.input, dbg.buildPrompt(), dbg.events)
-
-	// check exit video loop
-	if dbg.inputLoopRestart {
-		return nil
-	}
-
-	// errors returned by UserRead() functions are very rich. the
-	// following block interprets the error carefully and proceeds
-	// appropriately
-	if err != nil {
-		if !curated.IsAny(err) {
-			// if the error originated from outside of gopher2600 then
-			// it is probably serious or unexpected
-			switch err {
-			case io.EOF:
-				// treat EOF errors the same as terminal.UserAbort
-				err = curated.Errorf(terminal.UserAbort)
-			default:
-				// the error is probably serious. exit input loop with err
-				return err
-			}
-		}
-
-		// we now know the we have an project specific error
-		if curated.Is(err, terminal.UserInterrupt) {
-			// user interrupts are triggered by the user (in a terminal
-			// environment, usually by pressing ctrl-c)
-			dbg.handleInterrupt(inputter)
-		} else if curated.Is(err, terminal.UserAbort) {
-			// like UserInterrupt but with no confirmation stage
-			dbg.running = false
-			return nil
-		} else {
-			// all other errors are passed upwards to the calling function
-			return err
-		}
-	}
-
-	// sometimes UserRead can return zero bytes read, we need to filter
-	// this out before we try any
-	if inputLen > 0 {
-		// parse user input, taking note of whether the emulation should
-		// continue
-		err = dbg.parseInput(string(dbg.input[:inputLen-1]), inputter.IsInteractive(), false)
-		if err != nil {
-			dbg.printLine(terminal.StyleError, "%s", err)
-		}
-	}
-
-	return nil
-}
-
-func (dbg *Debugger) contEmulation(inputter terminal.Input) error {
-	quantumInstruction := func() error {
+func (dbg *Debugger) step(inputter terminal.Input, catchup bool) error {
+	callbackInstruction := func() error {
 		return dbg.ref.OnVideoCycle(dbg.lastBank)
 	}
 
-	quantumVideo := func() error {
+	callbackVideo := func() error {
 		var err error
 
-		if dbg.inputLoopRestart {
-			return err
+		if dbg.unwindLoopRestart != nil {
+			return nil
 		}
 
 		// format last CPU execution result for vcs step. this is in addition
@@ -340,7 +445,7 @@ func (dbg *Debugger) contEmulation(inputter terminal.Input) error {
 		}
 
 		// update debugger the same way for video quantum as for cpu quantum
-		err = quantumInstruction()
+		err = callbackInstruction()
 		if err != nil {
 			return err
 		}
@@ -348,9 +453,12 @@ func (dbg *Debugger) contEmulation(inputter terminal.Input) error {
 		// for video quantums we need to run any OnStep commands before
 		// starting a new inputLoop
 		if dbg.commandOnStep != nil {
-			err := dbg.processTokensList(dbg.commandOnStep)
-			if err != nil {
-				dbg.printLine(terminal.StyleError, "%s", err)
+			// we don't do this if we're in catchup mode
+			if !catchup {
+				err := dbg.processTokensList(dbg.commandOnStep)
+				if err != nil {
+					dbg.printLine(terminal.StyleError, "%s", err)
+				}
 			}
 		}
 
@@ -369,9 +477,9 @@ func (dbg *Debugger) contEmulation(inputter terminal.Input) error {
 
 	switch dbg.quantum {
 	case QuantumInstruction:
-		stepErr = dbg.vcs.Step(quantumInstruction)
+		stepErr = dbg.vcs.Step(callbackInstruction)
 	case QuantumVideo:
-		stepErr = dbg.vcs.Step(quantumVideo)
+		stepErr = dbg.vcs.Step(callbackVideo)
 	default:
 		stepErr = fmt.Errorf("unknown quantum mode")
 	}
@@ -381,13 +489,15 @@ func (dbg *Debugger) contEmulation(inputter terminal.Input) error {
 		return err
 	}
 
-	if dbg.inputLoopRestart {
+	if dbg.unwindLoopRestart != nil {
 		return nil
 	}
 
 	// update rewind state if the last CPU instruction took place during a new
-	// frame event
-	dbg.Rewind.RecordFrameState()
+	// frame event. but not if we're in catchup mode
+	if !catchup {
+		dbg.Rewind.RecordFrameState()
+	}
 
 	// check step error. note that we format and store last CPU
 	// execution result whether there was an error or not. in the case
@@ -438,12 +548,70 @@ func (dbg *Debugger) contEmulation(inputter terminal.Input) error {
 	return nil
 }
 
+// termRead uses the TermRead() function of the inputter and process the output
+// as required by the debugger.
+func (dbg *Debugger) termRead(inputter terminal.Input) error {
+	// get user input from terminal.Input implementatio
+	inputLen, err := inputter.TermRead(dbg.input, dbg.buildPrompt(), dbg.events)
+
+	// check exit video loop and return immediately if required
+	if dbg.unwindLoopRestart != nil {
+		return nil
+	}
+
+	// errors returned by UserRead() functions are very rich. the
+	// following block interprets the error carefully and proceeds
+	// appropriately
+	if err != nil {
+		if !curated.IsAny(err) {
+			// if the error originated from outside of gopher2600 then
+			// it is probably serious or unexpected
+			switch err {
+			case io.EOF:
+				// treat EOF errors the same as terminal.UserAbort
+				err = curated.Errorf(terminal.UserAbort)
+			default:
+				// the error is probably serious. exit input loop with err
+				return err
+			}
+		}
+
+		// we now know the we have an project specific error
+		if curated.Is(err, terminal.UserInterrupt) {
+			// user interrupts are triggered by the user (in a terminal
+			// environment, usually by pressing ctrl-c)
+			dbg.handleInterrupt(inputter)
+		} else if curated.Is(err, terminal.UserAbort) {
+			// like UserInterrupt but with no confirmation stage
+			dbg.running = false
+			return nil
+		} else {
+			// all other errors are passed upwards to the calling function
+			return err
+		}
+	}
+
+	// sometimes UserRead can return zero bytes read, we need to filter
+	// this out before we try any parsing
+	//
+	// parsing may cause a number of inputLoop flags to changes. for example:
+	// continueEmulation or runUntilHalt.
+	if inputLen > 0 {
+		err = dbg.parseInput(string(dbg.input[:inputLen-1]), inputter.IsInteractive(), false)
+		if err != nil {
+			dbg.printLine(terminal.StyleError, "%s", err)
+		}
+	}
+
+	return nil
+}
+
 // interrupt errors that are sent back to the debugger need some special care
-// depending on the current state.
+// depending on the current state:
 //
-// - if script recording is active then recording is ended
-// - for non-interactive input set running flag to false immediately
-// - otherwise, prompt use for confirmation that the debugger should quit.
+// * if script recording is active then recording is ended
+// * for non-interactive input set running flag to false immediately
+// * otherwise, prompt use for confirmation that the debugger should quit.
 func (dbg *Debugger) handleInterrupt(inputter terminal.Input) {
 	if dbg.scriptScribe.IsActive() {
 		// script recording is in progress so we insert SCRIPT END into the

commit abc96c90a3615d90df5c768c70b2d71183bd1353

more frequent checking of userinput
checking of communication between gui and emulation goroutines
was too infrequent for effective paddle movement

problem introduced in a7513e3e90

debugger/debugger.go
@@ -122,7 +122,7 @@ type Debugger struct {
 	events *terminal.ReadEvents
 
 	// how often the events field should be checked
-	eventCheckPulse *time.Ticker
+	readEventsPulse *time.Ticker
 
 	// cartridge disassembly
 	//
@@ -313,8 +313,15 @@ func NewDebugger(opts CommandLineOptions, create CreateUserInterface) (*Debugger
 		// copy of the arguments
 		opts: opts,
 
-		// the ticker to indicate whether we should check for events in the inputLoop
-		eventCheckPulse: time.NewTicker(50 * time.Millisecond),
+		// the ticker to indicate whether we should check for events in the
+		// inputLoop. this used to be set to 50ms but that is way too long and
+		// introduces a three frame lag to input devices. put another way, it
+		// results in the emulation missing crucial input information. this is
+		// particularly noticeable with paddle input because the paddle
+		// resistance value (see paddle.go file in the peripherals/controllers
+		// package) is only updated every three frames, causing the paddle to be
+		// effectively running three times slower than the screen
+		readEventsPulse: time.NewTicker(1 * time.Millisecond),
 	}
 
 	// emulator is starting in the "none" mode (the advangatge of this is that

debugger/loop_debugger.go
@@ -185,7 +185,7 @@ func (dbg *Debugger) inputLoop(inputter terminal.Input, nonInstructionQuantum bo
 		// waiting only when we reach this point in the loop. it will not delay
 		// the loop if the tick has not happened yet
 		select {
-		case <-dbg.eventCheckPulse.C:
+		case <-dbg.readEventsPulse.C:
 			err = dbg.readEventsHandler()
 			if err != nil {
 				if errors.Is(err, terminal.UserInterrupt) {

debugger/loop_playmode.go
@@ -120,7 +120,7 @@ func (dbg *Debugger) playLoop() error {
 		// received. this reduces CPU usage when paused
 		if dbg.state.Load().(govern.State) == govern.Paused {
 			select {
-			case <-dbg.eventCheckPulse.C:
+			case <-dbg.readEventsPulse.C:
 				err := dbg.readEventsHandler()
 				if err != nil {
 					return govern.Ending, err
@@ -128,7 +128,7 @@ func (dbg *Debugger) playLoop() error {
 			}
 		} else {
 			select {
-			case <-dbg.eventCheckPulse.C:
+			case <-dbg.readEventsPulse.C:
 				err := dbg.readEventsHandler()
 				if err != nil {
 					return govern.Ending, err