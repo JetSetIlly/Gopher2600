JetSetIlly/Gopher2600

commit db2d84c397628f5fe514c3965f0980fdcf565d10

corrected placement of cycle callback in JSR instruction
there is a JSR cycle that does not affect the bus. I now believe that it
is related to the loading of the new program counter value

hardware/cpu/cpu.go
@@ -295,8 +295,8 @@ type read8BitPCeffect int
 const (
 	brk read8BitPCeffect = iota
 	newOpcode
-	loNibble
-	hiNibble
+	loByte
+	hiByte
 )
 
 // read8BitPC reads 8 bits from the memory location pointed to by PC
@@ -343,10 +343,10 @@ func (mc *CPU) read8BitPC(effect read8BitPCeffect) error {
 			return fmt.Errorf("cpu: unimplemented instruction (%#02x) at (%#04x)", v, mc.PC.Address()-1)
 		}
 
-	case loNibble:
+	case loByte:
 		mc.LastResult.InstructionData = uint16(v)
 
-	case hiNibble:
+	case hiByte:
 		mc.LastResult.InstructionData = (uint16(v) << 8) | mc.LastResult.InstructionData
 	}
 
@@ -614,7 +614,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		// therefore, we don't set the address and we read the value through the PC
 
 		// +1 cycle
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -628,7 +628,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		// in the branch() function
 
 		// +1 cycle
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -652,7 +652,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		//
 		// while we must trest the value as an address (ie. as uint16) we
 		// actually only read an 8 bit value so we store the value as uint8
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -722,7 +722,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 	case instructions.IndexedIndirect: // x indexing
 		// +1 cycle
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -755,7 +755,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 	case instructions.IndirectIndexed: // y indexing
 		// +1 cycle
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -849,7 +849,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 	case instructions.ZeroPageIndexedX:
 		// +1 cycles
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -875,7 +875,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		// used exclusively for LDX ZeroPage,y
 
 		// +1 cycles
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -1321,7 +1321,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 	case instructions.Jsr:
 		// +1 cycle
-		err = mc.read8BitPC(loNibble)
+		err = mc.read8BitPC(loByte)
 		if err != nil {
 			return err
 		}
@@ -1329,14 +1329,6 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		// the current value of the PC is now correct, even though we've only read
 		// one byte of the address so far. remember, RTS increments the PC when
 		// read from the stack, meaning that the PC will be correct at that point
-
-		// with that in mind, we're not sure what this extra cycle is for
-		// +1 cycle
-		mc.LastResult.Cycles++
-		err = mc.cycleCallback()
-		if err != nil {
-			return err
-		}
 
 		// push MSB of PC onto stack, and decrement SP
 		// +1 cycle
@@ -1364,8 +1356,8 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 			return err
 		}
 
-		// perform jump
-		err = mc.read8BitPC(hiNibble)
+		// +1 cycle
+		err = mc.read8BitPC(hiByte)
 		if err != nil {
 			return err
 		}
@@ -1378,6 +1370,13 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		address = mc.LastResult.InstructionData
 		if !mc.NoFlowControl {
 			mc.PC.Load(address)
+		}
+
+		// +1 cycle for the loading of the PC. we do this even if NoFlowControl is true
+		mc.LastResult.Cycles++
+		err = mc.cycleCallback()
+		if err != nil {
+			return err
 		}
 
 	case instructions.Rts:

commit 07e3b95f6ec6d1b315b58d3f8b0b8bb8f132433c

cycle ordering was broken the JSR instruction
bug introduced in db2d84c39, which would have first appeared in release
v0.27.0

this subtle bug broke the alignment of the ball and missile in the worm
whomper demo (by Manuel Rotschkar)

this fix adds the cycle boundary after the lo-byte read of the
destination address

hardware/cpu/cpu.go
@@ -1326,6 +1326,11 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		if err != nil {
 			return err
 		}
+		mc.LastResult.Cycles++
+		err = mc.cycleCallback()
+		if err != nil {
+			return err
+		}
 
 		// the current value of the PC is now correct, even though we've only read
 		// one byte of the address so far. remember, RTS increments the PC when
@@ -1337,24 +1342,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		if err != nil {
 			return err
 		}
-		mc.LastResult.Cycles++
-		err = mc.cycleCallback()
-		if err != nil {
-			return err
-		}
-
-		// adjust stack pointer
 		mc.SP.Add(0xff, false)
-
-		// address of the hibyte being pushed to the stack is on the bus for an
-		// additional cycle
-		//
-		// it's not entirely clear what's happening here. it might be to
-		// accomodate the SP adjustment but there is no dead cycle later on when
-		// the SP is adjusted again; and nor is there one in RTS when the return
-		// address is read from the stack
-		//
-		// +1 cycle
 		mc.LastResult.Cycles++
 		err = mc.cycleCallback()
 		if err != nil {
@@ -1367,14 +1355,12 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		if err != nil {
 			return err
 		}
+		mc.SP.Add(0xff, false)
 		mc.LastResult.Cycles++
 		err = mc.cycleCallback()
 		if err != nil {
 			return err
 		}
-
-		// adjust stack pointer
-		mc.SP.Add(0xff, false)
 
 		// +1 cycle
 		err = mc.read8BitPC(hiByte)