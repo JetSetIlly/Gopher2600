JetSetIlly/Gopher2600

commit 69620c503895f1f66220edcdb3cfcc2f219a08b6

amount of data used for mapper fingerprinting reduced to 1k

hardware/memory/cartridge/fingerprint.go
@@ -23,6 +23,10 @@ import (
 	"github.com/jetsetilly/gopher2600/cartridgeloader"
 )
 
+// fingerprinting beyond the first 1k can easily result in a false positive. for
+// some cartridge types the limit is well defined.
+const fingerprintLimit = 1024
+
 // if anwhere parameter is true then the ELF magic number can appear anywhere
 // in the data (the b parameter). otherwise it must appear at the beginning of
 // the data
@@ -75,7 +79,7 @@ func (cart *Cartridge) fingerprintPlusROM(loader cartridgeloader.Loader) bool {
 	b := make([]byte, 3)
 	loader.Seek(0, io.SeekStart)
 
-	for i := 0; i < cartridgeloader.FingerprintLimit-len(b); i++ {
+	for i := 0; i < fingerprintLimit-len(b); i++ {
 		n, err := loader.Read(b)
 		if n < len(b) {
 			break
@@ -148,7 +152,7 @@ func fingerprintMnetwork(loader cartridgeloader.Loader) bool {
 	b := make([]byte, 3)
 	loader.Seek(0, io.SeekStart)
 
-	for i := 0; i < cartridgeloader.FingerprintLimit-len(b); i++ {
+	for i := 0; i < fingerprintLimit-len(b); i++ {
 		n, err := loader.Read(b)
 		if n < len(b) {
 			break
@@ -277,7 +281,7 @@ func fingerprintCDF(loader cartridgeloader.Loader) (bool, string) {
 	b := make([]byte, 4)
 	loader.Seek(0, io.SeekStart)
 
-	for i := 0; i < cartridgeloader.FingerprintLimit-len(b); i++ {
+	for i := 0; i < fingerprintLimit-len(b); i++ {
 		n, err := loader.Read(b)
 		if n < len(b) {
 			break

hardware/peripherals/fingerprint.go
@@ -73,9 +73,17 @@ func Fingerprint(port plugging.PortID, loader cartridgeloader.Loader) ports.NewP
 	return controllers.NewStick
 }
 
+// fingerprinting beyond the first 64k or so of cartridge data can result in
+// very slow fingerprinting, particular if looking at a large file that is not a
+// cartridge file at all
+//
+// The 64k value is arbitary but in practice it's a sufficiently large value and
+// any data beyond that limit is unlikely to reveal anything of worth
+const fingerprintLimit = 65536
+
 func matchPattern(patterns [][]byte, loader cartridgeloader.Loader) bool {
 	for _, p := range patterns {
-		if loader.ContainsLimit(cartridgeloader.FingerprintLimit, p) {
+		if loader.ContainsLimit(fingerprintLimit, p) {
 			return true
 		}
 	}

cartridgeloader/loader.go
@@ -34,14 +34,6 @@ import (
 // the maximum amount of data to preload
 const maxPreloadLength = 1048576
 
-// Fingerprinting beyond the first 64k or so of cartridge data can result in
-// very slow fingerprinting, particular if looking at a large file that is not a
-// cartridge file at all
-//
-// The 64k value is arbitary but in practice it's a sufficiently large value and
-// any data beyond that limit is unlikely to reveal anything of worth
-const FingerprintLimit = 65536
-
 // use this function when assigning to the Loader.preload field
 func preloadLimit(data []byte) []byte {
 	return data[:min(len(data), maxPreloadLength)]

commit 6abcc220fce7be568dc2cf114ce8fd984eb85b45

mNetwork incorrectly fingerprinted
bug introduced in 69620c50

hardware/memory/cartridge/fingerprint.go
@@ -23,10 +23,6 @@ import (
 	"github.com/jetsetilly/gopher2600/cartridgeloader"
 )
 
-// fingerprinting beyond the first 1k can easily result in a false positive. for
-// some cartridge types the limit is well defined.
-const fingerprintLimit = 1024
-
 // if anwhere parameter is true then the ELF magic number can appear anywhere
 // in the data (the b parameter). otherwise it must appear at the beginning of
 // the data
@@ -79,6 +75,9 @@ func (cart *Cartridge) fingerprintPlusROM(loader cartridgeloader.Loader) bool {
 	b := make([]byte, 3)
 	loader.Seek(0, io.SeekStart)
 
+	// fingerprinting beyond the first 1k can easily result in a false positive
+	const fingerprintLimit = 1024
+
 	for i := 0; i < fingerprintLimit-len(b); i++ {
 		n, err := loader.Read(b)
 		if n < len(b) {
@@ -117,6 +116,11 @@ func fingerprint3ePlus(loader cartridgeloader.Loader) bool {
 }
 
 func fingerprintMnetwork(loader cartridgeloader.Loader) bool {
+	// limit size of MNetwork cartridges to 16k
+	if loader.Size() > 16384 {
+		return false
+	}
+
 	// Bump 'n' Jump is the fussiest mnetwork cartridge I've found. Matching
 	// hotspots:
 	//
@@ -152,7 +156,7 @@ func fingerprintMnetwork(loader cartridgeloader.Loader) bool {
 	b := make([]byte, 3)
 	loader.Seek(0, io.SeekStart)
 
-	for i := 0; i < fingerprintLimit-len(b); i++ {
+	for i := 0; i < loader.Size()-len(b); i++ {
 		n, err := loader.Read(b)
 		if n < len(b) {
 			break
@@ -168,7 +172,7 @@ func fingerprintMnetwork(loader cartridgeloader.Loader) bool {
 			// the incorrect mask caused a false positive for Solaris when the
 			// threshold is 2.
 			//
-			// (20/06/21) this caused a falso positive for "Hack Em Hangly Pacman"
+			// (20/06/21) this caused a false positive for "Hack Em Hangly Pacman"
 			// when the threshold is 1
 			//
 			// change to only look for mirrors 0x1f and 0xff
@@ -280,6 +284,9 @@ func fingerprintCDF(loader cartridgeloader.Loader) (bool, string) {
 
 	b := make([]byte, 4)
 	loader.Seek(0, io.SeekStart)
+
+	// fingerprinting beyond the first 1k can easily result in a false positive
+	const fingerprintLimit = 1024
 
 	for i := 0; i < fingerprintLimit-len(b); i++ {
 		n, err := loader.Read(b)