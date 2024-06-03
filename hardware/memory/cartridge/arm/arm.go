JetSetIlly/Gopher2600

commit 8e4838d5829d54a5e7974840ccf14832d2728276

removed prefs update ticker from ARM
it's not necessary for performance and worse, it causes memory churn due
to how the gui caching package works

hardware/memory/cartridge/arm/arm.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	"math"
 	"strings"
-	"time"
 
 	"github.com/jetsetilly/gopher2600/coprocessor"
 	"github.com/jetsetilly/gopher2600/coprocessor/developer/faults"
@@ -214,12 +213,7 @@ type ARM struct {
 	// state of the ARM. saveable and restorable
 	state *ARMState
 
-	// updating the preferences every time run() is executed can be slow
-	// (because the preferences need to be synchronised between tasks). the
-	// prefsPulse ticker slows the rate at which updatePrefs() is called
-	prefsPulse *time.Ticker
-
-	// read from ARM.prefs every prefsPulse tick
+	// updated on every call to run()
 	abortOnMemoryFault      bool
 	misalignedAccessIsFault bool
 
@@ -261,7 +255,8 @@ type ARM struct {
 	// interface to an option development package
 	dev coprocessor.CartCoProcDeveloper
 
-	// whether cycle count or not. set from ARM.prefs at the start of every arm.Run()
+	// immediateMode controls whether cycle count or not. value updated from
+	// updatePrefs()
 	//
 	// used to cut out code that is required only for cycle counting. See
 	// Icycle, Scycle and Ncycle fields which are called so frequently we
@@ -294,21 +289,13 @@ func NewARM(env *environment.Environment, mmap architecture.Map, mem SharedMemor
 		hook:           hook,
 		byteOrder:      binary.LittleEndian,
 		executionCache: make(map[uint32][]decodeFunction),
-
-		// updated on every updatePrefs(). these are reasonable defaults
-		Clk:         70.0,
-		clklenFlash: 4.0,
-
-		state: &ARMState{},
+		state:          &ARMState{},
 	}
 
 	// disassembly printed to stdout
 	if disassembleToStdout {
 		arm.disasm = &coprocessor.CartCoProcDisassemblerStdout{}
 	}
-
-	// slow prefs update by 100ms
-	arm.prefsPulse = time.NewTicker(time.Millisecond * 100)
 
 	switch arm.mmap.ARMArchitecture {
 	case architecture.ARM7TDMI:
@@ -436,8 +423,7 @@ func (arm *ARM) resetRegisters() {
 }
 
 // updatePrefs should be called periodically to ensure that the current
-// preference values are being used in the ARM emulation. see also the
-// prefsPulse ticker
+// preference values are being used in the ARM emulation
 func (arm *ARM) updatePrefs() {
 	// update clock value from preferences
 	arm.Clk = float32(arm.env.Prefs.ARM.Clock.Get().(float64))
@@ -756,11 +742,7 @@ func (arm *ARM) checkProgramMemory(force bool) {
 }
 
 func (arm *ARM) run() (coprocessor.CoProcYield, float32) {
-	select {
-	case <-arm.prefsPulse.C:
-		arm.updatePrefs()
-	default:
-	}
+	arm.updatePrefs()
 
 	// number of iterations. only used when in immediate mode
 	var iterations int

commit f157f7723b7315a36a95070f297dfa55e0f92d3d

ARM memory latency calculation was incorrect
since moving the ARM clock setting to be preference driven
latency and cycle stretching factor for flash access was performed
too early. this resulting in a stretching factor of zero

bug introduced in 8e4838d. this affected v0.31.0

hardware/memory/cartridge/arm/arm.go
@@ -327,10 +327,6 @@ func NewARM(env *environment.Environment, mmap architecture.Map, mem SharedMemor
 	// by definition the ARM starts in a program ended state
 	arm.state.yield.Type = coprocessor.YieldProgramEnded
 
-	// clklen for flash based on flash latency setting
-	latencyInMhz := (1 / (arm.mmap.FlashLatency / 1000000000)) / 1000000
-	arm.clklenFlash = float32(math.Ceil(float64(arm.Clk) / latencyInMhz))
-
 	arm.resetPeripherals()
 	arm.resetRegisters()
 	arm.updatePrefs()
@@ -447,6 +443,10 @@ func (arm *ARM) resetRegisters() {
 func (arm *ARM) updatePrefs() {
 	// update clock value from preferences
 	arm.Clk = float32(arm.env.Prefs.ARM.Clock.Get().(float64))
+
+	// clklen for flash based on flash latency setting
+	latencyInMhz := (1 / (arm.mmap.FlashLatency / 1000000000)) / 1000000
+	arm.clklenFlash = float32(math.Ceil(float64(arm.Clk) / latencyInMhz))
 
 	// get clock regulator from preferences
 	arm.cycleRegulator = float32(arm.env.Prefs.ARM.CycleRegulator.Get().(float64))