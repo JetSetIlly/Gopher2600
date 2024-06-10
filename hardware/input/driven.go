JetSetIlly/Gopher2600

commit e2c89701a3980fd0b403b444041a5208430066bc

added vet target to Makefile
this runs 'go vet' but filtered through awk so we ignore noisy errors
that we're not interested in

fixed vet error caused by unreturned error type in input package

hardware/input/driven.go
@@ -43,7 +43,7 @@ func (inp *Input) handleDrivenEvents() error {
 			select {
 			case inp.drivenInputEvent = <-inp.fromDriver:
 				if inp.checkForDriven {
-					fmt.Errorf("input: driven input received before previous input was processed")
+					return fmt.Errorf("input: driven input received before previous input was processed")
 				}
 			default:
 				done = true
@@ -58,7 +58,7 @@ func (inp *Input) handleDrivenEvents() error {
 		select {
 		case inp.drivenInputEvent = <-inp.fromDriver:
 			if inp.checkForDriven {
-				fmt.Errorf("input: driven input received before previous input was processed")
+				return fmt.Errorf("input: driven input received before previous input was processed")
 			}
 			inp.checkForDriven = true
 		default:

Makefile
@@ -19,7 +19,7 @@ endif
 
 
 ### support targets
-.PHONY: all clean tidy generate check_git check_glsl glsl_validate check_pandoc readme_spell patch_file_integrity
+.PHONY: all clean tidy generate check_git check_glsl glsl_validate check_pandoc readme_spell patch_file_integrity vet
 
 all:
 	@echo "use release target to build release binary"
@@ -53,7 +53,12 @@ ifeq (, $(shell which pandoc))
 	$(error "pandoc not installed")
 endif
 
-readme_spell: check_pandoc
+check_awk:
+ifeq (, $(shell which awk))
+	$(error "awk not installed")
+endif
+
+readme_spell: check_pandoc check_awk
 	@pandoc README.md -t plain | aspell -a | sed '1d;$d' | cut -d ' ' -f 2 | awk 'length($0)>1' | sort | uniq
 # sed is used to chop off the first line of aspell output, which is a version banner
 
@@ -65,6 +70,18 @@ patch_file_integrity: *.patch
 		git apply --check $$file; \
 	done
 	@echo "patch files are fine"
+
+# using awk to ignore noisy errors:
+# 1) unkeyed imgui.Vec2 and imgui.Vec4 structs are plentiful and harmless
+# 2) lines starting with # simply indicate packages that have vet errors
+vet: check_awk
+	@go vet ./... 2>&1 | awk '\
+	BEGIN {ct = 0}\
+	/imgui.go\/v[0-9]*\.Vec[2,4] struct literal uses unkeyed fields/ {next}\
+	/^# / {next}\
+	{ct++; print $0}\
+	END { if (ct > 0) {exit 1} }\
+	'
 
 ### testing targets
 .PHONY: test race race_debug

commit 3b759149f94a6ed1462b16505bf327142c0892b7

fixed driven input related crash
bug introduced in e2c89701

hardware/input/driven.go
@@ -42,9 +42,6 @@ func (inp *Input) handleDrivenEvents() error {
 
 			select {
 			case inp.drivenInputEvent = <-inp.fromDriver:
-				if inp.checkForDriven {
-					return fmt.Errorf("input: driven input received before previous input was processed")
-				}
 			default:
 				done = true
 				inp.checkForDriven = false
@@ -57,9 +54,6 @@ func (inp *Input) handleDrivenEvents() error {
 	if inp.fromDriver != nil {
 		select {
 		case inp.drivenInputEvent = <-inp.fromDriver:
-			if inp.checkForDriven {
-				return fmt.Errorf("input: driven input received before previous input was processed")
-			}
 			inp.checkForDriven = true
 		default:
 		}