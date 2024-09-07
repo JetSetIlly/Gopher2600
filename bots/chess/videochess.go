JetSetIlly/Gopher2600

commit e534c43181fb051917f2c4207e8e09b4c27f8383

increased absolute number of scanlines to 350
Some PAL games go a lot higher than 313 scanlines meaning that the TV
could never synchronise

this breaks existing recordings

extended visible bottom for PAL games
extended to 305 (Acid Drop is an example of a game
that needs this additional space

hardware/television/specification/specifications.go
@@ -207,9 +207,8 @@ const (
 // specification. The value here is arbitrary but it represents the natural
 // resonance of the vertical oscilator
 //
-// For reference, this is equivalent to a frequency of approximately 50Hz (the
-// precise frequency depends on the exact TV specification)
-const AbsoluteMaxScanlines = 313
+// For reference, this is equivalent to a frequency of approximately 45Hz
+const AbsoluteMaxScanlines = 350
 
 // The absolute number of color clock allowed by the TV regardless of
 // specification
@@ -322,11 +321,12 @@ func init() {
 	SpecSECAM.IdealVisibleBottom = SpecPAL.AtariSafeVisibleBottom
 
 	// extended values:
-	// - Spike's Peak likes a bottom scanline of 250 (NTSC). this is the largest requirement I've seen.
+	// - NTSC: Spike's Peak likes a bottom scanline of 250
+	// - PAL: Acid drop extends the main play area to around 305 scanlines
 	SpecNTSC.ExtendedVisibleTop = 23
 	SpecNTSC.ExtendedVisibleBottom = 250
 	SpecPAL.ExtendedVisibleTop = 30
-	SpecPAL.ExtendedVisibleBottom = 299
+	SpecPAL.ExtendedVisibleBottom = 305
 	SpecPAL_M.ExtendedVisibleTop = 20
 	SpecPAL_M.ExtendedVisibleBottom = 249
 	SpecSECAM.ExtendedVisibleTop = 30

hardware/television/television.go
@@ -536,6 +536,10 @@ func (tv *Television) Signal(sig signal.SignalAttributes) {
 					tv.state.vsync.flybackScanline = tv.state.vsync.scanline + adj
 					tv.state.scanline = (tv.state.scanline * recovery) / 100
 				} else if tv.state.vsync.scanline > tv.state.vsync.flybackScanline {
+					// note that if the programmed number of scanlines between VSYNCs is
+					// greater than the specification.AbsoluteMaxScanlines then this condition
+					// will always be true and the screen will roll forever
+
 					// if the current and previous frames were created from a synchronised position then
 					// move the flyback scanline to the new value
 					//

commit 5cf739e938aa67e46ab9a654ffcaeacb5448b9ed

fixed hash for starting image for videochess bot
bug introduced in e534c43181fb051917f2c4207e8e09b4c27f8383

and present in release v0.31.0 to v0.34.0

bots/chess/videochess.go
@@ -40,7 +40,11 @@ const (
 	squareHeight    = 17
 )
 
-var startingImage = [sha1.Size]byte{206, 150, 33, 111, 96, 82, 105, 37, 18, 218, 111, 99, 43, 70, 231, 84, 152, 24, 218, 150}
+// this is a hash of the entire screen image, including VBLANK/Overscan areas.
+// the size of the screen image is based on the AbsoluteMaxScanlines value in
+// the specification package. if that value changes then this hash value must
+// also change - or the videochess start screen will not be recognised
+var startingImage = [sha1.Size]byte{210, 170, 210, 183, 150, 8, 250, 204, 136, 161, 157, 0, 160, 50, 198, 146, 68, 9, 76, 45}
 
 var cursorImage = [...][sha1.Size]byte{
 	{83, 220, 207, 9, 64, 158, 200, 51, 169, 237, 1, 196, 0, 207, 32, 77, 219, 49, 35, 217},        // d4

hardware/television/specification/specifications.go
@@ -207,7 +207,10 @@ const (
 // specification. The value here is arbitrary but it represents the natural
 // resonance of the vertical oscilator
 //
-// For reference, this is equivalent to a frequency of approximately 45Hz
+// For reference, this is equivalent to a frequency of approximately 45Hz.
+//
+// Changing this value will likely affect any previously recorded hashes of the
+// full screen. For example, playback recordings or the videochess bot
 const AbsoluteMaxScanlines = 350
 
 // The absolute number of color clock allowed by the TV regardless of