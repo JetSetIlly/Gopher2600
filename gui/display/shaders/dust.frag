4e0344286d8855f5ef8300e3c31b75154b61899a
JetSetIlly <stephen.t.illingworth@gmail.com>
2024-11-15 23:08:34 +0000

    screen reflections
    corrected scaling/positioning of screen relative to bevel image. the
    position of the bevel inside the window needs to be taken into account
    in order for the offset values to be consistent

--- gui/display/shaders/dust.frag
+++ gui/display/shaders/dust.frag
@@ -9,7 +9,9 @@ void main()
 	Out_Color = Frag_Color * texture(Texture, Frag_UV);
 
 	// add a small amount of perlin noise to the image
-	float perlin = fract(sin(dot(Frag_UV, vec2(12.9898, 78.233))*Time) * 43758.5453);
-	perlin *= 0.03;
-	Out_Color.rgb += vec3(perlin);
+	if (Out_Color.a >= 1.0) {
+		float perlin = fract(sin(dot(Frag_UV, vec2(12.9898, 78.233))*Time) * 43758.5453);
+		perlin *= 0.03;
+		Out_Color.rgb += vec3(perlin);
+	}
 }
--- gui/sdlimgui/gl32_playscr.go
+++ gui/sdlimgui/gl32_playscr.go
@@ -73,10 +73,14 @@ func (sh *playscrShader) setAttributes(env shaderEnvironment) {
 		f := sh.img.playScr.bevelHeight / sh.img.playScr.screenWidth
 		f *= bevels.Selected.Scale
 
+		ww, wh := sh.img.plt.windowSize()
+		wwr := ww / sh.img.playScr.bevelWidth
+		whr := wh / sh.img.playScr.bevelHeight
+
 		env.projMtx[0][0] *= f
 		env.projMtx[1][1] *= f
-		env.projMtx[3][0] = -f + bevels.Selected.OffsetX
-		env.projMtx[3][1] = f + bevels.Selected.OffsetY
+		env.projMtx[3][0] = -f + (bevels.Selected.OffsetX / wwr)
+		env.projMtx[3][1] = f + (bevels.Selected.OffsetY / whr)
 
 		sh.crt.colorShader.setAttributes(env)
 	}
