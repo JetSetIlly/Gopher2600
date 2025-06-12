JetSetIlly/Gopher2600

commit 95b004a9d360746ee00e834e8af5da6bd410b24e

added PokeNotify interface
because the TIA/RIOT addresses and the actual chip implementation are separated,
there was a flaw whereby a POKE to a TIA/RIOT address would not affect the chip
itself. the PokeNotify interface connects the memory to the chip for the
purposes of poking

the best solution is probably combining the TIA and RIOT implementations with
the memory functions. the separation was a very early design choice when I
didn't understand the 2600 very well and did not have a clear idea about how to
structure a Go program. but this is sufficient for now

hardware/memory/vcs/riot.go
@@ -45,6 +45,9 @@ type RIOTMemory struct {
 
 	readSignal  bool
 	readAddress uint16
+
+	// after poke is called after a poke operation
+	pokeNotify chipbus.PokeNotify
 }
 
 // NewRIOTMemory is the preferred method of initialisation for the RIOT memory mem
@@ -64,6 +67,11 @@ func NewRIOTMemory(env *environment.Environment) *RIOTMemory {
 	return chip
 }
 
+// SetPokeNotify implements the chipbus.Memory interface
+func (mem *RIOTMemory) SetPokeNotify(pokeNotify chipbus.PokeNotify) {
+	mem.pokeNotify = pokeNotify
+}
+
 // Snapshot creates a copy of RIOTRegisters in its current state
 func (mem *RIOTMemory) Snapshot() *RIOTMemory {
 	n := *mem
@@ -75,34 +83,36 @@ func (mem *RIOTMemory) Snapshot() *RIOTMemory {
 // Reset contents of RIOTRegisters
 func (mem *RIOTMemory) Reset() {
 	for i := range mem.memory {
-		mem.memory[i] = 0
+		mem.Poke(uint16(i+int(mem.origin)), 0)
 	}
 }
 
-// Peek is an implementation of memory.DebugBus. Address must be normalised
+// Peek is an implementation of the memory.Area interface
 func (mem *RIOTMemory) Peek(address uint16) (uint8, error) {
-	// an address might be in RIOT memory space but it is not READABLE by the
-	// CPU and therefore should not be accessible by the Peek() function. the
-	// RIOT chip view of the address should be done via the RIOT chip emulation
 	if cpubus.ReadAddress[address] == cpubus.UnnamedAddress {
 		return 0, fmt.Errorf("%w: %04x", cpubus.AddressError, address)
 	}
 	return mem.memory[address^mem.origin], nil
 }
 
-// Poke is an implementation of memory.DebugBus. Address must be normalised
+// Poke is an implementation of the memory.Area interface
 func (mem *RIOTMemory) Poke(address uint16, value uint8) error {
-	// an address might be in RIOT memory space but it is not WRITEABLE by the
-	// CPU and therefore should not be accessible by the Peek() function. the
-	// RIOT chip view of the address should be done via the RIOT chip emulation
 	if cpubus.WriteAddress[address] == cpubus.UnnamedAddress {
 		return fmt.Errorf("%w: %04x", cpubus.AddressError, address)
 	}
 	mem.memory[address^mem.origin] = value
+
+	if mem.pokeNotify != nil {
+		mem.pokeNotify.AfterPoke(chipbus.ChangedRegister{
+			Address:  address,
+			Value:    value,
+			Register: cpubus.WriteAddress[address],
+		})
+	}
 	return nil
 }
 
-// ChipRead is an implementation of memory.ChipBus.
+// ChipRead is an implementation of memory.ChipBus
 func (mem *RIOTMemory) ChipHasChanged() (chipbus.ChangedRegister, bool) {
 	if mem.writeSignal {
 		mem.writeSignal = false
@@ -112,17 +122,17 @@ func (mem *RIOTMemory) ChipHasChanged() (chipbus.ChangedRegister, bool) {
 	return chipbus.ChangedRegister{}, false
 }
 
-// ChipWrite is an implementation of memory.ChipBus
+// ChipWrite implements the chipbus.Memory interface
 func (mem *RIOTMemory) ChipWrite(reg chipbus.Register, data uint8) {
 	mem.memory[reg] = data
 }
 
-// ChipRefer is an implementation of memory.ChipBus
+// ChipRefer implements the chipbus.Memory interface
 func (mem *RIOTMemory) ChipRefer(reg chipbus.Register) uint8 {
 	return mem.memory[reg]
 }
 
-// LastReadAddress is an implementation of memory.ChipBus
+// LastReadAddress implements the chipbus.Memory interface
 func (mem *RIOTMemory) LastReadAddress() (bool, uint16) {
 	if mem.readSignal {
 		mem.readSignal = false
@@ -131,7 +141,7 @@ func (mem *RIOTMemory) LastReadAddress() (bool, uint16) {
 	return false, 0
 }
 
-// Read is an implementation of memory.CPUBus. Address must be normalised
+// Read is an implementation of the memory.Area interface
 func (mem *RIOTMemory) Read(address uint16) (uint8, uint8, error) {
 	mem.readAddress = address
 	mem.readSignal = true
@@ -146,7 +156,7 @@ func (mem *RIOTMemory) Read(address uint16) (uint8, uint8, error) {
 	return mem.memory[address^mem.origin], 0xff, nil
 }
 
-// Write is an implementation of memory.CPUBus. Address must be normalised
+// Write is an implementation of the memory.Area interface
 func (mem *RIOTMemory) Write(address uint16, data uint8) error {
 	// check that the last write to this memory mem has been serviced. this
 	// shouldn't ever happen.

hardware/memory/vcs/tia.go
@@ -113,6 +113,9 @@ type TIAMemory struct {
 	writeSignal  bool
 	writeAddress uint16
 	writeData    uint8
+
+	// after poke is called after a poke operation
+	pokeNotify chipbus.PokeNotify
 }
 
 // NewTIAMemory is the preferred method of initialisation for the TIA memory chip
@@ -135,6 +138,11 @@ func NewTIAMemory(env *environment.Environment) *TIAMemory {
 	return chip
 }
 
+// SetPokeNotify implements the chipbus.Memory interface
+func (mem *TIAMemory) SetPokeNotify(pokeNotify chipbus.PokeNotify) {
+	mem.pokeNotify = pokeNotify
+}
+
 // Snapshot creates a copy of TIARegisters in its current state
 func (mem *TIAMemory) Snapshot() *TIAMemory {
 	n := *mem
@@ -146,30 +154,33 @@ func (mem *TIAMemory) Snapshot() *TIAMemory {
 // Reset contents of TIARegisters
 func (mem *TIAMemory) Reset() {
 	for i := range mem.memory {
-		mem.memory[i] = 0
+		mem.Poke(uint16(i+int(mem.origin)), 0)
 	}
 }
 
-// Peek is an implementation of memory.DebugBus. Address must be normalised
+// Peek is an implementation of the memory.Area interface
 func (mem *TIAMemory) Peek(address uint16) (uint8, error) {
-	// an address might be in TIA memory space but it is not READABLE by the
-	// CPU and therefore should not be accessible by the Peek() function. the
-	// TIA chip view of the address should be done via the TIA chip emulation
 	if cpubus.ReadAddress[address] == cpubus.UnnamedAddress {
 		return 0, fmt.Errorf("%w: %04x", cpubus.AddressError, address)
 	}
 	return mem.memory[address^mem.origin], nil
 }
 
-// Poke is an implementation of memory.DebugBus. Address must be normalised
+// Poke is an implementation of the memory.Area interface
 func (mem *TIAMemory) Poke(address uint16, value uint8) error {
-	// an address might be in TIA memory space but it is not WRITEABLE by the
-	// CPU and therefore should not be accessible by the Peek() function. the
-	// TIA chip view of the address should be done via the TIA chip emulation
 	if cpubus.WriteAddress[address] == cpubus.UnnamedAddress {
 		return fmt.Errorf("%w: %04x", cpubus.AddressError, address)
 	}
 	mem.memory[address^mem.origin] = value
+
+	if mem.pokeNotify != nil {
+		mem.pokeNotify.AfterPoke(chipbus.ChangedRegister{
+			Address:  address,
+			Value:    value,
+			Register: cpubus.WriteAddress[address],
+		})
+	}
+
 	return nil
 }
 
@@ -183,25 +194,22 @@ func (mem *TIAMemory) ChipHasChanged() (chipbus.ChangedRegister, bool) {
 	return chipbus.ChangedRegister{}, false
 }
 
-// ChipWrite is an implementation of memory.ChipBus
+// ChipWrite implements the chipbus.Memory interface
 func (mem *TIAMemory) ChipWrite(reg chipbus.Register, data uint8) {
 	mem.memory[reg] = data
 }
 
-// ChipRefer is an implementation of memory.ChipBus
+// ChipRefer implements the chipbus.Memory interface
 func (mem *TIAMemory) ChipRefer(reg chipbus.Register) uint8 {
 	return mem.memory[reg]
 }
 
-// LatsReadAddress is an implementation of memory.ChipBus
+// LastReadAddress implements the chipbus.Memory interface
 func (mem *TIAMemory) LastReadAddress() (bool, uint16) {
 	return false, 0
 }
 
-// Read is an implementation of memory.CPUBus. Address must be mapped
-//
-// Returned data should be masked and randomised as appropriate according to
-// the TIADrivenPins mask.
+// Read is an implementation of the memory.Area interface
 func (mem *TIAMemory) Read(address uint16) (uint8, uint8, error) {
 	// if the address is not a valid read adress for the CPU, then we blindly
 	// return the value in the array, which in that case will be zero
@@ -213,7 +221,7 @@ func (mem *TIAMemory) Read(address uint16) (uint8, uint8, error) {
 	return mem.memory[address^mem.origin], TIADrivenPins, nil
 }
 
-// Write is an implementation of memory.CPUBus. Address must be mapped
+// Write is an implementation of the memory.Area interface
 func (mem *TIAMemory) Write(address uint16, data uint8) error {
 	// signal that chip memory has been changed. see ChipHasChanged() function
 	mem.writeAddress = address

hardware/riot/riot.go
@@ -92,3 +92,9 @@ func (riot *RIOT) QuickStep() {
 	// see comment above about riot.Ports.Step()
 	riot.Ports.Step()
 }
+
+// AfterPoke implements the chipbus.PokeNotify interface
+func (riot *RIOT) AfterPoke(reg chipbus.ChangedRegister) {
+	_ = riot.Timer.Update(reg)
+	_ = riot.Ports.Update(reg)
+}

hardware/tia/tia.go
@@ -176,20 +176,20 @@ func (tia *TIA) Plumb(env *environment.Environment, tv TV, mem chipbus.Memory, r
 // Update checks to see if ChipData applies tot he TIA and updates accordingly.
 //
 // Returns true if ChipData has *not* been serviced.
-func (tia *TIA) Update(data chipbus.ChangedRegister) bool {
-	switch data.Register {
+func (tia *TIA) Update(reg chipbus.ChangedRegister) bool {
+	switch reg.Register {
 	case cpubus.VSYNC:
-		tia.sig.VSync = data.Value&0x02 == 0x02
+		tia.sig.VSync = reg.Value&0x02 == 0x02
 		return false
 
 	case cpubus.VBLANK:
 		// homebrew Donkey Kong shows the need for a delay of at least one
 		// cycle for VBLANK. see area just before score box on play screen
-		tia.futureVblank.Schedule(1, data.Value)
+		tia.futureVblank.Schedule(1, reg.Value)
 		tia.pendingEvents++
 
 		// the VBLANK register also affects the RIOT riot sub-system
-		tia.riot.Update(data)
+		tia.riot.Update(reg)
 
 		return false
 
@@ -771,4 +771,18 @@ func (tia *TIA) QuickStep(ct int) {
 
 	// send signal to television
 	tia.tv.Signal(tia.sig)
+}
+
+// AfterPoke implements the chipbus.PokeNotify interface
+func (tia *TIA) AfterPoke(reg chipbus.ChangedRegister) {
+	_ = tia.Update(reg)
+	_ = tia.Video.UpdateSpritePositioning(reg)
+	_ = tia.Video.UpdateSpriteVariationsEarly(reg)
+	_ = tia.Video.UpdateColor(reg)
+	_ = tia.Video.UpdatePlayfieldAndBackgroundColor(reg)
+	_ = tia.Video.UpdatePlayfield(reg)
+	_ = tia.Video.UpdateSpriteHMOVE(reg)
+	_ = tia.Video.UpdateSpriteVariations(reg)
+	_ = tia.Video.UpdateSpritePixels(reg)
+	_ = tia.Audio.ReadMemRegisters(reg)
 }

hardware/memory/chipbus/chipbus.go
@@ -34,9 +34,17 @@ type ChangedRegister struct {
 	Register cpubus.Register
 }
 
+// PokeNotify links a memory area to the chip implementation
+type PokeNotify interface {
+	AfterPoke(ChangedRegister)
+}
+
 // Memory defines the operations for the memory system when accessed from the
 // VCS chips (TIA, RIOT)
 type Memory interface {
+	// SetPokeNotify links a memory area with the chip implementation
+	SetPokeNotify(PokeNotify)
+
 	// ChipWrite writes the data to the chip memory
 	ChipWrite(reg Register, data uint8)
 

hardware/memory/vcs/ram.go
@@ -22,14 +22,13 @@ import (
 	"github.com/jetsetilly/gopher2600/hardware/memory/memorymap"
 )
 
-// RAM represents the 128bytes of RAM in the PIA 6532 chip, found in the Atari
-// VCS.
+// RAM represents the 128bytes of RAM in the PIA 6532 chip, found in the Atari VCS
 type RAM struct {
 	env *environment.Environment
 	RAM []uint8
 }
 
-// NewRAM is the preferred method of initialisation for the RAM memory area.
+// NewRAM is the preferred method of initialisation for the RAM memory area
 func NewRAM(env *environment.Environment) *RAM {
 	ram := &RAM{
 		env: env,
@@ -38,7 +37,7 @@ func NewRAM(env *environment.Environment) *RAM {
 	return ram
 }
 
-// Snapshot creates a copy of RAM in its current state.
+// Snapshot creates a copy of RAM in its current state
 func (ram *RAM) Snapshot() *RAM {
 	n := *ram
 	n.RAM = make([]uint8, len(ram.RAM))
@@ -46,7 +45,7 @@ func (ram *RAM) Snapshot() *RAM {
 	return &n
 }
 
-// Reset contents of RAM.
+// Reset contents of RAM
 func (ram *RAM) Reset() {
 	for i := range ram.RAM {
 		if ram.env != nil && ram.env.Prefs.RandomState.Get().(bool) {
@@ -61,25 +60,23 @@ func (ram *RAM) String() string {
 	return hex.Dump(ram.RAM)
 }
 
-// Peek is the implementation of memory.DebugBus. Address must be
-// normalised.
+// Peek is an implementation of the memory.Area interface
 func (ram *RAM) Peek(address uint16) (uint8, error) {
 	v, _, err := ram.Read(address)
 	return v, err
 }
 
-// Poke is the implementation of memory.DebugBus. Address must be
-// normalised.
+// Poke is an implementation of the memory.Area interface
 func (ram *RAM) Poke(address uint16, value uint8) error {
 	return ram.Write(address, value)
 }
 
-// Read is an implementatio of memory.ChipBus. Address must be normalised.
+// Read is an implementation of the memory.Area interface
 func (ram *RAM) Read(address uint16) (uint8, uint8, error) {
 	return ram.RAM[address^memorymap.OriginRAM], 0xff, nil
 }
 
-// Write is an implementatio of memory.ChipBus. Address must be normalised.
+// Write is an implementation of the memory.Area interface
 func (ram *RAM) Write(address uint16, data uint8) error {
 	ram.RAM[address^memorymap.OriginRAM] = data
 	return nil

hardware/vcs.go
@@ -206,6 +206,10 @@ func (vcs *VCS) Reset() error {
 	}
 	vcs.TIA.Audio = audio
 
+	// link memory and chips for poking purposes
+	vcs.Mem.TIA.SetPokeNotify(vcs.TIA)
+	vcs.Mem.RIOT.SetPokeNotify(vcs.RIOT)
+
 	// other areas of the VCS are simply reset because the emulation may have
 	// altered the part of the state that we do *not* want to reset. notably,
 	// memory may have a cartridge attached - we wouldn't want to lose that.

commit fdc32d436737effb0543017f16b30075035d7d1d

the changes in 95b004a9d3 caused some problems in the regression tests
the Reset() function in the tia and riot memory interfaces was calling Poke()
which isn't really the correct way to do it

hardware/memory/vcs/riot.go
@@ -83,7 +83,7 @@ func (mem *RIOTMemory) Snapshot() *RIOTMemory {
 // Reset contents of RIOTMemory
 func (mem *RIOTMemory) Reset() {
 	for i := range mem.memory {
-		mem.Poke(uint16(i+int(mem.origin)), 0)
+		mem.memory[i] = 0
 	}
 }
 

hardware/memory/vcs/tia.go
@@ -154,7 +154,7 @@ func (mem *TIAMemory) Snapshot() *TIAMemory {
 // Reset contents of TIAMemory
 func (mem *TIAMemory) Reset() {
 	for i := range mem.memory {
-		mem.Poke(uint16(i+int(mem.origin)), 0)
+		mem.memory[i] = 0
 	}
 }
 