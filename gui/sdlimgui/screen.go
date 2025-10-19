JetSetIlly/Gopher2600

commit b6cef6c715608784641898923936d67cd918ea04

careful queueing for frames for ffmpeg rendering
audio can get out-of-sync when rewinding. direct use of the wavwriter
package has been replaced by a shim in the video package. currently it
just forwards everything to wavwriter but there may be a better way of
controlling what is written to the wav such that rewinding is supported

a better option might be to prevent rewinding from happening when video
recording is active

fixed gl21 rendering

gui/sdlimgui/screen.go
@@ -77,8 +77,8 @@ type screen struct {
 	// the number of frames the emulation currently is ahead
 	frameQueueSlack int
 
-	// the frame number of the last frame generated for rendering
-	lastFrameGenerated atomic.Int64
+	// the frame number of the last frame generated for video recording
+	lastVideoFrame atomic.Int64
 }
 
 const (
@@ -466,6 +466,24 @@ func (scr *screen) NewScanline(scanline int) error {
 
 // SetPixels implements the television.PixelRenderer interface.
 func (scr *screen) SetPixels(sig []signal.SignalAttributes, last int) error {
+	if scr.img.rnd.isRecording() {
+		scr.crit.section.Lock()
+		defer func() {
+			scr.crit.section.Unlock()
+			scr.emuWait <- true
+			<-scr.emuWaitAck
+		}()
+		scr.crit.plotIdx = 0
+		if len(sig) > len(scr.crit.frameQueue[0].sigs) {
+			panic("signal framequeue in sdlimgui is not large enough for the number of signals from the TV")
+		}
+		copy(scr.crit.frameQueue[scr.crit.plotIdx].sigs, sig)
+		scr.crit.frameQueue[scr.crit.plotIdx].num = scr.crit.frameInfo.FrameNum
+		scr.crit.lastY = last / specification.ClksScanline
+		scr.crit.lastX = last % specification.ClksScanline
+		return nil
+	}
+
 	// wait flag indicates whether to slow down the emulation
 	// * will only be set in playmode
 	wait := false
@@ -476,6 +494,7 @@ func (scr *screen) SetPixels(sig []signal.SignalAttributes, last int) error {
 	if scr.img.isPlaymode() {
 		switch scr.img.dbg.State() {
 		case govern.Rewinding:
+			wait = true
 			fallthrough
 		case govern.Paused:
 			scr.crit.queueRecovery = scr.crit.frameQueueLen
@@ -485,11 +504,9 @@ func (scr *screen) SetPixels(sig []signal.SignalAttributes, last int) error {
 				scr.crit.queueRecovery--
 			}
 
-			if !scr.img.rnd.isRecording() {
-				scr.crit.plotIdx++
-				if scr.crit.plotIdx >= scr.crit.frameQueueLen || scr.crit.plotIdx >= len(scr.crit.frameQueue) {
-					scr.crit.plotIdx = 0
-				}
+			scr.crit.plotIdx++
+			if scr.crit.plotIdx >= scr.crit.frameQueueLen || scr.crit.plotIdx >= len(scr.crit.frameQueue) {
+				scr.crit.plotIdx = 0
 			}
 
 			// if plot index has crashed into the render index then set wait flag
@@ -676,8 +693,6 @@ func (scr *screen) generatePresentationPixels(idx int) {
 
 		offset += 4
 	}
-
-	scr.lastFrameGenerated.Store(int64(scr.crit.frameQueue[idx].num))
 }
 
 // copy pixels from queue to the live copy.
@@ -689,6 +704,25 @@ func (scr *screen) copyPixelsDebugmode() {
 
 // copy pixels from queue to the live copy.
 func (scr *screen) copyPixelsPlaymode() {
+	if scr.img.rnd.isRecording() {
+		select {
+		case <-scr.emuWait:
+			scr.emuWaitAck <- true
+		default:
+		}
+		scr.crit.section.Lock()
+		defer scr.crit.section.Unlock()
+		scr.crit.renderIdx = scr.crit.plotIdx
+		scr.generatePresentationPixels(scr.crit.renderIdx)
+
+		if scr.img.dbg.State() != govern.Running {
+			scr.lastVideoFrame.Store(int64(-1))
+		} else {
+			scr.lastVideoFrame.Store(int64(scr.crit.frameQueue[scr.crit.renderIdx].num))
+		}
+		return
+	}
+
 	// check if emulation is running slowly. if it is then we disable some
 	// rendering options that would be confusing because of the slow running
 	fps, hz := scr.img.dbg.VCS().TV.GetActualFPS()
@@ -750,11 +784,10 @@ func (scr *screen) copyPixelsPlaymode() {
 		scr.crit.prevRenderIdx[0] = scr.crit.renderIdx
 
 		// advance render index
-		if !scr.img.rnd.isRecording() {
-			scr.crit.renderIdx++
-			if scr.crit.renderIdx >= scr.crit.frameQueueLen || scr.crit.renderIdx >= len(scr.crit.frameQueue) {
-				scr.crit.renderIdx = 0
-			}
+		scr.crit.renderIdx = scr.crit.plotIdx
+		scr.crit.renderIdx++
+		if scr.crit.renderIdx >= scr.crit.frameQueueLen || scr.crit.renderIdx >= len(scr.crit.frameQueue) {
+			scr.crit.renderIdx = 0
 		}
 
 		// render index has bumped into the plotting index. revert render index

gui/sdlimgui/gl21.go
@@ -51,7 +51,7 @@ func newRenderer(img *SdlImgui) renderer {
 		img:      img,
 		textures: make(map[uint32]gl21Texture),
 	}
-	rnd.video = video.NewFFMPEG(rnd)
+	rnd.video = video.NewFFMPEG(rnd, img.dbg.TV())
 	return rnd
 }
 
@@ -113,7 +113,7 @@ func (rnd *gl21) render() {
 			logger.Log(logger.Allow, "gl21", err.Error())
 		}
 		defer func() {
-			rnd.video.Process()
+			rnd.video.Process(int(rnd.img.screen.lastVideoFrame.Load()))
 		}()
 	}
 

gui/sdlimgui/gl32.go
@@ -149,7 +149,7 @@ func (rnd *gl32) render() {
 			logger.Log(logger.Allow, "gl32", err.Error())
 		}
 		defer func() {
-			rnd.video.Process()
+			rnd.video.Process(int(rnd.img.screen.lastVideoFrame.Load()))
 		}()
 	}
 

video/audio.go
@@ -1,0 +1,50 @@
+// This file is part of Gopher2600.
+//
+// Gopher2600 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gopher2600 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
+
+package video
+
+import (
+	"github.com/jetsetilly/gopher2600/hardware/television/signal"
+	"github.com/jetsetilly/gopher2600/wavwriter"
+)
+
+type audio struct {
+	wavs *wavwriter.WavWriter
+}
+
+func newAudio(tempAudioFilename string) (*audio, error) {
+	wavs, err := wavwriter.NewWavWriter(tempAudioFilename)
+	if err != nil {
+		return nil, err
+	}
+	return &audio{
+		wavs: wavs,
+	}, nil
+}
+
+// SetAudio implements the television.AudioMixer interface
+func (au *audio) SetAudio(sig []signal.AudioSignalAttributes) error {
+	return au.wavs.SetAudio(sig)
+}
+
+// EndMixing implements the television.AudioMixer interface
+func (au *audio) EndMixing() error {
+	return au.wavs.EndMixing()
+}
+
+// Reset implements the television.AudioMixer interface
+func (au *audio) Reset() {
+	au.wavs.Reset()
+}

video/ffmpeg.go
@@ -24,7 +24,6 @@ import (
 	"github.com/jetsetilly/gopher2600/hardware/television"
 	"github.com/jetsetilly/gopher2600/logger"
 	"github.com/jetsetilly/gopher2600/resources/unique"
-	"github.com/jetsetilly/gopher2600/wavwriter"
 )
 
 // Profile is used to specify which set of ffmpeg settings to use
@@ -71,10 +70,11 @@ type FFMPEG struct {
 	pipe    io.WriteCloser
 
 	// pixels is read by the ReadPixels() function of the supplied Renderer interface
-	pixels []uint8
+	pixels            []uint8
+	lastFrameRendered int
 
 	// we record audio to a separate file and then mux it with the video in a final step
-	wavs *wavwriter.WavWriter
+	wavs television.AudioMixer
 }
 
 func NewFFMPEG(rnd Renderer, tv Television) *FFMPEG {
@@ -250,8 +250,8 @@ func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, hz flo
 
 	vid.pixels = make([]uint8, vid.width*vid.height*4)
 
-	// create wavwriter
-	vid.wavs, err = wavwriter.NewWavWriter(vid.tempAudioFilename)
+	// create wavwriter via package specific audio package
+	vid.wavs, err = newAudio(vid.tempAudioFilename)
 	if err != nil {
 		return fmt.Errorf("ffmpeg: %w", err)
 	}
@@ -268,10 +268,15 @@ func (vid *FFMPEG) IsRecording() bool {
 	return vid.pipe != nil
 }
 
-func (vid *FFMPEG) Process() {
+func (vid *FFMPEG) Process(framenum int) {
 	if vid.pipe == nil {
 		return
 	}
+
+	if framenum != -1 && framenum <= vid.lastFrameRendered {
+		return
+	}
+	vid.lastFrameRendered = framenum
 
 	// get pixel data for frame and forward it to the running command
 	vid.rnd.ReadPixels(vid.width, vid.height, vid.pixels)

commit 3530aa1761793c717bd9ff8e229252167ee30a18

fixed serious performance regression introduced in b6cef6c7

gui/sdlimgui/screen.go
@@ -822,7 +822,6 @@ func (scr *screen) copyPixelsPlaymode() {
 		scr.crit.prevRenderIdx[0] = scr.crit.renderIdx
 
 		// advance render index
-		scr.crit.renderIdx = scr.crit.plotIdx
 		scr.crit.renderIdx++
 		if scr.crit.renderIdx >= scr.crit.frameQueueLen || scr.crit.renderIdx >= len(scr.crit.frameQueue) {
 			scr.crit.renderIdx = 0