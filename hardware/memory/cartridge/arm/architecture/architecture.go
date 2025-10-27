JetSetIlly/Gopher2600

commit b491dfb30f298b109a6f5e5594ad75a6121d80f6

reworked how memory mapping is specified for the ARM
plusROM memory mapping correctly distinguishes flash, RAM and CCM memory
and labels each type correctly

DPCp ROMs unwrapped by default and emulated "natively" using the DPC+
emulation

changed how latency is handled and used by the clock stretching logic.
the results have been confirmed to be the same (using Davies'
Boulderdash as a test case)

hardware/memory/cartridge/arm/architecture/architecture.go
@@ -45,6 +45,13 @@ const (
 	MAMfull
 )
 
+// Regions of memory in the architecture's map
+type MemoryRegion struct {
+	Origin  uint32
+	Memtop  uint32
+	Latency float64
+}
+
 // Map of the differences between architectures. The differences are led by the
 // cartridge architecture.
 type Map struct {
@@ -54,14 +61,8 @@ type Map struct {
 	// some ARM architectures allow misaligned accesses for some instructions
 	MisalignedAccesses bool
 
-	FlashOrigin uint32
-	FlashMemtop uint32
-
-	SRAMOrigin uint32
-	SRAMMemtop uint32
-
-	// the memory latency of the Flash memory block (in nanoseconds)
-	FlashLatency float64
+	// list of memory regions
+	Regions map[string]*MemoryRegion
 
 	// peripherals
 
@@ -107,6 +108,7 @@ type Map struct {
 func NewMap(cart CartArchitecture) Map {
 	mmap := Map{
 		CartArchitecture: cart,
+		Regions:          make(map[string]*MemoryRegion),
 	}
 
 	switch mmap.CartArchitecture {
@@ -119,12 +121,16 @@ func NewMap(cart CartArchitecture) Map {
 		mmap.ARMArchitecture = ARM7TDMI
 		mmap.MisalignedAccesses = false
 
-		mmap.FlashOrigin = 0x00000000
-		mmap.FlashMemtop = 0x0fffffff
-		mmap.SRAMOrigin = 0x40000000
-		mmap.SRAMMemtop = 0x4fffffff
-
-		mmap.FlashLatency = 50.0
+		mmap.Regions["Flash"] = &MemoryRegion{
+			Origin:  0x00000000,
+			Memtop:  0x0fffffff,
+			Latency: 50.0,
+		}
+		mmap.Regions["SRAM"] = &MemoryRegion{
+			Origin:  0x40000000,
+			Memtop:  0x4fffffff,
+			Latency: 10.0,
+		}
 
 		mmap.HasMAM = true
 		mmap.MAMCR = 0xe01fc000
@@ -161,15 +167,24 @@ func NewMap(cart CartArchitecture) Map {
 		mmap.ARMArchitecture = ARMv7_M
 		mmap.MisalignedAccesses = true
 
-		mmap.FlashOrigin = 0x20000000
-		mmap.FlashMemtop = 0x2fffffff
-		mmap.SRAMOrigin = 0x10000000
-		mmap.SRAMMemtop = 0x1fffffff
+		mmap.Regions["Flash"] = &MemoryRegion{
+			Origin:  0x08020000,
+			Memtop:  0x0802ffff,
+			Latency: 50.0,
+		}
+		mmap.Regions["SRAM"] = &MemoryRegion{
+			Origin:  0x20000000,
+			Memtop:  0x2fffffff,
+			Latency: 10.0,
+		}
+		mmap.Regions["CCM"] = &MemoryRegion{
+			Origin:  0x10000000,
+			Memtop:  0x1fffffff,
+			Latency: 1.0,
+		}
 
-		mmap.FlashLatency = 10.0
-
-		// there is not MAM in this architecture but the effect of MAMfull is
-		// what we want
+		// there is no MAM in this architecture but the effect of MAMfull is what we want
+		mmap.HasMAM = false
 		mmap.PreferredMAMCR = MAMfull
 
 		mmap.HasTIM2 = true
@@ -200,7 +215,11 @@ func NewMap(cart CartArchitecture) Map {
 	return mmap
 }
 
-// IsFlash returns true if address is in flash memory range.
-func (mmap *Map) IsFlash(addr uint32) bool {
-	return addr >= mmap.FlashOrigin && addr <= mmap.FlashMemtop
+func (mmap *Map) AddrLatency(addr uint32) float64 {
+	for _, r := range mmap.Regions {
+		if addr >= r.Origin && addr <= r.Memtop {
+			return r.Latency
+		}
+	}
+	return 1.0
 }

hardware/memory/cartridge/ace/ace.go
@@ -65,8 +65,8 @@ func NewAce(env *environment.Environment, loader cartridgeloader.Loader) (mapper
 	cart.mem.Plumb(cart.arm)
 
 	logger.Logf(env, "ACE", "ccm: %08x to %08x", cart.mem.ccmOrigin, cart.mem.ccmMemtop)
-	logger.Logf(env, "ACE", "flash: %08x to %08x", cart.mem.downloadOrigin, cart.mem.downloadMemtop)
-	logger.Logf(env, "ACE", "buffer: %08x to %08x", cart.mem.bufferOrigin, cart.mem.bufferMemtop)
+	logger.Logf(env, "ACE", "flash: %08x to %08x", cart.mem.flashOrigin, cart.mem.flashMemtop)
+	logger.Logf(env, "ACE", "buffer: %08x to %08x", cart.mem.sramOrigin, cart.mem.sramMemtop)
 	logger.Logf(env, "ACE", "gpio: %08x to %08x", cart.mem.gpioOrigin, cart.mem.gpioMemtop)
 
 	return cart, nil
@@ -223,7 +223,7 @@ func (cart *Ace) Step(clock float32) {
 func (cart *Ace) CopyBanks() []mapper.BankContent {
 	c := make([]mapper.BankContent, 1)
 	c[0] = mapper.BankContent{Number: 0,
-		Data:    cart.mem.buffer,
+		Data:    cart.mem.sram,
 		Origins: []uint16{memorymap.OriginCart},
 	}
 	return c

hardware/memory/cartridge/ace/memory.go
@@ -65,20 +65,20 @@ type aceMemory struct {
 	gpioOrigin uint32
 	gpioMemtop uint32
 
-	// SRAM is called CCM in the Uno/PlusROM architecture
+	// CCM is a faster RAM than SRAM
 	ccm       []byte
 	ccmOrigin uint32
 	ccmMemtop uint32
 
-	// download memory is divided into three segments
-	download       []byte
-	downloadOrigin uint32
-	downloadMemtop uint32
+	// flash memory is divided into three segments
+	flash       []byte
+	flashOrigin uint32
+	flashMemtop uint32
 
-	// the buffer and ARM segments must be consecutive
-	buffer       []byte
-	bufferOrigin uint32
-	bufferMemtop uint32
+	// the sram and ARM segments must be consecutive
+	sram       []byte
+	sramOrigin uint32
+	sramMemtop uint32
 
 	// minimal interface to the ARM
 	arm            interruptARM
@@ -129,8 +129,8 @@ func newAceMemory(env *environment.Environment, data []byte, armPrefs *preferenc
 	}
 
 	// CCM creation
-	mem.ccm = make([]byte, 0x00010000) // 4k
-	mem.ccmOrigin = mem.model.SRAMOrigin
+	mem.ccm = make([]byte, 0x00002000) // 8k
+	mem.ccmOrigin = mem.model.Regions["CCM"].Origin
 	mem.ccmMemtop = mem.ccmOrigin + uint32(len(mem.ccm)) - 1
 
 	// read header
@@ -164,23 +164,23 @@ func newAceMemory(env *environment.Environment, data []byte, armPrefs *preferenc
 		(uint32(data[aceHeaderEntryPoint+3]) << 24)
 	logger.Logf(mem.env, "ACE", "header: entrypoint: %08x", mem.header.entry)
 
-	mem.download = data[:]
+	mem.flash = data[:]
 	switch mem.header.version {
 	case "ACE-PC00":
-		mem.downloadOrigin = 0x08020000
+		mem.flashOrigin = mem.model.Regions["Flash"].Origin
 		mem.header.entry = 0x1028
 		logger.Logf(mem.env, "ACE", "header: entrypoint adjusted to: %08x", mem.header.entry)
 	case "ACE-UF00":
-		mem.downloadOrigin = 0x08020000
+		mem.flashOrigin = mem.model.Regions["Flash"].Origin
 	case "ACE-2600":
 		fallthrough
 	default:
 		return nil, fmt.Errorf("ACE: version: %s not supported", mem.header.version)
 	}
-	mem.downloadMemtop = mem.downloadOrigin + uint32(len(mem.download))
+	mem.flashMemtop = mem.flashOrigin + uint32(len(mem.flash))
 
 	// the placement of data in memory revolves around the ARM entry point
-	mem.resetPC = arm.AlignTo16bits(mem.downloadOrigin + mem.header.entry)
+	mem.resetPC = arm.AlignTo16bits(mem.flashOrigin + mem.header.entry)
 	mem.resetLR = mem.resetPC
 	mem.resetSP = mem.ccmMemtop - 3
 
@@ -196,18 +196,18 @@ func newAceMemory(env *environment.Environment, data []byte, armPrefs *preferenc
 	}
 
 	// placing nullFunction at end of ARM program
-	nullFunctionAddress := mem.downloadMemtop + 1
+	nullFunctionAddress := mem.flashMemtop + 1
 
 	// the code location of the null function must not be on a 16bit boundary
 	if arm.IsAlignedTo16bits(nullFunctionAddress) {
 		logger.Logf(mem.env, "ACE", "correcting alignment at end of ARM program")
-		mem.download = append(mem.download, 0x00)
-		mem.downloadMemtop++
+		mem.flash = append(mem.flash, 0x00)
+		mem.flashMemtop++
 		nullFunctionAddress++
 	}
 
-	mem.download = append(mem.download, nullFunction...)
-	mem.downloadMemtop += uint32(len(nullFunction))
+	mem.flash = append(mem.flash, nullFunction...)
+	mem.flashMemtop += uint32(len(nullFunction))
 
 	// although the code location of the null function is on a 16bit boundary
 	// (see above), the code is reached by interwork branching. we're using the
@@ -221,24 +221,10 @@ func newAceMemory(env *environment.Environment, data []byte, armPrefs *preferenc
 
 	logger.Logf(mem.env, "ACE", "null function place at %08x", nullFunctionAddress)
 
-	// choose size for the remainder of the flash memory and place at the flash
-	// origin value for architecture
-	const bufferOverhead = 64000
-	var buffserSize uint32
-
-	if len(data) < 128000-bufferOverhead {
-		buffserSize = 128000
-	} else if len(data) < 256000-bufferOverhead {
-		buffserSize = 256000
-	} else if len(data) < 512000-bufferOverhead {
-		buffserSize = 512000
-	} else {
-		buffserSize = bufferOverhead
-	}
-
-	mem.buffer = make([]byte, buffserSize)
-	mem.bufferOrigin = mem.model.FlashOrigin
-	mem.bufferMemtop = mem.bufferOrigin + uint32(len(mem.buffer)-1)
+	// generous amount for SRAM to accomodate DPCp
+	mem.sram = make([]byte, 0x20000)
+	mem.sramOrigin = mem.model.Regions["SRAM"].Origin
+	mem.sramMemtop = mem.sramOrigin + uint32(len(mem.sram)-1)
 
 	// set virtual argument. detailed information in the PlusCart firmware
 	// source:
@@ -246,36 +232,36 @@ func newAceMemory(env *environment.Environment, data []byte, armPrefs *preferenc
 	// atari-2600-pluscart-master/source/STM32firmware/PlusCart/Src/cartridge_emulation_ACE.c
 
 	// ROM file argument
-	binary.LittleEndian.PutUint32(mem.buffer, mem.downloadOrigin)
+	binary.LittleEndian.PutUint32(mem.sram, mem.flashOrigin)
 
 	// CCM memory argument
-	binary.LittleEndian.PutUint32(mem.buffer[4:], mem.ccmOrigin)
+	binary.LittleEndian.PutUint32(mem.sram[4:], mem.ccmOrigin)
 
 	// addresses of func_reboot_into_cartridge() and emulate_firmware_cartridge()
 	// for our purposes, the function needs only to jump back to the link address
-	binary.LittleEndian.PutUint32(mem.buffer[8:], nullFunctionAddress)
-	binary.LittleEndian.PutUint32(mem.buffer[12:], nullFunctionAddress)
+	binary.LittleEndian.PutUint32(mem.sram[8:], nullFunctionAddress)
+	binary.LittleEndian.PutUint32(mem.sram[12:], nullFunctionAddress)
 
 	// system clock argument
 	clk := int(armPrefs.Clock.Get().(float64) * 1000000)
-	binary.LittleEndian.PutUint32(mem.buffer[16:], uint32(clk))
+	binary.LittleEndian.PutUint32(mem.sram[16:], uint32(clk))
 
 	// ACE version number
 	aceVersion := 2
-	binary.LittleEndian.PutUint32(mem.buffer[20:], uint32(aceVersion))
+	binary.LittleEndian.PutUint32(mem.sram[20:], uint32(aceVersion))
 
 	// pluscart revision number
 	plusCartRevision := 3
-	binary.LittleEndian.PutUint32(mem.buffer[24:], uint32(plusCartRevision))
+	binary.LittleEndian.PutUint32(mem.sram[24:], uint32(plusCartRevision))
 
 	// GPIO addresses
-	binary.LittleEndian.PutUint32(mem.buffer[28:], ADDR_IDR)
-	binary.LittleEndian.PutUint32(mem.buffer[32:], DATA_IDR)
-	binary.LittleEndian.PutUint32(mem.buffer[36:], DATA_ODR)
-	binary.LittleEndian.PutUint32(mem.buffer[40:], DATA_MODER)
+	binary.LittleEndian.PutUint32(mem.sram[28:], ADDR_IDR)
+	binary.LittleEndian.PutUint32(mem.sram[32:], DATA_IDR)
+	binary.LittleEndian.PutUint32(mem.sram[36:], DATA_ODR)
+	binary.LittleEndian.PutUint32(mem.sram[40:], DATA_MODER)
 
 	// end of argument indicator
-	copy(mem.buffer[44:48], []byte{0x00, 0x26, 0xe4, 0xac})
+	copy(mem.sram[44:48], []byte{0x00, 0x26, 0xe4, 0xac})
 
 	// GPIO pins
 	mem.gpio = make([]byte, GPIO_MEMTOP-GPIO_ORIGIN+1)
@@ -294,10 +280,10 @@ func (mem *aceMemory) Snapshot() *aceMemory {
 	copy(m.gpio, mem.gpio)
 	m.ccm = make([]byte, len(mem.ccm))
 	copy(m.ccm, mem.ccm)
-	m.download = make([]byte, len(mem.download))
-	copy(m.download, mem.download)
-	m.buffer = make([]byte, len(mem.buffer))
-	copy(m.buffer, mem.buffer)
+	m.flash = make([]byte, len(mem.flash))
+	copy(m.flash, mem.flash)
+	m.sram = make([]byte, len(mem.sram))
+	copy(m.sram, mem.sram)
 	return &m
 }
 
@@ -323,11 +309,11 @@ func (mem *aceMemory) MapAddress(addr uint32, write bool, executing bool) (*[]by
 		return &mem.gpio, mem.gpioOrigin
 	}
 
-	if addr >= mem.bufferOrigin && addr <= mem.bufferMemtop {
-		return &mem.buffer, mem.bufferOrigin
+	if addr >= mem.sramOrigin && addr <= mem.sramMemtop {
+		return &mem.sram, mem.sramOrigin
 	}
-	if addr >= mem.downloadOrigin && addr <= mem.downloadMemtop {
-		return &mem.download, mem.downloadOrigin
+	if addr >= mem.flashOrigin && addr <= mem.flashMemtop {
+		return &mem.flash, mem.flashOrigin
 	}
 	if addr >= mem.ccmOrigin && addr <= mem.ccmMemtop {
 		return &mem.ccm, mem.ccmOrigin
@@ -351,13 +337,13 @@ func (a *aceMemory) Segments() []mapper.CartStaticSegment {
 	return []mapper.CartStaticSegment{
 		{
 			Name:   "Download",
-			Origin: a.downloadOrigin,
-			Memtop: a.downloadMemtop,
+			Origin: a.flashOrigin,
+			Memtop: a.flashMemtop,
 		},
 		{
 			Name:   "Buffer",
-			Origin: a.bufferOrigin,
-			Memtop: a.bufferMemtop,
+			Origin: a.sramOrigin,
+			Memtop: a.sramMemtop,
 		},
 		{
 			Name:   "CCM",
@@ -373,9 +359,9 @@ func (a *aceMemory) Segments() []mapper.CartStaticSegment {
 func (a *aceMemory) Reference(segment string) ([]uint8, bool) {
 	switch segment {
 	case "Download":
-		return a.download, true
+		return a.flash, true
 	case "Buffer":
-		return a.buffer, true
+		return a.sram, true
 	case "CCM":
 		return a.ccm, true
 	}

hardware/memory/cartridge/arm/arm.go
@@ -60,6 +60,9 @@ const cycleLimit = 1500000
 // running in immediate mode
 const instructionsLimit = 1300000
 
+// The latency above which the memory is considered slow for cycle counting purposes
+const slowMemoryLatency = 25.0
+
 // stepFunction variations are a result of different ARM architectures
 type stepFunction func(opcode uint16, memIdx int)
 
@@ -218,6 +221,9 @@ type ARM struct {
 	mem  SharedMemory
 	hook CartridgeHook
 
+	// map of clkLen values per possible latency values
+	clkLen map[float64]float32
+
 	// the binary interface for reading data returned by SharedMemory interface.
 	// defaults to LittleEndian
 	byteOrder binary.ByteOrder
@@ -248,8 +254,7 @@ type ARM struct {
 	// MAM also requires no stretching.
 	//
 	// updated from prefs on every Run() invocation
-	Clk         float32
-	clklenFlash float32
+	Clk float32
 
 	// value used to stretch (or shink) the number of cycles used by each
 	// instruction. a value of 1.0 is a neutral regulator
@@ -330,6 +335,7 @@ func NewARM(env *environment.Environment, mmap architecture.Map, mem SharedMemor
 		mmap:           mmap,
 		mem:            mem,
 		hook:           hook,
+		clkLen:         make(map[float64]float32),
 		byteOrder:      binary.LittleEndian,
 		executionCache: make(map[uint32][]decodeFunction),
 		state:          &ARMState{},
@@ -485,9 +491,13 @@ func (arm *ARM) updatePrefs() {
 	// update clock value from preferences
 	arm.Clk = float32(arm.env.Prefs.ARM.Clock.Get().(float64))
 
-	// clklen for flash based on flash latency setting
-	latencyInMhz := (1 / (arm.mmap.FlashLatency / 1000000000)) / 1000000
-	arm.clklenFlash = float32(math.Ceil(float64(arm.Clk) / latencyInMhz))
+	// update clk len values in architecture mmap
+	for _, r := range arm.mmap.Regions {
+		if _, ok := arm.clkLen[r.Latency]; !ok {
+			latencyInMhz := (1 / (r.Latency / 1000000000)) / 1000000
+			arm.clkLen[r.Latency] = float32(math.Ceil(float64(arm.Clk) / latencyInMhz))
+		}
+	}
 
 	// get clock regulator from preferences
 	arm.cycleRegulator = float32(arm.env.Prefs.ARM.CycleRegulator.Get().(float64))

hardware/memory/cartridge/arm/cycles.go
@@ -141,8 +141,8 @@ func (arm *ARM) storeRegisterCycles(addr uint32) {
 // add cycles accumulated during an BX to ARM code instruction. this is
 // definitely only an estimate.
 func (arm *ARM) armInterruptCycles(i ARMinterruptReturn) {
-	// we'll assume all writes are to flash memory
-	arm.state.stretchedCycles += float32(i.NumMemAccess) * arm.clklenFlash
+	// not taking into account latency of memory access
+	arm.state.stretchedCycles += float32(i.NumMemAccess)
 	arm.state.stretchedCycles += float32(i.NumAdditionalCycles)
 }
 

hardware/memory/cartridge/arm/cycles_arm7tdmi.go
@@ -47,30 +47,31 @@ func (arm *ARM) sCycle_ARM7TDMI(bus busAccess, addr uint32) {
 	}
 	arm.state.lastCycle = S
 
-	if !arm.mmap.IsFlash(addr) {
+	latency := arm.mmap.AddrLatency(addr)
+	if latency < slowMemoryLatency {
 		arm.state.stretchedCycles++
 		return
 	}
 
 	switch arm.state.mam.mamcr {
 	default:
-		arm.state.stretchedCycles += arm.clklenFlash
+		arm.state.stretchedCycles += arm.clkLen[latency]
 	case 0:
-		arm.state.stretchedCycles += arm.clklenFlash
+		arm.state.stretchedCycles += arm.clkLen[latency]
 	case 1:
 		// for MAM-1, we go to flash memory only if it's a program access (ie. not a data access)
 		if bus.isDataAccess() {
-			arm.state.stretchedCycles += arm.clklenFlash
+			arm.state.stretchedCycles += arm.clkLen[latency]
 		} else if arm.isLatched(S, bus, addr) {
 			arm.state.stretchedCycles++
 		} else {
-			arm.state.stretchedCycles += arm.clklenFlash
+			arm.state.stretchedCycles += arm.clkLen[latency]
 		}
 	case 2:
 		if arm.isLatched(S, bus, addr) {
 			arm.state.stretchedCycles++
 		} else {
-			arm.state.stretchedCycles += arm.clklenFlash
+			arm.state.stretchedCycles += arm.clkLen[latency]
 		}
 	}
 }
@@ -117,23 +118,24 @@ func (arm *ARM) nCycle_ARM7TDMI(bus busAccess, addr uint32) {
 	}
 	arm.state.lastCycle = N
 
-	if !arm.mmap.IsFlash(addr) {
-		arm.state.stretchedCycles += float32(mclkNonFlash)
+	latency := arm.mmap.AddrLatency(addr)
+	if latency < slowMemoryLatency {
+		arm.state.stretchedCycles++
 		return
 	}
 
 	switch arm.state.mam.mamcr {
 	default:
-		arm.state.stretchedCycles += arm.clklenFlash * float32(mclkFlash)
+		arm.state.stretchedCycles += arm.clkLen[latency] * float32(mclkFlash)
 	case 0:
-		arm.state.stretchedCycles += arm.clklenFlash * float32(mclkFlash)
+		arm.state.stretchedCycles += arm.clkLen[latency] * float32(mclkFlash)
 	case 1:
-		arm.state.stretchedCycles += arm.clklenFlash * float32(mclkFlash)
+		arm.state.stretchedCycles += arm.clkLen[latency] * float32(mclkFlash)
 	case 2:
 		if arm.isLatched(N, bus, addr) {
 			arm.state.stretchedCycles += float32(mclkNonFlash)
 		} else {
-			arm.state.stretchedCycles += arm.clklenFlash * float32(mclkFlash)
+			arm.state.stretchedCycles += arm.clkLen[latency] * float32(mclkFlash)
 		}
 	}
 }

hardware/memory/cartridge/arm/cycles_armv7_m.go
@@ -38,12 +38,8 @@ func (arm *ARM) sCycle_ARMv7_M(_ busAccess, addr uint32) {
 	}
 	arm.state.lastCycle = S
 
-	if !arm.mmap.IsFlash(addr) {
-		arm.state.stretchedCycles++
-		return
-	}
-
-	arm.state.stretchedCycles += arm.clklenFlash
+	latency := arm.mmap.AddrLatency(addr)
+	arm.state.stretchedCycles += arm.clkLen[latency]
 }
 
 func (arm *ARM) nCycle_ARMv7_M(_ busAccess, addr uint32) {
@@ -62,10 +58,11 @@ func (arm *ARM) nCycle_ARMv7_M(_ busAccess, addr uint32) {
 	}
 	arm.state.lastCycle = N
 
-	if !arm.mmap.IsFlash(addr) {
+	latency := arm.mmap.AddrLatency(addr)
+	if latency < slowMemoryLatency {
 		arm.state.stretchedCycles += float32(mclkNonFlash)
 		return
 	}
 
-	arm.state.stretchedCycles += arm.clklenFlash * float32(mclkFlash)
+	arm.state.stretchedCycles += arm.clkLen[latency] * float32(mclkFlash)
 }

hardware/memory/cartridge/cartridge.go
@@ -317,7 +317,7 @@ func (cart *Cartridge) Attach(cartload cartridgeloader.Loader) error {
 	case "DPC":
 		cart.mapper, err = newDPC(cart.env, cartload)
 	case "DPC+":
-		cart.mapper, err = dpcplus.NewDPCplus(cart.env, cartload, "LPC2000")
+		cart.mapper, err = dpcplus.NewDPCplus(cart.env, cartload, "DPC+")
 
 	case "CDF":
 		cart.mapper, err = cdf.NewCDF(cart.env, cartload, "CDFJ")
@@ -338,7 +338,7 @@ func (cart *Cartridge) Attach(cartload cartridgeloader.Loader) error {
 		cart.mapper, err = elf.NewElf(cart.env, cartload, false)
 
 	case "DPCp_in_ACE":
-		cart.mapper, err = dpcplus.NewDPCplus(cart.env, cartload, "STM32F407VGT6")
+		cart.mapper, err = dpcplus.NewDPCplus(cart.env, cartload, "DPCp")
 	case "ELF_in_ACE":
 		cart.mapper, err = elf.NewElf(cart.env, cartload, true)
 

commit b7773512d04c343e8865a3e8bf0afab65c0074c6

reduced latency of memory regions for STM32
problem introduced in b491dfb3

hardware/memory/cartridge/arm/architecture/architecture.go
@@ -177,13 +177,13 @@ func NewMap(cart CartArchitecture) Map {
 			Name:    "Flash",
 			Origin:  0x08020000,
 			Memtop:  0x0802ffff,
-			Latency: 50.0,
+			Latency: 1.0,
 		}
 		mmap.Regions["SRAM"] = &MemoryRegion{
 			Name:    "SRAM",
 			Origin:  0x20000000,
 			Memtop:  0x2fffffff,
-			Latency: 10.0,
+			Latency: 1.0,
 		}
 		mmap.Regions["CCM"] = &MemoryRegion{
 			Name:    "CCM",