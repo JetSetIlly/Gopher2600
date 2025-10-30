JetSetIlly/Gopher2600

commit 880586abf43772422d21a172d481de5419802989

improved HaltReason usage. added CPU KIL as a halt event

debugger/halt.go
@@ -23,6 +23,7 @@ import (
 
 type HaltReason struct {
 	Reason string
+	Detail string
 	Coords coords.TelevisionCoords
 }
 
@@ -104,7 +105,16 @@ func (h *haltCoordination) reset() {
 // check for a halt condition and set the halt flag if found. returns true if
 // emulation should continue and false if the emulation should halt
 func (h *haltCoordination) check() bool {
-	if h.cartridgeYield.Type != coprocessor.YieldProgramEnded {
+	if h.dbg.vcs.CPU.Killed {
+		h.haltReason = HaltReason{
+			Reason: "CPU KIL",
+			Coords: h.dbg.vcs.TV.GetCoords(),
+		}
+		h.halt = true
+		return false
+	}
+
+	if !h.cartridgeYield.Type.Normal() {
 		h.haltReason = HaltReason{
 			Reason: string(h.cartridgeYield.Type),
 			Coords: h.dbg.vcs.TV.GetCoords(),
@@ -133,7 +143,8 @@ func (h *haltCoordination) check() bool {
 			h.dbg.printLine(terminal.StyleFeedback, breakMessage)
 			h.halt = true
 			h.haltReason = HaltReason{
-				Reason: h.televisionHalt,
+				Reason: "Breakpoint",
+				Detail: breakMessage,
 				Coords: h.dbg.vcs.TV.GetCoords(),
 			}
 		}
@@ -142,7 +153,8 @@ func (h *haltCoordination) check() bool {
 			h.dbg.printLine(terminal.StyleFeedback, trapMessage)
 			h.halt = true
 			h.haltReason = HaltReason{
-				Reason: h.televisionHalt,
+				Reason: "Trap",
+				Detail: breakMessage,
 				Coords: h.dbg.vcs.TV.GetCoords(),
 			}
 		}
@@ -151,7 +163,8 @@ func (h *haltCoordination) check() bool {
 			h.dbg.printLine(terminal.StyleFeedback, watchMessage)
 			h.halt = true
 			h.haltReason = HaltReason{
-				Reason: h.televisionHalt,
+				Reason: "Watch",
+				Detail: breakMessage,
 				Coords: h.dbg.vcs.TV.GetCoords(),
 			}
 		}
@@ -159,7 +172,7 @@ func (h *haltCoordination) check() bool {
 		return !h.halt
 	}
 
-	// check volatile conditions
+	// check volatile conditions. there is no halt reason set for these
 	breakMessage := h.volatileBreakpoints.check()
 	trapMessage := h.volatileTraps.check()
 	h.halt = h.halt || breakMessage != "" || trapMessage != ""

gui/sdlimgui/manager_menu.go
@@ -300,6 +300,12 @@ func (wm *manager) drawMenu() {
 			},
 		)
 		wm.img.imguiTooltip(func() {
+			if haltReason.Detail != "" {
+				imgui.Text(haltReason.Detail)
+				imgui.Spacing()
+				imgui.Separator()
+				imgui.Spacing()
+			}
 			imgui.Text(fmt.Sprintf("Frame: %d", haltReason.Coords.Frame))
 			imgui.Text(fmt.Sprintf("Scanline: %d", haltReason.Coords.Scanline))
 			imgui.Text(fmt.Sprintf("Clock: %d", haltReason.Coords.Clock))

commit 5d0b10212798304a38a76f726815ce20b9cee903

CPU KIL halt check ignores ejected cartridges
fixes bug introduced in 880586ab. the bug caused the emulator to
immediately enter the debugger if playmode was launched without a ROM
specified

debugger/halt.go
@@ -105,7 +105,7 @@ func (h *haltCoordination) reset() {
 // check for a halt condition and set the halt flag if found. returns true if
 // emulation should continue and false if the emulation should halt
 func (h *haltCoordination) check() bool {
-	if h.dbg.vcs.CPU.Killed {
+	if h.dbg.vcs.CPU.Killed && !h.dbg.vcs.Mem.Cart.IsEjected() {
 		h.haltReason = HaltReason{
 			Reason: "CPU KIL",
 			Coords: h.dbg.vcs.TV.GetCoords(),