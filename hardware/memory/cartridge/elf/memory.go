JetSetIlly/Gopher2600

commit 36bd56e607453a57dd39a978f97db3e61e14b25f

implemented reset for ELF
there's a flaw in the reset procedure that has been reworked aroud with
an resetInhibit counter. ideally, we wouldn't have this but it works for
now

added Reset menu item to debugger

hardware/memory/cartridge/elf/memory.go
@@ -244,6 +244,7 @@ func (mem *elfMemory) Snapshot() *elfMemory {
 		} else {
 			m.sections[i] = mem.sections[i].Snapshot()
 		}
+		m.sectionsByName[mem.sections[i].name] = m.sections[i]
 	}
 
 	// sram is likely to have changed

gui/sdlimgui/manager_menu.go
@@ -81,6 +81,14 @@ func (wm *manager) drawMenu() {
 			wm.drawMenuEntry(m)
 		}
 
+		imguiSeparator()
+
+		if imgui.Selectable("  Reset") {
+			wm.img.dbg.PushFunction(func() {
+				_ = wm.img.dbg.VCS().Reset()
+			})
+		}
+
 		// if imgui.Selectable("  Save ROM") {
 		// 	wm.img.dbg.PushFunction(func() {
 		// 		_, err := wm.img.dbg.VCS().(*hardware.VCS).Mem.Cart.ROMDump()

hardware/memory/cartridge/elf/elf.go
@@ -50,6 +50,18 @@ type Elf struct {
 
 	// commandline extensions
 	commands *commandline.Commands
+
+	// the initial state created immediately after creation [at the end of NewELF()]
+	// we use these to return the mapper to the initial state when Reset() is called
+	resetStateARM *arm.ARMState
+	resetStateMem *elfMemory
+
+	// the reset doesn't work correctly when the cartridge has just been recreated/reset. this
+	// inhibit counter isn't a great solution but it works by inhibiting the reset procedure from
+	// firing before the count reaches zero
+	//
+	// set on Elf create and also after a successful reset
+	resetInhibit int
 }
 
 // elfReaderAt is an implementation of io.ReaderAt and is used with elf.NewFile()
@@ -172,7 +184,28 @@ func NewElf(env *environment.Environment, inACE bool) (mapper.CartMapper, error)
 		}
 	}
 
+	// create snapshot of this initial state
+	cart.resetStateARM = cart.arm.Snapshot()
+	cart.resetStateMem = cart.mem.Snapshot()
+	cart.resetInhibit = 4
+
 	return cart, nil
+}
+
+// Reset implements the mapper.CartMapper interface.
+func (cart *Elf) Reset() error {
+	if cart.resetInhibit > 0 {
+		cart.resetInhibit--
+		return nil
+	}
+	cart.mem.inhibitStrongarmAccess = true
+	defer func() { cart.mem.inhibitStrongarmAccess = false }()
+	cart.mem = cart.resetStateMem.Snapshot()
+	cart.mem.Plumb(cart.env, cart.arm)
+	cart.arm.Plumb(cart.env, cart.resetStateARM, cart.mem, cart)
+	cart.yieldHook = &coprocessor.StubCartYieldHook{}
+	cart.resetInhibit = 1
+	return nil
 }
 
 // MappedBanks implements the mapper.CartMapper interface.
@@ -239,11 +272,6 @@ func (cart *Elf) Plumb(env *environment.Environment) {
 	cart.mem.Plumb(cart.env, cart.arm)
 	cart.arm.Plumb(cart.env, cart.armState, cart.mem, cart)
 	cart.armState = nil
-}
-
-// Reset implements the mapper.CartMapper interface.
-func (cart *Elf) Reset() error {
-	return nil
 }
 
 // reset is distinct from Reset(). this reset function is implied by the

commit e9e319a1503b0ddee95dfb061a10b1e3f6108806

fixed serious concurrency bug introduced in 36bd56e6
elfMemory.Snapshot() should create a new map for sectionsByName. the
previous commit contained a partial bug fix, and as it happens, made the
bug worse in certain situations

hardware/memory/cartridge/elf/memory.go
@@ -238,6 +238,7 @@ func (mem *elfMemory) Snapshot() *elfMemory {
 	// snapshot sections. the Snapshot() function of the elfSection type decides
 	// how best to deal with the request
 	m.sections = make([]*elfSection, len(mem.sections))
+	m.sectionsByName = make(map[string]*elfSection)
 	for i := range mem.sections {
 		if mem.sections[i].readOnly() {
 			m.sections[i] = mem.sections[i]