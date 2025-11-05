JetSetIlly/Gopher2600

commit 3b54d4b5affcc8d8c4a2cf07a08736d921314e13

corrected read16Bit() function for zero page addressing

hardware/cpu/cpu.go
@@ -341,8 +341,8 @@ func (mc *CPU) write8Bit(address uint16, value uint8, phantom bool) error {
 //
 // side-effects:
 //   - calls cycleCallback after each 8bit read
-func (mc *CPU) read16Bit(address uint16) (uint16, error) {
-	mc.PhantomMemAccess = false
+func (mc *CPU) read16Bit(address uint16, phantom bool) (uint16, error) {
+	mc.PhantomMemAccess = phantom
 
 	lo, err := mc.mem.Read(address)
 	if err != nil {
@@ -356,7 +356,8 @@ func (mc *CPU) read16Bit(address uint16) (uint16, error) {
 		return 0, err
 	}
 
-	hi, err := mc.mem.Read(address + 1)
+	// advance address and being careful to preserve page
+	hi, err := mc.mem.Read((address & 0xff00) | ((address + 1) & 0x00ff))
 	if err != nil {
 		return 0, err
 	}
@@ -782,7 +783,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 			// normal, non-buggy behaviour
 
 			// +2 cycles
-			address, err = mc.read16Bit(indirectAddress)
+			address, err = mc.read16Bit(indirectAddress, false)
 			if err != nil {
 				return err
 			}
@@ -814,7 +815,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		}
 
 		// +2 cycles
-		address, err = mc.read16Bit(mc.acc8.Address())
+		address, err = mc.read16Bit(mc.acc8.Address(), false)
 		if err != nil {
 			return err
 		}
@@ -831,7 +832,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// +2 cycles
 		var indexedAddress uint16
-		indexedAddress, err = mc.read16Bit(indirectAddress)
+		indexedAddress, err = mc.read16Bit(indirectAddress, false)
 		if err != nil {
 			return err
 		}
@@ -1457,7 +1458,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// +2 cycles
 		var rtsAddress uint16
-		rtsAddress, err = mc.read16Bit(mc.SP.Address())
+		rtsAddress, err = mc.read16Bit(mc.SP.Address(), false)
 		if err != nil {
 			return err
 		}
@@ -1466,11 +1467,10 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		if !mc.NoFlowControl {
 			// load and correct PC
 			mc.PC.Load(rtsAddress)
+			// +1 cycle
+			_, err = mc.read8Bit(mc.PC.Address(), false)
 			mc.PC.Add(1)
 		}
-
-		// +1 cycle
-		_, err = mc.read8Bit(mc.PC.Address(), false)
 
 	case instructions.Brk:
 		// push PC onto register (same effect as JSR)
@@ -1522,7 +1522,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// perform jump
 		var brkAddress uint16
-		brkAddress, err = mc.read16Bit(BRK)
+		brkAddress, err = mc.read16Bit(BRK, false)
 		if err != nil {
 			return err
 		}
@@ -1572,7 +1572,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// +2 cycles
 		var rtiAddress uint16
-		rtiAddress, err = mc.read16Bit(mc.SP.Address())
+		rtiAddress, err = mc.read16Bit(mc.SP.Address(), false)
 		if err != nil {
 			return err
 		}

commit b4313ecd0da01aad936be1f916a4ea0836b20af1

corrected bug introduced in 3b54d4b5a
this caused the disassembly (noFlowControl) process to end prematurely

added LAX (immediate) instruction to list of non-working thomharte
tests. the instruction works but the internal parameter we're using is
not what the tests expect

hardware/cpu/cpu.go
@@ -355,9 +355,7 @@ func (mc *CPU) write8Bit(address uint16, value uint8, phantom bool) error {
 //
 // side-effects:
 //   - calls cycleCallback after each 8bit read
-func (mc *CPU) read16Bit(address uint16, phantom bool) (uint16, error) {
-	mc.PhantomMemAccess = phantom
-
+func (mc *CPU) read16Bit(address uint16) (uint16, error) {
 	lo, err := mc.mem.Read(address)
 	if err != nil {
 		return 0, err
@@ -797,7 +795,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 			// normal, non-buggy behaviour
 
 			// +2 cycles
-			address, err = mc.read16Bit(indirectAddress, false)
+			address, err = mc.read16Bit(indirectAddress)
 			if err != nil {
 				return err
 			}
@@ -829,7 +827,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 		}
 
 		// +2 cycles
-		address, err = mc.read16Bit(mc.acc8.Address(), false)
+		address, err = mc.read16Bit(mc.acc8.Address())
 		if err != nil {
 			return err
 		}
@@ -846,7 +844,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// +2 cycles
 		var indexedAddress uint16
-		indexedAddress, err = mc.read16Bit(indirectAddress, false)
+		indexedAddress, err = mc.read16Bit(indirectAddress)
 		if err != nil {
 			return err
 		}
@@ -1420,19 +1418,19 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// +2 cycles
 		var rtsAddress uint16
-		rtsAddress, err = mc.read16Bit(mc.SP.Address(), false)
+		rtsAddress, err = mc.read16Bit(mc.SP.Address())
 		if err != nil {
 			return err
 		}
 
 		mc.SP.Add(1, false)
 		if !mc.NoFlowControl {
-			// load and correct PC
 			mc.PC.Load(rtsAddress)
-			// +1 cycle
-			_, err = mc.read8Bit(mc.PC.Address(), false)
-			mc.PC.Add(1)
 		}
+
+		// +1 cycle
+		_, err = mc.read8Bit(mc.PC.Address(), false)
+		mc.PC.Add(1)
 
 	case instructions.Brk:
 		// push PC onto register (same effect as JSR)
@@ -1468,7 +1466,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// perform jump
 		var brkAddress uint16
-		brkAddress, err = mc.read16Bit(BRK, false)
+		brkAddress, err = mc.read16Bit(BRK)
 		if err != nil {
 			return err
 		}
@@ -1516,7 +1514,7 @@ func (mc *CPU) ExecuteInstruction(cycleCallback func() error) error {
 
 		// +2 cycles
 		var rtiAddress uint16
-		rtiAddress, err = mc.read16Bit(mc.SP.Address(), false)
+		rtiAddress, err = mc.read16Bit(mc.SP.Address())
 		if err != nil {
 			return err
 		}

hardware/cpu/tests/thomharte/thomharte_test.go
@@ -184,7 +184,10 @@ func TestThomHarte(t *testing.T) {
 	}
 }
 
-var notWorking = []uint8{0x93, 0x9f, 0x9b, 0x9c, 0x9e}
+// 0xab (LAX immediate) is working but the "internal parameter" we're using in the cpu package is
+// not what the tests expect. the instruction is therefore disabled until we add the option to set
+// the internal parameter manually
+var notWorking = []uint8{0x93, 0x9f, 0x9b, 0x9c, 0x9e, 0xab}
 
 func testThomHarte(t *testing.T, opcode uint8, force bool) {
 	testFile := filepath.Join(testsPath, fmt.Sprintf("%02x.json", opcode))