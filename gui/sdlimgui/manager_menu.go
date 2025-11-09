JetSetIlly/Gopher2600

commit 3b90034779fee03b0adc116784173214d4241250

pixel renderers can optionally implement DisplayRefreshRate() function
this function reports to the limiter the refresh rate of the display, as
opposed to the emulated television. the limiter can then use that to
quantise the emulated refresh rate to better match the display

the quanisation process is disabled for now

hardware/television/limiter.go
@@ -1,0 +1,66 @@
+// This file is part of Gopher2600.
+//
+// Gopher2600 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gopher2600 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gopher2600.  If not, see <https://www.gnu.org/licenses/>.
+
+package television
+
+// NudgeFPSLimiter stops the FPS limiter for the specified number of frames. A value
+// of zero (or less) will stop any existing nudge
+func (tv *Television) NudgeFPSLimiter(frames int) {
+	if frames < 0 {
+		frames = 0
+	}
+	tv.lmtr.Nudge.Store(int32(frames))
+}
+
+// SetFPSLimit whether the emulation should wait for FPS limiter. Returns the
+// setting as it was previously.
+func (tv *Television) SetFPSLimit(limit bool) bool {
+	prev := tv.lmtr.Active
+	tv.lmtr.Active = limit
+
+	// notify all pixel renderers that are interested in the FPS limiter
+	for i := range tv.renderers {
+		if r, ok := tv.renderers[i].(PixelRendererFPSLimiter); ok {
+			r.SetFPSLimit(limit)
+		}
+	}
+
+	return prev
+}
+
+// SetFPS requests the number frames per second. This overrides the frame rate of
+// the specification. A negative value restores frame rate to the ideal value
+// (the frequency of the incoming signal).
+func (tv *Television) SetFPS(fps float32) {
+	tv.lmtr.SetLimit(fps)
+}
+
+// GetReqFPS returns the requested number of frames per second. Compare with
+// GetActualFPS() to check for accuracy.
+//
+// IS goroutine safe.
+func (tv *Television) GetReqFPS() float32 {
+	return tv.lmtr.RequestedFPS.Load().(float32)
+}
+
+// GetActualFPS returns the current number of frames per second and the
+// detected frequency of the TV signal.
+//
+// Note that FPS measurement still works even when the frame limiter is disabled.
+//
+// IS goroutine safe.
+func (tv *Television) GetActualFPS() (float32, float32) {
+	return tv.lmtr.Measured.Load().(float32), tv.lmtr.RefreshRate.Load().(float32)
+}

hardware/television/limiter/limiter.go
@@ -22,6 +22,10 @@ import (
 	"github.com/jetsetilly/gopher2600/hardware/television/specification"
 )
 
+type Display interface {
+	DisplayRefreshRate() (float32, bool)
+}
+
 type Limiter struct {
 	// whether to wait for fps limited each frame
 	Active bool
@@ -75,6 +79,9 @@ type Limiter struct {
 
 	// nudge the limiter so that it doesn't wait for the specified number of frames
 	Nudge atomic.Int32
+
+	// the display the limiter is working for
+	display Display
 }
 
 // NewLimiter is preferred method of initialising a new instance of the Limiter
@@ -93,6 +100,12 @@ func NewLimiter() *Limiter {
 	lmtr.SetLimit(MatchRefreshRate)
 
 	return &lmtr
+}
+
+// Set the display interface for the limiter
+func (lmtr *Limiter) SetDisplay(display Display) {
+	lmtr.display = display
+	lmtr.SetLimit(lmtr.RequestedFPS.Load().(float32))
 }
 
 // Set the refresh rate for the limiter. This is equivalent to the refresh rate
@@ -128,6 +141,16 @@ func (lmtr *Limiter) SetLimit(fps float32) {
 	// the actual requested fps rate (taken into account the possibility of a negative number
 	// to indicate the refresh rate)
 	lmtr.RequestedFPS.Store(fps)
+
+	// quantise refresh rate based on refresh rate of television
+	if lmtr.display != nil {
+		hz, quantise := lmtr.display.DisplayRefreshRate()
+		if quantise {
+			if fps >= hz*0.96 && fps <= hz*1.04 {
+				fps = hz
+			}
+		}
+	}
 
 	// set scale and duration to wait according to requested FPS rate
 	lmtr.pulseCt = 0

hardware/television/television.go
@@ -188,6 +188,10 @@ type Television struct {
 	// list of PixelRenderer implementations to consult
 	renderers []PixelRenderer
 
+	// the most recently added pixel rendererer that implements PixelRendererDisplay. there can only
+	// be one of these at any one time and a new one added will replace the previous display
+	rendererDisplay PixelRendererDisplay
+
 	// list of FrameTrigger implementations to consult
 	frameTriggers []FrameTrigger
 
@@ -358,8 +362,15 @@ func (tv *Television) AddDebugger(dbg Debugger) {
 	tv.debugger = dbg
 }
 
-// AddPixelRenderer adds an implementation of PixelRenderer.
+// AddPixelRenderer adds an implementation of PixelRenderer. If the PixelRenderer is also an
+// implementation of PixelRendererDisplay then note that there can only be one such renderer and any
+// such renderer previously added will be replaced.
 func (tv *Television) AddPixelRenderer(r PixelRenderer) {
+	if r, ok := r.(PixelRendererDisplay); ok {
+		tv.RemovePixelRenderer(tv.rendererDisplay)
+		tv.rendererDisplay = r
+		tv.lmtr.SetDisplay(r)
+	}
 	for i := range tv.renderers {
 		if tv.renderers[i] == r {
 			return
@@ -371,6 +382,12 @@ func (tv *Television) AddPixelRenderer(r PixelRenderer) {
 // RemovePixelRenderer removes a single PixelRenderer implementation from the
 // list of renderers. Order is not maintained.
 func (tv *Television) RemovePixelRenderer(r PixelRenderer) {
+	if r, ok := r.(PixelRendererDisplay); ok {
+		if tv.rendererDisplay == r {
+			tv.rendererDisplay = nil
+			tv.lmtr.SetDisplay(r)
+		}
+	}
 	for i := range tv.renderers {
 		if tv.renderers[i] == r {
 			tv.renderers[i] = tv.renderers[len(tv.renderers)-1]
@@ -976,56 +993,6 @@ func (tv *Television) SetEmulationState(state govern.State) error {
 	return nil
 }
 
-// NudgeFPSLimiter stops the FPS limiter for the specified number of frames. A value
-// of zero (or less) will stop any existing nudge
-func (tv *Television) NudgeFPSLimiter(frames int) {
-	if frames < 0 {
-		frames = 0
-	}
-	tv.lmtr.Nudge.Store(int32(frames))
-}
-
-// SetFPSLimit whether the emulation should wait for FPS limiter. Returns the
-// setting as it was previously.
-func (tv *Television) SetFPSLimit(limit bool) bool {
-	prev := tv.lmtr.Active
-	tv.lmtr.Active = limit
-
-	// notify all pixel renderers that are interested in the FPS limiter
-	for i := range tv.renderers {
-		if r, ok := tv.renderers[i].(PixelRendererFPSLimiter); ok {
-			r.SetFPSLimit(limit)
-		}
-	}
-
-	return prev
-}
-
-// SetFPS requests the number frames per second. This overrides the frame rate of
-// the specification. A negative value restores frame rate to the ideal value
-// (the frequency of the incoming signal).
-func (tv *Television) SetFPS(fps float32) {
-	tv.lmtr.SetLimit(fps)
-}
-
-// GetReqFPS returns the requested number of frames per second. Compare with
-// GetActualFPS() to check for accuracy.
-//
-// IS goroutine safe.
-func (tv *Television) GetReqFPS() float32 {
-	return tv.lmtr.RequestedFPS.Load().(float32)
-}
-
-// GetActualFPS returns the current number of frames per second and the
-// detected frequency of the TV signal.
-//
-// Note that FPS measurement still works even when the frame limiter is disabled.
-//
-// IS goroutine safe.
-func (tv *Television) GetActualFPS() (float32, float32) {
-	return tv.lmtr.Measured.Load().(float32), tv.lmtr.RefreshRate.Load().(float32)
-}
-
 // GetResetSpecID returns the specification that the television resets to. This
 // is useful for learning more about how the television was created
 func (tv *Television) GetResetSpecID() string {
@@ -1058,46 +1025,4 @@ func (tv *Television) GetCoords() coords.TelevisionCoords {
 
 func (tv *Television) IsFrameNum(frame int) bool {
 	return tv.state.frameNum == frame
-}
-
-// SetRotation instructs the television to a different orientation. In truth,
-// the television just forwards the request to the pixel renderers.
-func (tv *Television) SetRotation(rotation specification.Rotation) {
-	for _, r := range tv.renderers {
-		if s, ok := r.(PixelRendererRotation); ok {
-			s.SetRotation(rotation)
-		}
-	}
-}
-
-// GetResizer returns a copy of the television resizer in it's current state.
-func (tv *Television) GetResizer() Resizer {
-	// make copy of resizer and set the validFrom fields
-	rz := tv.state.resizer
-	rz.previewFrameNum = tv.state.frameInfo.FrameNum
-	return rz
-}
-
-// SetResizer sets the state of the television resizer and sets the current
-// frame info accordingly.
-//
-// Note that the Resizer type does not include specification information. When
-// transferring state between television instances it is okay to call SetSpec()
-// but it should be done before SetResizer() is called
-func (tv *Television) SetResizer(rz Resizer) {
-	tv.state.resizer = rz
-	if tv.state.resizer.usingVBLANK {
-		tv.state.frameInfo.VisibleTop = tv.state.resizer.vblankTop
-		tv.state.frameInfo.VisibleBottom = tv.state.resizer.vblankBottom
-	} else {
-		tv.state.frameInfo.VisibleTop = tv.state.resizer.blackTop
-		tv.state.frameInfo.VisibleBottom = tv.state.resizer.blackBottom
-	}
-
-	// call new frame for all pixel renderers. this will force the new size
-	// information to be handled immediately but it won't result in a "phantom"
-	// frame because we won't change the FrameNum field in the FrameInfo
-	for _, r := range tv.renderers {
-		r.NewFrame(tv.state.frameInfo)
-	}
 }

gui/sdlimgui/screen.go
@@ -886,3 +886,8 @@ func (scr *screen) copyPixelsPlaymode() {
 		scr.img.dbg.VCS().TV.NudgeFPSLimiter(1)
 	}
 }
+
+// DisplayRefreshRate implements the television.PixelRendererRotation interface
+func (scr *screen) DisplayRefreshRate() (float32, bool) {
+	return float32(scr.img.plt.mode.RefreshRate), false
+}

hardware/television/protocol.go
@@ -92,6 +92,17 @@ type PixelRenderer interface {
 	EndRendering() error
 }
 
+// PixelRendererDisplay is a special case of the the PixelRenderer interface. it should be
+// implemented by a renderer that works with a hardware display.
+type PixelRendererDisplay interface {
+	PixelRenderer
+
+	// returns refresh rate of display and whether the limiter should quantise the emulated frame
+	// rate to the monitor speed. the boolean indicates whether the current purpose of the display
+	// is suitable for quantisation by a frame limiter
+	DisplayRefreshRate() (float32, bool)
+}
+
 // PixelRendererRotation is an extension to the PixelRenderer interface. Pixel
 // renderes that implement this interface can show the television image in a
 // rotated aspect. Not all pixel renderers need to worry about rotation.

hardware/television/resizer.go
@@ -326,3 +326,45 @@ func (rz *Resizer) commit(state *State) error {
 
 	return nil
 }
+
+// SetRotation instructs the television to a different orientation. In truth,
+// the television just forwards the request to the pixel renderers.
+func (tv *Television) SetRotation(rotation specification.Rotation) {
+	for _, r := range tv.renderers {
+		if s, ok := r.(PixelRendererRotation); ok {
+			s.SetRotation(rotation)
+		}
+	}
+}
+
+// GetResizer returns a copy of the television resizer in it's current state.
+func (tv *Television) GetResizer() Resizer {
+	// make copy of resizer and set the validFrom fields
+	rz := tv.state.resizer
+	rz.previewFrameNum = tv.state.frameInfo.FrameNum
+	return rz
+}
+
+// SetResizer sets the state of the television resizer and sets the current
+// frame info accordingly.
+//
+// Note that the Resizer type does not include specification information. When
+// transferring state between television instances it is okay to call SetSpec()
+// but it should be done before SetResizer() is called
+func (tv *Television) SetResizer(rz Resizer) {
+	tv.state.resizer = rz
+	if tv.state.resizer.usingVBLANK {
+		tv.state.frameInfo.VisibleTop = tv.state.resizer.vblankTop
+		tv.state.frameInfo.VisibleBottom = tv.state.resizer.vblankBottom
+	} else {
+		tv.state.frameInfo.VisibleTop = tv.state.resizer.blackTop
+		tv.state.frameInfo.VisibleBottom = tv.state.resizer.blackBottom
+	}
+
+	// call new frame for all pixel renderers. this will force the new size
+	// information to be handled immediately but it won't result in a "phantom"
+	// frame because we won't change the FrameNum field in the FrameInfo
+	for _, r := range tv.renderers {
+		r.NewFrame(tv.state.frameInfo)
+	}
+}

commit f57df2884d2e91958943d7857576eee1c0a74d10

fixed frame rate issue for 50Hz (PAL) games
bug introduced in 3b90034

gui/sdlimgui/manager_menu.go
@@ -262,8 +262,7 @@ func (wm *manager) drawMenu() {
 	// FPS information
 	if wm.img.dbg.State() == govern.Running {
 		actual, _ := wm.img.dbg.VCS().TV.GetActualFPS()
-		req := wm.img.dbg.VCS().TV.GetReqFPS()
-		if req < 1.0 {
+		if actual < 1.0 {
 			textFromRight("< 1 fps", true, nil)
 		} else if math.IsInf(float64(actual), 0) {
 			textFromRight("- fps", true, nil)

gui/sdlimgui/win_control.go
@@ -209,7 +209,7 @@ func (win *winControl) drawStep() {
 }
 
 func (win *winControl) drawFPS() {
-	req := win.img.dbg.VCS().TV.GetReqFPS()
+	ideal := win.img.dbg.VCS().TV.GetIdealFPS()
 	actual, _ := win.img.dbg.VCS().TV.GetActualFPS()
 	frameInfo := win.img.cache.TV.GetFrameInfo()
 
@@ -221,8 +221,8 @@ func (win *winControl) drawFPS() {
 	defer imgui.PopItemWidth()
 
 	// fps slider
-	if imgui.SliderFloatV("##fps", &req, 1, 100, "%.0f fps", imgui.SliderFlagsNone) {
-		win.img.dbg.PushFunction(func() { win.img.dbg.VCS().TV.SetFPS(req) })
+	if imgui.SliderFloatV("##fps", &ideal, 1, 100, "%.0f fps", imgui.SliderFlagsNone) {
+		win.img.dbg.PushFunction(func() { win.img.dbg.VCS().TV.SetFPS(ideal) })
 	}
 
 	// reset to specification rate on right mouse click
@@ -232,16 +232,16 @@ func (win *winControl) drawFPS() {
 
 	imgui.Spacing()
 	if win.img.dbg.State() == govern.Running {
-		if actual <= req*0.95 {
+		if actual <= ideal*0.95 {
 			imgui.Text("running below requested FPS")
-		} else if actual > req*1.05 {
+		} else if actual > ideal*1.05 {
 			imgui.Text("running above requested FPS")
 		} else {
 			imgui.Text("running at requested FPS")
 		}
-	} else if req < frameInfo.Spec.RefreshRate*0.95 {
+	} else if ideal < frameInfo.Spec.RefreshRate*0.95 {
 		imgui.Text(fmt.Sprintf("below ideal frequency of %.0fHz", frameInfo.Spec.RefreshRate))
-	} else if req > frameInfo.Spec.RefreshRate*1.05 {
+	} else if ideal > frameInfo.Spec.RefreshRate*1.05 {
 		imgui.Text(fmt.Sprintf("above ideal frequency of %.0fHz", frameInfo.Spec.RefreshRate))
 	} else {
 		imgui.Text(fmt.Sprintf("ideal frequency %.0fHz", frameInfo.Spec.RefreshRate))

hardware/television/limiter.go
@@ -47,12 +47,12 @@ func (tv *Television) SetFPS(fps float32) {
 	tv.lmtr.SetLimit(fps)
 }
 
-// GetReqFPS returns the requested number of frames per second. Compare with
-// GetActualFPS() to check for accuracy.
+// GetIdealFPS returns the ideal number of frames per second. Compare with GetActualFPS() to check for
+// accuracy and if the emulation is achieving full speed
 //
 // IS goroutine safe.
-func (tv *Television) GetReqFPS() float32 {
-	return tv.lmtr.RequestedFPS.Load().(float32)
+func (tv *Television) GetIdealFPS() float32 {
+	return tv.lmtr.IdealFPS.Load().(float32)
 }
 
 // GetActualFPS returns the current number of frames per second and the

hardware/television/limiter/limiter.go
@@ -36,8 +36,11 @@ type Limiter struct {
 	// atomic value
 	RefreshRate atomic.Value // float32
 
-	// the requested number of frames per second
-	RequestedFPS atomic.Value // float32
+	// the ideal number of frames per second if everything was working nicely
+	IdealFPS atomic.Value // float32
+
+	// the actual value sent to the SetLimit() function. no
+	requestedFPS atomic.Value // float32
 
 	// whether the requested frame rate is to equal the refresh rate
 	requestMatchRefreshRate atomic.Value // bool
@@ -105,7 +108,7 @@ func NewLimiter() *Limiter {
 // Set the display interface for the limiter
 func (lmtr *Limiter) SetDisplay(display Display) {
 	lmtr.display = display
-	lmtr.SetLimit(lmtr.RequestedFPS.Load().(float32))
+	lmtr.SetLimit(lmtr.requestedFPS.Load().(float32))
 }
 
 // Set the refresh rate for the limiter. This is equivalent to the refresh rate
@@ -123,6 +126,9 @@ const MatchRefreshRate float32 = -1.0
 // Set frame limit. Use a value of MatchRefreshRate to indicate that the limiter should equal the
 // television refresh rate.
 func (lmtr *Limiter) SetLimit(fps float32) {
+	// the actual fps value passed to the function
+	lmtr.requestedFPS.Store(fps)
+
 	if fps <= 0.0 {
 		fps = lmtr.RefreshRate.Load().(float32)
 		lmtr.requestMatchRefreshRate.Store(fps == lmtr.RefreshRate.Load().(float32))
@@ -138,10 +144,6 @@ func (lmtr *Limiter) SetLimit(fps float32) {
 		return
 	}
 
-	// the actual requested fps rate (taken into account the possibility of a negative number
-	// to indicate the refresh rate)
-	lmtr.RequestedFPS.Store(fps)
-
 	// quantise refresh rate based on refresh rate of television
 	if lmtr.display != nil {
 		hz, quantise := lmtr.display.DisplayRefreshRate()
@@ -151,6 +153,9 @@ func (lmtr *Limiter) SetLimit(fps float32) {
 			}
 		}
 	}
+
+	// the ideal fps rate including quantisation
+	lmtr.IdealFPS.Store(fps)
 
 	// set scale and duration to wait according to requested FPS rate
 	lmtr.pulseCt = 0

hardware/television/television.go
@@ -385,7 +385,7 @@ func (tv *Television) RemovePixelRenderer(r PixelRenderer) {
 	if r, ok := r.(PixelRendererDisplay); ok {
 		if tv.rendererDisplay == r {
 			tv.rendererDisplay = nil
-			tv.lmtr.SetDisplay(r)
+			tv.lmtr.SetDisplay(nil)
 		}
 	}
 	for i := range tv.renderers {