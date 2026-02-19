JetSetIlly/Gopher2600

commit 26aab0fd2d1a54e7d1ef762fc091cebb3088ce34

correct SARA emulation when it is used for program data
the chip used for SARA is not fast enough for the fetch-execute cycle of
the CPU. it is fast enough for normal data access. the new emulation
distinguishes between the two scenarios and indirectly prevents SARA
from being used for program data

hardware/memory/cartridge/mapper_atari.go
@@ -241,15 +241,41 @@ func (cart *atari) SetBank(bank string) error {
 	return nil
 }
 
+// the number of cycles required for SARA RAM to recover from an access. this value is assigned to
+// saraRecovery when RAM is accessed.
+//
+// the value is one more than required because we reduce saraRecovery() in the Step() function,
+// which comes after any access
+//
+// the reason for this value and for the "recovery" mecahnism at all, is because SARA ram is too
+// slow to supply data for consecutive cycles. the current working theory is that the RAM "reacts"
+// faster when moving from a CS disabled state than when CS is already enabled
+const saraCycles = 2
+
 // access implements the mapper.CartMapper interface. the bool return value is true if the address
 // has been recognised and serviced.
 func (cart *atari) access(addr uint16) (uint8, uint8, bool) {
 	if cart.state.ram != nil {
-		if addr <= 0x7f {
-			return 0, 0, true
-		}
-		if addr >= 0x80 && addr <= 0xff {
-			return cart.state.ram[addr-superchipSize], mapper.CartDrivenPins, true
+		if addr&0x0f00 == 0x0000 {
+			sara := cart.env.Prefs.Cartridge.EmulateSARA.Get().(bool)
+			if sara && cart.state.saraRecovery > 0 && addr&0x7f != cart.state.saraAddr {
+				cart.state.saraRecovery = saraCycles
+				return 0, 0, true
+			}
+
+			switch addr & 0x0f80 {
+			case 0x0000:
+				cart.state.saraRecovery = saraCycles
+				cart.state.saraAddr = addr & 0x7f
+				return 0, 0, true
+			case 0x0080:
+				if cart.state.sara && cart.state.saraRecovery > 0 && addr&0x7f != cart.state.saraAddr {
+					return 0, 0, true
+				}
+				cart.state.saraRecovery = saraCycles
+				cart.state.saraAddr = addr & 0x7f
+				return cart.state.ram[cart.state.saraAddr], mapper.CartDrivenPins, true
+			}
 		}
 	}
 	return 0, mapper.CartDrivenPins, false
@@ -258,15 +284,26 @@ func (cart *atari) access(addr uint16) (uint8, uint8, bool) {
 // accessVolatile implements the mapper.CartMapper interface.
 func (cart *atari) accessVolatile(addr uint16, data uint8, poke bool) error {
 	if cart.state.ram != nil {
-		if addr <= 0x7f {
-			cart.state.ram[addr] = data
-			return nil
+		if addr&0x0f00 == 0x0000 {
+			if poke {
+				cart.banks[cart.state.bank][addr&0x7f] = data
+				return nil
+			}
+			sara := cart.env.Prefs.Cartridge.EmulateSARA.Get().(bool)
+			if sara && cart.state.saraRecovery > 0 && addr&0x7f != cart.state.saraAddr {
+				cart.state.saraRecovery = saraCycles
+				return nil
+			}
+			switch addr & 0x0f80 {
+			case 0x0000:
+				cart.state.saraRecovery = saraCycles
+				cart.state.saraAddr = addr & 0x7f
+				cart.state.ram[cart.state.saraAddr] = data
+				return nil
+			case 0x0080:
+				return nil
+			}
 		}
-	}
-
-	if poke {
-		cart.banks[cart.state.bank][addr] = data
-		return nil
 	}
 
 	return nil
@@ -291,6 +328,9 @@ func (cart *atari) AccessPassive(addr uint16, data uint8) error {
 
 // Step implements the mapper.CartMapper interface.
 func (cart *atari) Step(_ float32) {
+	if cart.state != nil && cart.state.saraRecovery > 0 {
+		cart.state.saraRecovery--
+	}
 }
 
 // GetRAM implements the mapper.CartRAMBus interface.
@@ -301,10 +341,13 @@ func (cart *atari) GetRAM() []mapper.CartRAM {
 
 	r := make([]mapper.CartRAM, 1)
 	r[0] = mapper.CartRAM{
-		Label:  "Superchip",
-		Origin: 0x1080,
-		Data:   make([]uint8, len(cart.state.ram)),
-		Mapped: true,
+		Label:                "Superchip",
+		Origin:               0x1080,
+		Data:                 make([]uint8, len(cart.state.ram)),
+		Mapped:               true,
+		CycleSensitive:       true,
+		CycleSensitiveActive: cart.state.sara,
+		Cycles:               cart.state.saraRecovery,
 	}
 
 	copy(r[0].Data, cart.state.ram)
@@ -333,11 +376,17 @@ func (cart *atari) CopyBanks() []mapper.BankContent {
 }
 
 // AddSuperchip implements the mapper.OptionalSuperchip interface.
-func (cart *atari) AddSuperchip(force bool) {
+func (cart *atari) AddSuperchip(force bool, sara bool) {
 	if force || cart.needsSuperchip {
 		cart.mappingID = fmt.Sprintf("%s (SC)", cart.mappingID)
 		cart.state.ram = make([]uint8, superchipSize)
+		cart.state.sara = sara
 	}
+}
+
+// SetEmulateSARA implements the mapper.OptionalSuperchip interface.
+func (cart *atari) SetEmulateSARA(sara bool) {
+	cart.state.sara = sara
 }
 
 // atari4k is the original and most straightforward format:
@@ -862,6 +911,11 @@ type atariState struct {
 	// some atari ROMs support aditional RAM. this is sometimes referred to as
 	// the superchip. ram is only added when it is detected
 	ram []uint8
+
+	// the SARA chip was often used to implement additional RAM in basic atari like cartridges. see
+	// the comment for the saraCycles constant for more detail
+	saraRecovery int
+	saraAddr     uint16
 }
 
 func newAtariState() *atariState {

gui/sdlimgui/imgui_helpers.go
@@ -371,3 +371,16 @@ func drawDisabled(disabled bool, f func()) {
 	f()
 	imgui.EndDisabled()
 }
+
+// like drawDisabled() but the widget is invisible
+func drawInvisible(invisble bool, f func()) {
+	if !invisble {
+		f()
+		return
+	}
+	imgui.BeginDisabled()
+	imgui.PushStyleVarFloat(imgui.StyleVarAlpha, 0.0)
+	f()
+	imgui.PopStyleVar()
+	imgui.EndDisabled()
+}

gui/sdlimgui/sdlimgui.go
@@ -574,3 +574,16 @@ func (img *SdlImgui) commitStaticMemory(address uint32, data uint8) {
 		img.dbg.VCS().Mem.Cart.GetStaticBus().ReferenceStatic().Write8bit(address, data)
 	})
 }
+
+func (img *SdlImgui) drawEmulateSARACheckbox() {
+	emulateSARA := img.dbg.VCS().Env.Prefs.EmulateSARA.Get().(bool)
+	if imgui.Checkbox("Emulate Cycle Limitations", &emulateSARA) {
+		img.dbg.VCS().Env.Prefs.EmulateSARA.Set(emulateSARA)
+
+		// the preference value does not automatically handle changing the current cartridge
+		// value, so we do it here from the GUI
+		img.dbg.PushFunctionImmediate(func() {
+			img.dbg.VCS().Mem.Cart.SetEmulateSARA(emulateSARA)
+		})
+	}
+}

gui/sdlimgui/win_cart_ram.go
@@ -37,6 +37,7 @@ type winCartRAM struct {
 
 	// required dimensions of mapped/unmapped indicator
 	mappedIndicatorDim imgui.Vec2
+	readyIndicatorDim  imgui.Vec2
 }
 
 func newWinCartRAM(img *SdlImgui) (window, error) {
@@ -46,6 +47,7 @@ func newWinCartRAM(img *SdlImgui) (window, error) {
 
 func (win *winCartRAM) init() {
 	win.mappedIndicatorDim = imguiGetFrameDim(" mapped ", " unmapped ")
+	win.readyIndicatorDim = imguiGetFrameDim(" ready ", " not ready (x) ")
 }
 
 func (win *winCartRAM) id() string {
@@ -175,12 +177,34 @@ func (win *winCartRAM) draw(ram []mapper.CartRAM) {
 			// status line
 			win.statusHeight = imguiMeasureHeight(func() {
 				imgui.PushStyleVarFloat(imgui.StyleVarFrameRounding, readOnlyButtonRounding)
+				defer imgui.PopStyleVar()
+
 				if current.Mapped {
 					imguiColourButton(win.img.cols.True, " mapped ", win.mappedIndicatorDim)
 				} else {
 					imguiColourButton(win.img.cols.False, " unmapped ", win.mappedIndicatorDim)
 				}
-				imgui.PopStyleVar()
+
+				imgui.Spacing()
+
+				if current.CycleSensitive {
+					win.img.drawEmulateSARACheckbox()
+
+					imgui.SameLineV(0, 25)
+					drawInvisible(!current.CycleSensitiveActive, func() {
+						if current.Cycles > 0 {
+							var s string
+							if current.Cycles == 1 {
+								s = fmt.Sprintf(" %d cycle ", current.Cycles)
+							} else {
+								s = fmt.Sprintf(" %d cycles ", current.Cycles)
+							}
+							imguiColourButton(win.img.cols.False, s, win.readyIndicatorDim)
+						} else {
+							imguiColourButton(win.img.cols.True, " ready ", win.readyIndicatorDim)
+						}
+					})
+				}
 			})
 
 			imgui.EndTabItem()

gui/sdlimgui/win_prefs.go
@@ -391,6 +391,10 @@ func (win *winPrefs) drawVCS() {
 		}
 	}
 
+	if imgui.CollapsingHeaderV("SARA", imgui.TreeNodeFlagsNone) {
+		win.img.drawEmulateSARACheckbox()
+	}
+
 	imgui.Spacing()
 	if imgui.CollapsingHeaderV("Audio", imgui.TreeNodeFlagsNone) {
 		// enable options

gui/sdlimgui/win_prefs_television.go
@@ -279,6 +279,19 @@ func (win *winPrefs) drawPALPhase() {
 	}
 }
 
+func (win *winPrefs) drawEmulateSARA() {
+	emulateSARA := win.img.dbg.VCS().Env.Prefs.EmulateSARA.Get().(bool)
+	if imgui.Checkbox("Emulate Cycle Limitations", &emulateSARA) {
+		win.img.dbg.VCS().Env.Prefs.EmulateSARA.Set(emulateSARA)
+
+		// the preference value does not automatically handle changing the current cartridge
+		// value, so we do it here from the GUI
+		win.img.dbg.PushFunctionImmediate(func() {
+			win.img.dbg.VCS().Mem.Cart.SetEmulateSARA(emulateSARA)
+		})
+	}
+}
+
 func (win *winPrefs) drawVSYNC() {
 	var label string
 

hardware/memory/cartridge/cartridge.go
@@ -372,13 +372,13 @@ func (cart *Cartridge) Attach(cartload cartridgeloader.Loader) error {
 	// RAM
 	if forceSuperchip {
 		if superchip, ok := cart.mapper.(mapper.OptionalSuperchip); ok {
-			superchip.AddSuperchip(true)
+			superchip.AddSuperchip(true, cart.env.Prefs.EmulateSARA.Get().(bool))
 		} else {
 			logger.Logf(cart.env, "cartridge", "cannot add superchip to %s mapper", cart.ID())
 		}
 	} else if auto {
 		if superchip, ok := cart.mapper.(mapper.OptionalSuperchip); ok {
-			superchip.AddSuperchip(false)
+			superchip.AddSuperchip(false, cart.env.Prefs.EmulateSARA.Get().(bool))
 		}
 	}
 
@@ -629,4 +629,11 @@ func (cart *Cartridge) Patch(offset int, data uint8) error {
 		return cart.Patch(offset, data)
 	}
 	return fmt.Errorf("cartridge is not patchable")
+}
+
+// Patch implements the mapper.OptionalSuperchip interface
+func (cart *Cartridge) SetEmulateSARA(v bool) {
+	if c, ok := cart.mapper.(mapper.OptionalSuperchip); ok {
+		c.SetEmulateSARA(v)
+	}
 }

hardware/memory/cartridge/mapper/mapper.go
@@ -122,8 +122,10 @@ type PlumbFromDifferentEmulation interface {
 // an optional superchip. This shouldn't be used to decide if a cartridge has
 // additional RAM or not. Use the CartRAMbus interface for that.
 type OptionalSuperchip interface {
-	// the force argument causes the superchip to be added whether it needs it or not
-	AddSuperchip(force bool)
+	// the force argument causes the superchip to be added whether it needs it or not. the sara
+	// argument means the superchip will have the speed limitations of the sara chip
+	AddSuperchip(force bool, sara bool)
+	SetEmulateSARA(bool)
 }
 
 // CartRAMbus is implemented for catridge mappers that have an addressable RAM
@@ -157,6 +159,12 @@ type CartRAM struct {
 	Origin uint16
 	Data   []uint8
 	Mapped bool
+
+	// some cartridge RAM hardware implementations have restrictions on how quickly they can be
+	// accessed. (ie. SARA ram)
+	CycleSensitive       bool
+	CycleSensitiveActive bool
+	Cycles               int
 }
 
 // CartRegistersBus defines the operations required for a debugger to access

commit 973e8e212802cffb2cf43c0f44254c7522da831e

fixed atari accessVolatile() when poke=true
broken in 26aab0f

hardware/memory/cartridge/mapper_atari.go
@@ -301,6 +301,11 @@ func (cart *atari) accessVolatile(addr uint16, data uint8, poke bool) error {
 		}
 	}
 
+	if poke {
+		cart.banks[cart.state.bank][addr] = data
+		return nil
+	}
+
 	return nil
 }
 