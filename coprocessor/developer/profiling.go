6be5391cdae25e8273acc9cef1c9e6c82b1def19
JetSetIlly <stephen.t.illingworth@gmail.com>
2026-03-28 17:20:03 +0000

    arm profiling: boundary check for callstack
    an out-of-range error was possible if the emulation was running during a
    reset

--- coprocessor/developer/profiling.go
+++ coprocessor/developer/profiling.go
@@ -88,56 +88,58 @@ func (dev *Developer) ProcessProfiling() {
 			// not a stub function and not the main function
 			if !ln.Function.IsStub() && ln.Function != dev.source.MainFunction {
 				l := len(dev.callstack.Stack) - 1
-				prevCallStack := dev.callstack.Stack[l]
+				if l >= 0 {
+					prevCallStack := dev.callstack.Stack[l]
 
-				// change callstack if function has changed
-				if ln.Function != prevCallStack.Function {
-					var popped bool
+					// change callstack if function has changed
+					if ln.Function != prevCallStack.Function {
+						var popped bool
 
-					// try to pop entry from callstack
-					for i := 1; i < l && !popped; i++ {
-						if ln.Function == dev.callstack.Stack[l-i].Function {
-							chop := dev.callstack.Stack[l-i+1:]
-							dev.callstack.Stack = dev.callstack.Stack[:l-i+1]
+						// try to pop entry from callstack
+						for i := 1; i < l && !popped; i++ {
+							if ln.Function == dev.callstack.Stack[l-i].Function {
+								chop := dev.callstack.Stack[l-i+1:]
+								dev.callstack.Stack = dev.callstack.Stack[:l-i+1]
 
-							// flag functions which look like they are part of an
-							// optimised call stack
-							if len(chop) > 1 {
-								for _, ln := range chop {
-									ln.Function.OptimisedCallStack = true
+								// flag functions which look like they are part of an
+								// optimised call stack
+								if len(chop) > 1 {
+									for _, ln := range chop {
+										ln.Function.OptimisedCallStack = true
+									}
 								}
+
+								// setting popped will cause the loop to end early
+								popped = true
+							}
+						}
+
+						// push function on to callstack if we haven't popped
+						if !popped {
+							dev.callstack.Stack = append(dev.callstack.Stack, ln)
+
+							// create/update callers list for function
+							var n int
+							l, ok := dev.callstack.Callers[ln.Function.Name]
+							if ok {
+								n = sort.Search(len(l), func(i int) bool {
+									return ln == l[i]
+								})
 							}
 
-							// setting popped will cause the loop to end early
-							popped = true
+							if !ok || (n > len(l) && l[n] != dev.prevProfileLine) {
+								l = append(l, dev.prevProfileLine)
+								sort.Slice(l, func(i, j int) bool {
+									return l[i].Function.Name < l[j].Function.Name
+								})
+								dev.callstack.Callers[ln.Function.Name] = l
+							}
+
+							// increase count of how many times this function has
+							// been called this frame
+							ln.Function.NumCalls.Call(focus)
+							ln.Function.CyclesPerCall.Call(focus)
 						}
-					}
-
-					// push function on to callstack if we haven't popped
-					if !popped {
-						dev.callstack.Stack = append(dev.callstack.Stack, ln)
-
-						// create/update callers list for function
-						var n int
-						l, ok := dev.callstack.Callers[ln.Function.Name]
-						if ok {
-							n = sort.Search(len(l), func(i int) bool {
-								return ln == l[i]
-							})
-						}
-
-						if !ok || (n > len(l) && l[n] != dev.prevProfileLine) {
-							l = append(l, dev.prevProfileLine)
-							sort.Slice(l, func(i, j int) bool {
-								return l[i].Function.Name < l[j].Function.Name
-							})
-							dev.callstack.Callers[ln.Function.Name] = l
-						}
-
-						// increase count of how many times this function has
-						// been called this frame
-						ln.Function.NumCalls.Call(focus)
-						ln.Function.CyclesPerCall.Call(focus)
 					}
 				}
 			}
