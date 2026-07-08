JetSetIlly/Gopher2600

commit 4ddd3949093b54fac8f564b78e153089f1258324

video recording now locked to emulated TV rate
recording of video does not begin until television frame is stable. the
refresh rate of the television at the moment it becomes stable is used
as the base framerate (output is always 60fps regardless)

gui/sdlimgui/gl32.go
@@ -268,8 +268,7 @@ func (rnd *gl32) render() {
 				if rnd.img.isPlaymode() {
 					err := rnd.video.Preprocess(
 						rnd.img.cache.VCS.Mem.Cart.ShortName,
-						int32(fbw), int32(fbh), float32(rnd.img.plt.mode.RefreshRate),
-						video.ProfileFast)
+						int32(fbw), int32(fbh), video.ProfileFast)
 					if err != nil {
 						logger.Log(logger.Allow, "gl32", err.Error())
 					}

video/ffmpeg.go
@@ -262,10 +262,19 @@ func (vid *FFMPEG) Destroy() {
 	}
 }
 
-func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, hz float32, profile Profile) error {
+func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, profile Profile) error {
 	if !vid.enabled {
 		vid.Destroy()
 		return nil
+	}
+
+	fi := vid.tv.GetFrameInfo()
+
+	if vid.hz == 0 {
+		if !fi.Stable {
+			return nil
+		}
+		vid.hz = fi.RefreshRate
 	}
 
 	if vid.pipe != nil {
@@ -273,17 +282,12 @@ func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, hz flo
 			vid.Destroy()
 			return fmt.Errorf("ffmpeg: size of frame has changed")
 		}
-		if vid.hz != hz {
-			vid.Destroy()
-			return fmt.Errorf("ffmpeg: refresh rate of monitor has changed")
-		}
 		return nil
 	}
 
 	vid.cartName = cartName
 	vid.width = width
 	vid.height = height
-	vid.hz = hz
 	vid.profile = profile
 
 	// the base for the output file's name. we append .mp4 for the video file and .wav for the audio file
@@ -296,7 +300,7 @@ func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, hz flo
 		"-f", "rawvideo",
 		"-pix_fmt", "rgba",
 		"-s", fmt.Sprintf("%dx%d", vid.width, vid.height),
-		"-r", fmt.Sprintf("%.02f", hz), // incoming frame rate
+		"-r", fmt.Sprintf("%.02f", vid.hz), // incoming frame rate
 		"-i", "-", // stdin pipe created below
 	}
 

commit 498a73bf3e34d2c9cba704344c4c8ad32ebf29e6

removed race condition in video recording
introduced in commit 4ddd394

gui/sdlimgui/gl32.go
@@ -266,14 +266,23 @@ func (rnd *gl32) render() {
 			// do screenshots and video processing at the end of the first pass
 			if pass == 0 {
 				if rnd.img.isPlaymode() {
-					err := rnd.video.Preprocess(
-						rnd.img.cache.VCS.Mem.Cart.ShortName,
-						int32(fbw), int32(fbh), video.ProfileFast)
-					if err != nil {
-						logger.Log(logger.Allow, "gl32", err.Error())
+					rnd.img.screen.crit.section.Lock()
+					frameInfo := rnd.img.screen.crit.frameInfo
+					rnd.img.screen.crit.section.Unlock()
+
+					// process video if frame is stable
+					if frameInfo.Stable {
+						err := rnd.video.Preprocess(
+							rnd.img.cache.VCS.Mem.Cart.ShortName,
+							int32(fbw), int32(fbh), frameInfo.RefreshRate,
+							video.ProfileFast)
+						if err != nil {
+							logger.Log(logger.Allow, "gl32", err.Error())
+						}
+						rnd.video.Process(int(rnd.img.screen.lastVideoFrame.Load()))
 					}
+
 					rnd.scrsht.process(int32(fbw), int32(fbh))
-					rnd.video.Process(int(rnd.img.screen.lastVideoFrame.Load()))
 				}
 			}
 		}

video/ffmpeg.go
@@ -267,19 +267,16 @@ func (vid *FFMPEG) Destroy() {
 	}
 }
 
-func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, profile Profile) error {
+func (vid *FFMPEG) Preprocess(cartName string, width int32, height int32, hz float32, profile Profile) error {
 	if !vid.enabled {
 		vid.Destroy()
 		return nil
 	}
 
-	fi := vid.tv.GetFrameInfo()
-
+	// frame rate for the video is only set when the video has not current value, which happens at
+	// the outset of the recording
 	if vid.hz == 0 {
-		if !fi.Stable {
-			return nil
-		}
-		vid.hz = fi.RefreshRate
+		vid.hz = hz
 	}
 
 	if vid.pipe != nil {