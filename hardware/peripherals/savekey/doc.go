cc76fd532221e8f0ca4fa1f22b07cd1c16af01e0
JetSetIlly <stephen.t.illingworth@gmail.com>
2026-09-11 18:47:56 +0100

    fixed bug in savekey when reading the EEPROM
    the EEPROM address is advanced after every read/write. when writing the
    address wraps at the page boundary. when reading however, it does not
    wrap at the page boundary but only at the end of memory

--- hardware/peripherals/savekey/doc.go
+++ hardware/peripherals/savekey/doc.go
@@ -20,7 +20,14 @@
 // The SaveKey type implements the ports.Peripheral interface and can be
 // inserted into a VCS port like any other peripheral.
 //
-// SaveKey information taken from "AtariVox Programmer's Guide" (16/11/04) by Alex Herbert
+// SaveKey information taken from "AtariVox Programmer's Guide" (16/11/04) by Alex
+// Herbert. A variation of that document can be founc on the 7800 8bitdev wiki.
 //
-// allocation list at https://atariage.com/atarivox/atarivox_mem_list.html (13/09/2020).
+// https://7800.8bitdev.org/index.php/AtariVox_for_the_7800
+//
+// Datasheet for the 26LC256 used in the SaveKey.
+//
+// https://ww1.microchip.com/downloads/aemDocuments/documents/MPD/ProductDocuments/DataSheets/25LCXXXX-8K-256K-SPI-Serial-EEPROM-High-Temp-Family-Data-Sheet-DS20002131.pdf
+//
+// Allocation list at https://atariage.com/atarivox/atarivox_mem_list.html (13/09/2020).
 package savekey
